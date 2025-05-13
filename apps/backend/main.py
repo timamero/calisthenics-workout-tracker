65cc44439a62bff3d5b7441b57c9c6e625f39556
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-05-13 13:36:56 -0700

    Create test route with httpx

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
@@ -13,3 +14,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/test")
+def read_test():
+    r = httpx.get("https://www.example.org/")
+    return r.text
