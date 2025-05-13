10a904ff2591d5bd779fc88558f5ccdbb201b13d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-05-13 01:10:56 -0700

    Remove unused css files

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
-// import './index.css'
 import App from './App.tsx'
 
 const theme = createTheme({
