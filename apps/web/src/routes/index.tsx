2f1ff9385de3400880911eeb5783a09bfaf7a4fd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-05-13 01:41:44 -0700

    Set up tanstack router

---
+++ apps/web/src/routes/index.tsx
@@ -0,0 +1,36 @@
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>Welcome Home</AppShell.Main>
+    </AppShell>
+  )
+}
