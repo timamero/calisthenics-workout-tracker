e22fc9fd8da5a337472c572f0320cd286e95b2da
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-05-13 03:44:31 -0700

    Create bottom tab

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { createStaticNavigation, StaticParamList } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
 } from 'react-native-paper';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import HomeScreen from './screens/Home';
 import AboutScreen from './screens/About';
 
 
-type RootStackParamList = StaticParamList<typeof RootStack>;
+type RootStackParamList = StaticParamList<typeof MyTabs>;
 
 declare global {
   namespace ReactNavigation {
@@ -18,23 +18,33 @@ declare global {
   }
 }
 
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
-  screenOptions: {
-    headerStyle: { backgroundColor: 'tomato' },
-  },
-  screens: {
-    Home: {
-      screen: HomeScreen,
-      options: {
-        title: 'Home',
+const MyTabs = createBottomTabNavigator({
+  screenOptions: ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      if (route.name === 'Home') {
+        iconName = focused
+          ? 'home'
+          : 'home-outline';
+      } else if (route.name === 'About') {
+        iconName = focused ? 'information-circle' : 'information-circle-outline';
       }
+
+      // You can return any component that you like here!
+      // @ts-ignore
+      return <Ionicons name={iconName} size={size} color={color} />;
     },
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  }),
+  screens: {
+    Home: HomeScreen,
     About: AboutScreen,
   },
 });
 
-const Navigation = createStaticNavigation(RootStack);
+const Navigation = createStaticNavigation(MyTabs);
 
 const theme = {
   ...DefaultTheme,
