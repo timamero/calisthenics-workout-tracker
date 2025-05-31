07c0d9f1da2b9276d29e5fd514fb080b9fa4b55e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-05-30 18:03:36 -0700

    Fix eslint config file

--- apps/frontend/web/eslint.config.js
+++ apps/frontend/web/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -11,7 +12,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      'plugin:prettier/recommended',
+      prettierRecommended,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
