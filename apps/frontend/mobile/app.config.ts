f0c05b8072d89c0c858aaf66190ff577f14de492
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-06-06 20:06:05 -0700

    Set dynamic values for config

--- apps/frontend/mobile/app.config.ts
+++ apps/frontend/mobile/app.config.ts
@@ -1,28 +1,51 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => {
-  const profile = process.env.APP_VARIANT;
-  if (profile === 'preview') {
-    return {
-      ...config,
-      name: 'cwt_mobile-preview',
-      slug: 'cwt-mobile',
-      icon: './assets/icon-preview.png',
-      android: {
-        ...config.android,
-        package: 'com.fcamero.cwtmobile_preview',
-      },
-    };
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.fcamero.cwtmobile_development';
   }
-  // Default to development
-  return {
-    ...config,
-    name: 'cwt_mobile-dev',
-    slug: 'cwt-mobile',
-    icon: './assets/icon-development.png',
-    android: {
-      ...config.android,
-      package: 'com.fcamero.cwtmobile_development',
-    },
-  };
+
+  if (IS_PREVIEW) {
+    return 'com.fcamero.cwtmobile_preview';
+  }
+
+  return 'com.yourname.stickersmash';
 };
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'CWT (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'CWT (Preview)';
+  }
+
+  return 'CWT';
+};
+
+const getIcon = () => {
+  if (IS_DEV) {
+    return './assets/icon-development.png';
+  }
+
+  if (IS_PREVIEW) {
+    return './assets/icon-preview.png';
+  }
+
+  return 'CWT';
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: getAppName(),
+  slug: config.slug ?? 'cwt-mobile',
+  icon: getIcon(),
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+});
