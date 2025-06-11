186b9a40922a5b7b9b38d40f46dc8edca1a72457
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-06-11 00:26:03 -0700

    Remove testing library

--- apps/frontend/web/src/tests/sanity.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-describe('Sanity Check', () => {
-  test('renders simple JSX', () => {
-    render(<div>Hello World</div>);
-    screen.debug();
-    expect(screen.getByText('Hello World')).toBeVisible();
-  });
-});
