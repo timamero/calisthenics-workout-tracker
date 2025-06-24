24149651ba65c691dcc1071f49955136f141972a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-06-24 13:05:31 -0700

    Add sample counter

--- apps/frontend/web/src/views/Index.tsx
+++ apps/frontend/web/src/views/Index.tsx
@@ -1,10 +1,15 @@
 import { Title } from '@mantine/core';
+import { useBearStore } from '@cwt/state/counter';
 
 export const Index = () => {
+  const bears = useBearStore((state) => state.bears);
+  const increase = useBearStore((state) => state.increase);
   return (
     <div>
       <Title>Welcome Home!</Title>
       <p>This application is in a container.</p>
+      <p>{bears} bears around here</p>
+      <button onClick={() => increase(1)}>one up</button>
     </div>
   );
 };
