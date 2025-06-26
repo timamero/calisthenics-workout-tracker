d3c01749ae4819d1daaee2782e7602fab14cfbb5
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-06-26 12:20:42 -0700

    Use sample schema

--- apps/frontend/web/src/views/Index.tsx
+++ apps/frontend/web/src/views/Index.tsx
@@ -1,13 +1,18 @@
 import { Title } from '@mantine/core';
 import { useBearStore } from '@cwt/state/counter';
+import { User } from '@cwt/schema/sampleSchema';
 
 export const Index = () => {
   const bears = useBearStore((state) => state.bears);
   const increase = useBearStore((state) => state.increase);
+  const user: User = {
+    name: 'Jane Doe',
+    xp: 100,
+  };
   return (
     <div>
-      <Title>Welcome Home!</Title>
-      <p>This application is in a container.</p>
+      <Title>Welcome Home {user.name}!</Title>
+      <p>xp = {user.xp}</p>
       <p>{bears} bears around here</p>
       <button onClick={() => increase(1)}>one up</button>
     </div>
