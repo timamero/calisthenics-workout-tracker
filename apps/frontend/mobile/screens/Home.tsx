45cc78d5814ccf13e200a160c75f8f9e95065a20
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-06-26 12:37:38 -0700

    Use sample schema

--- apps/frontend/mobile/screens/Home.tsx
+++ apps/frontend/mobile/screens/Home.tsx
@@ -1,16 +1,21 @@
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useBearStore } from '@cwt/state/counter';
+import { User } from '@cwt/schema/sampleSchema';
 
 export default function HomeScreen() {
   const navigation = useNavigation<any>();
   const bears = useBearStore((state) => state.bears);
   const increase = useBearStore((state) => state.increase);
+  const user: User = {
+    name: 'Jane Doe',
+    xp: 100,
+  };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome Home!</Text>
-      <Text>Testing preview build</Text>
+      <Text>Welcome Home {user.name}!</Text>
+      <Text>xp = {user.xp}</Text>
       <Button
         title="Go to About"
         onPress={() => navigation.navigate('About')}
