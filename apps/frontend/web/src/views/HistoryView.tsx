431b4d56779a0999e4e2545ff5284ed0e2bf67e0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-01 20:26:49 -0700

    Create view components

---
+++ apps/frontend/web/src/views/HistoryView.tsx
@@ -0,0 +1,10 @@
+import { Title } from '@mantine/core';
+
+export const HistoryView = () => {
+  return (
+    <div>
+      <Title>History Page</Title>
+      <p>This page will display the user's past workout logs.</p>
+    </div>
+  );
+};
---
+++ apps/frontend/web/src/views/SettingsView.tsx
@@ -0,0 +1,10 @@
+import { Title } from '@mantine/core';
+
+export const SettingsView = () => {
+  return (
+    <div>
+      <Title>Settings Page</Title>
+      <p>This page will contain the app settings.</p>
+    </div>
+  );
+};
