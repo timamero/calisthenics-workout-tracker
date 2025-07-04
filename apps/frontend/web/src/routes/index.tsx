a06cc3450fb034f7b7c873a44d8912ce61c90efe
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-03 21:35:36 -0700

    Move HomeView definition to index

--- apps/frontend/web/src/routes/index.tsx
+++ apps/frontend/web/src/routes/index.tsx
@@ -1,6 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { HomeView } from '../views/HomeView';
+import { Title } from '@mantine/core';
+import { useBearStore } from '@cwt/state/counter';
+import { User } from '@cwt/schema/sampleSchema';
+// import { HomeView } from '../views/HomeView';
 
 export const Route = createFileRoute('/')({
   component: HomeView,
 });
+
+function HomeView() {
+  const bears = useBearStore((state) => state.bears);
+  const increase = useBearStore((state) => state.increase);
+  const user: User = {
+    name: 'Jane Doe',
+    xp: 100,
+  };
+  return (
+    <div>
+      <Title>Welcome Home {user.name}!</Title>
+      <p>xp = {user.xp}</p>
+      <p>{bears} bears around here</p>
+      <button onClick={() => increase(1)}>one up</button>
+    </div>
+  );
+}
