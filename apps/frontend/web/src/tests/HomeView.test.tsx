16df56d5fffd550fe12e3f29adec59bb3595d8ae
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-09 01:15:58 -0700

    Create test component

--- apps/frontend/web/src/tests/HomeView.test.tsx
+++ apps/frontend/web/src/tests/HomeView.test.tsx
@@ -1,13 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { HomeView } from '../views/HomeView';
+// import { HomeView } from '../views/HomeView';
+import { Title } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
-test.skip('loads and displays greeting', async () => {
+export const HomeView = () => {
+  return (
+    <div>
+      <Title>Welcome Home Jane Doe!!</Title>
+    </div>
+  );
+};
+
+test('loads and displays greeting', async () => {
   // ARRANGE
-  render(<HomeView />);
+  render(
+    <MantineProvider>
+      <HomeView />
+    </MantineProvider>,
+  );
 
   // ASSERT
-  expect(screen.getByRole('heading')).toHaveTextContent(
-    'Welcome Home Jane Doe!',
-  );
+  // expect(screen.getByRole('heading')).toHaveTextContent(
+  //   'Welcome Home Jane Doe!',
+  // );
+  const heading = await screen.findByRole('heading', {
+    name: /Welcome Home Jane Doe!/i,
+  });
+  expect(heading).toBeInTheDocument();
+  expect(heading).toHaveTextContent('Welcome Home Jane Doe!');
+  // await waitFor(() => {
+  //   expect(screen.getByRole('heading')).toHaveTextContent(
+  //     'Welcome Home Jane Doe!',
+  //   );
+  // });
 });
