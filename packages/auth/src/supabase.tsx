ca63f287f31e22e89d94588f6532fac62eb54e9a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-16 00:13:38 -0700

    Create function ot create supabase client

--- packages/auth/src/supabase.tsx
+++ packages/auth/src/supabase.tsx
@@ -1,4 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with auth
-export const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+export const createSupabaseClient = (supabaseUrl, supabaseAnonKey) => {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and Anon Key must be provided in environment variables.')
+  }
+
+  try {
+    return createClient(supabaseUrl, supabaseAnonKey)
+  } catch (error) {
+    console.error('Error creating Supabase client:', error)
+    throw error
+  }
+}
