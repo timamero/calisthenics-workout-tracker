627cad737daebcda29bec5675e364c783310b9ae
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-16 21:47:07 -0700

    Fix ts types

--- packages/auth/src/createUser.tsx
+++ packages/auth/src/createUser.tsx
@@ -4,8 +4,9 @@
  * @returns {Promise<Object|null>} The created user object or null if an error occurred.
  * @throws Will log an error if user creation fails.
  */
+import { SupabaseClient } from '@supabase/supabase-js';
 
-export const createUser = async (supabase) => {
+export const createUser = async (supabase:SupabaseClient) => {
   const { data, error } = await supabase.auth.signUp({
     email: 'example@email.com',
     password: 'example-password',
