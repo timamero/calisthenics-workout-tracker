ed30d24204c91f5ef6c9f5593645a73d240a806d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-22 02:07:44 -0700

    Log in user on form submit

--- apps/frontend/web/src/routes/login.tsx
+++ apps/frontend/web/src/routes/login.tsx
@@ -7,29 +7,44 @@ import {
   Box,
   Title,
 } from '@mantine/core';
+import { signIn } from '@cwt/auth/signIn';
+import { supabase } from '../supabaseClient';
 
 export const Route = createFileRoute('/login')({
   component: LoginView,
 });
 
 function LoginView() {
+  const handleSignIn = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const email = (event.target as HTMLFormElement).email.value;
+    const password = (event.target as HTMLFormElement).password.value;
+
+    // TODO: Validate
+
+    const user = signIn(supabase, email, password);
+    console.log('User:', user);
+  };
+
   return (
     <Box maw={400} mx="auto" mt="xl">
       <Title order={2} mb="md">
         Log In
       </Title>
-      <form>
+      <form onSubmit={handleSignIn}>
         <TextInput
           label="Email"
           placeholder="Enter your email"
           size="md"
           mb="md"
+          name="email"
         />
         <PasswordInput
           label="Password"
           placeholder="Enter your password"
           size="md"
           mb="md"
+          name="password"
         />
         <Group mt="md" right="0">
           <Button type="submit" size="md">
