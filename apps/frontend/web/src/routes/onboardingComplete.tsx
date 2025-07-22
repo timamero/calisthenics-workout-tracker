cf5095b31d2752084c573637792e61c31bc7d6d0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-21 23:40:55 -0700

    Create onboardingComplete page

---
+++ apps/frontend/web/src/routes/onboardingComplete.tsx
@@ -0,0 +1,20 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Title, Stack, Button } from '@mantine/core';
+
+export const Route = createFileRoute('/onboardingComplete')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      <Title>Welcome User!</Title>
+      <p>Congratulations! Your profile is set up. </p>
+      <Stack align="flex-start" justify="center" mt={40}>
+        <Button variant="filled" size="md" radius="md">
+          Go to Dashboard
+        </Button>
+      </Stack>
+    </div>
+  );
+}
