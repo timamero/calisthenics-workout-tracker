cde13d0cea643ff9b83264f91d0d6681882beaee
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-23 21:00:24 -0700

    Create sign up screen

---
+++ apps/frontend/mobile/screens/SignupScreen.tsx
@@ -0,0 +1,33 @@
+import { View } from 'react-native';
+import { useTheme, TextInput, Text, Button } from 'react-native-paper';
+
+export default function SignupScreen() {
+  const theme = useTheme();
+
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        gap: 16,
+        paddingHorizontal: 16,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Text variant="displayLarge">Log In</Text>
+      <TextInput label="Email" />
+      <TextInput label="Password" />
+      <TextInput label="Confirm Password" />
+      <Button
+        mode="contained"
+        buttonColor={theme.colors.primary}
+        onPress={() => console.log('Press to Sign Up')}
+      >
+        Sign Up
+      </Button>
+    </View>
+  );
+}
