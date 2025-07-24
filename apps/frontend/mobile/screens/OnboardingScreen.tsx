7f605b9e31b29153965fd2a5dd9a2ccdf004698e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-24 00:23:04 -0700

    Update styles

--- apps/frontend/mobile/screens/OnboardingScreen.tsx
+++ apps/frontend/mobile/screens/OnboardingScreen.tsx
@@ -26,129 +26,216 @@ export default function OnboardingScreen() {
       contentContainerStyle={{
         display: 'flex',
         flexDirection: 'column',
-        // flex: 1,
-        gap: 16,
+        gap: 32,
         paddingHorizontal: 16,
+        paddingVertical: 32,
         alignItems: 'stretch',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
       }}
     >
       <Card>
-        <Card.Title
-          title="Profile"
-          subtitle="Welcome! Let's set up your profile."
-        />
         <Card.Content>
-          <Text variant="titleLarge">Profile</Text>
-          <Text variant="bodyMedium">
+          <Text variant="headlineLarge">Profile</Text>
+          <Text variant="bodyMedium" style={{ marginBottom: 16 }}>
             Welcome! Let&apos;s set up your profile.
           </Text>
-          <TextInput label="First Name" />
-          <TextInput label="Last Name" />
-          <TextInput label="Birthday" />
+          <View style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
+            <TextInput label="First Name" />
+            <TextInput label="Last Name" />
+            <TextInput label="Birthday" />
+          </View>
         </Card.Content>
       </Card>
       <Card>
-        <Card.Title
-          title="Fitness Information"
-          subtitle="Tell us about your fitness journey."
-        />
         <Card.Content>
-          <Text variant="titleLarge">Fitness Information</Text>
-          <Text variant="bodyMedium">Tell us about your fitness journey.</Text>
-          <TextInput label="Enter your weight" />
-          <RadioButton.Group
-            onValueChange={(newValue) => setWeightUnit(newValue)}
-            value={weightUnit}
+          <Text variant="headlineLarge">Fitness Information</Text>
+          <Text variant="bodyMedium" style={{ marginBottom: 16 }}>
+            Tell us about your fitness journey.
+          </Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 8,
+              marginVertical: 16,
+            }}
           >
-            <View>
-              <Text>lbs</Text>
-              <RadioButton value="lbs" />
-            </View>
-            <View>
-              <Text>kg</Text>
-              <RadioButton value="kg" />
-            </View>
-          </RadioButton.Group>
-          <TextInput label="Enter your height" />
-          <RadioButton.Group
-            onValueChange={(newValue) => setHeightUnit(newValue)}
-            value={heightUnit}
+            <TextInput label="Enter your weight" style={{ flex: 1 }} />
+            <RadioButton.Group
+              onValueChange={(newValue) => setWeightUnit(newValue)}
+              value={weightUnit}
+            >
+              <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
+                <View>
+                  <Text>lbs</Text>
+                  <RadioButton value="lbs" />
+                </View>
+                <View>
+                  <Text>kg</Text>
+                  <RadioButton value="kg" />
+                </View>
+              </View>
+            </RadioButton.Group>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 8,
+            }}
           >
-            <View>
-              <Text>inches</Text>
-              <RadioButton value="inches" />
-            </View>
-            <View>
-              <Text>cm</Text>
-              <RadioButton value="cm" />
-            </View>
-          </RadioButton.Group>
-          <Text>Fitness Level</Text>
-          <RadioButton.Group
-            onValueChange={(newValue) => setFitnessLevel(newValue)}
-            value={fitnessLevel}
-          >
-            <View>
-              <Text>Beginner</Text>
-              <RadioButton value="Beginner" />
-            </View>
-            <View>
-              <Text>Intermediate</Text>
-              <RadioButton value="Intermediate" />
-            </View>
-            <View>
-              <Text>Advanced</Text>
-              <RadioButton value="Advanced" />
-            </View>
-          </RadioButton.Group>
+            <TextInput label="Enter your height" style={{ flex: 1 }} />
+            <RadioButton.Group
+              onValueChange={(newValue) => setHeightUnit(newValue)}
+              value={heightUnit}
+            >
+              <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
+                <View>
+                  <Text>inches</Text>
+                  <RadioButton value="inches" />
+                </View>
+                <View>
+                  <Text>cm</Text>
+                  <RadioButton value="cm" />
+                </View>
+              </View>
+            </RadioButton.Group>
+          </View>
+          <View style={{ marginVertical: 32 }}>
+            <Text variant="headlineSmall">Fitness Level</Text>
+            <RadioButton.Group
+              onValueChange={(newValue) => setFitnessLevel(newValue)}
+              value={fitnessLevel}
+            >
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  gap: 20,
+                }}
+              >
+                <View>
+                  <Text>Beginner</Text>
+                  <RadioButton value="Beginner" />
+                </View>
+                <View>
+                  <Text>Intermediate</Text>
+                  <RadioButton value="Intermediate" />
+                </View>
+                <View>
+                  <Text>Advanced</Text>
+                  <RadioButton value="Advanced" />
+                </View>
+              </View>
+            </RadioButton.Group>
+          </View>
 
-          <Text>Primary Fitness Goal</Text>
-          <Checkbox.Item label="Build Strength" status="unchecked" />
-          <Checkbox.Item label="Lose Weight" status="unchecked" />
-          <Checkbox.Item label="Improve Endurance" status="unchecked" />
-          <Checkbox.Item label="General Fitness" status="unchecked" />
-          <Checkbox.Item label="Other" status="unchecked" />
+          <Text variant="headlineSmall">Primary Fitness Goal</Text>
+
+          <View style={{ display: 'flex', gap: 0 }}>
+            <Checkbox.Item
+              label="Build Strength"
+              status="unchecked"
+              style={{ marginVertical: 0, paddingVertical: 0 }}
+            />
+            <Checkbox.Item
+              label="Lose Weight"
+              status="unchecked"
+              style={{ marginVertical: 0, paddingVertical: 0 }}
+            />
+            <Checkbox.Item
+              label="Improve Endurance"
+              status="unchecked"
+              style={{ marginVertical: 0, paddingVertical: 0 }}
+            />
+            <Checkbox.Item
+              label="General Fitness"
+              status="unchecked"
+              style={{ marginVertical: 0, paddingVertical: 0 }}
+            />
+            <Checkbox.Item
+              label="Other"
+              status="unchecked"
+              style={{ marginVertical: 0, paddingVertical: 0 }}
+            />
+          </View>
         </Card.Content>
       </Card>
       <Card>
-        <Card.Title
-          title="Preferences"
-          subtitle="Almost there! Set your preferences."
-        />
         <Card.Content>
-          <Text variant="titleLarge">Preferences</Text>
-          <Text variant="bodyMedium">Almost there! Set your preferences.</Text>
-          <Text>Preferred Workout Duration</Text>
-          <RadioButton.Group
-            onValueChange={(newValue) => setDuration(newValue)}
-            value={duration}
+          <Text variant="headlineLarge">Preferences</Text>
+          <Text variant="bodyMedium" style={{ marginBottom: 16 }}>
+            Almost there! Set your preferences.
+          </Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
           >
-            <View>
-              <Text>15-30 min</Text>
-              <RadioButton value="15-30 min" />
-            </View>
-            <View>
-              <Text>30-45 min</Text>
-              <RadioButton value="30-45 min" />
-            </View>
-            <View>
-              <Text>45-60 min</Text>
-              <RadioButton value="45-60 min" />
-            </View>
-            <View>
-              <Text>60+ min</Text>
-              <RadioButton value="60+ min" />
-            </View>
-          </RadioButton.Group>
+            <Text variant="headlineSmall">Preferred Workout Duration</Text>
+            <RadioButton.Group
+              onValueChange={(newValue) => setDuration(newValue)}
+              value={duration}
+            >
+              <View>
+                <Text>15-30 min</Text>
+                <RadioButton value="15-30 min" />
+              </View>
+              <View>
+                <Text>30-45 min</Text>
+                <RadioButton value="30-45 min" />
+              </View>
+              <View>
+                <Text>45-60 min</Text>
+                <RadioButton value="45-60 min" />
+              </View>
+              <View>
+                <Text>60+ min</Text>
+                <RadioButton value="60+ min" />
+              </View>
+            </RadioButton.Group>
 
-          <Text>Available Equipment</Text>
-          <Checkbox.Item label="None" status="unchecked" />
-          <Checkbox.Item label="Pull-up Bar" status="unchecked" />
-          <Checkbox.Item label="Parallel Bars" status="unchecked" />
-          <Checkbox.Item label="Rings" status="unchecked" />
-          <Checkbox.Item label="Weights" status="unchecked" />
-          <Checkbox.Item label="Resistance Bands" status="unchecked" />
+            <View style={{ marginVertical: 32 }}>
+              <Text variant="headlineSmall">Available Equipment</Text>
+              <View style={{ display: 'flex', gap: 0 }}>
+                <Checkbox.Item
+                  label="None"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+                <Checkbox.Item
+                  label="Pull-up Bar"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+                <Checkbox.Item
+                  label="Parallel Bars"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+                <Checkbox.Item
+                  label="Rings"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+                <Checkbox.Item
+                  label="Weights"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+                <Checkbox.Item
+                  label="Resistance Bands"
+                  status="unchecked"
+                  style={{ marginVertical: 0, paddingVertical: 0 }}
+                />
+              </View>
+            </View>
+          </View>
         </Card.Content>
       </Card>
       <Button
