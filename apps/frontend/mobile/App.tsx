1921ab650065b12d4559ed5bdf56b901c5d4c8be
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-24 23:04:21 -0700

    Create loading view

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { View } from 'react-native';
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
+  ActivityIndicator,
 } from 'react-native-paper';
 import Navigation from './navigation';
 
@@ -16,6 +18,28 @@ const theme = {
 };
 
 export default function App() {
+  const loading = true;
+  if (loading) {
+    return (
+      <PaperProvider theme={theme}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+            gap: 16,
+            paddingHorizontal: 16,
+            alignItems: 'stretch',
+            justifyContent: 'center',
+            backgroundColor: theme.colors.background,
+          }}
+        >
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      </PaperProvider>
+    );
+  }
+
   return (
     <PaperProvider theme={theme}>
       <Navigation />
