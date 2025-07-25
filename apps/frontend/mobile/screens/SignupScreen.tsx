3238847aacdb4a3da20e03f4a1b87ed19fc9ca7e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-24 22:37:19 -0700

    Create state variables

--- apps/frontend/mobile/screens/SignupScreen.tsx
+++ apps/frontend/mobile/screens/SignupScreen.tsx
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { useTheme, TextInput, Text, Button } from 'react-native-paper';
 
 export default function SignupScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const theme = useTheme();
 
   return (
@@ -18,8 +23,25 @@ export default function SignupScreen() {
       }}
     >
       <Text variant="displaySmall">Sign Up</Text>
-      <TextInput label="Email" />
-      <TextInput label="Password" />
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TextInput
+        label="Confirm Password"
+        value={confirmPassword}
+        onChangeText={setConfirmPassword}
+        secureTextEntry
+      />
       <TextInput label="Confirm Password" />
       <Button
         mode="contained"
