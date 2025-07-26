8c4ff700b584a9122ae7fd61014fdd97bcf780a6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-25 23:28:59 -0700

    Add dev logs

--- packages/auth/src/session.tsx
+++ packages/auth/src/session.tsx
@@ -7,11 +7,13 @@
 import { SupabaseClient } from '@supabase/supabase-js';
 
 export const session = async (supabase:SupabaseClient) => {
+  console.log('Retrieving session from Supabase...');
   const { data, error } = await supabase.auth.getSession()
 
   if (error) {
     console.error('Error retrieving session', error)
     return null
   }
+  console.log('Session retrieved successfully')
   return data.session
 }
