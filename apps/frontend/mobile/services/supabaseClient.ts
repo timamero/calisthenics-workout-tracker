94d9e727834d00b0b2bb7396fb445ee33ff4cb4d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-25 23:14:17 -0700

    Add AsyncStorage to client initialization

--- apps/frontend/mobile/services/supabaseClient.ts
+++ apps/frontend/mobile/services/supabaseClient.ts
@@ -1,6 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // eslint-disable-next-line import/no-unresolved
 import { supabaseClient } from '@cwt/auth/supabase';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
-export const supabase = supabaseClient(supabaseUrl, supabaseAnonKey);
+const options = {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+};
+export const supabase = supabaseClient(supabaseUrl, supabaseAnonKey, options);
