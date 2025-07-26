8f959bc87f847dd5cdc8b09bc0b17324828fa398
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-25 23:30:39 -0700

    Add option parameter

--- packages/auth/src/supabase.tsx
+++ packages/auth/src/supabase.tsx
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with auth
-export const supabaseClient = (supabaseUrl:string, supabaseAnonKey:string) => {
+export const supabaseClient = (supabaseUrl:string, supabaseAnonKey:string, options: any) => {
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Supabase URL and Anon Key must be provided in environment variables.')
   }
 
   try {
-    return createClient(supabaseUrl, supabaseAnonKey)
+    return createClient(supabaseUrl, supabaseAnonKey, options)
   } catch (error) {
     console.error('Error creating Supabase client:', error)
     throw error
