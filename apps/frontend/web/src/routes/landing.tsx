593e6abcec2dcb98c9deb9fc1568d1907c7b7b30
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-27 00:05:22 -0700

    Test protected route with no authorization

--- apps/frontend/web/src/routes/landing.tsx
+++ apps/frontend/web/src/routes/landing.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Button, Stack } from '@mantine/core';
-// https://mantine.dev/core/stack/
+
+import { useAuthStore } from '@cwt/state/auth';
+import { getProtectedData } from '@cwt/api/protectedData';
+
 export const Route = createFileRoute('/landing')({
   component: LandingView,
 });
 
 function LandingView() {
+  const session = useAuthStore((state) => state.session);
+  useEffect(() => {
+    console.log(
+      'Fetching data from protected route in landing (expecting no data)...',
+    );
+    const baseUrl = 'http://127.0.0.1:8000';
+
+    const asyncFetchProtectedData = async () => {
+      if (session) {
+        console.log('token: ', session.access_token);
+        await getProtectedData(baseUrl, session.access_token);
+      } else {
+        console.log('No protected data to fetch on landing page.');
+      }
+    };
+    asyncFetchProtectedData();
+  }, []);
   return (
     <div>
       <Title>Sign Up or Log In</Title>
