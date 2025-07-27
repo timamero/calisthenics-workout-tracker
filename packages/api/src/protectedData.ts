acd8ae121bdf37f3f3252e95a504e29bb83c0085
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-26 23:53:48 -0700

    Add header to request

--- packages/api/src/protectedData.ts
+++ packages/api/src/protectedData.ts
@@ -1,14 +1,21 @@
-export async function getProtectedData(baseUrl: string) {
+export async function getProtectedData(baseUrl: string, token: string) {
   const url = `${baseUrl}/protected-route`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers
+    });
     if (!response.ok) {
       throw new Error(`Resonse status: $(response.status}`);
     }
 
     const json = await response.json();
-    console.log('Data fethed from FastAPI: ', json);
+    console.log('Protected data fethed from FastAPI: ', json);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching protected data:', error);
   }
 }
