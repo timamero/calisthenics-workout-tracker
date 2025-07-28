9ba3093f94f9f17771fcaebb9d82c1d147976846
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-28 01:28:53 -0700

    Get data from protected route

--- apps/frontend/mobile/screens/SettingsScreen.tsx
+++ apps/frontend/mobile/screens/SettingsScreen.tsx
@@ -1,17 +1,33 @@
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { useTheme, Button } from 'react-native-paper';
 
 // eslint-disable-next-line import/no-unresolved
 import { signOut } from '@cwt/auth/signOut';
+import { useAuthStore } from '@cwt/state/auth';
+import { getProtectedData } from '@cwt/api/protectedData';
+
 import { supabase } from '../services/supabaseClient';
 
 export default function SettingsScreen() {
   const theme = useTheme();
+  const session = useAuthStore((state) => state.session);
 
   const handleLogOut = async () => {
     await signOut(supabase);
-    // setSession(null); // Clear the session in the store
   };
+
+  useEffect(() => {
+    console.log('Fetching data from protected route in settings...');
+    const baseUrl = 'http://REDACTED_IP:8000';
+
+    const asyncFetchProtectedData = async () => {
+      if (session) {
+        await getProtectedData(baseUrl, session.access_token);
+      }
+    };
+    asyncFetchProtectedData();
+  }, [session]);
 
   return (
     <View
