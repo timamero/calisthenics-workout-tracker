e2c3ed45e0ce0f820e627fd52b757d8baa985f26
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 15:00:36 -0700

    Get auth header in request

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from app.schemas.exercise import ExerciseSchema
 from app.api.utils.exercises import get_exercises
@@ -9,10 +9,14 @@ router = APIRouter(prefix="/exercises")
 
 
 @router.get("/", response_model=List[ExerciseSchema])
-def read_exercises():
+# @router.get("/")
+def read_exercises(request: Request):
     """
     Retrieve a list of exercises.
     """
+    print(f"exercises get request headers: {request.headers}")
+    print(f"exercises get request headers auth: {request.headers.get("Authorization")}")
+    # auth_header = request.headers.get("Authorization")
     exercises = get_exercises()
 
     return exercises
