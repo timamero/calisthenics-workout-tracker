810951ada48689ddc34f48cecf69d2400e736fed
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 15:11:45 -0700

    Pass auth token to get exercises

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -9,19 +9,15 @@ router = APIRouter(prefix="/exercises")
 
 
 @router.get("/", response_model=List[ExerciseSchema])
-# @router.get("/")
 def read_exercises(request: Request):
     """
     Retrieve a list of exercises.
     """
-    print(f"exercises get request headers: {request.headers}")
-    print(f"exercises get request headers auth: {request.headers.get("Authorization")}")
     auth_header = request.headers.get("Authorization")
     if not auth_header or not auth_header.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Authentication required")
 
-    print("getting exercises")
-    exercises = get_exercises()
+    access_token = auth_header.split(" ")[1]
 
+    exercises = get_exercises(access_token)
     return exercises
-    # return {"HELLO": "WORLD"}
