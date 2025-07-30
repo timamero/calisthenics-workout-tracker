c1be03dd6b765c93587b910e413f6bad0a3ae08d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 15:00:03 -0700

    Use get_supabase_client

--- apps/backend/app/api/utils/exercises.py
+++ apps/backend/app/api/utils/exercises.py
@@ -1,7 +1,8 @@
-from app.services.supabase_client import supabase
+from app.services.supabase_client import get_supabase_client
 
 
-def get_exercises():
+def get_exercises(access_token: str):
+    supabase = get_supabase_client(access_token)
     try:
         response = supabase.table("exercises").select("*").range(0, 10).execute()
         return response.data
