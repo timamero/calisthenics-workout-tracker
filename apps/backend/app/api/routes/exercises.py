7fd79e24dfcf50f25dd6bf4f2e430a1f5e1041d6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 15:59:01 -0700

    Create route to get exercise by id

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -21,3 +21,27 @@ def read_exercises(request: Request):
 
     exercises = get_exercises(access_token)
     return exercises
+
+
+@router.get("/{exercise_id}", response_model=ExerciseSchema)
+def read_exercise_item(exercise_id: int):
+    """
+    Retrieve a list of exercises.
+    """
+    # auth_header = request.headers.get("Authorization")
+    # if not auth_header or not auth_header.startswith("Bearer "):
+    #     raise HTTPException(status_code=401, detail="Authentication required")
+
+    # access_token = auth_header.split(" ")[1]
+
+    exercise = {
+        "id": 1,
+        "name": "Sample Exercise",
+        "target_muscles": ["biceps", "triceps"],
+        "required_equipment": ["dumbbells"],
+        "emphasis": "strength",
+        "difficulty": "beginner",
+        "tags": ["sample", "exercise"],
+        "instructions": "Follow the instructions to complete the exercise.",
+    }
+    return exercise
