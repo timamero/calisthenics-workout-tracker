31c9da965ecace9ea5e1a7ca27b11c0826b89818
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 21:19:50 -0700

    Get token from auth header

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -28,23 +28,12 @@ def read_exercise_item(exercise_id: str, request: Request):
     """
     Retrieve a list of exercises.
     """
-    # auth_header = request.headers.get("Authorization")
-    # if not auth_header or not auth_header.startswith("Bearer "):
-    #     raise HTTPException(status_code=401, detail="Authentication required")
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Authentication required")
 
-    access_token = None
-    # access_token = auth_header.split(" ")[1]
-
-    # exercise = {
-    #     "id": 1,
-    #     "name": "Sample Exercise",
-    #     "target_muscles": ["biceps", "triceps"],
-    #     "required_equipment": ["dumbbells"],
-    #     "emphasis": "strength",
-    #     "difficulty": "beginner",
-    #     "tags": ["sample", "exercise"],
-    #     "instructions": "Follow the instructions to complete the exercise.",
-    # }
+    # access_token = None
+    access_token = auth_header.split(" ")[1]
 
     exercise = get_exercise_by_id(exercise_id=exercise_id, access_token=access_token)
     return exercise
