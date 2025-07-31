7816985bb143300b4af1e01a85558297350eecba
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-31 14:53:24 -0700

    Add types and set master and displayed exercises list

--- packages/state/src/exercises.ts
+++ packages/state/src/exercises.ts
@@ -1,15 +1,22 @@
 import { create } from 'zustand';
-// Need to export exercises schemas
+import { Exercise, Muscles, Equipment, Emphasis, Difficulty } from '@cwt/schema/exerciseSchema';
+
+type Filter = { muscle: Muscles; equipment: Equipment; emphasis: Emphasis; difficulty: Difficulty}
+
 interface ExercisesState {
-  masterExercises: any;
-  displayedExercises: any;
-  filter: any;
+  masterExercises: Exercise[] | null;
+  displayedExercises: Exercise[] | null;
+  filter: Filter;
   search: string;
+  setMasterExercises: (masterExercises: Exercise[]) => void;
+  setDisplayedExercises: (displayed: Exercise[]) => void;
 }
 
 export const useExercisesStore = create<ExercisesState>((set) => ({
   masterExercises: null,
   displayedExercises: null,
-  filter: null,
+  filter: {muscle: [""], equipment: [""], emphasis: "", difficulty: ""},
   search: "",
+  setMasterExercises: (masterExercises) => set({ masterExercises }),
+  setDisplayedExercises: (displayedExercises) => set({ displayedExercises }),
 }));
