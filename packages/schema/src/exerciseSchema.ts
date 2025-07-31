eebfd8865cfdf2f2c655b5daec4a2455c3a40dfd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-31 14:16:27 -0700

    Create Equipment schema

--- packages/schema/src/exerciseSchema.ts
+++ packages/schema/src/exerciseSchema.ts
@@ -1,9 +1,13 @@
 import * as z from "zod";
 
 const muscles = ["chest", "shoulders", "biceps", "triceps", "latissimus dorsi", "obliques", "abs", "calf", "quadriceps", "hamstrings", "glutes", "lower back", "rhomboid", "full body"]
+const equipment = ["gymnastic rings", "resistance bands", "bench", "weighted vest", "bar", "parallel bars", "speed rope", "suspension trainer", "paralletes", "ab roller", "pull up bars", "box"]
 
 export const Muscles = z.array(z.enum(muscles as [string, ...string[]]))
 export type Muscles = z.infer<typeof Muscles>;
+
+export const Equipment = z.array(z.enum(equipment as [string, ...string[]]));
+export type Equipment = z.infer<typeof Equipment>;
 
 export const Emphasis = z.enum(["plyometrics", "mobility", "power", "endurance", "strength"]);
 export type Emphasis = z.infer<typeof Emphasis>;
@@ -15,7 +19,7 @@ export const Exercise = z.object({
   id: z.number(),
   name: z.string(),
   target_muscles: z.array(z.enum(muscles as [string, ...string[]])),
-  required_equipment: z.array(z.string()).nullable().optional(),
+  required_equipment: z.array(z.enum(equipment as [string, ...string[]])).nullable().optional(),
   emphasis: z.enum(["plyometrics", "mobility", "power", "endurance", "strength"]),
   difficulty: z.enum(["beginner", "intermediate", "advanced"]),
   tags: z.array(z.string()),
