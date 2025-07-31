f99f1e76be635309a1f06ddf26d5d3588c939828
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-31 14:05:58 -0700

    Create Muscles schema

--- packages/schema/src/exerciseSchema.ts
+++ packages/schema/src/exerciseSchema.ts
@@ -1,5 +1,10 @@
 import * as z from "zod";
 
+const muscles = ["chest", "shoulders", "biceps", "triceps", "latissimus dorsi", "obliques", "abs", "calf", "quadriceps", "hamstrings", "glutes", "lower back", "rhomboid", "full body"]
+
+export const Muscles = z.array(z.enum(muscles as [string, ...string[]]))
+export type Muscles = z.infer<typeof Muscles>;
+
 export const Emphasis = z.enum(["plyometrics", "mobility", "power", "endurance", "strength"]);
 export type Emphasis = z.infer<typeof Emphasis>;
 
@@ -9,7 +14,7 @@ export type Difficulty = z.infer<typeof Difficulty>;
 export const Exercise = z.object({
   id: z.number(),
   name: z.string(),
-  target_muscles: z.array(z.string()),
+  target_muscles: z.array(z.enum(muscles as [string, ...string[]])),
   required_equipment: z.array(z.string()).nullable().optional(),
   emphasis: z.enum(["plyometrics", "mobility", "power", "endurance", "strength"]),
   difficulty: z.enum(["beginner", "intermediate", "advanced"]),
