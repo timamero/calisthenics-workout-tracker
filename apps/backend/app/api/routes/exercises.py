3fda1ecadee942a37f6962c109fe0a33f8457a50
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-30 22:08:29 -0700

    Create filter query parameters

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -1,23 +1,72 @@
-from typing import List
+from typing import List, Literal, Annotated
 
-from fastapi import APIRouter, Request, HTTPException
+from pydantic import BaseModel
+from fastapi import APIRouter, Request, HTTPException, Query
 
 from app.schemas.exercise import ExerciseSchema
 from app.api.utils.exercises import get_exercises, get_exercise_by_id
 
 router = APIRouter(prefix="/exercises")
 
+sample_exercises = [
+    {
+        "id": 1,
+        "name": "Standard Push-Up",
+        "target_muscles": ["chest", "triceps", "shoulders"],
+        "required_equipment": [],
+        "emphasis": "strength",
+        "difficulty": "beginner",
+        "tags": ["push", "upper"],
+    },
+    {
+        "id": 10,
+        "name": "Lunge",
+        "target_muscles": ["quadriceps", "glutes", "hamstrings"],
+        "required_equipment": [],
+        "emphasis": "strength",
+        "difficulty": "beginner",
+        "tags": ["leg day", "lower"],
+    },
+]
+
+
+class ExerciseFilterParams(BaseModel):
+    muscles: list[str] = []
+    equipments: list[str] = []
+    difficulty: Literal["beginner", "intermediate", "advanced", ""] = ""
+    emphasis: Literal[
+        "plyometrics", "mobility", "power", "endurance", "strength", ""
+    ] = ""
+
+
+# @router.get("/", response_model=List[ExerciseSchema])
+# def read_exercises(request: Request):
+#     """
+#     Retrieve a list of exercises.
+#     """
+#     # auth_header = request.headers.get("Authorization")
+#     # if not auth_header or not auth_header.startswith("Bearer "):
+#     #     raise HTTPException(status_code=401, detail="Authentication required")
+
+#     # access_token = auth_header.split(" ")[1]
+#     access_token = None
+
+#     exercises = get_exercises(access_token)
+#     return exercises
+
 
 @router.get("/", response_model=List[ExerciseSchema])
-def read_exercises(request: Request):
+def read_filtered_exercises(filter_query: Annotated[ExerciseFilterParams, Query()]):
     """
     Retrieve a list of exercises.
     """
-    auth_header = request.headers.get("Authorization")
-    if not auth_header or not auth_header.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Authentication required")
+    print("Filter Query:", filter_query)
+    # auth_header = request.headers.get("Authorization")
+    # if not auth_header or not auth_header.startswith("Bearer "):
+    #     raise HTTPException(status_code=401, detail="Authentication required")
 
-    access_token = auth_header.split(" ")[1]
+    # access_token = auth_header.split(" ")[1]
+    access_token = None
 
     exercises = get_exercises(access_token)
     return exercises
