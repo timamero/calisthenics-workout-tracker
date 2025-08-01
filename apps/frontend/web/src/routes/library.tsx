abdd4ed1f28944be95931c1c30fadcae84bc6276
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-07-31 23:23:16 -0700

    Update card component style

--- apps/frontend/web/src/routes/library.tsx
+++ apps/frontend/web/src/routes/library.tsx
@@ -326,7 +326,7 @@ export const Route = createFileRoute('/library')({
 
 function ExerciseCard({ exercise }: { exercise: Exercise }) {
   return (
-    <Paper shadow="xs" p="md" radius="lg">
+    <Paper shadow="lg" p="md" radius="lg">
       <Stack
         bg="var(--mantine-color-body)"
         align="stretch"
@@ -334,32 +334,39 @@ function ExerciseCard({ exercise }: { exercise: Exercise }) {
         gap="sm"
       >
         <Group justify="space-between" mb="sm">
-          <Title order={2} size="h3">
+          <Title order={2} size="h5">
             {exercise.name}
           </Title>
           <Badge color="yellow">{exercise.difficulty}</Badge>
         </Group>
-        <Group>
-          <Text c="gray.7">MUSCLES: </Text>
-          {exercise.target_muscles.map((muscle, i) => {
-            return (
-              <Badge color="pink" key={i}>
-                {muscle}
-              </Badge>
-            );
-          })}
+        <Group gap="sm" wrap="nowrap" align="flex-start">
+          <Text tt="uppercase" size="xs" c="gray.7">
+            Muscles:{' '}
+          </Text>
+          <Group gap={8}>
+            {exercise.target_muscles.map((muscle, i) => {
+              return (
+                <Badge color="pink" key={i}>
+                  {muscle}
+                </Badge>
+              );
+            })}
+          </Group>
         </Group>
-        <Group>
-          <Text c="gray.7">EQUIPMENT: </Text>
-          {exercise.required_equipment == null ||
-          exercise.required_equipment.length == 0 ? (
-            <Badge>None</Badge>
-          ) : (
-            exercise.required_equipment.map((equipment, i) => (
-              <Badge key={i}>{equipment}</Badge>
-            ))
-          )}
-          <Badge>None</Badge>
+        <Group gap="sm" wrap="nowrap" align="flex-start">
+          <Text tt="uppercase" size="xs" c="gray.7">
+            Equipment:{' '}
+          </Text>
+          <Group gap={8}>
+            {exercise.required_equipment == null ||
+            exercise.required_equipment.length == 0 ? (
+              <Badge>None</Badge>
+            ) : (
+              exercise.required_equipment.map((equipment, i) => (
+                <Badge key={i}>{equipment}</Badge>
+              ))
+            )}
+          </Group>
         </Group>
       </Stack>
     </Paper>
