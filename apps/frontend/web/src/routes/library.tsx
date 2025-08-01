f8d02acdf1a08b00f7f93c774c96f9a34e3c8be2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-01 00:12:59 -0700

    Use exercise from store

--- apps/frontend/web/src/routes/library.tsx
+++ apps/frontend/web/src/routes/library.tsx
@@ -1,21 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Stack } from '@mantine/core';
-import { Exercise } from '@cwt/schema/exerciseSchema';
+import { useExercisesStore } from '@cwt/state/exercises';
 
 import ExerciseCard from '../components/ExerciseCard';
-
-
 
 export const Route = createFileRoute('/library')({
   component: LibraryView,
 });
 
 function LibraryView() {
+  const exercises = useExercisesStore((state) => state.displayedExercises);
   return (
     <div>
       <Title>Library Page</Title>
       <Stack align="stretch" justify="center" gap="lg">
-        {sampleExercises.map((exercise, i) => (
+        {exercises.map((exercise, i) => (
           <ExerciseCard key={i} exercise={exercise} />
         ))}
       </Stack>
