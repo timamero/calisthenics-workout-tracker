75923e0c5fa18ae012869d8ed575b49d2b4c295b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-03 04:42:27 -0700

    Apply Filter

--- packages/state/src/types.ts
+++ packages/state/src/types.ts
@@ -3,7 +3,7 @@ import { Muscles, Equipment, Emphasis, Difficulty, Selection, musclesEnum,
   emphasisEnum,
   difficultyEnum, } from '@cwt/schema/exerciseSchema';
 
-export type Filter = { muscle: Muscles; equipment: Equipment; emphasis: Emphasis[]; difficulty: Difficulty[]}
+export type Filter = { target_muscles: Muscles; required_equipment: Equipment; emphasis: Emphasis[]; difficulty: Difficulty[]}
 export type FilterGroup = keyof Filter;
-export const filterKeys: FilterGroup[] = ["muscle", "equipment", "emphasis", "difficulty"];
+export const filterKeys: FilterGroup[] = ["target_muscles", "required_equipment", "emphasis", "difficulty"];
 export type FilterCheckbox = { group: FilterGroup, selection: Selection,  value?: boolean }
