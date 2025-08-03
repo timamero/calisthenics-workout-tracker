0848dda5d5ed06183b7df1d7d6d40a085820d7ab
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-02 23:05:17 -0700

    Create exercises filter slice

---
+++ packages/state/src/slices/library/exercisesFilterSlice.ts
@@ -0,0 +1,26 @@
+import { StateCreator } from 'zustand';
+
+import { Muscles, Equipment, Emphasis, Difficulty, Selection, musclesEnum,
+  equipmentEnum,
+  emphasisEnum,
+  difficultyEnum, } from '@cwt/schema/exerciseSchema';
+
+export type Filter = { muscle: Muscles; equipment: Equipment; emphasis: Emphasis[]; difficulty: Difficulty[]}
+export type FilterGroup = keyof Filter;
+export const filterKeys: FilterGroup[] = ["muscle", "equipment", "emphasis", "difficulty"];
+type FilterCheckbox = { group: FilterGroup, selection: Selection,  value: boolean }
+
+
+const muscleSelections: FilterCheckbox[] = musclesEnum.map((a) => ({group: "muscle", selection: a, value: false}))
+const equipmentSelections: FilterCheckbox[] = equipmentEnum.map((a) => ({group: "muscle", selection: a, value: false}))
+const emphasisSelections: FilterCheckbox[] = emphasisEnum.map((a) => ({group: "muscle", selection: a, value: false}))
+const difficultySelections: FilterCheckbox[] = difficultyEnum.map((a) => ({group: "muscle", selection: a, value: false}))
+const initialFilterCheckboxSelections: FilterCheckbox[] = [...muscleSelections, ...equipmentSelections, ...emphasisSelections, ...difficultySelections]
+
+export interface ExercisesFilterSlice {
+  filterCheckboxSelections: FilterCheckbox[]
+}
+
+export const createExercisesFilterSlice: StateCreator<ExercisesFilterSlice, [], [], ExercisesFilterSlice> = (set) => ({
+  filterCheckboxSelections: initialFilterCheckboxSelections,
+})
--- packages/state/src/store.ts
+++ packages/state/src/store.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { createExercisesSlice, ExercisesSlice } from './slices/library/exercisesSlice';
+import { createExercisesFilterSlice, ExercisesFilterSlice } from './slices/library/exercisesFilterSlice';
 
-export const useStore = create<ExercisesSlice>((...a) => ({
+export const useStore = create<ExercisesSlice & ExercisesFilterSlice>((...a) => ({
   ...createExercisesSlice(...a),
+  ...createExercisesFilterSlice(...a)
 }))
