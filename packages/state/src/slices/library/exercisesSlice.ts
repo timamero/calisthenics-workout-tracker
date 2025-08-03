5621de341de1a7f1aff88e546d28e6349d7b3dfd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-03 04:42:27 -0700

    Apply Filter

--- packages/state/src/slices/library/exercisesSlice.ts
+++ packages/state/src/slices/library/exercisesSlice.ts
@@ -1,22 +1,26 @@
 import { create, StateCreator } from 'zustand';
 
-import { Exercise, Muscles, Equipment, Emphasis, Difficulty } from '@cwt/schema/exerciseSchema';
+import { Exercise, Muscles, Equipment, Emphasis, Difficulty, Selection } from '@cwt/schema/exerciseSchema';
+import { FilterCheckbox } from '../../types';
+import { StoreState } from '../../store';
 
 
-export type Filter = { muscle: Muscles; equipment: Equipment; emphasis: Emphasis[]; difficulty: Difficulty[]}
+// export type Filter = { muscle: Muscles; equipment: Equipment; emphasis: Emphasis[]; difficulty: Difficulty[]}
 
 export interface ExercisesSlice {
   masterExercises: Exercise[]
   displayedExercises: Exercise[]
+  isFilterApplied: boolean;
   setExercises: (exercises: Exercise[]) => void
-  filterDisplayedExercises: (filter: Filter) => void
+  filterDisplayedExercises: () => void
   filterDisplayedExercisesBySearch: (search: string) => void
   resetDisplayedExercises: () => void
 }
 
-export const createExercisesSlice: StateCreator<ExercisesSlice, [], [], ExercisesSlice> = (set) => ({
+export const createExercisesSlice: StateCreator<StoreState, [], [], ExercisesSlice> = (set, get) => ({
   masterExercises: [],
   displayedExercises: [],
+  isFilterApplied: false,
   setExercises: (exercises) => set(() => {
     const sortedExercises = exercises.sort((a, b) => a.name.localeCompare(b.name))
     return {
@@ -24,10 +28,44 @@ export const createExercisesSlice: StateCreator<ExercisesSlice, [], [], Exercise
       displayedExercises: sortedExercises,
     }
   }),
-  filterDisplayedExercises: (filter) => set((state) => {
-    // code to filter exercises
+  filterDisplayedExercises: () => set((state) => {
+    const appliedFilters = get().appliedFilterSelections
+
+    if (appliedFilters.length === 0) return {
+      isFilterApplied: false
+    }
+
+    const appliedFilterGroupNames = appliedFilters.map((obj) => obj.group);
+    const uniqueAppliedFilterGroupNames = Array.from(new Set(appliedFilterGroupNames));
+    const appliedFilterSelections: Selection[] = appliedFilters.map((obj) => obj.selection)
+    const filteredExercises = state.masterExercises.filter((obj) => {
+      const conditionals: boolean[] = []
+      uniqueAppliedFilterGroupNames.forEach((group) => {
+        if (typeof obj[group] === "string") {
+          if (appliedFilterSelections.includes(obj[group])) {
+            conditionals.push(true)
+          } else {
+            conditionals.push(false)
+          }
+        } else if (Array.isArray(obj[group])) {
+          const arrayConditionals: boolean[] = []
+          obj[group].forEach((item) => {
+            if (appliedFilterSelections.includes(item)) {
+              arrayConditionals.push(true)
+            } else {
+              arrayConditionals.push(false)
+            }
+          })
+
+          conditionals.push(arrayConditionals.some((con) => con))
+        }
+      })
+
+      return conditionals.every((con) => con)
+    })
     return {
-      displayedExercises: state.displayedExercises
+      isFilterApplied: true,
+      displayedExercises: filteredExercises
     }
   }),
   filterDisplayedExercisesBySearch: (search) => set((state) => {
