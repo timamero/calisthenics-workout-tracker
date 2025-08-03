96daac5d4037198f6fc53e1c5c3eec526e8ca096
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-02 23:43:58 -0700

    Fix group value

--- packages/state/src/slices/library/exercisesFilterSlice.ts
+++ packages/state/src/slices/library/exercisesFilterSlice.ts
@@ -12,9 +12,9 @@ type FilterCheckbox = { group: FilterGroup, selection: Selection,  value: boolea
 
 
 const muscleSelections: FilterCheckbox[] = musclesEnum.map((a) => ({group: "muscle", selection: a, value: false}))
-const equipmentSelections: FilterCheckbox[] = equipmentEnum.map((a) => ({group: "muscle", selection: a, value: false}))
-const emphasisSelections: FilterCheckbox[] = emphasisEnum.map((a) => ({group: "muscle", selection: a, value: false}))
-const difficultySelections: FilterCheckbox[] = difficultyEnum.map((a) => ({group: "muscle", selection: a, value: false}))
+const equipmentSelections: FilterCheckbox[] = equipmentEnum.map((a) => ({group: "equipment", selection: a, value: false}))
+const emphasisSelections: FilterCheckbox[] = emphasisEnum.map((a) => ({group: "emphasis", selection: a, value: false}))
+const difficultySelections: FilterCheckbox[] = difficultyEnum.map((a) => ({group: "difficulty", selection: a, value: false}))
 const initialFilterCheckboxSelections: FilterCheckbox[] = [...muscleSelections, ...equipmentSelections, ...emphasisSelections, ...difficultySelections]
 
 export interface ExercisesFilterSlice {
