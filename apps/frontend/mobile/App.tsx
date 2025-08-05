1e1fb234622eea178c21d0ddeb00d8cca45bc3dd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 02:55:01 -0700

    Configure theme for customization

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -15,13 +15,20 @@ import { useStore } from '@cwt/state/store';
 import { supabase } from './services/supabaseClient';
 import Navigation from './navigation';
 
-const theme = {
+export type CustomTheme = typeof DefaultTheme & {
+  colors: typeof DefaultTheme.colors & {
+    blue: string;
+  };
+};
+
+const theme: CustomTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'thistle',
-    background: 'snow',
+    primary: 'rbg(255, 99, 71)', // tomato
+    secondary: 'rgb(219, 100, 50)',
+    background: 'rgb(255, 255, 255)',
+    blue: 'rgb(34, 139, 230)',
   },
 };
 
