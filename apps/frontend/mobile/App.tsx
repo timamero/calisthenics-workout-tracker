c2e0ce88ec2b98dea381ce4a150d0b2d55fd2987
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 03:37:28 -0700

    refactor: Move theme to separate file

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -1,36 +1,15 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import {
-  MD3LightTheme as DefaultTheme,
-  PaperProvider,
-  ActivityIndicator,
-  Text,
-} from 'react-native-paper';
+import { PaperProvider, ActivityIndicator, Text } from 'react-native-paper';
 import { Subscription } from '@supabase/supabase-js';
 
 import { useAuthStore } from '@cwt/state/auth';
 import { Exercise } from '@cwt/schema/exerciseSchema';
 import { useStore } from '@cwt/state/store';
 
+import theme from './theme';
 import { supabase } from './services/supabaseClient';
 import Navigation from './navigation';
-
-export type CustomTheme = typeof DefaultTheme & {
-  colors: typeof DefaultTheme.colors & {
-    blue: string;
-  };
-};
-
-const theme: CustomTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'rbg(255, 99, 71)', // tomato
-    secondary: 'rgb(219, 100, 50)',
-    background: 'rgb(255, 255, 255)',
-    blue: 'rgb(34, 139, 230)',
-  },
-};
 
 const sampleExercises: Exercise[] = [
   {
