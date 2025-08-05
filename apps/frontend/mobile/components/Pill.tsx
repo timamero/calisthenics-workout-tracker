08dde3d8098ada3d2a9ac601ac99170c05e488fa
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 04:29:09 -0700

    Update font styles

--- apps/frontend/mobile/components/Pill.tsx
+++ apps/frontend/mobile/components/Pill.tsx
@@ -3,29 +3,22 @@ import { Text, useTheme } from 'react-native-paper';
 
 import { CustomTheme } from '../theme';
 
-export enum sizeTypes {
-  sm = 'sm',
-  lg = 'lg',
-}
-
 interface PillProps {
   children: string;
-  size?: sizeTypes;
 }
 
-export default function Pill({ size = sizeTypes.sm, children }: PillProps) {
+export default function Pill({ children }: PillProps) {
   const { colors } = useTheme() as CustomTheme;
-
-  const textSize = size === sizeTypes.sm ? 12 : 14;
   return (
     <Text
+      variant="bodySmall"
       style={{
         paddingInline: 12,
         paddingBlock: 4,
         backgroundColor: colors.blue,
         borderRadius: 12,
-        fontSize: textSize,
         color: 'white',
+        textTransform: 'uppercase',
       }}
     >
       {children}
