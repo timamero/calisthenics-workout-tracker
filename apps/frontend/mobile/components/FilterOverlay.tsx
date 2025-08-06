de825651951703c1c958631364017475a34eb3ac
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-06 15:10:38 -0700

    Create checked item style

--- apps/frontend/mobile/components/FilterOverlay.tsx
+++ apps/frontend/mobile/components/FilterOverlay.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { Modal, Portal, Checkbox } from 'react-native-paper';
+import { Modal, Portal, Checkbox, useTheme } from 'react-native-paper';
 import { View } from 'react-native';
 
 import { Text } from '../customText';
+import theme from '../theme';
 
 export type FilterOverlayProps = {
   visible: boolean;
@@ -13,9 +14,18 @@ export default function FilterOverlay({
   visible,
   handleHideModal,
 }: FilterOverlayProps) {
+  // const theme = useTheme()
   const [checked, setChecked] = React.useState(false);
 
   const containerStyle = { backgroundColor: 'white', padding: 20 };
+
+  const checkboxBgColor = checked
+    ? 'rgba(255, 99, 71, 0.1)'
+    : theme.colors.background;
+  const checkboxBorderColor = checked
+    ? theme.colors.primary
+    : 'rgb(222, 226, 230)';
+  const labelFontWeight = checked ? '700' : '400';
 
   return (
     <Portal>
@@ -41,7 +51,8 @@ export default function FilterOverlay({
             style={{
               borderWidth: 1,
               borderRadius: 40,
-              borderColor: 'rgb(222, 226, 230)',
+              borderColor: checkboxBorderColor,
+              backgroundColor: checkboxBgColor,
             }}
           >
             <Checkbox.Item
@@ -51,6 +62,7 @@ export default function FilterOverlay({
               labelStyle={{
                 textTransform: 'uppercase',
                 color: 'rgb(46, 46, 46)',
+                fontWeight: labelFontWeight,
               }}
               status={checked ? 'checked' : 'unchecked'}
               onPress={() => {
