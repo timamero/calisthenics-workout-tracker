45e5e3e3a8991db6cc838b388d51f83f3799b1c5
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-06 15:45:54 -0700

    Create FilterCheckbox component

--- apps/frontend/mobile/components/FilterOverlay/FilterOverlay.tsx
+++ apps/frontend/mobile/components/FilterOverlay/FilterOverlay.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Modal, Portal, Checkbox, useTheme } from 'react-native-paper';
+import { Modal, Portal } from 'react-native-paper';
 import { View } from 'react-native';
 
 import { Text } from '../../customText';
-// import theme from '../../theme';
+
+import FilterCheckbox from './FilterCheckbox';
 
 export type FilterOverlayProps = {
   visible: boolean;
@@ -14,18 +15,7 @@ export default function FilterOverlay({
   visible,
   handleHideModal,
 }: FilterOverlayProps) {
-  const theme = useTheme();
-  const [checked, setChecked] = React.useState(false);
-
   const containerStyle = { backgroundColor: 'white', padding: 20 };
-
-  const checkboxBgColor = checked
-    ? 'rgba(255, 99, 71, 0.1)'
-    : theme.colors.background;
-  const checkboxBorderColor = checked
-    ? theme.colors.primary
-    : 'rgb(222, 226, 230)';
-  const labelFontWeight = checked ? '700' : '400';
 
   return (
     <Portal>
@@ -47,39 +37,7 @@ export default function FilterOverlay({
             flexDirection: 'row',
           }}
         >
-          <View
-            style={{
-              borderWidth: 1,
-              borderRadius: 40,
-              borderColor: checkboxBorderColor,
-              backgroundColor: checkboxBgColor,
-            }}
-          >
-            <Checkbox.Item
-              label="Chest"
-              labelVariant="bodySmall"
-              uncheckedColor="rgb(222, 226, 230)"
-              labelStyle={{
-                textTransform: 'uppercase',
-                color: 'rgb(46, 46, 46)',
-                fontWeight: labelFontWeight,
-              }}
-              status={checked ? 'checked' : 'unchecked'}
-              onPress={() => {
-                setChecked(!checked);
-              }}
-              background={{
-                color: 'rgba(255, 99, 71, 0.3)',
-                radius: 40,
-                borderless: true,
-              }}
-              style={{
-                paddingVertical: 2,
-                paddingHorizontal: 8,
-                paddingLeft: 12,
-              }}
-            />
-          </View>
+          <FilterCheckbox />
         </View>
       </Modal>
     </Portal>
