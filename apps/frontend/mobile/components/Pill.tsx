0aca573c51dba74389fa63c7292f4a08a9bcf123
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 22:55:21 -0700

    Move styles to StyleSheet object

--- apps/frontend/mobile/components/Pill.tsx
+++ apps/frontend/mobile/components/Pill.tsx
@@ -1,5 +1,5 @@
 import { Text, useTheme } from 'react-native-paper';
-// import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { CustomTheme } from '../theme';
 
@@ -8,20 +8,24 @@ interface PillProps {
 }
 
 export default function Pill({ children }: PillProps) {
-  const { colors } = useTheme() as CustomTheme;
+  const theme = useTheme() as CustomTheme;
+  const styles = getStyles(theme);
+
   return (
-    <Text
-      variant="bodySmall"
-      style={{
-        paddingInline: 12,
-        paddingBlock: 4,
-        backgroundColor: colors.blue,
-        borderRadius: 12,
-        color: 'white',
-        textTransform: 'uppercase',
-      }}
-    >
+    <Text variant="bodySmall" style={styles.pill}>
       {children}
     </Text>
   );
 }
+
+const getStyles = (theme: CustomTheme) =>
+  StyleSheet.create({
+    pill: {
+      paddingInline: 12,
+      paddingBlock: 4,
+      backgroundColor: theme.colors.blue,
+      borderRadius: 12,
+      color: 'white',
+      textTransform: 'uppercase',
+    },
+  });
