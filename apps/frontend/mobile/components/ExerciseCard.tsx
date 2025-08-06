765db2f805b97aab238f763842165bab9cd0de75
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 22:55:21 -0700

    Move styles to StyleSheet object

--- apps/frontend/mobile/components/ExerciseCard.tsx
+++ apps/frontend/mobile/components/ExerciseCard.tsx
@@ -1,86 +1,85 @@
 import { Card, useTheme } from 'react-native-paper';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { CustomTheme } from '../theme';
 import { Text } from '../customText';
 
 import Pill from './Pill';
-// use colors from mantine to match web
-// set up theme, the earlier i set it up the better in the long run
+
 export default function ExerciseCard() {
-  const { colors } = useTheme() as CustomTheme;
+  const theme = useTheme() as CustomTheme;
+  const styles = getStyles(theme);
 
   return (
-    <Card
-      style={{
-        marginBlock: 12,
-        marginInline: 36,
-        backgroundColor: colors.background,
-        boxShadow:
-          'rgba(0, 0, 0, 0.05) 0px 1px 3px 0px, rgba(0, 0, 0, 0.05) 0px 28px 23px -7px, rgba(0, 0, 0, 0.04) 0px 12px 12px -7px',
-        padding: 16,
-      }}
-    >
-      <Card.Content style={{ paddingHorizontal: 0, paddingVertical: 0 }}>
-        <View style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'flex-end',
-            }}
-          >
-            <Pill>Beginner</Pill>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-              marginBottom: 12,
-            }}
-          >
-            <Text style={{ fontSize: 16, fontWeight: 700 }}>Pull Ups</Text>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 12,
-            }}
-          >
-            <Text
-              variant="bodySmall"
-              style={{ textTransform: 'uppercase', color: colors.grey }}
-            >
-              Muscle:
-            </Text>
-            <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
-              <Pill>Bicep</Pill>
-              <Pill>Triceps</Pill>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 12,
-            }}
-          >
-            <Text
-              variant="bodySmall"
-              style={{ textTransform: 'uppercase', color: colors.grey }}
-            >
-              Equipment:
-            </Text>
-            <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
-              <Pill>None</Pill>
-            </View>
+    <Card style={styles.card}>
+      <Card.Content style={styles.cardContent}>
+        {/* <View style={{ display: 'flex', flexDirection: 'column', gap: 12 }}> */}
+        <View style={styles.difficultyContainer}>
+          <Pill>Beginner</Pill>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Pull Ups</Text>
+        </View>
+        <View style={styles.exerciseMetadataContainer}>
+          <Text variant="bodySmall" style={styles.exerciseMetadataTitle}>
+            Muscle:
+          </Text>
+          <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
+            <Pill>Bicep</Pill>
+            <Pill>Triceps</Pill>
           </View>
         </View>
+        <View style={styles.exerciseMetadataContainer}>
+          <Text variant="bodySmall" style={styles.exerciseMetadataTitle}>
+            Equipment:
+          </Text>
+          <View style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
+            <Pill>None</Pill>
+          </View>
+        </View>
+        {/* </View> */}
       </Card.Content>
     </Card>
   );
 }
+
+const getStyles = (theme: CustomTheme) =>
+  StyleSheet.create({
+    card: {
+      marginBlock: 12,
+      marginInline: 36,
+      backgroundColor: theme.colors.background,
+      boxShadow:
+        'rgba(0, 0, 0, 0.05) 0px 1px 3px 0px, rgba(0, 0, 0, 0.05) 0px 28px 23px -7px, rgba(0, 0, 0, 0.04) 0px 12px 12px -7px',
+      padding: 16,
+    },
+    cardContent: {
+      paddingHorizontal: 0,
+      paddingVertical: 0,
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 12,
+    },
+    difficultyContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    },
+    titleContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      marginBottom: 12,
+    },
+    title: { fontSize: 16, fontWeight: 700 },
+    exerciseMetadataContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 12,
+    },
+    exerciseMetadataTitle: {
+      textTransform: 'uppercase',
+      color: theme.colors.grey,
+    },
+  });
