691864456336fd67fffb287f03b7248e1cb955b4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-05 22:55:21 -0700

    Move styles to StyleSheet object

--- apps/frontend/mobile/components/ExerciseList.tsx
+++ apps/frontend/mobile/components/ExerciseList.tsx
@@ -5,9 +5,7 @@ import { useStore } from '@cwt/state/store';
 import ExerciseCard from './ExerciseCard';
 
 export default function ExerciseList() {
-  // const theme = useTheme();
   const exercises = useStore((state) => state.displayedExercises);
-  console.log('sample exercises list', exercises[0]);
 
   return (
     <ScrollView
