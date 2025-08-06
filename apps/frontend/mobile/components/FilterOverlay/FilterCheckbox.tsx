ee4cb7a447f0b9ee790741679b685a7121d5b7ce
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-06 15:45:54 -0700

    Create FilterCheckbox component

---
+++ apps/frontend/mobile/components/FilterOverlay/FilterCheckbox.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Checkbox, useTheme } from 'react-native-paper';
+import { View } from 'react-native';
+
+export default function FilterCheckbox() {
+  const theme = useTheme();
+  const [checked, setChecked] = React.useState(false);
+
+  const checkboxBgColor = checked
+    ? 'rgba(255, 99, 71, 0.1)'
+    : theme.colors.background;
+  const checkboxBorderColor = checked
+    ? theme.colors.primary
+    : 'rgb(222, 226, 230)';
+  const labelFontWeight = checked ? '700' : '400';
+  return (
+    <View
+      style={{
+        borderWidth: 1,
+        borderRadius: 40,
+        borderColor: checkboxBorderColor,
+        backgroundColor: checkboxBgColor,
+      }}
+    >
+      <Checkbox.Item
+        label="Chest"
+        labelVariant="bodySmall"
+        uncheckedColor="rgb(222, 226, 230)"
+        labelStyle={{
+          textTransform: 'uppercase',
+          color: 'rgb(46, 46, 46)',
+          fontWeight: labelFontWeight,
+        }}
+        status={checked ? 'checked' : 'unchecked'}
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        background={{
+          color: 'rgba(255, 99, 71, 0.3)',
+          radius: 40,
+          borderless: true,
+        }}
+        style={{
+          paddingVertical: 2,
+          paddingHorizontal: 8,
+          paddingLeft: 12,
+        }}
+      />
+    </View>
+  );
+}
