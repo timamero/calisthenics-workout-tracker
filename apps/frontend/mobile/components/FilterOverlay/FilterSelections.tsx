5dad1c7dbe6b0b7c432232f17cff11c60d169a56
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-06 18:47:55 -0700

    Update styles and display groups of selections

--- apps/frontend/mobile/components/FilterOverlay/FilterSelections.tsx
+++ apps/frontend/mobile/components/FilterOverlay/FilterSelections.tsx
@@ -1,36 +1,55 @@
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { useShallow } from 'zustand/shallow';
+
+import { useStore } from '@cwt/state/store';
 
 import { Text } from '../../customText';
 
 import FilterCheckbox from './FilterCheckbox';
 
 export default function FilterSelections() {
+  const filterCheckboxSelections = useStore(
+    useShallow((state) => state.filterCheckboxSelections),
+  );
+
+  const filterGroupNames = filterCheckboxSelections.map((obj) => obj.group);
+  const uniqueFilterGroupNames = [...new Set(filterGroupNames)];
+
   return (
-    <View>
-      <Text
-        style={{ textTransform: 'uppercase', fontWeight: 400 }}
-        variant="headlineMedium"
-      >
-        Muscles
-      </Text>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          rowGap: 12,
-          columnGap: 8,
-        }}
-      >
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-        <FilterCheckbox />
-      </View>
-    </View>
+    <ScrollView style={{ height: 460 }}>
+      {uniqueFilterGroupNames.map((group, i) => {
+        return (
+          <>
+            <Text
+              style={{ textTransform: 'uppercase', fontWeight: 400 }}
+              variant="headlineMedium"
+              key={i}
+            >
+              {group}
+            </Text>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                rowGap: 12,
+                columnGap: 8,
+                marginTop: 12,
+                marginBottom: 20,
+              }}
+            >
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+              <FilterCheckbox />
+            </View>
+          </>
+        );
+      })}
+    </ScrollView>
   );
 }
