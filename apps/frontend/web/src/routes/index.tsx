92a5cf61c4656b73430249a26e417cfd94bdd9ab
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 14:28:08 -0700

    Remove sample data fetch

--- apps/frontend/web/src/routes/index.tsx
+++ apps/frontend/web/src/routes/index.tsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title } from '@mantine/core';
 
 import { useBearStore } from '@cwt/state/counter';
 import { User } from '@cwt/schema/sampleSchema';
-import { getData } from '@cwt/api/projectData';
 
 export const Route = createFileRoute('/')({
   component: HomeView,
@@ -17,12 +15,6 @@ function HomeView() {
     name: 'Jane Doe',
     xp: 100,
   };
-
-  useEffect(() => {
-    console.log('Fetching data from public route...');
-    const baseUrl = 'http://127.0.0.1:8000';
-    getData(baseUrl);
-  }, []);
 
   return (
     <div>
