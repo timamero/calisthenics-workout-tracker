4af0eaa9822dad4e5d02167d8e638568a949d31e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 01:23:17 -0700

    Handle opening and closing exercise detail overlay

--- apps/frontend/mobile/screens/LibraryScreen.tsx
+++ apps/frontend/mobile/screens/LibraryScreen.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { ScrollView, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
+import { Exercise } from '@cwt/schema/exerciseSchema';
+
+import { ExerciseDetailContext } from '../contexts/ExerciseDetailContext';
+
 import ExerciseList from '../components/ExerciseList';
 import SearchBar from '../components/SearchBar';
 import Filter from '../components/Filter';
@@ -12,24 +16,49 @@ export default function LibraryScreen() {
   const theme = useTheme();
 
   const [visible, setVisible] = React.useState(false);
-
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
 
+  const [exerciseDetail, setExerciseDetail] = React.useState<Exercise>({
+    id: 1,
+    name: '',
+    target_muscles: [],
+    emphasis: '',
+    difficulty: '',
+    tags: [],
+    instructions: [],
+    required_equipment: null,
+  });
+  const [visibleExerciseDetail, setVisibleExerciseDetail] =
+    React.useState(false);
+  const showExerciseDetailModal = () => setVisibleExerciseDetail(true);
+  const hideExerciseDetailModal = () => setVisibleExerciseDetail(false);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: theme.colors.background,
+    <ExerciseDetailContext.Provider
+      value={{
+        exercise: exerciseDetail,
+        setExercise: setExerciseDetail,
+        visible: visibleExerciseDetail,
+        setVisible: setVisibleExerciseDetail,
+        showModal: showExerciseDetailModal,
+        hideModal: hideExerciseDetailModal,
       }}
     >
-      <ScrollView>
-        <SearchBar />
-        <Filter handleShowModal={showModal} />
-        <ExerciseList />
-      </ScrollView>
-      <FilterOverlay visible={visible} handleHideModal={hideModal} />
-      <ExerciseDetailOverlay />
-    </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: theme.colors.background,
+        }}
+      >
+        <ScrollView>
+          <SearchBar />
+          <Filter handleShowModal={showModal} />
+          <ExerciseList />
+        </ScrollView>
+        <FilterOverlay visible={visible} handleHideModal={hideModal} />
+        <ExerciseDetailOverlay />
+      </View>
+    </ExerciseDetailContext.Provider>
   );
 }
