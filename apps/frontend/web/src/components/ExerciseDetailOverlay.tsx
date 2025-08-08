10b58979d4032713d4de1cea51a724f72396ffb4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-07 22:19:24 -0700

    Display all exercise data in overlay

--- apps/frontend/web/src/components/ExerciseDetailOverlay.tsx
+++ apps/frontend/web/src/components/ExerciseDetailOverlay.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Modal } from '@mantine/core';
+import { Modal, Stack, Text, Badge, Flex } from '@mantine/core';
 
 import { ExerciseDetailContext } from '../contexts/ExerciseDetailContext';
 
@@ -8,24 +8,126 @@ export default function ExerciseDetailOverlay() {
   const detailHandlers = useContext(ExerciseDetailContext)?.handlers;
   const detailOpened = useContext(ExerciseDetailContext)?.opened;
 
+  const difficultyColor =
+    exerciseDetail?.difficulty == 'beginner'
+      ? 'blue'
+      : exerciseDetail?.difficulty == 'intermediate'
+        ? 'yellow'
+        : 'red';
+
   const handleCloseModal = () => {
     if (detailHandlers) {
       detailHandlers.close();
     }
   };
+  const muscleMetadata = exerciseDetail?.target_muscles.map((muscle, i) => {
+    return (
+      <Badge size="lg" variant="light" color="grape" key={i}>
+        {muscle}
+      </Badge>
+    );
+  });
+  const equipmentMetadata = exerciseDetail?.required_equipment?.map(
+    (equipment, i) => {
+      return (
+        <Badge size="lg" color="dark" variant="outline" key={i}>
+          {equipment}
+        </Badge>
+      );
+    },
+  );
+  const instructions = exerciseDetail?.instructions.map((instruction, i) => {
+    return <Text key={i}>{instruction}</Text>;
+  });
   return (
     <Modal
       opened={detailOpened || false}
       onClose={handleCloseModal}
+      fullScreen
       title={exerciseDetail?.name}
       styles={{
         title: {
           fontFamily: 'var(--mantine-font-family-headings)',
           fontWeight: 700,
+          fontSize: 40,
         },
       }}
     >
-      <p>Exercise details</p>
+      <Stack>
+        <Stack>
+          <Text tt="uppercase" size="md" c="gray.7">
+            Difficulty
+          </Text>
+          <Badge size="lg" color={difficultyColor}>
+            {exerciseDetail?.difficulty}
+          </Badge>
+        </Stack>
+        <Stack>
+          <Text tt="uppercase" size="md" c="gray.7">
+            Emphasis
+          </Text>
+          <Badge size="lg" color="dark" variant="outline" bg="orange.1">
+            {exerciseDetail?.emphasis}
+          </Badge>
+        </Stack>
+        <Text tt="uppercase" size="md" c="gray.7">
+          Target Muscles
+        </Text>
+        <Flex
+          // mih={50}
+          // bg="rgba(0, 0, 0, .3)"
+          gap="md"
+          justify="flex-start"
+          align="flex-start"
+          direction="row"
+          wrap="wrap"
+        >
+          {muscleMetadata}
+        </Flex>
+        <Text tt="uppercase" size="md" c="gray.7">
+          Required Equipment
+        </Text>
+        <Flex
+          // mih={50}
+          // bg="rgba(0, 0, 0, .3)"
+          gap="md"
+          justify="flex-start"
+          align="flex-start"
+          direction="row"
+          wrap="wrap"
+        >
+          {exerciseDetail?.required_equipment == null ||
+          exerciseDetail.required_equipment.length == 0 ? (
+            <Badge color="dark" variant="transparent">
+              ---
+            </Badge>
+          ) : (
+            equipmentMetadata
+          )}
+        </Flex>
+        <Stack>
+          <Text tt="uppercase" size="md" c="gray.7">
+            Instructions
+          </Text>
+          <Stack>{instructions}</Stack>
+        </Stack>
+      </Stack>
     </Modal>
   );
 }
+
+// {
+//     id: 1,
+//     name: 'Standard Push-Up',
+//     target_muscles: ['chest', 'triceps', 'shoulders'],
+//     required_equipment: [],
+//     emphasis: 'strength',
+//     difficulty: 'beginner',
+//     tags: ['push', 'upper'],
+//     instructions: [
+//       '1. Start in a strong plank position. Hands slightly wider than shoulders. Fingers forward.',
+//       '2. Lower your chest towards the floor, keeping elbows close to your body and core tight.',
+//       '3. Push through your palms, extending arms fully to return to the plank.',
+//       '4. Keep your body straight throughout the movement.',
+//     ],
+//   },
