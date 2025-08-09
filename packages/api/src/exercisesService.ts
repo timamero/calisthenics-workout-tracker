89ec583eabef842cadd7c5e8b5449398ec3578b2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-09 13:31:58 -0700

    Update apiFetch arguments

--- packages/api/src/exercisesService.ts
+++ packages/api/src/exercisesService.ts
@@ -2,5 +2,5 @@ import { apiFetch } from "./httpClient";
 import { Exercise } from "@cwt/schema/exerciseSchema";
 
 export function getExercises (baseUrl: string, token: string) {
-  return apiFetch<Exercise[]>(baseUrl, '/exercises', token)
+  return apiFetch<Exercise[]>(baseUrl, '/exercises', 'GET', token)
 }
