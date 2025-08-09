8201808188ee06a4f957f6fe6d913a63dfb1f438
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 21:24:09 -0700

    Clean up code

--- apps/frontend/web/src/routes/__root.tsx
+++ apps/frontend/web/src/routes/__root.tsx
@@ -6,7 +6,6 @@ import { AppShell, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { NavLink } from '@mantine/core';
 import { supabase } from '../services/supabaseClient';
-// import type { Subscription } from '@supabase/auth-js';
 
 import { useSupabaseAuth } from '@cwt/hooks/useSupabaseAuth';
 import { useAuthStore } from '@cwt/state/auth';
@@ -35,107 +34,17 @@ function RootComponent() {
     { label: 'Settings', to: '/settings' },
   ];
 
-  console.log('before useSupabaseAuth');
   useSupabaseAuth(supabase, setSession);
-  console.log('after useSupabaseAuth');
 
   useEffect(() => {
     const asyncFetchData = async () => {
       if (supabaseSession?.access_token) {
         const exercises = await getExercises(supabaseSession.access_token);
-        // if (exercises) {
         setExercises(exercises);
-        // }
       }
     };
     asyncFetchData();
   }, [supabaseSession, setExercises]);
-
-  // useEffect(() => {
-  //   let authListener: {
-  //     unsubscribe: () => void;
-  //     subscription: Subscription;
-  //   } | null = null;
-
-  //   const setupAuth = async () => {
-  //     console.log(
-  //       'Setting up Supabase authentication listener and initial session check...',
-  //     );
-  //     // setLoading(true);
-
-  //     // Get the initial session from Supabase's storage
-  //     try {
-  //       const {
-  //         data: { session: initialSession },
-  //       } = await supabase.auth.getSession();
-  //       setSession(initialSession);
-  //       console.log(
-  //         'Initial session retrieved:',
-  //         initialSession ? 'present' : 'null',
-  //       );
-  //     } catch (error) {
-  //       console.error('Error getting initial session:', error);
-  //       setSession(null); // Ensure session is null on error
-  //     }
-
-  //     // Set up the real-time auth state listener
-  //     const { data } = supabase.auth.onAuthStateChange(
-  //       (event, currentSession) => {
-  //         console.log(
-  //           'Auth event:',
-  //           event,
-  //           'Current Session:',
-  //           currentSession ? 'present' : 'null',
-  //         );
-
-  //         switch (event) {
-  //           case 'SIGNED_IN':
-  //           case 'USER_UPDATED':
-  //             setSession(currentSession);
-  //             console.log('User signed in or updated, session set.');
-  //             break;
-  //           case 'SIGNED_OUT':
-  //             setSession(null);
-  //             console.log('User signed out, session cleared.');
-  //             break;
-  //           case 'PASSWORD_RECOVERY':
-  //             console.log('Password recovery initiated.');
-  //             // You might want to navigate to a password reset screen here
-  //             break;
-  //           case 'INITIAL_SESSION':
-  //             // This event fires after getSession() if a session exists.
-  //             // It's often redundant if you've already called getSession()
-  //             // but good to log for debugging.
-  //             console.log('Initial session event received.');
-  //             setSession(currentSession);
-  //             break;
-  //           default:
-  //             // Handle other events if necessary
-  //             console.log(`Unhandled auth event: ${event}`);
-  //             break;
-  //         }
-  //         // setLoading(false);
-  //       },
-  //     );
-  //     authListener = {
-  //       subscription: data.subscription,
-  //       unsubscribe: () => data.subscription.unsubscribe(),
-  //     }; // Assign the listener object to the variable for cleanup
-
-  //     // Ensure loading is set to false after initial check and listener setup
-  //     // This handles cases where no auth event fires immediately but initial session is known.
-  //     // setLoading(false);
-  //   };
-
-  //   setupAuth();
-
-  //   return () => {
-  //     if (authListener) {
-  //       authListener.unsubscribe();
-  //       console.log('Supabase auth listener unsubscribed.');
-  //     }
-  //   };
-  // }, [setSession]);
 
   if (!supabaseSession) {
     return (
@@ -170,32 +79,6 @@ function RootComponent() {
             onClick={() => opened && toggle()}
           />
         ))}
-        {/* <NavLink label="Home" component={Link} to="/" onClick={toggle} />
-        <NavLink
-          label="Start Workout"
-          component={Link}
-          to="/startWorkout"
-          onClick={toggle}
-        />
-        <NavLink
-          label="Library"
-          component={Link}
-          to="/library"
-          onClick={toggle}
-        />
-        <NavLink
-          label="Past Workouts"
-          component={Link}
-          to="/history"
-          onClick={toggle}
-        />
-        <NavLink label="Profile" component={Link} to="/user" onClick={toggle} />
-        <NavLink
-          label="Settings"
-          component={Link}
-          to="/settings"
-          onClick={toggle}
-        /> */}
       </AppShell.Navbar>
 
       <AppShell.Main>
--- packages/hooks/src/useSupabaseAuth.ts
+++ packages/hooks/src/useSupabaseAuth.ts
@@ -1,28 +1,12 @@
 import * as React from 'react';
-// import { useAuthStore } from '@cwt/state/auth';
 import { SupabaseClient, Session } from '@supabase/supabase-js';
 
 export function useSupabaseAuth(client: SupabaseClient, setSession: (session: Session | null) => void) {
-  // const setSession = useAuthStore((state) => state.setSession);
 
   React.useEffect(() => {
     (async () => {
       const {data: { session }} = await client.auth.getSession();
       setSession(session);
-        //     // Get the initial session from Supabase's storage
-      // try {
-      //   const {
-      //     data: { session: initialSession },
-      //   } = await client.auth.getSession();
-      //   setSession(initialSession);
-      //   console.log(
-      //     'Initial session retrieved:',
-      //     initialSession ? 'present' : 'null',
-      //   );
-      // } catch (error) {
-      //   console.error('Error getting initial session:', error);
-      //   setSession(null); // Ensure session is null on error
-      // }
 
       const {data: listener} = client.auth.onAuthStateChange((_event, session) => {
         switch (_event) {
@@ -39,13 +23,6 @@ export function useSupabaseAuth(client: SupabaseClient, setSession: (session: Se
             console.log('Password recovery initiated.');
             // You might want to navigate to a password reset screen here
             break;
-          // case 'INITIAL_SESSION':
-          //   // This event fires after getSession() if a session exists.
-          //   // It's often redundant if you've already called getSession()
-          //   // but good to log for debugging.
-          //   console.log('Initial session event received.');
-          //   setSession(session);
-          //   break;
           default:
             // Handle other events if necessary
             console.log(`Unhandled auth event: ${_event}`);
