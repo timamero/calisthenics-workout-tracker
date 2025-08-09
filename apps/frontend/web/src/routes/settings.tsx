e28d2101aa9304f10be6eb63d3465a4db393c716
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 22:48:20 -0700

    Remove sample routes and fetching

--- apps/frontend/web/src/routes/settings.tsx
+++ apps/frontend/web/src/routes/settings.tsx
@@ -1,33 +1,17 @@
-import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Button } from '@mantine/core';
 import { signOut } from '@cwt/auth/signOut';
 import { supabase } from '../services/supabaseClient';
-
-import { useAuthStore } from '@cwt/state/auth';
-import { getProtectedData } from '@cwt/api/protectedData';
 
 export const Route = createFileRoute('/settings')({
   component: SettingsView,
 });
 
 function SettingsView() {
-  const session = useAuthStore((state) => state.session);
   const handleSignOut = () => {
     signOut(supabase);
   };
 
-  useEffect(() => {
-    console.log('Fetching data from protected route in settings...');
-    const baseUrl = 'http://127.0.0.1:8000';
-
-    const asyncFetchProtectedData = async () => {
-      if (session) {
-        await getProtectedData(baseUrl, session.access_token);
-      }
-    };
-    asyncFetchProtectedData();
-  }, [session]);
   return (
     <div>
       <Title>Settings Page</Title>
