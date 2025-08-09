bbacfc6d68924ce4a84b34f74c55c81a2a7652df
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 22:48:20 -0700

    Remove sample routes and fetching

--- apps/frontend/web/src/routes/landing.tsx
+++ apps/frontend/web/src/routes/landing.tsx
@@ -1,31 +1,11 @@
-import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Button, Stack } from '@mantine/core';
-
-import { useAuthStore } from '@cwt/state/auth';
-import { getProtectedData } from '@cwt/api/protectedData';
 
 export const Route = createFileRoute('/landing')({
   component: LandingView,
 });
 
 function LandingView() {
-  const session = useAuthStore((state) => state.session);
-  useEffect(() => {
-    console.log(
-      'Fetching data from protected route in landing (expecting no data)...',
-    );
-    const baseUrl = 'http://127.0.0.1:8000';
-
-    const asyncFetchProtectedData = async () => {
-      if (session) {
-        await getProtectedData(baseUrl, session.access_token);
-      } else {
-        console.log('No protected data to fetch on landing page.');
-      }
-    };
-    asyncFetchProtectedData();
-  }, [session]);
   return (
     <div>
       <Title>Sign Up or Log In</Title>
