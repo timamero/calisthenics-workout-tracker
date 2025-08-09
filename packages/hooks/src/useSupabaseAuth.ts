0dab2faf4f348bbcd42a330200887325600c5d53
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-09 00:48:05 -0700

    Fix async function structure

--- packages/hooks/src/useSupabaseAuth.ts
+++ packages/hooks/src/useSupabaseAuth.ts
@@ -4,38 +4,40 @@ import { SupabaseClient, Session } from '@supabase/supabase-js';
 export function useSupabaseAuth(client: SupabaseClient, setSession: (session: Session | null) => void, setLoading: (loading: boolean) => void) {
 
   React.useEffect(() => {
-    (async () => {
+    const getSession = async () => {
       setLoading(true);
 
       const {data: { session }} = await client.auth.getSession();
       setSession(session);
+    };
 
-      const {data: listener} = client.auth.onAuthStateChange((_event, session) => {
-        setLoading(true);
-        switch (_event) {
-          case 'SIGNED_IN':
-          case 'USER_UPDATED':
-            setSession(session);
-            console.log('User signed in or updated, session set.');
-            break;
-          case 'SIGNED_OUT':
-            setSession(null);
-            console.log('User signed out, session cleared.');
-            break;
-          case 'PASSWORD_RECOVERY':
-            console.log('Password recovery initiated.');
-            // You might want to navigate to a password reset screen here
-            break;
-          default:
-            // Handle other events if necessary
-            console.log(`Unhandled auth event: ${_event}`);
-            break;
-          }
-          setLoading(false);
-      });
+    getSession();
 
-      setLoading(false);
-      return () => listener.subscription.unsubscribe();
-    })();
-  }, [setSession])
+    const {data: listener} = client.auth.onAuthStateChange((_event, session) => {
+      setLoading(true);
+      switch (_event) {
+        case 'SIGNED_IN':
+        case 'USER_UPDATED':
+          setSession(session);
+          console.log('User signed in or updated, session set.');
+          break;
+        case 'SIGNED_OUT':
+          setSession(null);
+          console.log('User signed out, session cleared.');
+          break;
+        case 'PASSWORD_RECOVERY':
+          console.log('Password recovery initiated.');
+          // You might want to navigate to a password reset screen here
+          break;
+        default:
+          // Handle other events if necessary
+          console.log(`Unhandled auth event: ${_event}`);
+          break;
+        }
+        setLoading(false);
+    });
+
+    setLoading(false);
+    return () => listener.subscription.unsubscribe();
+  }, [setSession, client])
 }
