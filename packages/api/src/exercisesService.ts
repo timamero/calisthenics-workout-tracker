d779673772a7b4f18c610c564aa8ab7556153ae3
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 19:17:58 -0700

    Use apiFetch

--- packages/api/src/exercisesService.ts
+++ packages/api/src/exercisesService.ts
@@ -1,24 +1,29 @@
+import { apiFetch } from "./httpClient";
 import { Exercise } from "@cwt/schema/exerciseSchema";
 
-export async function getExercises(baseUrl: string, token: string): Promise<Exercise[] | void> {
-  const url = `${baseUrl}/exercises`;
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-  }
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: headers
-    });
-    if (!response.ok) {
-      throw new Error(`API Package: Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log('API Package: Exercises fethed from FastAPI: ', json);
-    return json
-  } catch (error) {
-    console.error('API Package: Error fetching exercises:', error);
-  }
+export function getExercises (baseUrl: string, token: string) {
+  return apiFetch<Exercise[]>(baseUrl, '/exercises', token)
 }
+
+// export async function getExercises(baseUrl: string, token: string): Promise<Exercise[] | void> {
+//   const url = `${baseUrl}/exercises`;
+//   const headers = {
+//     'Content-Type': 'application/json',
+//     'Authorization': `Bearer ${token}`
+//   }
+//   try {
+//     const response = await fetch(url, {
+//       method: 'GET',
+//       headers: headers
+//     });
+//     if (!response.ok) {
+//       throw new Error(`API Package: Response status: ${response.status}`);
+//     }
+
+//     const json = await response.json();
+//     console.log('API Package: Exercises fethed from FastAPI: ', json);
+//     return json
+//   } catch (error) {
+//     console.error('API Package: Error fetching exercises:', error);
+//   }
+// }
