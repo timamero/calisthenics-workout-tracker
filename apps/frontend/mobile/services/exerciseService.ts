94a71bb670b85f8d7bbef4db7bd2ce2242c4e8c9
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-08 23:44:24 -0700

    Fix environment variable typo

--- apps/frontend/mobile/services/exerciseService.ts
+++ apps/frontend/mobile/services/exerciseService.ts
@@ -2,7 +2,7 @@ import { getExercises as apiGetExercises } from '@cwt/api/exercisesService';
 import { Exercise } from '@cwt/schema/exerciseSchema';
 import { sampleExercises } from '@cwt/mocks/sampleExercises';
 
-const baseUrl = process.env.EXPO_BASE_URL || '';
+const baseUrl = process.env.EXPO_PUBLIC_BASE_URL || '';
 
 export async function getExercises(token: string): Promise<Exercise[]> {
   try {
