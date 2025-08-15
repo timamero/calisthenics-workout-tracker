da3c18f4a8ef8e0ba62af3c9da5feaa418c8b6ea
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-14 23:37:46 -0700

    CWT-136 Create route and util to insert log

--- apps/backend/app/api/routes/workout.py
+++ apps/backend/app/api/routes/workout.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, HTTPException
-from app.api.utils.workout import insert_workout_build, get_workout_builds
-from app.schemas.workout import WorkoutBuildSchema
+from app.api.utils.workout import (
+    insert_workout_build,
+    get_workout_builds,
+    insert_workout_log,
+)
+from app.schemas.workout import WorkoutBuildSchema, WorkoutLogSchema
 
 router = APIRouter(prefix="/workout")
 
@@ -20,6 +24,23 @@ def save_build(build: WorkoutBuildSchema):
         raise HTTPException(status_code=400, detail="Invalid request")
 
     return workout_build
+
+
+@router.post("/log")
+def save_log(log: WorkoutLogSchema):
+    """
+    Insert workout log.
+    """
+    # auth_header = request.headers.get("Authorization")
+    # if not auth_header or not auth_header.startswith("Bearer "):
+    #     raise HTTPException(status_code=401, detail="Authentication required")
+
+    # access_token = auth_header.split(" ")[1]
+    workout_log = insert_workout_log(log)
+    if not workout_log:
+        raise HTTPException(status_code=400, detail="Invalid request")
+
+    return workout_log
 
 
 @router.get("/builds")
