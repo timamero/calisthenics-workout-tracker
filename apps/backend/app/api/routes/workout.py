7d42e5807183003761c944acab8431399e7c0085
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-14 21:21:47 -0700

    CWT-136 Call utility function to insert to database

--- apps/backend/app/api/routes/workout.py
+++ apps/backend/app/api/routes/workout.py
@@ -8,12 +8,15 @@ router = APIRouter(prefix="/workout")
 @router.post("/build")
 def save_build(build: WorkoutBuildSchema):
     """
-    Retrieve a list of exercises.
+    Insert workout build.
     """
     # auth_header = request.headers.get("Authorization")
     # if not auth_header or not auth_header.startswith("Bearer "):
     #     raise HTTPException(status_code=401, detail="Authentication required")
 
     # access_token = auth_header.split(" ")[1]
+    insert_workout_build(build)
+    # if not insert_workout_build(build):
+    #     raise HTTPException(status_code=400, detail="Invalid request")
 
     return build
