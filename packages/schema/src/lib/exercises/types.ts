5477943708841d3493b3099a8e39a54ea15a7e94
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-15 23:10:43 -0700

    CWT-137 Clean up code

--- packages/schema/src/lib/exercises/types.ts
+++ packages/schema/src/lib/exercises/types.ts
@@ -1,4 +1,4 @@
-import * as z from "zod";
+import { z } from "zod";
 
 import {
   ExerciseAttributesSchema,
@@ -12,6 +12,6 @@ import {
 export type Muscle = z.infer<typeof MuscleSchema>;
 export type Equipment= z.infer<typeof EquipmentSchema>;
 export type Emphasis = z.infer<typeof EmphasisSchema>;
-export type Dificulty = z.infer<typeof DifficultySchema>;
+export type Difficulty = z.infer<typeof DifficultySchema>;
 export type ExerciseAttributes = z.infer<typeof ExerciseAttributesSchema>;  // Prevously Selection
 export type Exercise = z.infer<typeof Exercise>;
