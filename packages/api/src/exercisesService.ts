ab170fd407c12a3b546b77f6bc69c9ca9a38cab2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-16 03:05:28 -0700

    CWT-137 Use updated exercise schema

--- packages/api/src/exercisesService.ts
+++ packages/api/src/exercisesService.ts
@@ -1,5 +1,5 @@
 import { apiFetch } from "./httpClient";
-import { Exercise } from "@cwt/schema/exerciseSchema";
+import { Exercise } from "@cwt/schema/exercises";
 
 export function getExercises (baseUrl: string, token: string) {
   return apiFetch<Exercise[]>(baseUrl, '/exercises', 'GET', token)
