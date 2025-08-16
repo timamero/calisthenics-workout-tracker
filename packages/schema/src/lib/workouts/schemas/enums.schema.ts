6444004e7257be603d2cbd7fbd8acee0eea0d8d4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-16 03:32:29 -0700

    CWT-137 Create workout data schema

--- packages/schema/src/lib/workouts/schemas/enums.schema.ts
+++ packages/schema/src/lib/workouts/schemas/enums.schema.ts
@@ -8,4 +8,3 @@ export const GoalSchema = z.enum(constants.goal);
 export const SourceSchema = z.enum(constants.source);
 export const StatusSchema = z.enum(constants.status);
 export const TrackingTypeSchema = z.enum(constants.tracking_type);
-export const TrackingTypesSchema = z.array(TrackingTypeSchema);
--- packages/schema/src/lib/workouts/schemas/workoutBase.schema.ts
+++ packages/schema/src/lib/workouts/schemas/workoutBase.schema.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod';
 
 import { GoalSchema, StatusSchema } from './enums.schema';
+import { WorkoutDataSchema } from './workoutData.schema';
 
 export const WorkoutBase = z.object({
   id: z.number(),
   updated_at: z.date(),
   title: z.optional(z.string().max(70)),
   description: z.optional(z.string().max(500)),
-  // workout data here
+  workout_data: WorkoutDataSchema,
   status: StatusSchema,
   goal: z.optional(GoalSchema),
   notes: z.optional(z.string().max(750)),
