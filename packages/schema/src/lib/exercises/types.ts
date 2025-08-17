25847d48a12b6a76498cf2e93e4fdb700b79724a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-17 00:58:22 -0700

    CWT-138 Create exercise filter types

--- packages/schema/src/lib/exercises/types.ts
+++ packages/schema/src/lib/exercises/types.ts
@@ -7,6 +7,9 @@ import {
   EquipmentSchema,
   EmphasisSchema,
   DifficultySchema,
+  ExerciseFilterSchema,
+  ExerciseFilterKeySchema,
+  ExerciseFilterCheckboxSchema
 } from './schemas';
 
 export type Muscle = z.infer<typeof MuscleSchema>;
@@ -15,3 +18,6 @@ export type Emphasis = z.infer<typeof EmphasisSchema>;
 export type Difficulty = z.infer<typeof DifficultySchema>;
 export type ExerciseAttributes = z.infer<typeof ExerciseAttributesSchema>; // Prevously Selection
 export type Exercise = z.infer<typeof Exercise>;
+export type ExerciseFilter = z.infer<typeof ExerciseFilterSchema>;
+export type ExerciseFilterKey = z.infer<typeof ExerciseFilterKeySchema>;
+export type ExerciseFilterCheckbox = z.infer<typeof ExerciseFilterCheckboxSchema>;
