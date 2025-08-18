4a4800a4a165652305a3be480c1b8a0aa776c885
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-17 22:18:37 -0700

    CWT-138 Update schema

--- packages/schema/src/lib/exercises/types.ts
+++ packages/schema/src/lib/exercises/types.ts
@@ -2,14 +2,15 @@ import { z } from 'zod';
 
 import {
   ExerciseAttributesSchema,
-  Exercise,
+  ExerciseSchema,
+  ExerciseKeySchema,
   MuscleSchema,
   EquipmentSchema,
   EmphasisSchema,
   DifficultySchema,
   ExerciseFilterSchema,
   ExerciseFilterKeySchema,
-  ExerciseFilterCheckboxSchema
+  ExerciseFilterCheckboxSchema,
 } from './schemas';
 
 export type Muscle = z.infer<typeof MuscleSchema>;
@@ -17,7 +18,11 @@ export type Equipment = z.infer<typeof EquipmentSchema>;
 export type Emphasis = z.infer<typeof EmphasisSchema>;
 export type Difficulty = z.infer<typeof DifficultySchema>;
 export type ExerciseAttributes = z.infer<typeof ExerciseAttributesSchema>; // Prevously Selection
-export type Exercise = z.infer<typeof Exercise>;
+export type Exercise = z.infer<typeof ExerciseSchema>;
+export type ExerciseKeys = z.infer<typeof ExerciseKeySchema>;
 export type ExerciseFilter = z.infer<typeof ExerciseFilterSchema>;
 export type ExerciseFilterKey = z.infer<typeof ExerciseFilterKeySchema>;
-export type ExerciseFilterCheckbox = z.infer<typeof ExerciseFilterCheckboxSchema>;
+export type ExerciseFilterCheckbox = z.infer<
+  typeof ExerciseFilterCheckboxSchema
+>;
+export type ExerciseSelectionKeys = keyof Pick<Exercise, ExerciseFilterKey>;
