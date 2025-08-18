d55380278b3a649233ffee3fe895fed049b83e61
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-17 21:50:27 -0700

    CWT-138 Update name

--- packages/schema/src/lib/exercises/schemas/exerciseFilter.schema.ts
+++ packages/schema/src/lib/exercises/schemas/exerciseFilter.schema.ts
@@ -1,18 +1,23 @@
 import { z } from 'zod';
-import { DifficultySchema, EmphasisSchema, EquipmentSchema, MuscleSchema } from './enums.schema';
-import { ExerciseAttributesSchema } from './exercise-attributes.schema';
+import {
+  DifficultySchema,
+  EmphasisSchema,
+  EquipmentSchema,
+  MuscleSchema,
+} from './enums.schema';
+import { ExerciseAttributesSchema } from './exerciseAttributes.schema';
 
 export const ExerciseFilterSchema = z.object({
   target_muscles: z.array(MuscleSchema),
   required_equipment: z.array(EquipmentSchema),
   emphasis: z.array(EmphasisSchema),
-  difficulty: z.array(DifficultySchema)
-})
+  difficulty: z.array(DifficultySchema),
+});
 
-export const ExerciseFilterKeySchema = ExerciseFilterSchema.keyof()
+export const ExerciseFilterKeySchema = ExerciseFilterSchema.keyof();
 
 export const ExerciseFilterCheckboxSchema = z.object({
   key: ExerciseFilterKeySchema,
   selection: ExerciseAttributesSchema,
-  value: z.optional(z.boolean())
-})
+  value: z.optional(z.boolean()),
+});
