0c3a8b4b633f3abd98414f8a2e887e798ee76ba4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-17 21:49:15 -0700

    CWT-138 Update name

--- packages/schema/src/lib/exercises/schemas/index.ts
+++ packages/schema/src/lib/exercises/schemas/index.ts
@@ -1,4 +1,4 @@
 export * from './exercise.schema';
-export * from './exercise-attributes.schema';
+export * from './exerciseAttributes.schema';
 export * from './exerciseFilter.schema';
 export * from './enums.schema';
