41b2c58e139ac5f44ec5098098891a4295d08267
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-17 22:18:37 -0700

    CWT-138 Update schema

---
+++ packages/schema/src/lib/exercises/schemas/exerciseAttributes.schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+import {
+  MuscleSchema,
+  EquipmentSchema,
+  EmphasisSchema,
+  DifficultySchema,
+} from './enums.schema';
+
+// Previously SelectionSchema
+export const ExerciseAttributesSchema = z.union([
+  MuscleSchema,
+  EquipmentSchema,
+  EmphasisSchema,
+  DifficultySchema,
+]);
