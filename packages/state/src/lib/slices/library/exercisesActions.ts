5ee69107966dbcaca74482c08b2c0960144cdf28
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-18 19:05:32 -0700

    CWT-138 Refactor function to filter exercises

--- packages/state/src/lib/slices/library/exercisesActions.ts
+++ packages/state/src/lib/slices/library/exercisesActions.ts
@@ -8,41 +8,24 @@ export function filterExercises(
   exercisesToFilter: Exercise[],
   appliedFilters: ExerciseFilterCheckbox[]
 ) {
-  const appliedFilterGroupNames = appliedFilters.map((obj) => obj.key);
-  const uniqueAppliedFilterGroupNames = Array.from(
-    new Set(appliedFilterGroupNames)
-  );
   const appliedFilterSelections: ExerciseAttributes[] = appliedFilters.map(
-    (obj) => obj.selection
+    (f) => f.selection
   );
 
-  const filteredExercises = exercisesToFilter.filter((obj) => {
-    const conditionals: boolean[] = [];
-    uniqueAppliedFilterGroupNames.forEach((key) => {
-      if (typeof obj[key] === "string") {
-        if (appliedFilterSelections.includes(obj[key])) {
-          conditionals.push(true);
-        } else {
-          conditionals.push(false);
-        }
-      } else if (Array.isArray(obj[key])) {
-        const arrayConditionals: boolean[] = [];
-        obj[key].forEach((item) => {
-          if (appliedFilterSelections.includes(item as ExerciseAttributes)) {
-            arrayConditionals.push(true);
-          } else {
-            arrayConditionals.push(false);
-          }
-        });
+  return exercisesToFilter.filter((exercise) => 
+    appliedFilters.every(({ key }) => {
+      const value = exercise[key];
 
-        conditionals.push(arrayConditionals.some((con) => con));
+      // Some exercise attributes are type string or list
+      if (typeof value === "string") {
+        return appliedFilterSelections.includes(value)
       }
-    });
-
-    return conditionals.every((con) => con);
-  });
-
-  return filteredExercises;
+      if (Array.isArray(value)) {
+        return value.some((v) => appliedFilterSelections.includes(v as ExerciseAttributes))
+      }
+      return false
+    })
+  )
 }
 
 export function sortExercises(exercises: Exercise[]) {
