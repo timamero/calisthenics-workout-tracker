df92482a3ba1371eb9ae2cfe9a922d487ba92bec
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-19 02:29:52 -0700

    CWT-138 Add useShallow to prevent rerenders

--- packages/hooks/src/useFiltersAndSearchStatus.ts
+++ packages/hooks/src/useFiltersAndSearchStatus.ts
@@ -1,9 +1,13 @@
+import { useShallow } from "zustand/shallow";
+
 import { useStore, StoreState } from "@cwt/state/store";
 import { selectHasFilters, selectHasSearch } from "@cwt/state/library";
 
 export const useFiltersAndSearchStatus = () => {
-  return useStore((state: StoreState) => ({
-    hasFilters: selectHasFilters(state),
-    hasSearch: selectHasSearch(state),
-  }));
+  return useStore(
+    useShallow((state: StoreState) => ({
+      hasFilters: selectHasFilters(state),
+      hasSearch: selectHasSearch(state),
+    }))
+  );
 };
