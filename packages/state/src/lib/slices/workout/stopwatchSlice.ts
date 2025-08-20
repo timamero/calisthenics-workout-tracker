bc0e219fbf46ad783cb3e91401fcd1fd7a856bbb
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-20 15:50:15 -0700

    CWT-138 Add files and make progress with pseudocode

---
+++ packages/state/src/lib/slices/workout/stopwatchSlice.ts
@@ -0,0 +1,2 @@
+// Track the duration of workout logging
+// https://chatgpt.com/c/68a64e56-86bc-832a-a4ed-4588244a2c8a
--- packages/state/src/lib/slices/workout/workoutLogSlice.ts
+++ packages/state/src/lib/slices/workout/workoutLogSlice.ts
@@ -1,1 +1,8 @@
 // ****** Workout logging flow
+// When user starts workout logging workout state is initialized with metadata and mode is set to edit
+// Also when a workout log starts, a stopwatch state is started to track the duration 
+// Edit mode allows a user to create, modify, and delete exercises and sets the same way as in workout building
+// When user switches to edit, the stopwatch stops. It starts again when you switch back to log mode
+// Later release, user will be able to stop and reset the stopwatch
+// When the user is done logging the workout, they can save the workout. Workout is added to workout logs list and sent to database.
+// Ensure the log status is changed to finalized before sending to database.
