dd08a7303c520ff48d96d47d2162f739f7774c55
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-20 12:59:37 -0700

    CWT-138 Create workout slice files and make progress with pseudocode

---
+++ packages/state/src/lib/slices/workout/workoutBuildSlice.ts
@@ -0,0 +1,8 @@
+// ****** Workout building flow
+// When user starts workout building workout state is initialized with metadata and mode is set to build
+// The possible values for mode are build, edit, log (will get to edit and log modes in workout logging flow)
+// Initial status of workout is draft
+// When user selects and exercise and clicks "Add Exercise" button, the exercise is added to workout state and initialized with one set (the first set cannot be deleted)
+// For each exercise, user can add sets, delete sets (over set 1), update tracking field value, and delete an exercise
+// When the user is done building the workout, they can save the workout. Workout is added to workout builds list and sent to database.
+// Ensure the build status is changed to finalized.
---
+++ packages/state/src/lib/slices/workout/workoutsSlice.ts
@@ -0,0 +1,2 @@
+// ****** Workout builds and logs list
+// When user first loads the app, the app will fetch the latest 20 workout logs
