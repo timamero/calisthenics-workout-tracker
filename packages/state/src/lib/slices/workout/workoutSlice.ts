d74074cf2c837439f81eae27c432b292be2f547e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-19 18:17:19 -0700

    CWT-138 Started creating psseudocode

---
+++ packages/state/src/lib/slices/workout/workoutSlice.ts
@@ -0,0 +1,4 @@
+// ****** Workout building flow
+// When user starts workout building workout state is initialized with metadata and mode is set to build
+// The possible values for mode are build, edit, log (will get to edit and log modes in workout logging flow)
+// When user selects and exercise and clicks "Add Exercise" button, the exercise is added to workout state and initialized with one set
