492f482eb7667b3f8326cbcf0d680a9b730cdcbf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-20 22:08:11 -0700

    CWT-138 Create workoutLogSlice

--- packages/state/src/lib/slices/workout/workoutLogSlice.ts
+++ packages/state/src/lib/slices/workout/workoutLogSlice.ts
@@ -6,3 +6,29 @@
 // Later release, user will be able to stop and reset the stopwatch
 // When the user is done logging the workout, they can save the workout. Workout is added to workout logs list and sent to database.
 // Ensure the log status is changed to finalized before sending to database.
+import { StateCreator } from "zustand";
+
+import type { WorkoutLog } from "@cwt/schema/workouts";
+
+import { StoreState } from "../../store";
+
+const INITIALIZED_WORKOUT_LOG: Partial<WorkoutLog> = {
+  title: "New workout log",
+  workout_data: {exercises: []},
+  status: "draft",
+  date: new Date()
+}
+export interface WorkoutLogSlice {
+  workoutLog: Partial<WorkoutLog> | null;
+  initializeWorkoutLog: () => void;
+}
+
+export const createWorkoutLogSlice: StateCreator<
+  StoreState,
+  [],
+  [],
+  WorkoutLogSlice
+> = (set, get) => ({
+  workoutLog: null,
+  initializeWorkoutLog: () => set(() => ({workoutBuild: INITIALIZED_WORKOUT_LOG}))
+})
--- packages/state/src/lib/store.ts
+++ packages/state/src/lib/store.ts
@@ -3,11 +3,13 @@ import { createExercisesSlice, ExercisesSlice } from './slices/library/exercises
 import { createExercisesFilterSlice, ExercisesFilterSlice } from './slices/library/exercisesFilterSlice';
 import { createWorkoutsSlice, WorkoutsSlice } from './slices/workout/workoutsSlice';
 import { createWorkoutBuildSlice, WorkoutBuildSlice } from './slices/workout/workoutBuildSlice';
+import { createWorkoutLogSlice, WorkoutLogSlice } from './slices/workout/workoutLogSlice';
 
-export type StoreState = ExercisesSlice & ExercisesFilterSlice & WorkoutsSlice & WorkoutBuildSlice
+export type StoreState = ExercisesSlice & ExercisesFilterSlice & WorkoutsSlice & WorkoutBuildSlice & WorkoutLogSlice
 export const useStore = create<StoreState>((...a) => ({
   ...createExercisesSlice(...a),
   ...createExercisesFilterSlice(...a),
   ...createWorkoutsSlice(...a),
-  ...createWorkoutBuildSlice(...a)
+  ...createWorkoutBuildSlice(...a),
+  ...createWorkoutLogSlice(...a)
 }))
