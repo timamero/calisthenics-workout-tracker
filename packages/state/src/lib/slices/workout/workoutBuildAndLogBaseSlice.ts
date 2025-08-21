704040c2b1846c539c33a39bcad767657ceb9956
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-20 22:10:04 -0700

    CWT-138 Create initial code for base slice

--- packages/state/src/lib/slices/workout/workoutBuildAndLogBaseSlice.ts
+++ packages/state/src/lib/slices/workout/workoutBuildAndLogBaseSlice.ts
@@ -1,1 +1,9 @@
 // This slice will contain shared functionality between workout builds and logs
+// Create combined action to set mode and initialize workout log/build
+import { StateCreator } from "zustand";
+
+import type { WorkoutBuild, WorkoutLog } from "@cwt/schema/workouts";
+
+export interface WorkoutBuildAndLogBaseSlice {
+  mode: string;
+}
