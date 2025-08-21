99dfcd7349c25e4b35ff69d55a1e856871455ced
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-20 21:29:39 -0700

    CWT-138 Create workoutsSlice

--- packages/state/src/lib/store.ts
+++ packages/state/src/lib/store.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import { createExercisesSlice, ExercisesSlice } from './slices/library/exercisesSlice';
 import { createExercisesFilterSlice, ExercisesFilterSlice } from './slices/library/exercisesFilterSlice';
+import { createWorkoutsSlice, WorkoutsSlice } from './slices/workout/workoutsSlice';
 
-export type StoreState = ExercisesSlice & ExercisesFilterSlice
+export type StoreState = ExercisesSlice & ExercisesFilterSlice & WorkoutsSlice
 export const useStore = create<StoreState>((...a) => ({
   ...createExercisesSlice(...a),
-  ...createExercisesFilterSlice(...a)
+  ...createExercisesFilterSlice(...a),
+  ...createWorkoutsSlice(...a)
 }))
