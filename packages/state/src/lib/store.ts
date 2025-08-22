6851a50290d98c62b6d74432f2c8df805b6a2c86
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-21 22:47:30 -0700

    CWT-138 Create stopwatchSlice

--- packages/state/src/lib/store.ts
+++ packages/state/src/lib/store.ts
@@ -3,11 +3,13 @@ import { createExercisesSlice, ExercisesSlice } from './slices/library/exercises
 import { createExercisesFilterSlice, ExercisesFilterSlice } from './slices/library/exercisesFilterSlice';
 import { createWorkoutsSlice, WorkoutsSlice } from './slices/workout/workoutsSlice';
 import { createWorkoutBuildAndLogSlice, WorkoutBuildAndLogSlice } from './slices/workout/workoutBuildAndLogSlice';
+import { createStopwatchSlice, StopwatchSlice } from './slices/workout/stopwatchSlice';
 
-export type StoreState = ExercisesSlice & ExercisesFilterSlice & WorkoutsSlice & WorkoutBuildAndLogSlice
+export type StoreState = ExercisesSlice & ExercisesFilterSlice & WorkoutsSlice & WorkoutBuildAndLogSlice & StopwatchSlice
 export const useStore = create<StoreState>((...a) => ({
   ...createExercisesSlice(...a),
   ...createExercisesFilterSlice(...a),
   ...createWorkoutsSlice(...a),
-  ...createWorkoutBuildAndLogSlice(...a)
+  ...createWorkoutBuildAndLogSlice(...a),
+  ...createStopwatchSlice(...a)
 }))
