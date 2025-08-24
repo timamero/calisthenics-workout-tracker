bd2b91eb44b0b8b42c224a91ddbedacb9e2cb5ad
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-24 01:20:06 -0700

    CWT-149 Create service to post build

--- packages/api/src/workoutsService.ts
+++ packages/api/src/workoutsService.ts
@@ -9,6 +9,14 @@ export function getWorkoutBuilds(baseUrl: string, token: string) {
   return apiFetch<WorkoutBuild[]>(baseUrl, "/workout/builds", "GET", token);
 }
 
-export function postWorkoutLog(baseUrl: string, token: string) {
-  return apiFetch<WorkoutLog[]>(baseUrl, "/workout/log", "POST", token);
+export function postWorkoutLog(baseUrl: string, token: string, body: BodyInit) {
+  return apiFetch<WorkoutLog>(baseUrl, "/workout/log", "POST", token, body);
+}
+
+export function postWorkoutBuild(
+  baseUrl: string,
+  token: string,
+  body: BodyInit
+) {
+  return apiFetch<WorkoutBuild>(baseUrl, "/workout/build", "POST", token, body);
 }
