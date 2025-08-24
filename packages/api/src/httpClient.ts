ae6bd0cd21c34c2855ad2537ddbb8e871ddae3ee
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-24 01:14:50 -0700

    CWT-149 Create workoutsService file

--- packages/api/src/httpClient.ts
+++ packages/api/src/httpClient.ts
@@ -1,19 +1,26 @@
-type Method = 'GET'
+type Method = "GET" | "POST";
 
 // Need to update later to add options for POST and DELETE requests
-export async function apiFetch<T>(baseUrl: string, endpoint: string, method?: Method, token?: string): Promise<T> {
+export async function apiFetch<T>(
+  baseUrl: string,
+  endpoint: string,
+  method?: Method,
+  token?: string
+): Promise<T> {
   if (!method) {
-    method = 'GET'
+    method = "GET";
   }
 
   const headers: Record<string, string> = {
-    'Content-Type': 'application/json',
-  }
-  if (token) headers['Authorization'] = `Bearer ${token}`;
+    "Content-Type": "application/json",
+  };
+  if (token) headers["Authorization"] = `Bearer ${token}`;
 
-  const res = await fetch(`${baseUrl}${endpoint}`, { method, headers })
+  const res = await fetch(`${baseUrl}${endpoint}`, { method, headers });
   if (!res.ok) {
-    throw new Error(`API Error at endpoint ${endpoint}: ${res.status} ${res.statusText}`)
+    throw new Error(
+      `API Error at endpoint ${endpoint}: ${res.status} ${res.statusText}`
+    );
   }
-  return res.json() as Promise<T>
+  return res.json() as Promise<T>;
 }
