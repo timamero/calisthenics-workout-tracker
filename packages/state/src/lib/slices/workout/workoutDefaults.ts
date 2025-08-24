87452253fd4537b8d8e9744a778d37824d97f23b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-24 00:26:20 -0700

    CWT-138 Clean code

--- packages/state/src/lib/slices/workout/workoutDefaults.ts
+++ packages/state/src/lib/slices/workout/workoutDefaults.ts
@@ -1,19 +1,9 @@
 import { Set, SetFields } from "@cwt/schema/workouts";
 
 export const INITIALIZED_SET: Omit<Set, "fields"> = {
-  // fields: { reps: 0, rest: "30S" }, // TODO: Set default fields depeneding on the tracked values
   completed: false,
   completed_at: null,
 };
 
 export const DEFAULT_REP_SET: SetFields = { reps: 0, rest: "30S" };
 export const DEFAULT_DURATION_SET: SetFields = { duration: "30S", rest: "30S" };
-
-// export const INITIALIZED_EXERCISE: Pick<WorkoutExercise, "sets"> = {
-//   // tracked: ["reps"], // TODO: Get default tracking field from exercise object
-//   sets: [
-//     {
-//       ...INITIALIZED_SET,
-//     },
-//   ],
-// };
