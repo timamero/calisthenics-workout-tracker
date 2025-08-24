39dd0e060cad449e4cb4d2c1a083b12c3b9408c1
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-24 00:25:32 -0700

    CWT-138 Conditionally create set fields

--- packages/state/src/lib/slices/workout/workoutDefaults.ts
+++ packages/state/src/lib/slices/workout/workoutDefaults.ts
@@ -1,16 +1,19 @@
-import { Set, WorkoutExercise } from "@cwt/schema/workouts";
+import { Set, SetFields } from "@cwt/schema/workouts";
 
-export const INITIALIZED_SET: Set = {
-  fields: { reps: 0, rest: "30S" }, // TODO: Set default fields depeneding on the tracked values
+export const INITIALIZED_SET: Omit<Set, "fields"> = {
+  // fields: { reps: 0, rest: "30S" }, // TODO: Set default fields depeneding on the tracked values
   completed: false,
   completed_at: null,
 };
 
-export const INITIALIZED_EXERCISE: Pick<WorkoutExercise, "sets"> = {
-  // tracked: ["reps"], // TODO: Get default tracking field from exercise object
-  sets: [
-    {
-      ...INITIALIZED_SET,
-    },
-  ],
-};
+export const DEFAULT_REP_SET: SetFields = { reps: 0, rest: "30S" };
+export const DEFAULT_DURATION_SET: SetFields = { duration: "30S", rest: "30S" };
+
+// export const INITIALIZED_EXERCISE: Pick<WorkoutExercise, "sets"> = {
+//   // tracked: ["reps"], // TODO: Get default tracking field from exercise object
+//   sets: [
+//     {
+//       ...INITIALIZED_SET,
+//     },
+//   ],
+// };
