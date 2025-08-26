6a1521f85f127835ae5e2173581207ba29c4f8c7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-25 21:17:15 -0700

    CWT-140 Use TanStack loader to load workout builds

--- apps/frontend/web/src/routes/workoutDashboard.tsx
+++ apps/frontend/web/src/routes/workoutDashboard.tsx
@@ -1,18 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Stack, Group, ScrollArea, Text } from '@mantine/core';
 
+import { useAuthStore } from '@cwt/state/auth';
 import { useStore } from '@cwt/state/store';
+
+import { getWorkoutBuilds } from '../services/workoutsService';
 
 import CardButton from '../components/common/CardButton';
 import LargeButton from '../components/common/LargeButton';
 
 
 export const Route = createFileRoute('/workoutDashboard')({
+  loader: async () => {
+    const supabaseSession = useAuthStore.getState().session
+    if (supabaseSession?.access_token) {
+      const workoutBuilds = await getWorkoutBuilds(supabaseSession.access_token);
+      useStore.getState().setWorkouts([], workoutBuilds)
+      return workoutBuilds
+    }
+    return []
+  },
   component: WorkoutDashboardView,
 });
 
 function WorkoutDashboardView() {
-  const workoutBuilds = useStore((state) => state.masterWorkoutBuilds)
+  const workoutBuilds = Route.useLoaderData()
   console.log('workout builds in workout page', workoutBuilds)
 
   const workoutBuildCards = workoutBuilds.map((wo, i) => {
