96154cca8453712aa5323d3d8521a146df9c20dd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-26 12:57:36 -0700

    CWT-140 Create dynamic route with workout mode

--- apps/frontend/web/src/components/common/LargeButton.tsx
+++ apps/frontend/web/src/components/common/LargeButton.tsx
@@ -1,14 +1,15 @@
 import { Paper, Stack, UnstyledButton } from '@mantine/core';
+import { Link } from '@tanstack/react-router';
 import type { ReactNode } from 'react';
 
 import classes from './LargeButton.module.css';
 
 interface LargeButtonProps {
-  href?: string;
+  to: string;
   children: ReactNode;
 }
 
-export default function LargeButton({ href, children }: LargeButtonProps) {
+export default function LargeButton({ to, children }: LargeButtonProps) {
   const handleClick = () => {
     console.log('clicked item');
   };
@@ -16,8 +17,8 @@ export default function LargeButton({ href, children }: LargeButtonProps) {
     <UnstyledButton
       onClick={handleClick}
       className={classes.button}
-      component="a"
-      href={href}
+      component={Link}
+      to={to}
     >
       <Paper p="md" radius="sm" bg="none">
         <Stack align="center" justify="center" gap="sm">
