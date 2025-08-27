fa1f00f51b2ac245730a64aaa0a61fbb73f5e858
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-27 01:52:49 -0700

    CWT-140 Create Sets component

---
+++ apps/frontend/web/src/components/Workout/Sets.tsx
@@ -0,0 +1,30 @@
+import { Stack, Text } from '@mantine/core';
+
+import type { WorkoutExercise } from '@cwt/schema/workouts';
+
+import RepField from './RepField';
+import TimeField from './TimeField';
+
+export default function Sets({
+  tracked,
+  sets,
+}: Pick<WorkoutExercise, 'sets' | 'tracked'>) {
+  const workoutSets = sets.map((set, i) => {
+    const fields = tracked.map((field) => {
+      if (field === 'reps') {
+        return <RepField value={set.fields.reps!} />;
+      }
+      if (field === 'duration') {
+        return <TimeField />;
+      }
+      return <></>;
+    });
+    return (
+      <Stack>
+        <Text>{`Set ${i + 1}`}</Text>
+        {fields}
+      </Stack>
+    );
+  });
+  return <Stack>{workoutSets}</Stack>;
+}
--- apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
@@ -3,8 +3,9 @@ import { Stack, Text } from '@mantine/core';
 import { useStore } from '@cwt/state/store';
 import type { WorkoutExercise } from '@cwt/schema/workouts';
 
-import RepField from './RepField';
-import TimeField from './TimeField';
+// import RepField from './RepField';
+// import TimeField from './TimeField';
+import Sets from './Sets';
 
 interface WorkoutExerciseProps {
   workoutExercise: WorkoutExercise;
@@ -16,27 +17,10 @@ export default function WorkoutExercise({
   const getExerciseNameById = useStore((state) => state.getExerciseNameByID);
   const name = getExerciseNameById(workoutExercise.exercise_id);
 
-  const sets = workoutExercise.sets.map((set, i) => {
-    const fields = workoutExercise.tracked.map((field) => {
-      if (field === 'reps') {
-        return <RepField value={set.fields.reps!} />;
-      }
-      if (field === 'duration') {
-        return <TimeField />;
-      }
-      return <></>;
-    });
-    return (
-      <Stack>
-        <Text>{`Set ${i + 1}`}</Text>
-        {fields}
-      </Stack>
-    );
-  });
   return (
     <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
       <Text>{name}</Text>
-      {sets}
+      <Sets tracked={workoutExercise.tracked} sets={workoutExercise.sets} />
     </Stack>
   );
 }
