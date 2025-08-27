b5d2f4cf033d5e2fce11eed0f2bf3053210cbaf0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-26 21:31:28 -0700

    CWT-140 Display placeholder element when workout is empty

--- apps/frontend/web/src/routes/workout.tsx
+++ apps/frontend/web/src/routes/workout.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { Title, Stack, Button, Group } from '@mantine/core';
+import { Title, Stack, Button, Group, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
 import { useStore } from '@cwt/state/store';
@@ -15,8 +15,10 @@ function WorkoutView() {
   const [cancelOverlayOpened, cancelOverlayHandler] = useDisclosure(false);
 
   const mode = useStore((state) => state.mode);
+  const workout = useStore((state) => state.workout);
   const resetWorkout = useStore((state) => state.resetWorkout);
   console.log('mode set to: ', mode);
+  console.log('workout state: ', workout);
 
   const onCancelWorkoutClick = () => {
     navigate({
@@ -25,18 +27,35 @@ function WorkoutView() {
     resetWorkout();
   };
 
+  const EmptyWorkoutPlaceholder = () => {
+    if (workout!.workout_data.exercises.length === 0) {
+      return (
+        <Stack align="center" bd="2px dashed gray.6" w="max-content" p="lg">
+          <Text size="lg" fw={800}>
+            Ready to start building your workout?
+          </Text>
+          <Text c="gray.8">Add your first exercise to begin</Text>
+        </Stack>
+      );
+    }
+    // return <div></div>;
+  };
+
   return (
     <Stack gap="xl">
-      <Title size="h6">Build Workout</Title>
-      <Group justify="center">
-        <Button
-          variant="subtle"
-          color="gray"
-          onClick={() => cancelOverlayHandler.open()}
-        >
-          Cancel workout building
-        </Button>
-      </Group>
+      <Title size="h6">{workout!.title}</Title>
+      <Stack gap="xl" align="center">
+        <EmptyWorkoutPlaceholder />
+        <Group justify="center">
+          <Button
+            variant="subtle"
+            color="gray"
+            onClick={() => cancelOverlayHandler.open()}
+          >
+            Cancel workout building
+          </Button>
+        </Group>
+      </Stack>
       <ConfirmationOverlay
         title="Cancel Workout Building"
         message="Confirm cancelling workout building. This will discard the current workout."
