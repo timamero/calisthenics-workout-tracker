1d6c60fa6cca360617b54770c98c9b73fe7127e0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-27 01:34:32 -0700

    CWT-140 List fields in set

--- apps/frontend/web/src/components/Workout/RepField.tsx
+++ apps/frontend/web/src/components/Workout/RepField.tsx
@@ -1,9 +1,9 @@
 import { Stack, TextInput } from '@mantine/core';
 
-export default function RepField() {
+export default function RepField({ value }: { value: number }) {
   return (
     <Stack>
-      <TextInput label="Reps" placeholder="0" />
+      <TextInput label="Reps" placeholder="0" value={value} />
     </Stack>
   );
 }
--- apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
@@ -16,19 +16,27 @@ export default function WorkoutExercise({
   const getExerciseNameById = useStore((state) => state.getExerciseNameByID);
   const name = getExerciseNameById(workoutExercise.exercise_id);
 
-  const fields = workoutExercise.tracked.map((field) => {
-    if (field === 'reps') {
-      return <RepField />;
-    }
-    if (field === 'duration') {
-      return <TimeField />;
-    }
-    return <></>;
+  const sets = workoutExercise.sets.map((set, i) => {
+    const fields = workoutExercise.tracked.map((field) => {
+      if (field === 'reps') {
+        return <RepField value={set.fields.reps!} />;
+      }
+      if (field === 'duration') {
+        return <TimeField />;
+      }
+      return <></>;
+    });
+    return (
+      <Stack>
+        <Text>{`Set ${i + 1}`}</Text>
+        {fields}
+      </Stack>
+    );
   });
   return (
-    <Stack>
+    <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
       <Text>{name}</Text>
-      {fields}
+      {sets}
     </Stack>
   );
 }
