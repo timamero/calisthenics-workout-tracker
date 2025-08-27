d8f3c7464f17c777eed283704579a5b872a8c825
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-27 01:20:07 -0700

    CWT-140 Create field components

---
+++ apps/frontend/web/src/components/Workout/RepField.tsx
@@ -0,0 +1,9 @@
+import { Stack, TextInput } from '@mantine/core';
+
+export default function RepField() {
+  return (
+    <Stack>
+      <TextInput label="Reps" placeholder="0" />
+    </Stack>
+  );
+}
---
+++ apps/frontend/web/src/components/Workout/TimeField.tsx
@@ -0,0 +1,9 @@
+import { Stack, TextInput } from '@mantine/core';
+
+export default function TimeField() {
+  return (
+    <Stack>
+      <TextInput label="Time" placeholder="0" />
+    </Stack>
+  );
+}
