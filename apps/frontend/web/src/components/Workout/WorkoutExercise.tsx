54bf13bb880248d6005579106904b9aeaa44e84a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-27 01:20:07 -0700

    CWT-140 Create field components

--- apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutExercise.tsx
@@ -3,6 +3,9 @@ import { Stack, Text } from '@mantine/core';
 import { useStore } from '@cwt/state/store';
 import type { WorkoutExercise } from '@cwt/schema/workouts';
 
+import RepField from './RepField';
+import TimeField from './TimeField';
+
 interface WorkoutExerciseProps {
   workoutExercise: WorkoutExercise;
 }
@@ -12,9 +15,20 @@ export default function WorkoutExercise({
 }: WorkoutExerciseProps) {
   const getExerciseNameById = useStore((state) => state.getExerciseNameByID);
   const name = getExerciseNameById(workoutExercise.exercise_id);
+
+  const fields = workoutExercise.tracked.map((field) => {
+    if (field === 'reps') {
+      return <RepField />;
+    }
+    if (field === 'duration') {
+      return <TimeField />;
+    }
+    return <></>;
+  });
   return (
     <Stack>
       <Text>{name}</Text>
+      {fields}
     </Stack>
   );
 }
