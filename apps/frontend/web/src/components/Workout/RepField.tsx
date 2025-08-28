6cc8035b3382bbf80ed952d607a37a65d0cc3704
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-08-27 21:43:53 -0700

    CWT-140 Add onChange to TextInput

--- apps/frontend/web/src/components/Workout/RepField.tsx
+++ apps/frontend/web/src/components/Workout/RepField.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Stack, TextInput } from '@mantine/core';
 
 export default function RepField({ value }: { value: number }) {
+  const [reps, setReps] = useState<number>(value);
   return (
     <Stack>
-      <TextInput label="Reps" placeholder="0" value={value} />
+      <TextInput
+        label="Reps"
+        placeholder={value.toString()}
+        value={reps}
+        onChange={(event) => setReps(Number(event.currentTarget.value))}
+      />
     </Stack>
   );
 }
