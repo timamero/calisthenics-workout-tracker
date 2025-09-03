5281a895a015aacdd88558885c1efe562c0b5d76
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-02 21:43:07 -0700

    CWT-140 Remove Stack wrapper

--- apps/frontend/web/src/components/Workout/RepField.tsx
+++ apps/frontend/web/src/components/Workout/RepField.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
-import { Stack, TextInput } from '@mantine/core';
+import { TextInput } from '@mantine/core';
 
 export default function RepField({ value }: { value: number }) {
   const [reps, setReps] = useState<number>(value);
   return (
-    <Stack>
-      <TextInput
-        label="Reps"
-        placeholder={value.toString()}
-        value={reps}
-        onChange={(event) => setReps(Number(event.currentTarget.value))}
-      />
-    </Stack>
+    <TextInput
+      label="Reps"
+      placeholder={value.toString()}
+      value={reps}
+      onChange={(event) => setReps(Number(event.currentTarget.value))}
+    />
   );
 }
