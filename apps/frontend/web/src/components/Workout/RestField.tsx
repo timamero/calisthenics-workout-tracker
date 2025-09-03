df3051c6de6f2e962ede2a588bb333a289a4580f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-02 22:32:10 -0700

    CWT-140 Create DurationInput component and use for Rest and Time field

--- apps/frontend/web/src/components/Workout/RestField.tsx
+++ apps/frontend/web/src/components/Workout/RestField.tsx
@@ -1,5 +1,5 @@
-import { TimePicker } from '@mantine/dates';
+import DurationInput from '../common/DurationInput';
 
 export default function RestField() {
-  return <TimePicker label="Rest" min="00:00" max="99:60" />;
+  return <DurationInput label="Rest" />;
 }
