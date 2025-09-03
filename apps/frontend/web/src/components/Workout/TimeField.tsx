39ac74ea4f3cef1e2477943763349a032d0c5778
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-02 21:43:07 -0700

    CWT-140 Remove Stack wrapper

--- apps/frontend/web/src/components/Workout/TimeField.tsx
+++ apps/frontend/web/src/components/Workout/TimeField.tsx
@@ -1,10 +1,5 @@
-import { Stack } from '@mantine/core';
 import { TimePicker } from '@mantine/dates';
 
 export default function TimeField() {
-  return (
-    <Stack>
-      <TimePicker label="Time" min="00:00" max="99:60" />
-    </Stack>
-  );
+  return <TimePicker label="Time" min="00:00" max="99:60" />;
 }
