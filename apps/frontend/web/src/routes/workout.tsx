9e460429193900f712732e1713acface01827bad
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-04 08:01:05 -0700

    CWT-140 Clean up code

--- apps/frontend/web/src/routes/workout.tsx
+++ apps/frontend/web/src/routes/workout.tsx
@@ -18,11 +18,8 @@ function WorkoutView() {
   const [addExerciseOverlayOpened, addExerciseOverlayHandler] =
     useDisclosure(false);
 
-  // const mode = useStore((state) => state.mode);
   const workout = useStore((state) => state.workout);
   const resetWorkout = useStore((state) => state.resetWorkout);
-  // console.log('mode set to: ', mode);
-  // console.log('workout state: ', workout);
 
   const onCancelWorkoutClick = () => {
     navigate({
