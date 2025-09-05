f81270804500ffaa27f768911b1dd00c35aaf530
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-05 11:59:42 -0700

    CWT-140 Fix time delta format and create updated function to get seconds

--- apps/frontend/web/src/utils/transformations.ts
+++ apps/frontend/web/src/utils/transformations.ts
@@ -12,3 +12,22 @@ export function getNumberBeforeS(inputString: string): string {
   }
   return '';
 }
+
+/**
+ * Extracts the number of seconds from an ISO 8601 duration string formatted as "PT{number}S"
+ * Example: "PT45S" => "45"
+ */
+export function getSecondsInDuration(inputString: string): string {
+  if (typeof inputString !== 'string') {
+    console.error('Invalid input: Please provide a string.');
+    return '';
+  }
+
+  const regex = /^PT(\d+)S$/;
+  const match = inputString.match(regex);
+
+  if (match) {
+    return match[1];
+  }
+  return '';
+}
