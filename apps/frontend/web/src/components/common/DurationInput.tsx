b43a027d15644c3bc09fdc35bb796d1e857068a8
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-04 23:33:10 -0700

    CWT-140 Clean up code

--- apps/frontend/web/src/components/common/DurationInput.tsx
+++ apps/frontend/web/src/components/common/DurationInput.tsx
@@ -1,8 +1,6 @@
 import { Stack, Group, TextInput } from '@mantine/core';
 import type { Dispatch, SetStateAction } from 'react';
 
-// import type { Set } from '@cwt/schema/workouts';
-
 interface DurationInputProps {
   label: string;
   sec: string;
@@ -13,27 +11,8 @@ interface DurationInputProps {
 export default function DurationInput({
   label,
   sec,
-  // setSec,
   handleChange,
 }: DurationInputProps) {
-  // Only allow numbers between 0 and 300
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const value = event.target.value;
-  //   // Allow empty string for controlled input
-  //   if (value === '') {
-  //     setSec('');
-  //     return;
-  //   }
-  //   // Validate: only numbers, no leading zeros except for '0'
-  //   if (/^(0|[1-9][0-9]{0,2})$/.test(value)) {
-  //     const num = Number(value);
-  //     if (num >= 0 && num <= 300) {
-  //       setSec(num.toString());
-  //     }
-  //   }
-  //   // Otherwise, do not update
-  // };
-
   return (
     <Stack>
       <Group>
