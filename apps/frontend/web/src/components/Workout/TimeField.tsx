693f994d338b49aad1718a1b09aa5d948f7ff04b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-04 23:57:26 -0700

    CWT-140 Update workout state on time field change

--- apps/frontend/web/src/components/Workout/TimeField.tsx
+++ apps/frontend/web/src/components/Workout/TimeField.tsx
@@ -1,7 +1,58 @@
 import { useState } from 'react';
-import DurationInput from '../common/DurationInput';
 
-export default function TimeField() {
-  const [sec, setSec] = useState<string>("");
-  return <DurationInput label="Time" sec={sec} setSec={setSec} />;
+import type { Set } from '@cwt/schema/workouts';
+
+import DurationInput from '../common/DurationInput';
+import { getNumberBeforeS } from '../../utils/transformations';
+
+interface TimeFieldProps {
+  set: Set;
+  index: number;
+  value: string;
+  handleSetFieldChange: (
+    set: Set,
+    setIndex: number,
+    updatedField: Pick<Set, 'fields'>,
+  ) => void;
+}
+
+export default function TimeField({
+  set,
+  index,
+  value,
+  handleSetFieldChange,
+}: TimeFieldProps) {
+  const [sec, setSec] = useState<string>(getNumberBeforeS(value));
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    // Allow empty string for controlled input
+    if (value === '') {
+      setSec('');
+      const updatedField: Pick<Set, 'fields'> = {
+        fields: { duration: '' },
+      };
+      handleSetFieldChange(set, index, updatedField);
+      return;
+    }
+    // Validate: only numbers, no leading zeros except for '0'
+    if (/^(0|[1-9][0-9]{0,2})$/.test(value)) {
+      const num = Number(value);
+      if (num >= 0 && num <= 300) {
+        setSec(num.toString());
+        const updatedField: Pick<Set, 'fields'> = {
+          fields: { duration: event.currentTarget.value + 'S' },
+        };
+        handleSetFieldChange(set, index, updatedField);
+      }
+    }
+    // Otherwise, do not update
+  };
+  return (
+    <DurationInput
+      label="Time"
+      sec={sec}
+      setSec={setSec}
+      handleChange={handleChange}
+    />
+  );
 }
