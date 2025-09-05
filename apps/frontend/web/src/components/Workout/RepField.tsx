abb22125d44e2032c86497a97302d2a10e07d0d7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-04 21:15:33 -0700

    CWT-140 Update workout state on rep change

--- apps/frontend/web/src/components/Workout/RepField.tsx
+++ apps/frontend/web/src/components/Workout/RepField.tsx
@@ -1,14 +1,39 @@
 import { useState } from 'react';
 import { TextInput } from '@mantine/core';
 
-export default function RepField({ value }: { value: number }) {
+import type { Set } from '@cwt/schema/workouts';
+
+interface RepFieldProps {
+  set: Set;
+  index: number;
+  value: number;
+  handleSetFieldChange: (
+    set: Set,
+    setIndex: number,
+    updatedField: Pick<Set, 'fields'>,
+  ) => void;
+}
+
+export default function RepField({
+  set,
+  index,
+  value,
+  handleSetFieldChange,
+}: RepFieldProps) {
   const [reps, setReps] = useState<number>(value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setReps(Number(event.currentTarget.value));
+    const updatedField: Pick<Set, 'fields'> = {
+      fields: { reps: Number(event.currentTarget.value) },
+    };
+    handleSetFieldChange(set, index, updatedField);
+  };
   return (
     <TextInput
       label="Reps"
       placeholder={value.toString()}
       value={reps}
-      onChange={(event) => setReps(Number(event.currentTarget.value))}
+      onChange={handleChange}
     />
   );
 }
