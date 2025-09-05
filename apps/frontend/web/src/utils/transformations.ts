2c2815f7a98804e37f15c7588bfc9533ab0f53cc
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-05 12:00:46 -0700

    CWT-140 Remove superseded code

--- apps/frontend/web/src/utils/transformations.ts
+++ apps/frontend/web/src/utils/transformations.ts
@@ -1,18 +1,3 @@
-export function getNumberBeforeS(inputString: string): string {
-  if (typeof inputString !== 'string') {
-    console.error('Invalid input: Please provide a string.');
-    return '';
-  }
-
-  const regex = /^(\d+)S$/;
-  const match = inputString.match(regex);
-
-  if (match) {
-    return parseInt(match[1], 10).toString();
-  }
-  return '';
-}
-
 /**
  * Extracts the number of seconds from an ISO 8601 duration string formatted as "PT{number}S"
  * Example: "PT45S" => "45"
