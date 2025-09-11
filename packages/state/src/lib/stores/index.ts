d1c754727f7030d79d3f571223484526a5b7c9de
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-11 13:35:30 -0700

    CWT-142 Create exercise library store

--- packages/state/src/lib/stores/index.ts
+++ packages/state/src/lib/stores/index.ts
@@ -1,4 +1,9 @@
 import { useWorkoutDraftStore } from './workouts/workoutDraftStore';
 import { useWorkoutLibraryStore } from './workouts/workoutLibraryStore';
+import { useExerciseLibraryStore } from './exercises/exerciseLibraryStore';
 
-export { useWorkoutDraftStore, useWorkoutLibraryStore };
+export {
+  useWorkoutDraftStore,
+  useWorkoutLibraryStore,
+  useExerciseLibraryStore,
+};
