9ebc5fe66cf601b817c191c302cb46b5e33ed71f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-11 20:33:49 -0700

    CWT-142 Fix file name

--- packages/state/src/lib/stores/index.ts
+++ packages/state/src/lib/stores/index.ts
@@ -1,7 +1,7 @@
 import { useWorkoutDraftStore } from './workouts/workoutDraftStore';
 import { useWorkoutLibraryStore } from './workouts/workoutLibraryStore';
 import { useExerciseLibraryStore } from './exercises/exerciseLibraryStore';
-import { useExercisesFilterStore } from './exercises/exercisesFilterSlice';
+import { useExercisesFilterStore } from './exercises/exercisesFilterStore';
 import { useExercisesSearchStore } from './exercises/exercisesSearchStore';
 
 export {
