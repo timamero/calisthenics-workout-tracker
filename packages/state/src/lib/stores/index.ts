a5ccf393e21861c539e2399f1768a51c61fca9e7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-11 20:32:31 -0700

    CWT-142 Create exercise search store

--- packages/state/src/lib/stores/index.ts
+++ packages/state/src/lib/stores/index.ts
@@ -2,10 +2,12 @@ import { useWorkoutDraftStore } from './workouts/workoutDraftStore';
 import { useWorkoutLibraryStore } from './workouts/workoutLibraryStore';
 import { useExerciseLibraryStore } from './exercises/exerciseLibraryStore';
 import { useExercisesFilterStore } from './exercises/exercisesFilterSlice';
+import { useExercisesSearchStore } from './exercises/exercisesSearchStore';
 
 export {
   useWorkoutDraftStore,
   useWorkoutLibraryStore,
   useExerciseLibraryStore,
   useExercisesFilterStore,
+  useExercisesSearchStore,
 };
