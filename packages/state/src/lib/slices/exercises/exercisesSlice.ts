47efb1e1fba6d448ac1d63010aa73fcf300e1b02
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-11 21:57:46 -0700

    CWT-142 Disable code

--- packages/state/src/lib/slices/exercises/exercisesSlice.ts
+++ packages/state/src/lib/slices/exercises/exercisesSlice.ts
@@ -1,80 +1,80 @@
-import { StateCreator } from 'zustand';
+// import { StateCreator } from 'zustand';
 
-import { Exercise } from '@cwt/schema/exercises';
+// import { Exercise } from '@cwt/schema/exercises';
 
-import { StoreState } from '../../store';
-import {
-  filterExercises,
-  filterExercisesBySearch,
-  sortExercises,
-} from './exercisesActions';
+// import { StoreState } from '../../store';
+// import {
+//   filterExercises,
+//   filterExercisesBySearch,
+//   sortExercises,
+// } from './exercisesActions';
 
-export interface ExercisesSlice {
-  masterExercises: Exercise[];
-  displayedExercises: Exercise[];
-  exerciseSearch: string;
-  appliedExerciseSearch: string;
-  setExercises: (exercises: Exercise[]) => void;
-  setExerciseSearch: (search: string) => void;
-  setAppliedExerciseSearch: (search: string) => void;
-  refreshDisplayedExercises: () => void;
-  getExerciseByID: (id: number) => Exercise;
-  getExerciseNameByID: (id: number) => string;
-}
+// export interface ExercisesSlice {
+//   masterExercises: Exercise[];
+//   displayedExercises: Exercise[];
+//   exerciseSearch: string;
+//   appliedExerciseSearch: string;
+//   setExercises: (exercises: Exercise[]) => void;
+//   setExerciseSearch: (search: string) => void;
+//   setAppliedExerciseSearch: (search: string) => void;
+//   refreshDisplayedExercises: () => void;
+//   getExerciseByID: (id: number) => Exercise;
+//   getExerciseNameByID: (id: number) => string;
+// }
 
-export const createExercisesSlice: StateCreator<
-  StoreState,
-  [['zustand/immer', never]],
-  [],
-  ExercisesSlice
-> = (set, get) => ({
-  masterExercises: [],
-  displayedExercises: [],
-  exerciseSearch: '',
-  appliedExerciseSearch: '',
-  setExercises: (exercises) =>
-    set(() => {
-      const sortedExercises = sortExercises(exercises);
-      return {
-        masterExercises: sortedExercises,
-        displayedExercises: sortedExercises,
-      };
-    }),
-  setExerciseSearch: (search) => set(() => ({ exerciseSearch: search })),
-  setAppliedExerciseSearch: (search) =>
-    set(() => ({ appliedExerciseSearch: search })),
-  refreshDisplayedExercises: () =>
-    set((state) => {
-      let result = state.masterExercises;
+// export const createExercisesSlice: StateCreator<
+//   StoreState,
+//   [['zustand/immer', never]],
+//   [],
+//   ExercisesSlice
+// > = (set, get) => ({
+//   masterExercises: [],
+//   displayedExercises: [],
+//   exerciseSearch: '',
+//   appliedExerciseSearch: '',
+//   setExercises: (exercises) =>
+//     set(() => {
+//       const sortedExercises = sortExercises(exercises);
+//       return {
+//         masterExercises: sortedExercises,
+//         displayedExercises: sortedExercises,
+//       };
+//     }),
+//   setExerciseSearch: (search) => set(() => ({ exerciseSearch: search })),
+//   setAppliedExerciseSearch: (search) =>
+//     set(() => ({ appliedExerciseSearch: search })),
+//   refreshDisplayedExercises: () =>
+//     set((state) => {
+//       let result = state.masterExercises;
 
-      if (state.appliedFilterSelections.length > 0) {
-        result = filterExercises(result, state.appliedFilterSelections);
-      }
+//       if (state.appliedFilterSelections.length > 0) {
+//         result = filterExercises(result, state.appliedFilterSelections);
+//       }
 
-      if (state.appliedExerciseSearch) {
-        result = filterExercisesBySearch(result, state.exerciseSearch);
-      }
+//       if (state.appliedExerciseSearch) {
+//         result = filterExercisesBySearch(result, state.exerciseSearch);
+//       }
 
-      return {
-        displayedExercises: result,
-        exerciseSearch: state.appliedExerciseSearch
-          ? state.appliedExerciseSearch
-          : '',
-      };
-    }),
-  getExerciseByID: (id) => {
-    return get().masterExercises.find(
-      (exercise) => exercise.id === id,
-    ) as Exercise;
-  },
-  getExerciseNameByID: (id) => {
-    return get().masterExercises.find((exercise) => exercise.id === id)
-      ?.name as string;
-  },
-});
+//       return {
+//         displayedExercises: result,
+//         exerciseSearch: state.appliedExerciseSearch
+//           ? state.appliedExerciseSearch
+//           : '',
+//       };
+//     }),
+//   getExerciseByID: (id) => {
+//     return get().masterExercises.find(
+//       (exercise) => exercise.id === id,
+//     ) as Exercise;
+//   },
+//   getExerciseNameByID: (id) => {
+//     return get().masterExercises.find((exercise) => exercise.id === id)
+//       ?.name as string;
+//   },
+// });
 
-export const selectHasFilters = (state: StoreState): boolean =>
-  state.appliedFilterSelections.length > 0;
+// export const selectHasFilters = (state: StoreState): boolean =>
+//   state.appliedFilterSelections.length > 0;
 
-export const selectHasSearch = (state: StoreState): boolean =>
-  Boolean(state.appliedExerciseSearch);
+// export const selectHasSearch = (state: StoreState): boolean =>
+//   Boolean(state.appliedExerciseSearch);
