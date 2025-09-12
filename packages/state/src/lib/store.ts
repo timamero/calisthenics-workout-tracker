1a0455d34b8a548a4e3ecdc658afd490e6b3790a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-11 21:57:46 -0700

    CWT-142 Disable code

--- packages/state/src/lib/store.ts
+++ packages/state/src/lib/store.ts
@@ -1,24 +1,24 @@
-import { create } from 'zustand';
-import { immer } from 'zustand/middleware/immer';
+// import { create } from 'zustand';
+// import { immer } from 'zustand/middleware/immer';
 
-import {
-  createExercisesSlice,
-  ExercisesSlice,
-} from './slices/exercises/exercisesSlice';
-import {
-  createExercisesFilterSlice,
-  ExercisesFilterSlice,
-} from './slices/exercises/exercisesFilterSliceSuperseded';
-import {
-  createStopwatchSlice,
-  StopwatchSlice,
-} from './slices/workouts/stopwatchSlice';
+// import {
+//   createExercisesSlice,
+//   ExercisesSlice,
+// } from './slices/exercises/exercisesSlice';
+// import {
+//   createExercisesFilterSlice,
+//   ExercisesFilterSlice,
+// } from './slices/exercises/exercisesFilterSliceSuperseded';
+// import {
+//   createStopwatchSlice,
+//   StopwatchSlice,
+// } from './slices/workouts/stopwatchSlice';
 
-export type StoreState = ExercisesSlice & ExercisesFilterSlice & StopwatchSlice;
-export const useStore = create<StoreState>()(
-  immer((...a) => ({
-    ...createExercisesSlice(...a),
-    ...createExercisesFilterSlice(...a),
-    ...createStopwatchSlice(...a),
-  })),
-);
+// export type StoreState = ExercisesSlice & ExercisesFilterSlice & StopwatchSlice;
+// export const useStore = create<StoreState>()(
+//   immer((...a) => ({
+//     ...createExercisesSlice(...a),
+//     ...createExercisesFilterSlice(...a),
+//     ...createStopwatchSlice(...a),
+//   })),
+// );
