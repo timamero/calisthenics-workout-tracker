8aeba96a8d43c20e0228975c5b782b8c422fd079
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-13 23:51:56 -0700

    CWT-142 Add comment

--- packages/state/src/counter.ts
+++ packages/state/src/counter.ts
@@ -1,11 +1,12 @@
-import { create } from 'zustand'
+// Sample Zustand store
+import { create } from 'zustand';
 
 interface BearState {
-  bears: number
-  increase: (by: number) => void
+  bears: number;
+  increase: (by: number) => void;
 }
 
 export const useBearStore = create<BearState>()((set) => ({
   bears: 0,
   increase: (by) => set((state) => ({ bears: state.bears + by })),
-}))
+}));
