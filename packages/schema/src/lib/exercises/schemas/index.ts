b6311de3105cf1518fa76f6a214ecb0effdf1780
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-14 01:48:11 -0700

    CWT-142 Disable superseded code

--- packages/schema/src/lib/exercises/schemas/index.ts
+++ packages/schema/src/lib/exercises/schemas/index.ts
@@ -1,5 +1,5 @@
-export * from './exerciseAttributes.schema';
-export * from './exerciseFilter.schema';
+// export * from './exerciseAttributes.schema';
+// export * from './exerciseFilter.schema';
 export * from './enums.schema';
 
 export * from './api/exercise.schema';
--- packages/schema/src/lib/exercises/types.ts
+++ packages/schema/src/lib/exercises/types.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 import {
-  ExerciseAttributesSchemaSuperseded as ExerciseAttributesSchema,
+  // ExerciseAttributesSchemaSuperseded as ExerciseAttributesSchema,
   ExerciseResponseSchema,
   MuscleSchema,
   EquipmentSchema,
   EmphasisSchema,
   DifficultySchema,
   TrackingSchema,
-  ExerciseFilterKeySchema,
-  ExerciseFilterCheckboxSchema,
+  // ExerciseFilterKeySchema,
+  // ExerciseFilterCheckboxSchema,
   AttributesSchema,
   FilterCheckboxSchema,
   FilterCheckboxKeySchema,
@@ -20,12 +20,12 @@ export type Equipment = z.infer<typeof EquipmentSchema>;
 export type Emphasis = z.infer<typeof EmphasisSchema>;
 export type Difficulty = z.infer<typeof DifficultySchema>;
 export type Tracking = z.infer<typeof TrackingSchema>;
-export type ExerciseAttributes = z.infer<typeof ExerciseAttributesSchema>;
+// export type ExerciseAttributes = z.infer<typeof ExerciseAttributesSchema>;
 export type Attributes = z.infer<typeof AttributesSchema>;
 export type FilterCheckbox = z.infer<typeof FilterCheckboxSchema>;
 export type FilterCheckboxKey = z.infer<typeof FilterCheckboxKeySchema>;
 export type ExerciseResponse = z.infer<typeof ExerciseResponseSchema>;
-export type ExerciseFilterKey = z.infer<typeof ExerciseFilterKeySchema>;
-export type ExerciseFilterCheckbox = z.infer<
-  typeof ExerciseFilterCheckboxSchema
->;
+// export type ExerciseFilterKey = z.infer<typeof ExerciseFilterKeySchema>;
+// export type ExerciseFilterCheckbox = z.infer<
+//   typeof ExerciseFilterCheckboxSchema
+// >;
