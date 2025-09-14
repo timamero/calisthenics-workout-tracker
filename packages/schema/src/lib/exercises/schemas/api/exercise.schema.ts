318ef1b01dc07ff47d52918c8c8132b5afbec6fd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-14 00:29:49 -0700

    CWT-142 Create exercise response schema

---
+++ packages/schema/src/lib/exercises/schemas/api/exercise.schema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+import {
+  MuscleSchema,
+  EquipmentSchema,
+  EmphasisSchema,
+  DifficultySchema,
+  TrackingSchema,
+} from '../enums.schema';
+
+export const ExerciseResponseSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  target_muscles: z.array(MuscleSchema),
+  required_equipment: z.array(z.optional(EquipmentSchema)),
+  emphasis: EmphasisSchema,
+  difficulty: DifficultySchema,
+  tags: z.array(z.string()),
+  instructions: z.array(z.string()),
+  default_tracking_type: z.array(TrackingSchema),
+});
--- packages/schema/src/lib/exercises/schemas/index.ts
+++ packages/schema/src/lib/exercises/schemas/index.ts
@@ -2,3 +2,5 @@ export * from './exercise.schema';
 export * from './exerciseAttributes.schema';
 export * from './exerciseFilter.schema';
 export * from './enums.schema';
+
+export * from './api/exercise.schema';
