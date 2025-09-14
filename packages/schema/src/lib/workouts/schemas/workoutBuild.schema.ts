88785a339ea75eaac2564846530b677d22e7076c
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-14 00:14:08 -0700

    CWT-142 Move workout build api schemas to api folder

--- packages/schema/src/lib/workouts/schemas/workoutBuild.schema.ts
+++ packages/schema/src/lib/workouts/schemas/workoutBuild.schema.ts
@@ -11,30 +11,30 @@ export const WorkoutBuildSchema = z.object({
   source: SourceSchema,
 });
 
-export const WorkoutBuildRequestSchema = z.object({
-  updated_at: z.nullable(z.iso.time()),
-  title: z.nullable(z.string().max(70)),
-  description: z.nullable(z.string().max(500)),
-  workout_data: WorkoutDataSchema,
-  status: StatusSchema,
-  goal: z.nullable(GoalSchema),
-  notes: z.nullable(z.string().max(750)),
-  user_id: z.nullable(z.uuid()),
-  estimated_duration: z.nullable(z.iso.time()),
-  source: SourceSchema,
-});
+// export const WorkoutBuildRequestSchema = z.object({
+//   updated_at: z.nullable(z.iso.time()),
+//   title: z.nullable(z.string().max(70)),
+//   description: z.nullable(z.string().max(500)),
+//   workout_data: WorkoutDataSchema,
+//   status: StatusSchema,
+//   goal: z.nullable(GoalSchema),
+//   notes: z.nullable(z.string().max(750)),
+//   user_id: z.nullable(z.uuid()),
+//   estimated_duration: z.nullable(z.iso.time()),
+//   source: SourceSchema,
+// });
 
-export const WorkoutBuildResponseSchema = z.object({
-  id: z.number(),
-  created_at: z.iso.time(),
-  updated_at: z.nullable(z.iso.time()),
-  user_id: z.optional(z.uuid()),
-  title: z.nullable(z.string().max(70)),
-  description: z.optional(z.string().max(500)),
-  workout_data: WorkoutDataSchema,
-  status: StatusSchema,
-  goal: z.optional(GoalSchema),
-  notes: z.nullable(z.string().max(750)),
-  estimated_duration: z.nullable(z.iso.time()),
-  source: SourceSchema,
-});
+// export const WorkoutBuildResponseSchema = z.object({
+//   id: z.number(),
+//   created_at: z.iso.time(),
+//   updated_at: z.nullable(z.iso.time()),
+//   user_id: z.optional(z.uuid()),
+//   title: z.nullable(z.string().max(70)),
+//   description: z.optional(z.string().max(500)),
+//   workout_data: WorkoutDataSchema,
+//   status: StatusSchema,
+//   goal: z.optional(GoalSchema),
+//   notes: z.nullable(z.string().max(750)),
+//   estimated_duration: z.nullable(z.iso.time()),
+//   source: SourceSchema,
+// });
