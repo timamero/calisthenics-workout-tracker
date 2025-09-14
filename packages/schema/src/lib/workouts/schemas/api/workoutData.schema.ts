4b0ec20316744af03a726d721820e9fe4eba80b3
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-14 00:14:08 -0700

    CWT-142 Move workout build api schemas to api folder

---
+++ packages/schema/src/lib/workouts/schemas/api/workoutData.schema.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+import { TrackingTypeSchema } from '../enums.schema';
+
+export const SetFieldsSchema = z.object({
+  reps: z.optional(z.number()),
+  weight: z.optional(z.number()),
+  time: z.optional(z.iso.time()),
+  rest: z.optional(z.iso.time()),
+});
+
+export const SetSchema = z.object({
+  id: z.uuid(),
+  fields: SetFieldsSchema,
+  completed: z.boolean(),
+  completed_at: z.nullable(z.date()),
+});
+
+export const WorkoutExerciseSchema = z.object({
+  id: z.uuid(),
+  exercise_id: z.int(),
+  tracked: z.array(TrackingTypeSchema),
+  sets: z.array(SetSchema),
+});
+
+export const WorkoutDataSchema = z.object({
+  exercises: z.array(WorkoutExerciseSchema),
+});
--- packages/schema/src/lib/workouts/schemas/index.ts
+++ packages/schema/src/lib/workouts/schemas/index.ts
@@ -2,3 +2,5 @@ export * from './workoutBuild.schema';
 export * from './workoutLog.schema';
 export * from './workoutData.schema';
 export * from './enums.schema';
+
+export * from './api/workoutBuild.schema';
