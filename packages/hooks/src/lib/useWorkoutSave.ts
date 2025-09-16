ebbb9562aeb518a1aac42351d198c5d743396164
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-15 20:30:42 -0700

    CWT-142 Update hook to use new state actions

--- packages/hooks/src/lib/useWorkoutSave.ts
+++ packages/hooks/src/lib/useWorkoutSave.ts
@@ -1,18 +1,25 @@
-import { useWorkoutDraftStore } from "@cwt/state/stores";
-import { useAuthStore } from "@cwt/state/stores";
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+import { useAuthStore } from '@cwt/state/stores';
 
 export function useWorkoutSave() {
   const user = useAuthStore((state) => state.user);
-  const setWorkoutToSave = useWorkoutDraftStore(
-    (state) => state.setWorkoutToSave
+  const initializeWorkoutToSave = useWorkoutDraftStore(
+    (state) => state.initializeWorkoutToSave
   );
+  const addUserIDToWorkoutToSave = useWorkoutDraftStore(
+    (state) => state.addUserIDToWorkoutToSave
+  );
+  // const setWorkoutToSave = useWorkoutDraftStore(
+  //   (state) => state.setWorkoutToSave
+  // );
 
   // Wrap the original setWorkoutToSave to inject user_id
   const setWorkoutToSaveWithUser = () => {
-    setWorkoutToSave(); // Create initializeWorkoutToSave and call here
+    initializeWorkoutToSave(); // Create initializeWorkoutToSave and call here
     const workoutToSave = useWorkoutDraftStore.getState().workoutToSave;
     if (workoutToSave && user?.id) {
-      workoutToSave.user_id = user.id;
+      addUserIDToWorkoutToSave(user.id);
+      // workoutToSave.user_id = user.id;
       // useWorkoutDraftStore.getState().setWorkoutToSave(workoutToSave); // Enable after updating setWorkoutToSave to accept parameters
     }
   };
