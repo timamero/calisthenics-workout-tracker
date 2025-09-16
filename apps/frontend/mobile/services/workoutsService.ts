55c032335448329f2edea094e005d000a01293c7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-15 23:56:28 -0700

    CWT-141 Create workoutService file

---
+++ apps/frontend/mobile/services/workoutsService.ts
@@ -0,0 +1,30 @@
+import {
+  getWorkoutBuilds as apiGetWorkoutBuilds,
+  postWorkoutBuild as apiPostWorkoutBuild,
+} from '@cwt/api/workoutsService';
+import type { WorkoutBuildResponse } from '@cwt/schema/workouts';
+import { sampleWorkoutBuilds } from '@cwt/mocks/sampleWorkoutBuilds';
+
+const baseUrl = process.env.EXPO_PUBLIC_BASE_URL || '';
+const environment = process.env.EXPO_PUBLIC_ENVIRONMENT || null;
+
+export async function getWorkoutBuilds(
+  token: string,
+): Promise<WorkoutBuildResponse[]> {
+  if (environment === 'local') {
+    console.log('Mobile: Local environment, return sample workout builds.');
+    return sampleWorkoutBuilds;
+  }
+  return apiGetWorkoutBuilds(baseUrl, token);
+}
+
+export async function postWorkoutBuild(
+  token: string,
+  body: BodyInit,
+): Promise<WorkoutBuildResponse | null> {
+  if (environment === 'local') {
+    console.log('Mobile: Local environment, return workout build body.');
+    return JSON.parse(body as string);
+  }
+  return apiPostWorkoutBuild(baseUrl, token, body);
+}
