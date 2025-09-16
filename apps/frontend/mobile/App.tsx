d26838d899a13313f60ebde48dcd58178fde0953
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-16 00:32:28 -0700

    CWT-141 fetch workout data and set workouts state

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { PaperProvider, ActivityIndicator, Text } from 'react-native-paper';
 
-import { useExerciseLibraryStore, useAuthStore } from '@cwt/state/stores';
+import {
+  useExerciseLibraryStore,
+  useAuthStore,
+  useWorkoutLibraryStore,
+} from '@cwt/state/stores';
 import { useSupabaseAuth } from '@cwt/hooks';
 
 import theme from './theme';
 import { supabase } from './services/supabaseClient';
 import Navigation from './navigation';
 import { getExercises } from './services/exercisesService';
+import { getWorkoutBuilds } from './services/workoutsService';
 
 export default function App() {
   const loading = useAuthStore((state) => state.loading);
@@ -17,6 +22,7 @@ export default function App() {
   const setSession = useAuthStore((state) => state.setSession);
   const setLoading = useAuthStore((state) => state.setLoading);
   const setExercises = useExerciseLibraryStore((state) => state.setExercises);
+  const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
 
   useSupabaseAuth(supabase, setSession, setLoading);
 
@@ -24,13 +30,19 @@ export default function App() {
     const asyncFetchData = async () => {
       if (supabaseSession?.access_token) {
         const exercises = await getExercises(supabaseSession.access_token);
+        const workoutBuilds = await getWorkoutBuilds(
+          supabaseSession.access_token,
+        );
         if (exercises) {
           setExercises(exercises);
+        }
+        if (workoutBuilds) {
+          setWorkouts([], workoutBuilds);
         }
       }
     };
     asyncFetchData();
-  }, [setExercises, supabaseSession]);
+  }, [setExercises, supabaseSession, setWorkouts]);
 
   if (loading) {
     return (
