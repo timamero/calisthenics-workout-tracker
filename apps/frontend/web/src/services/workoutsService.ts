339f7b7954b5b39817d4d80e84c3df04c9d7e940
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-15 23:41:28 -0700

    CWT-141 Move error handling to api workoutService

--- apps/frontend/web/src/services/workoutsService.ts
+++ apps/frontend/web/src/services/workoutsService.ts
@@ -11,30 +11,20 @@ const environment = import.meta.env.VITE_ENVIRONMENT || null;
 export async function getWorkoutBuilds(
   token: string,
 ): Promise<WorkoutBuildResponse[]> {
-  try {
-    if (environment === 'local') {
-      console.log('Web: Local environment, return sample workout builds.');
-      return sampleWorkoutBuilds;
-    }
-    return await apiGetWorkoutBuilds(baseUrl, token);
-  } catch (error) {
-    console.error('Web: Error fetching workout builds from API.', error);
-    throw error;
+  if (environment === 'local') {
+    console.log('Web: Local environment, return sample workout builds.');
+    return sampleWorkoutBuilds;
   }
+  return await apiGetWorkoutBuilds(baseUrl, token);
 }
 
 export async function postWorkoutBuild(
   token: string,
   body: BodyInit,
 ): Promise<WorkoutBuildResponse | null> {
-  try {
-    if (environment === 'local') {
-      console.log('Web: Local environment, return workout build body.');
-      return JSON.parse(body as string);
-    }
-    return await apiPostWorkoutBuild(baseUrl, token, body);
-  } catch (error) {
-    console.error('Web: Error posting workout to API.', error);
-    throw error;
+  if (environment === 'local') {
+    console.log('Web: Local environment, return workout build body.');
+    return JSON.parse(body as string);
   }
+  return await apiPostWorkoutBuild(baseUrl, token, body);
 }
--- packages/api/src/workoutsService.ts
+++ packages/api/src/workoutsService.ts
@@ -5,29 +5,39 @@ export function getWorkoutLogs(baseUrl: string, token: string) {
   return apiFetch<WorkoutLog[]>(baseUrl, '/workout/logs', 'GET', token);
 }
 
-export function getWorkoutBuilds(baseUrl: string, token: string) {
-  return apiFetch<WorkoutBuildResponse[]>(
-    baseUrl,
-    '/workout/builds',
-    'GET',
-    token,
-  );
+export async function getWorkoutBuilds(baseUrl: string, token: string) {
+  try {
+    return await apiFetch<WorkoutBuildResponse[]>(
+      baseUrl,
+      '/workout/builds',
+      'GET',
+      token,
+    );
+  } catch (error) {
+    console.error('Web: Error fetching workout builds from API.', error);
+    throw error;
+  }
 }
 
 export function postWorkoutLog(baseUrl: string, token: string, body: BodyInit) {
   return apiFetch<WorkoutLog>(baseUrl, '/workout/log', 'POST', token, body);
 }
 
-export function postWorkoutBuild(
+export async function postWorkoutBuild(
   baseUrl: string,
   token: string,
   body: BodyInit,
 ) {
-  return apiFetch<WorkoutBuildResponse>(
-    baseUrl,
-    '/workout/build',
-    'POST',
-    token,
-    body,
-  );
+  try {
+    return await apiFetch<WorkoutBuildResponse>(
+      baseUrl,
+      '/workout/build',
+      'POST',
+      token,
+      body,
+    );
+  } catch (error) {
+    console.error('Web: Error posting workout to API.', error);
+    throw error;
+  }
 }
