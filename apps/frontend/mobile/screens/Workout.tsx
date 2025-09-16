523711ea73258cccaaccab85f783572f2c502359
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-16 03:27:00 -0700

    CWT-142 Remove back button in header

--- apps/frontend/mobile/screens/Workout.tsx
+++ apps/frontend/mobile/screens/Workout.tsx
@@ -9,7 +9,10 @@ export default function WorkoutScreen() {
   const theme = useTheme() as CustomTheme;
   const navigation = useNavigation<any>();
 
-  navigation.setOptions({ title: 'Workout Title Here' });
+  navigation.setOptions({
+    title: 'Workout Title Here',
+    headerLeft: () => null,
+  });
   return (
     <View
       style={{
