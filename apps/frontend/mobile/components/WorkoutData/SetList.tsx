c834b9a43c82aef8d852c79b108606bb3f5a377e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-18 15:19:50 -0700

    CWT-141 Create SetList component

---
+++ apps/frontend/mobile/components/WorkoutData/SetList.tsx
@@ -0,0 +1,72 @@
+import { View } from 'react-native';
+import { useTheme, Button } from 'react-native-paper';
+
+import type { SetFields } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+
+import { Text } from '../../customText';
+import { CustomTheme } from '../../theme';
+import FieldsList from './FieldsList';
+
+interface SetListProps {
+  exerciseIndex: number;
+  handleOpenDeleteSetDialog: () => void;
+}
+
+export default function SetList({
+  exerciseIndex,
+  handleOpenDeleteSetDialog,
+}: SetListProps) {
+  const theme = useTheme() as CustomTheme;
+
+  const sets = useWorkoutDraftStore((state) => state.workoutData).exercises[
+    exerciseIndex
+  ].sets;
+  const tracked = useWorkoutDraftStore((state) => state.workoutData).exercises[
+    exerciseIndex
+  ].tracked;
+
+  const setSelectedSetIndexToMod = useWorkoutDraftStore(
+    (state) => state.setSelectedSetIndexToMod,
+  );
+  const setSelectedExerciseIndexToMod = useWorkoutDraftStore(
+    (state) => state.setSelectedExerciseIndexToMod,
+  );
+  const updateField = useWorkoutDraftStore((state) => state.updateField);
+
+  const handleSetFieldChange = (
+    setIndex: number,
+    updatedField: Partial<SetFields>,
+  ) => {
+    setSelectedSetIndexToMod(setIndex);
+    updateField(exerciseIndex, updatedField);
+  };
+
+  const setList = sets.map((set, i) => {
+    const onDeleteSetPress = () => {
+      setSelectedSetIndexToMod(i);
+      setSelectedExerciseIndexToMod(exerciseIndex);
+      handleOpenDeleteSetDialog();
+    };
+
+    return (
+      <View key={`set-${i}`}>
+        <View>
+          <Text style={{ color: theme.colors.light }}>{`Set ${i + 1}`}</Text>
+          {sets.length > 1 && (
+            <Button mode="outlined" onPress={() => onDeleteSetPress()}>
+              Delete
+            </Button>
+          )}
+        </View>
+        <FieldsList
+          tracked={{ tracked }}
+          set={set}
+          setIndex={i}
+          handleSetFieldChange={handleSetFieldChange}
+        />
+      </View>
+    );
+  });
+  return <View>{setList}</View>;
+}
