21650437e9360903f0bd9e7eabd5cd4d2137b75d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-18 05:24:27 -0700

    CWT-141 Delete exercise on press

--- apps/frontend/mobile/components/WorkoutData/WorkoutExercise.tsx
+++ apps/frontend/mobile/components/WorkoutData/WorkoutExercise.tsx
@@ -1,13 +1,14 @@
 import { View } from 'react-native';
-import { Divider } from 'react-native-paper';
+import { Divider, useTheme, Button } from 'react-native-paper';
 
 import type { WorkoutExercise as WorkoutExerciseType } from '@cwt/schema/workouts';
 import {
   useExerciseLibraryStore,
-  // useWorkoutDraftStore,
+  useWorkoutDraftStore,
 } from '@cwt/state/stores';
 
 import { Text } from '../../customText';
+import { CustomTheme } from '../../theme';
 
 interface WorkoutExerciseProps {
   exerciseIndex: number;
@@ -18,13 +19,36 @@ export default function WorkoutExercise({
   exerciseIndex,
   workoutExercise,
 }: WorkoutExerciseProps) {
+  const theme = useTheme() as CustomTheme;
+
+  const deleteExercise = useWorkoutDraftStore((state) => state.removeExercise);
   const getExerciseNameById = useExerciseLibraryStore(
     (state) => state.getExerciseNameByID,
   );
   const name = getExerciseNameById(workoutExercise.exercise_id);
   return (
     <View>
-      <Text>{name}</Text>
+      <View>
+        <Text
+          variant="bodyLarge"
+          style={{
+            color: theme.colors.light,
+            fontWeight: 800,
+            textAlign: 'center',
+          }}
+        >
+          {name}
+        </Text>
+        <Button
+          mode="text"
+          onPress={() => deleteExercise(exerciseIndex)}
+          style={{
+            marginRight: 24,
+          }}
+        >
+          Cancel
+        </Button>
+      </View>
       <Divider />
     </View>
   );
