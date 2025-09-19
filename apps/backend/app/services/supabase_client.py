4b44fecd68790b9c2afa43e98d01b85ae5ae583a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-19 15:14:03 -0700

    CWT-144 Use anon key

--- apps/backend/app/services/supabase_client.py
+++ apps/backend/app/services/supabase_client.py
@@ -3,12 +3,11 @@ from supabase import create_client, Client, ClientOptions
 from app.core.config import settings
 
 url: str = settings.supabase_url
-# key: str = settings.supabase_anon_key  # Use this key when integrating with frontend
-key: str = settings.supabase_service_role_key  # Use this key when using backend only
+key: str = settings.supabase_anon_key  # Use this key when integrating with frontend
+# key: str = settings.supabase_service_role_key  # Use this key when using backend only
 
 
 def get_supabase_client(access_token: str | None = None) -> Client:
-
     if access_token:
         options = ClientOptions(headers={"Authorization": f"Bearer {access_token}"})
         client: Client = create_client(url, key, options)
