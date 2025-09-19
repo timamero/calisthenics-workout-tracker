5846f51d975bd9c124c0cc61cc4efe6a9673a06c
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-18 17:18:11 -0700

    CWT-141 Create and implement SetContext to remove prop drilling

--- apps/frontend/mobile/components/WorkoutData/FieldsList.tsx
+++ apps/frontend/mobile/components/WorkoutData/FieldsList.tsx
@@ -1,29 +1,46 @@
+import * as React from 'react';
 import { View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 import type { SetFields, Set, WorkoutExercise } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+
+import { SetContext } from '../../contexts/SetContext';
+import { WorkoutExerciseContext } from '../../contexts/WorkoutExerciseContext';
 
 interface FieldsListProps {
-  tracked: Pick<WorkoutExercise, 'tracked'>;
-  set: Set;
-  handleSetFieldChange: (updatedField: Partial<SetFields>) => void;
+  // tracked: Pick<WorkoutExercise, 'tracked'>;
+  // set: Set;
+  // handleSetFieldChange: (updatedField: Partial<SetFields>) => void;
 }
 
-export default function FieldsList({
-  tracked,
-  set,
-  handleSetFieldChange,
-}: FieldsListProps) {
-  const fields = tracked.tracked.map((field) => {
+export default function FieldsList(
+  {
+    // tracked,
+    // set,
+    // handleSetFieldChange,
+  }: FieldsListProps,
+) {
+  const exerciseIndex = React.useContext(WorkoutExerciseContext)!.exerciseIndex;
+  const set = React.useContext(SetContext)!.set;
+  const setIndex = React.useContext(SetContext)!.setIndex;
+  const handleSetFieldChange =
+    React.useContext(SetContext)!.handleSetFieldChange;
+
+  const tracked = useWorkoutDraftStore((state) => state.workoutData).exercises[
+    exerciseIndex
+  ].tracked;
+
+  const fields = tracked.map((field, i) => {
     if (field === 'reps') {
       return (
-        <View key={set.id}>
+        <View key={`${set.id}-${i}`}>
           <TextInput
             keyboardType="number-pad"
             label="Reps"
             value={set.fields.reps!.toString()}
             onChangeText={(text) =>
-              handleSetFieldChange({ reps: Number(text) })
+              handleSetFieldChange(setIndex, { reps: Number(text) })
             }
           />
         </View>
--- apps/frontend/mobile/components/WorkoutData/SetList.tsx
+++ apps/frontend/mobile/components/WorkoutData/SetList.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
+import { SetFields } from '@cwt/schema/workouts';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
+import { SetContext } from '../../contexts/SetContext';
 import SetContainer from './SetContainer';
 
 interface SetListProps {
@@ -14,8 +16,32 @@ export default function SetList({ exerciseIndex }: SetListProps) {
     exerciseIndex
   ].sets;
 
+  const setSelectedSetIndexToMod = useWorkoutDraftStore(
+    (state) => state.setSelectedSetIndexToMod,
+  );
+  const updateField = useWorkoutDraftStore((state) => state.updateField);
+
+  const handleSetFieldChange = (
+    setIndex: number,
+    updatedField: Partial<SetFields>,
+  ) => {
+    setSelectedSetIndexToMod(setIndex);
+    updateField(exerciseIndex, updatedField);
+  };
+
   const setList = sets.map((set, i) => {
-    return <SetContainer key={set.id} setIndex={i} />;
+    return (
+      <SetContext.Provider
+        key={set.id}
+        value={{
+          setIndex: i,
+          set: set,
+          handleSetFieldChange: handleSetFieldChange,
+        }}
+      >
+        <SetContainer key={set.id} />
+      </SetContext.Provider>
+    );
   });
   return <View>{setList}</View>;
 }
