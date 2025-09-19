a59e3620aa55080864439932b6d65c5c49f6d2a5
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-18 23:04:02 -0700

    CWT-141 Update set style

--- apps/frontend/mobile/components/WorkoutData/Set.tsx
+++ apps/frontend/mobile/components/WorkoutData/Set.tsx
@@ -1,8 +1,6 @@
 import { View } from 'react-native';
 import { useTheme, Button } from 'react-native-paper';
 
-import type { Set } from '@cwt/schema/workouts';
-
 import { Text } from '../../customText';
 import { CustomTheme } from '../../theme';
 import FieldsList from './FieldsList';
@@ -21,14 +19,34 @@ export default function Set({
   const theme = useTheme() as CustomTheme;
 
   return (
-    <View key={`set-${setIndex}`}>
-      <View>
+    <View
+      key={`set-${setIndex}`}
+      style={{
+        borderWidth: 1,
+        borderColor: theme.colors.darkGrey,
+        paddingInline: 8,
+        paddingBlock: 16,
+        marginBlock: 8,
+      }}
+    >
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
         <Text
           style={{ color: theme.colors.light }}
         >{`Set ${setIndex + 1}`}</Text>
         {showDeleteButton && (
-          <Button mode="outlined" onPress={() => onDeleteSetPress()}>
-            Delete
+          <Button
+            mode="outlined"
+            onPress={() => onDeleteSetPress()}
+            labelStyle={{ marginVertical: 8, marginHorizontal: 16 }}
+          >
+            Delete Set
           </Button>
         )}
       </View>
