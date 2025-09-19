31965c46c3e10441da4c0617ce43d21939bad99a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-18 17:22:01 -0700

    CWT-141 Clean up code

--- apps/frontend/mobile/components/WorkoutData/SetContainer.tsx
+++ apps/frontend/mobile/components/WorkoutData/SetContainer.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import type { SetFields } from '@cwt/schema/workouts';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
 import { WorkoutExerciseContext } from '../../contexts/WorkoutExerciseContext';
@@ -18,9 +17,6 @@ export default function SetContainer() {
   const sets = useWorkoutDraftStore((state) => state.workoutData).exercises[
     exerciseIndex
   ].sets;
-  // const tracked = useWorkoutDraftStore((state) => state.workoutData).exercises[
-  //   exerciseIndex
-  // ].tracked;
 
   const setSelectedSetIndexToMod = useWorkoutDraftStore(
     (state) => state.setSelectedSetIndexToMod,
@@ -28,12 +24,6 @@ export default function SetContainer() {
   const setSelectedExerciseIndexToMod = useWorkoutDraftStore(
     (state) => state.setSelectedExerciseIndexToMod,
   );
-  // const updateField = useWorkoutDraftStore((state) => state.updateField);
-
-  // const handleSetFieldChange = (updatedField: Partial<SetFields>) => {
-  //   setSelectedSetIndexToMod(setIndex);
-  //   updateField(exerciseIndex, updatedField);
-  // };
 
   const onDeleteSetPress = () => {
     setSelectedSetIndexToMod(setIndex);
@@ -44,11 +34,8 @@ export default function SetContainer() {
   return (
     <Set
       setIndex={setIndex}
-      // set={sets[setIndex]}
       showDeleteButton={sets.length > 1}
-      // tracked={{ tracked }}
       onDeleteSetPress={onDeleteSetPress}
-      // handleSetFieldChange={handleSetFieldChange}
     />
   );
 }
