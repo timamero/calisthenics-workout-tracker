467af6de68b1a436967bb964be799d26cd234fdf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-19 01:28:07 -0700

    CWT-141 Save workout to state

--- apps/frontend/mobile/screens/Workout.tsx
+++ apps/frontend/mobile/screens/Workout.tsx
@@ -3,7 +3,12 @@ import { View, BackHandler, ScrollView } from 'react-native';
 import { useTheme, Button, FAB } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
-import { useWorkoutDraftStore } from '@cwt/state/stores';
+import {
+  useWorkoutDraftStore,
+  useAuthStore,
+  useWorkoutLibraryStore,
+} from '@cwt/state/stores';
+import { useWorkoutSave } from '@cwt/hooks';
 
 import WorkoutTitleContainer from '../components/WorkoutData/WorkoutTitleContainer';
 import WorkoutData from '../components/WorkoutData/';
@@ -20,17 +25,50 @@ export default function WorkoutScreen() {
   const workoutData = useWorkoutDraftStore((state) => state.workoutData); // DELETE THIS LINE LATER
   console.log('workout - workoutData: ', JSON.stringify(workoutData)); // DELETE THIS LINE LATER
 
+  const mode = useWorkoutDraftStore((state) => state.mode);
   const resetWorkout = useWorkoutDraftStore((state) => state.resetWorkout);
+  const supabaseSession = useAuthStore((state) => state.session);
+  const completeWorkout = useWorkoutLibraryStore(
+    (state) => state.completeWorkout,
+  );
 
-  const [isCancelWorktoutDialogVisible, setIsCancelWorktoutDialogVisible] =
+  const { setWorkoutToSaveWithUser } = useWorkoutSave();
+
+  const [isCancelWorkoutDialogVisible, setIsCancelWorkoutDialogVisible] =
     React.useState<boolean>(false);
   const [isAddExerciseOverlayVisible, setIsAddExerciseOverlayVisible] =
     React.useState<boolean>(false);
+  const [isSaveWorkoutDialogVisible, setIsSaveWorkoutDialogVisible] =
+    React.useState<boolean>(false);
 
   const onCancelWorkoutPress = () => {
-    setIsCancelWorktoutDialogVisible(false);
+    setIsCancelWorkoutDialogVisible(false);
     navigation.navigate('App', { screen: 'WorkoutDashboard' });
     resetWorkout();
+  };
+
+  const onSaveWorkoutPress = () => {
+    setWorkoutToSaveWithUser();
+    const workoutToSave = useWorkoutDraftStore.getState().workoutToSave;
+    if (!supabaseSession || !workoutToSave) {
+      console.error('Session not found or workout data invalid');
+      return;
+    }
+
+    const result = workoutToSave; // TEMP
+    // const body = JSON.stringify(workoutToSave);
+    // const result = await postWorkoutBuild(supabaseSession.access_token, body);
+    if (result) {
+      completeWorkout(workoutToSave, mode!);
+      resetWorkout();
+    } else {
+      // TODO: Save to state called unsavedBuilds
+      resetWorkout();
+      console.error('Workout build post request failed');
+    }
+
+    setIsSaveWorkoutDialogVisible(false);
+    navigation.navigate('App', { screen: 'WorkoutDashboard' });
   };
 
   React.useEffect(() => {
@@ -38,7 +76,7 @@ export default function WorkoutScreen() {
       headerLeft: () => (
         <Button
           mode="text"
-          onPress={() => setIsCancelWorktoutDialogVisible(true)}
+          onPress={() => setIsCancelWorkoutDialogVisible(true)}
           style={{
             marginRight: 24,
           }}
@@ -53,7 +91,7 @@ export default function WorkoutScreen() {
 
   React.useEffect(() => {
     const onBackPress = () => {
-      setIsCancelWorktoutDialogVisible(true);
+      setIsCancelWorkoutDialogVisible(true);
 
       return true;
     };
@@ -93,6 +131,26 @@ export default function WorkoutScreen() {
           }}
           onPress={() => setIsAddExerciseOverlayVisible(true)}
         />
+        <FAB
+          icon="check"
+          mode="elevated"
+          label="Complete Workout"
+          theme={{
+            colors: {
+              primaryContainer: theme.colors.background,
+              onPrimaryContainer: theme.colors.light,
+            },
+          }}
+          style={{
+            position: 'absolute',
+            margin: 16,
+            left: 0,
+            bottom: 0,
+            borderWidth: 2,
+            borderColor: theme.colors.primary,
+          }}
+          onPress={() => setIsSaveWorkoutDialogVisible(true)}
+        />
       </View>
       <AddExerciseOverlay
         isVisible={isAddExerciseOverlayVisible}
@@ -100,11 +158,19 @@ export default function WorkoutScreen() {
         workoutDataScrollViewRef={workoutDataScrollViewRef}
       />
       <ConfirmationDialog
+        title="Save Workout Template"
+        message="Complete workout building and save this template."
+        confirmButtonLabel="Save Workout Template"
+        isVisible={isSaveWorkoutDialogVisible}
+        handleHideDialog={setIsSaveWorkoutDialogVisible}
+        onConfirmationPress={onSaveWorkoutPress}
+      />
+      <ConfirmationDialog
         title="Cancel Workout Building"
         message="Confirm cancelling workout building. This will discard the current workout."
         confirmButtonLabel="Discard this workout"
-        isVisible={isCancelWorktoutDialogVisible}
-        handleHideDialog={setIsCancelWorktoutDialogVisible}
+        isVisible={isCancelWorkoutDialogVisible}
+        handleHideDialog={setIsCancelWorkoutDialogVisible}
         onConfirmationPress={onCancelWorkoutPress}
       />
     </View>
