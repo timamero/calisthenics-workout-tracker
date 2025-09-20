28a4b488ab8846966da2eb8b2a1316a4174c1526
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-19 23:22:12 -0700

    CWT-143 Create EmptyWorkoutPlaceholder component

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutData.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutData.tsx
@@ -1,29 +1,20 @@
 import { Stack, Text } from '@mantine/core';
 
-// import { useStore } from '@cwt/state/store';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
 import WorkoutExercise from './WorkoutExercise';
+import EmptyWorkoutPlaceholder from './EmptyWorkoutPlaceholder';
 
 export default function WorkoutData() {
   const workoutData = useWorkoutDraftStore((state) => state.workoutData);
 
+  const workoutExercisesLength = useWorkoutDraftStore(
+    (state) => state.workoutData,
+  ).exercises.length;
+
   if (!workoutData) {
     return <Text>Loading</Text>;
   }
-
-  const EmptyWorkoutPlaceholder = () => {
-    if (workoutData.exercises.length === 0) {
-      return (
-        <Stack align="center" bd="2px dashed gray.6" p="lg">
-          <Text size="lg" fw={800}>
-            Ready to start building your workout?
-          </Text>
-          <Text c="gray.8">Add your first exercise to begin</Text>
-        </Stack>
-      );
-    }
-  };
 
   const workoutExercises = workoutData.exercises.map((ex, i) => {
     return (
@@ -32,7 +23,7 @@ export default function WorkoutData() {
   });
   return (
     <Stack gap="xl" align="center">
-      <EmptyWorkoutPlaceholder />
+      {workoutExercisesLength === 0 && <EmptyWorkoutPlaceholder />}
       {workoutExercises}
     </Stack>
   );
