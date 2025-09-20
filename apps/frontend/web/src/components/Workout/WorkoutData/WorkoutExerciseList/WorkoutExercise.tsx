9a96fd76c09b041e8a4b22603185f0a72cb5cdfd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-20 00:05:01 -0700

    CWT-143 Move overlay to parent and create exercise context

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise.tsx
@@ -1,30 +1,46 @@
+import * as React from 'react';
 import { Stack, Text, Button, Group } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+// import { useDisclosure } from '@mantine/hooks';
 
 import { useExerciseLibraryStore } from '@cwt/state/stores';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 import type { WorkoutExercise } from '@cwt/schema/workouts';
 
+import { WorkoutExerciseContext } from '../../../../contexts/WorkoutExerciseContext';
 import Sets from './Sets';
-import ConfirmationOverlay from '../../../common/ConfirmationOverlay';
+// import ConfirmationOverlay from '../../../common/ConfirmationOverlay';
 
-interface WorkoutExerciseProps {
-  exerciseIndex: number;
-  workoutExercise: WorkoutExercise;
-}
+// interface WorkoutExerciseProps {
+//   exerciseIndex: number;
+//   workoutExercise: WorkoutExercise;
+// }
 
-export default function WorkoutExercise({
-  exerciseIndex,
-  workoutExercise,
-}: WorkoutExerciseProps) {
+export default function WorkoutExercise() {
+  const exerciseIndex: number = React.useContext(
+    WorkoutExerciseContext,
+  )!.exerciseIndex;
+  const workoutExercise: WorkoutExercise = React.useContext(
+    WorkoutExerciseContext,
+  )!.workoutExercise;
+  const deleteExOverlayHandler = React.useContext(
+    WorkoutExerciseContext,
+  )!.deleteExOverlayHandler;
+
   const addSet = useWorkoutDraftStore((state) => state.addSet);
-  const deleteExercise = useWorkoutDraftStore((state) => state.removeExercise);
+  const setSelectedExerciseIndexToDel = useWorkoutDraftStore(
+    (state) => state.setSelectedExerciseIndexToMod,
+  );
+  // const deleteExercise = useWorkoutDraftStore((state) => state.removeExercise);
   const getExerciseNameById = useExerciseLibraryStore(
     (state) => state.getExerciseNameByID,
   );
   const name = getExerciseNameById(workoutExercise.exercise_id);
 
-  const [deleteExOverlayOpened, deleteExOverlayHandler] = useDisclosure(false);
+  // const [deleteExOverlayOpened, deleteExOverlayHandler] = useDisclosure(false);
+  const handleDeleteExerciseClick = () => {
+    setSelectedExerciseIndexToDel(exerciseIndex);
+    deleteExOverlayHandler.open();
+  };
 
   return (
     <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
@@ -33,7 +49,7 @@ export default function WorkoutExercise({
         <Button
           color="red"
           variant="white"
-          onClick={() => deleteExOverlayHandler.open()}
+          onClick={() => handleDeleteExerciseClick()}
         >
           Delete
         </Button>
@@ -50,14 +66,14 @@ export default function WorkoutExercise({
       >
         Add Set
       </Button>
-      <ConfirmationOverlay
+      {/* <ConfirmationOverlay
         title="Delete Exercise"
         message="Delete exercise from this workout?"
         confirmButtonLabel="Delete exercise"
         opened={deleteExOverlayOpened}
         handler={deleteExOverlayHandler}
         onConfirmationClick={() => deleteExercise(exerciseIndex)}
-      />
+      /> */}
     </Stack>
   );
 }
