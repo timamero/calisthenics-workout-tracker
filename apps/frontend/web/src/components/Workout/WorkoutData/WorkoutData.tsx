a12812d3a32b237b5c3ee8ef0c7f9019089a8d09
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-20 00:05:01 -0700

    CWT-143 Move overlay to parent and create exercise context

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutData.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutData.tsx
@@ -1,17 +1,26 @@
 import { Stack, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
-// import WorkoutExercise from './WorkoutExercise';
 import EmptyWorkoutPlaceholder from './EmptyWorkoutPlaceholder';
 import WorkoutExerciseList from './WorkoutExerciseList';
+import ConfirmationOverlay from '../../common/ConfirmationOverlay';
 
 export default function WorkoutData() {
+  const [deleteExOverlayOpened, deleteExOverlayHandler] = useDisclosure(false);
+
   const workoutData = useWorkoutDraftStore((state) => state.workoutData);
 
   const workoutExercisesLength = useWorkoutDraftStore(
     (state) => state.workoutData,
   ).exercises.length;
+  const selectedExerciseIndexToMod = useWorkoutDraftStore(
+    (state) => state.selectedExerciseIndexToMod,
+  );
+
+  // const deleteSet = useWorkoutDraftStore((state) => state.deleteSet);
+  const deleteExercise = useWorkoutDraftStore((state) => state.removeExercise);
 
   if (!workoutData) {
     return <Text>Loading</Text>;
@@ -26,7 +35,15 @@ export default function WorkoutData() {
     <Stack gap="xl" align="center">
       {workoutExercisesLength === 0 && <EmptyWorkoutPlaceholder />}
       {/* {workoutExercises} */}
-      <WorkoutExerciseList />
+      <WorkoutExerciseList deleteExOverlayHandler={deleteExOverlayHandler} />
+      <ConfirmationOverlay
+        title="Delete Exercise"
+        message="Delete exercise from this workout?"
+        confirmButtonLabel="Delete exercise"
+        opened={deleteExOverlayOpened}
+        handler={deleteExOverlayHandler}
+        onConfirmationClick={() => deleteExercise(selectedExerciseIndexToMod!)}
+      />
     </Stack>
   );
 }
--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExerciseList.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExerciseList.tsx
@@ -1,13 +1,31 @@
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
+import type { UseDisclosureHandlers } from '@mantine/hooks';
+
+import { WorkoutExerciseContext } from '../../../../contexts/WorkoutExerciseContext';
 import WorkoutExercise from './WorkoutExercise';
 
-export default function WorkoutExerciseList() {
+interface WorkoutExerciseListProps {
+  deleteExOverlayHandler: UseDisclosureHandlers;
+}
+
+export default function WorkoutExerciseList({
+  deleteExOverlayHandler,
+}: WorkoutExerciseListProps) {
   const workoutData = useWorkoutDraftStore((state) => state.workoutData);
 
   const workoutExercises = workoutData.exercises.map((ex, i) => {
     return (
-      <WorkoutExercise key={ex.id} workoutExercise={ex} exerciseIndex={i} />
+      <WorkoutExerciseContext.Provider
+        key={ex.id}
+        value={{
+          workoutExercise: ex,
+          exerciseIndex: i,
+          deleteExOverlayHandler: deleteExOverlayHandler,
+        }}
+      >
+        <WorkoutExercise />
+      </WorkoutExerciseContext.Provider>
     );
   });
   return <>{workoutExercises}</>;
