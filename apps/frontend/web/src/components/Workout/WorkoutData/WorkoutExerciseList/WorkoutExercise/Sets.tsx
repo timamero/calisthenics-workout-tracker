f952aba718fa86d047c388965acc356ff5da07b2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-22 13:15:12 -0700

    CWT-143 Organize files

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise/Sets.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise/Sets.tsx
@@ -1,116 +1,116 @@
-import { Stack, Text, Group, Button } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+// import { Stack, Text, Group, Button } from '@mantine/core';
+// import { useDisclosure } from '@mantine/hooks';
 
-import type { WorkoutExercise } from '@cwt/schema/workouts';
-// import { useStore } from '@cwt/state/store';
-import { useWorkoutDraftStore } from '@cwt/state/stores';
+// import type { WorkoutExercise } from '@cwt/schema/workouts';
+// // import { useStore } from '@cwt/state/store';
+// import { useWorkoutDraftStore } from '@cwt/state/stores';
 
-import RepField from './RepField';
-import DurationField from './DurationField';
-import ConfirmationOverlay from '../../../../common/ConfirmationOverlay';
+// import RepField from './RepField';
+// import DurationField from './DurationField';
+// import ConfirmationOverlay from '../../../../common/ConfirmationOverlay';
 
-export default function Sets({
-  tracked,
-  sets,
-  exerciseIndex,
-}: Pick<WorkoutExercise, 'sets' | 'tracked'> & { exerciseIndex: number }) {
-  const setSelectedSetIndexToMod = useWorkoutDraftStore(
-    (state) => state.setSelectedSetIndexToMod,
-  );
-  const deleteSet = useWorkoutDraftStore((state) => state.deleteSet);
-  const updateField = useWorkoutDraftStore((state) => state.updateField);
+// export default function Sets({
+//   tracked,
+//   sets,
+//   exerciseIndex,
+// }: Pick<WorkoutExercise, 'sets' | 'tracked'> & { exerciseIndex: number }) {
+//   const setSelectedSetIndexToMod = useWorkoutDraftStore(
+//     (state) => state.setSelectedSetIndexToMod,
+//   );
+//   const deleteSet = useWorkoutDraftStore((state) => state.deleteSet);
+//   const updateField = useWorkoutDraftStore((state) => state.updateField);
 
-  const [deleteSetOverlayOpened, deleteSetOverlayHandler] =
-    useDisclosure(false);
+//   const [deleteSetOverlayOpened, deleteSetOverlayHandler] =
+//     useDisclosure(false);
 
-  const handleSetFieldChange = (
-    setIndex: number,
-    updatedField: {
-      reps?: number | undefined;
-      weight?: number | undefined;
-      time?: string | undefined;
-      rest?: string | undefined;
-    },
-  ) => {
-    setSelectedSetIndexToMod(setIndex);
-    updateField(exerciseIndex, updatedField);
-  };
+//   const handleSetFieldChange = (
+//     setIndex: number,
+//     updatedField: {
+//       reps?: number | undefined;
+//       weight?: number | undefined;
+//       time?: string | undefined;
+//       rest?: string | undefined;
+//     },
+//   ) => {
+//     setSelectedSetIndexToMod(setIndex);
+//     updateField(exerciseIndex, updatedField);
+//   };
 
-  const workoutSets = sets.map((set, i) => {
-    const fields = tracked.map((field) => {
-      if (field === 'reps') {
-        return (
-          <Stack key={set.id}>
-            <RepField
-              index={i}
-              value={set.fields.reps!}
-              handleSetFieldChange={handleSetFieldChange}
-            />
-            <DurationField
-              index={i}
-              value={set.fields.rest!}
-              fieldName="rest"
-              handleSetFieldChange={handleSetFieldChange}
-              label="Rest"
-            />
-          </Stack>
-        );
-      }
-      if (field === 'time') {
-        return (
-          <Stack key={set.id}>
-            <DurationField
-              index={i}
-              value={set.fields.time!}
-              fieldName="time"
-              handleSetFieldChange={handleSetFieldChange}
-              label="Time"
-            />
-            <DurationField
-              index={i}
-              value={set.fields.rest!}
-              fieldName="rest"
-              handleSetFieldChange={handleSetFieldChange}
-              label="Rest"
-            />
-          </Stack>
-        );
-      }
-      return <></>;
-    });
+//   const workoutSets = sets.map((set, i) => {
+//     const fields = tracked.map((field) => {
+//       if (field === 'reps') {
+//         return (
+//           <Stack key={set.id}>
+//             <RepField
+//               index={i}
+//               value={set.fields.reps!}
+//               handleSetFieldChange={handleSetFieldChange}
+//             />
+//             <DurationField
+//               index={i}
+//               value={set.fields.rest!}
+//               fieldName="rest"
+//               handleSetFieldChange={handleSetFieldChange}
+//               label="Rest"
+//             />
+//           </Stack>
+//         );
+//       }
+//       if (field === 'time') {
+//         return (
+//           <Stack key={set.id}>
+//             <DurationField
+//               index={i}
+//               value={set.fields.time!}
+//               fieldName="time"
+//               handleSetFieldChange={handleSetFieldChange}
+//               label="Time"
+//             />
+//             <DurationField
+//               index={i}
+//               value={set.fields.rest!}
+//               fieldName="rest"
+//               handleSetFieldChange={handleSetFieldChange}
+//               label="Rest"
+//             />
+//           </Stack>
+//         );
+//       }
+//       return <></>;
+//     });
 
-    const handleDeleteSetOpen = (i: number) => {
-      setSelectedSetIndexToMod(i);
-      deleteSetOverlayHandler.open();
-    };
-    return (
-      <Stack key={`set-${i}`} bg="gray.1">
-        <Group>
-          <Text>{`Set ${i + 1}`}</Text>
-          <Button
-            color="red"
-            variant="white"
-            onClick={() => handleDeleteSetOpen(i)}
-          >
-            Delete
-          </Button>
-        </Group>
-        {fields}
-      </Stack>
-    );
-  });
+//     const handleDeleteSetOpen = (i: number) => {
+//       setSelectedSetIndexToMod(i);
+//       deleteSetOverlayHandler.open();
+//     };
+//     return (
+//       <Stack key={`set-${i}`} bg="gray.1">
+//         <Group>
+//           <Text>{`Set ${i + 1}`}</Text>
+//           <Button
+//             color="red"
+//             variant="white"
+//             onClick={() => handleDeleteSetOpen(i)}
+//           >
+//             Delete
+//           </Button>
+//         </Group>
+//         {fields}
+//       </Stack>
+//     );
+//   });
 
-  return (
-    <Stack>
-      {workoutSets}
-      <ConfirmationOverlay
-        title="Delete Set"
-        message="Delete set from this exercise?"
-        confirmButtonLabel="Delete"
-        opened={deleteSetOverlayOpened}
-        handler={deleteSetOverlayHandler}
-        onConfirmationClick={() => deleteSet(exerciseIndex)}
-      />
-    </Stack>
-  );
-}
+//   return (
+//     <Stack>
+//       {workoutSets}
+//       <ConfirmationOverlay
+//         title="Delete Set"
+//         message="Delete set from this exercise?"
+//         confirmButtonLabel="Delete"
+//         opened={deleteSetOverlayOpened}
+//         handler={deleteSetOverlayHandler}
+//         onConfirmationClick={() => deleteSet(exerciseIndex)}
+//       />
+//     </Stack>
+//   );
+// }
