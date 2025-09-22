2106ad830577ee0b58cd6112fc4e984591bfaa8a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-22 13:13:08 -0700

    CWT-143 Create SetList and SetContext

---
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise/SetList/SetList.tsx
@@ -0,0 +1,44 @@
+import type { SetFields } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+
+import { SetContext } from '../../../../../../contexts/SetContext';
+
+interface SetListProps {
+  exerciseIndex: number;
+}
+
+export default function SetList({ exerciseIndex }: SetListProps) {
+  const sets = useWorkoutDraftStore((state) => state.workoutData).exercises[
+    exerciseIndex
+  ].sets;
+
+  const setSelectedSetIndexToMod = useWorkoutDraftStore(
+    (state) => state.setSelectedSetIndexToMod,
+  );
+  const updateField = useWorkoutDraftStore((state) => state.updateField);
+
+  const handleSetFieldChange = (
+    setIndex: number,
+    updatedField: Partial<SetFields>,
+  ) => {
+    setSelectedSetIndexToMod(setIndex);
+    updateField(exerciseIndex, updatedField);
+  };
+
+  const setList = sets.map((set, i) => {
+    return (
+      <SetContext.Provider
+        key={set.id}
+        value={{
+          setIndex: i,
+          set: set,
+          handleSetFieldChange: handleSetFieldChange,
+        }}
+      >
+        {/* <SetContainer key={set.id} /> */}
+        <div>placeholder</div>
+      </SetContext.Provider>
+    );
+  });
+  return <>{setList}</>;
+}
