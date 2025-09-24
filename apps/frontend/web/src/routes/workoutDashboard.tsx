3edbc9c42b7bf4762531b28e00f69a1218d4487a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-24 00:01:34 -0700

    CWT-143 Display workout logs in dashboard

--- apps/frontend/web/src/routes/workoutDashboard.tsx
+++ apps/frontend/web/src/routes/workoutDashboard.tsx
@@ -7,8 +7,12 @@ import {
   useWorkoutLibraryStore,
   useAuthStore,
 } from '@cwt/state/stores';
+import type {
+  WorkoutBuildResponse,
+  WorkoutLogResponse,
+} from '@cwt/schema/workouts';
 
-import { getWorkoutBuilds } from '../services/workoutsService';
+import { getWorkoutBuilds, getWorkoutLogs } from '../services/workoutsService';
 
 import CardButton from '../components/common/CardButton';
 import LargeButton from '../components/common/LargeButton';
@@ -17,15 +21,23 @@ export const Route = createFileRoute('/workoutDashboard')({
   loader: async () => {
     const displayedWorkoutBuilds =
       useWorkoutLibraryStore.getState().displayedWorkoutBuilds;
-    if (displayedWorkoutBuilds && displayedWorkoutBuilds.length > 0) {
-      return displayedWorkoutBuilds;
+    const displayedWorkoutLogs =
+      useWorkoutLibraryStore.getState().displayedWorkoutLogs;
+    if (
+      displayedWorkoutBuilds &&
+      displayedWorkoutBuilds.length > 0 &&
+      displayedWorkoutLogs &&
+      displayedWorkoutLogs.length > 0
+    ) {
+      return { logs: displayedWorkoutLogs, builds: displayedWorkoutBuilds };
     }
     const supabaseSession = useAuthStore.getState().session;
     if (supabaseSession?.access_token) {
       const workoutBuilds = await getWorkoutBuilds(
         supabaseSession.access_token,
       );
-      return workoutBuilds;
+      const workoutLogs = await getWorkoutLogs(supabaseSession.access_token);
+      return { logs: workoutLogs, builds: workoutBuilds };
     }
     return [];
   },
@@ -33,14 +45,17 @@ export const Route = createFileRoute('/workoutDashboard')({
 });
 
 function WorkoutDashboardView() {
-  const workoutBuilds = Route.useLoaderData();
+  const workouts: {
+    logs: WorkoutLogResponse[];
+    builds: WorkoutBuildResponse[];
+  } = Route.useLoaderData();
 
   const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
 
   // On initial load, set the workout builds in the store
   useEffect(() => {
-    setWorkouts([], workoutBuilds);
-  }, [setWorkouts, workoutBuilds]);
+    setWorkouts(workouts.logs, workouts.builds);
+  }, [workouts, setWorkouts]);
 
   const initializeWorkout = useWorkoutDraftStore(
     (state) => state.initializeWorkout,
@@ -53,7 +68,17 @@ function WorkoutDashboardView() {
     initializeWorkout('edit');
   };
 
-  const workoutBuildCards = workoutBuilds.map((wo, i) => {
+  const workoutBuildCards = workouts.builds.map((wo, i) => {
+    const workoutTitle = wo.title ? wo.title : `Workout Template ${i + 1}`;
+    return (
+      <CardButton key={i}>
+        <Title order={3} size="h5">
+          {workoutTitle}
+        </Title>
+      </CardButton>
+    );
+  });
+  const workoutLogCards = workouts.logs.map((wo, i) => {
     const workoutTitle = wo.title ? wo.title : `Workout Template ${i + 1}`;
     return (
       <CardButton key={i}>
@@ -72,8 +97,13 @@ function WorkoutDashboardView() {
       <LargeButton to="/workout" onButtonClick={handleCreateWorkoutLogClick}>
         <Text>Start Blank Workout</Text>
       </LargeButton>
+      <Text>Start Workout From Template</Text>
       <ScrollArea>
         <Group wrap="nowrap">{workoutBuildCards}</Group>
+      </ScrollArea>
+      <Text>Start Workout From Recent Workout</Text>
+      <ScrollArea>
+        <Group wrap="nowrap">{workoutLogCards}</Group>
       </ScrollArea>
     </Stack>
   );
