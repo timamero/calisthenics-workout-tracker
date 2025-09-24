521f4d6d22e335c0e890e4fa724f6da9d4a7e924
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-24 11:55:41 -0700

    CWT-146 Create request and response workout log schemas

--- apps/backend/app/schemas/workout.py
+++ apps/backend/app/schemas/workout.py
@@ -1,5 +1,5 @@
 from typing import List, Optional, Literal
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 from uuid import UUID
 
 from pydantic import BaseModel, Field
@@ -58,21 +58,54 @@ class WorkoutBuildResponseSchema(BaseModel):
     updated_at: Optional[datetime]
 
 
-class BaseWorkoutSchema(BaseModel):
-    id: int
-    created_at: datetime
-    updated_at: Optional[datetime]
+class WorkoutLogRequestSchema(BaseModel):
+    workout_build_id: Optional[int]
+    date: datetime
     title: Optional[str] = Field(None, max_length=70)
     description: Optional[str] = Field(None, max_length=500)
+    duration: Optional[timedelta]
     workout_data: WorkoutDataSchema
+    rpe: Optional[int] = Field(None, le=10)
+    notes: Optional[str] = Field(None, max_length=750)
     status: Literal["draft", "finalized", "archived"]
     goal: Optional[Literal["function", "endurance", "hypertrophy", "strength", "power"]]
-    notes: Optional[str] = Field(None, max_length=750)
-
-
-class WorkoutLogSchema(BaseWorkoutSchema):
+    updated_at: Optional[datetime]
     user_id: UUID
-    workout_build_id: int
-    date: date
-    duration: timedelta
+
+
+class WorkoutLogResponseSchema(BaseModel):
+    id: int
+    created_at: datetime
+    workout_build_id: Optional[int]
+    date: datetime
+    title: Optional[str] = Field(None, max_length=70)
+    description: Optional[str] = Field(None, max_length=500)
+    duration: Optional[timedelta]
+    workout_data: WorkoutDataSchema
     rpe: Optional[int] = Field(None, le=10)
+    notes: Optional[str] = Field(None, max_length=750)
+    status: Literal["draft", "finalized", "archived"]
+    goal: Optional[Literal["function", "endurance", "hypertrophy", "strength", "power"]]
+    updated_at: Optional[datetime]
+    user_id: UUID
+
+
+# class BaseWorkoutSchema(BaseModel):
+#     id: int
+#     created_at: datetime
+#     updated_at: Optional[datetime]
+#     title: Optional[str] = Field(None, max_length=70)
+#     description: Optional[str] = Field(None, max_length=500)
+#     workout_data: WorkoutDataSchema
+#     status: Literal["draft", "finalized", "archived"]
+#     goal:
+#       Optional[Literal["function", "endurance", "hypertrophy", "strength", "power"]]
+#     notes: Optional[str] = Field(None, max_length=750)
+
+
+# class WorkoutLogSchema(BaseWorkoutSchema):
+#     user_id: UUID
+#     workout_build_id: int
+#     date: date
+#     duration: timedelta
+#     rpe: Optional[int] = Field(None, le=10)
