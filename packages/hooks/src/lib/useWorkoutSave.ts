5af8e975a3b040104c8fa5132526af41479e4180
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-24 03:29:53 -0700

    cwt-143 Update hook to add duration to workoutToSave

--- packages/hooks/src/lib/useWorkoutSave.ts
+++ packages/hooks/src/lib/useWorkoutSave.ts
@@ -1,4 +1,7 @@
-import { useWorkoutDraftStore } from '@cwt/state/stores';
+import {
+  useWorkoutDraftStore,
+  useWorkoutStopwatchStore,
+} from '@cwt/state/stores';
 import { useAuthStore } from '@cwt/state/stores';
 
 export function useWorkoutSave() {
@@ -13,6 +16,9 @@ export function useWorkoutSave() {
   const addUserIDToWorkoutToSave = useWorkoutDraftStore(
     (state) => state.addUserIDToWorkoutToSave
   );
+  const addDurationToWorkoutSave = useWorkoutDraftStore(
+    (state) => state.addDurationToWorkoutToSave
+  );
 
   const setWorkoutToSaveWithUser = () => {
     initializeWorkoutToSave();
@@ -22,5 +28,35 @@ export function useWorkoutSave() {
     }
   };
 
-  return { setWorkoutToSaveWithUser };
+  const setWorkoutToSaveWithUserAndDuration = () => {
+    initializeWorkoutToSave();
+    const workoutToSave = useWorkoutDraftStore.getState().workoutToSave;
+    const durationInMilliseconds = useWorkoutStopwatchStore
+      .getState()
+      .getTime();
+    const durationInISO = msToIsoDuration(durationInMilliseconds);
+    if (workoutToSave && user?.id) {
+      addUserIDToWorkoutToSave(user.id);
+    }
+    if (workoutToSave && durationInISO) {
+      addDurationToWorkoutSave(durationInISO);
+    }
+  };
+
+  return { setWorkoutToSaveWithUser, setWorkoutToSaveWithUserAndDuration };
+}
+
+function msToIsoDuration(ms: number): string {
+  let seconds = Math.floor(ms / 1000);
+  const hours = Math.floor(seconds / 3600);
+  seconds %= 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+
+  let duration = 'PT';
+  if (hours > 0) duration += hours + 'H';
+  if (minutes > 0) duration += minutes + 'M';
+  if (seconds > 0 || duration === 'PT') duration += seconds + 'S';
+
+  return duration;
 }
