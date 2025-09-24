98019b7e5e7cc6dbfcc79e10e163c1e50259079f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-24 03:01:45 -0700

    CWT-143 Export stopwatch store

--- packages/state/src/lib/stores/index.ts
+++ packages/state/src/lib/stores/index.ts
@@ -1,5 +1,6 @@
 import { useWorkoutDraftStore } from './workouts/workoutDraftStore';
 import { useWorkoutLibraryStore } from './workouts/workoutLibraryStore';
+import { useWorkoutStopwatchStore } from './workouts/workoutStopWatchStore';
 import { useExerciseLibraryStore } from './exercises/exerciseLibraryStore';
 import { useExercisesFilterStore } from './exercises/exercisesFilterStore';
 import { useExercisesSearchStore } from './exercises/exercisesSearchStore';
@@ -8,6 +9,7 @@ import { useAuthStore } from './authStore';
 export {
   useWorkoutDraftStore,
   useWorkoutLibraryStore,
+  useWorkoutStopwatchStore,
   useExerciseLibraryStore,
   useExercisesFilterStore,
   useExercisesSearchStore,
