fdac3b77d2c717acfc9aef54cff833fe6f99cc42
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-09-28 22:41:18 -0700

    CWT-145 Clean up code

--- apps/frontend/mobile/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise/SetList/Set.tsx
+++ apps/frontend/mobile/components/Workout/WorkoutData/WorkoutExerciseList/WorkoutExercise/SetList/Set.tsx
@@ -1,4 +1,4 @@
-import { NativeMethods, View } from 'react-native';
+import { View } from 'react-native';
 import { useTheme, Button, Checkbox } from 'react-native-paper';
 
 import { Mode } from '@cwt/schema/workouts';
