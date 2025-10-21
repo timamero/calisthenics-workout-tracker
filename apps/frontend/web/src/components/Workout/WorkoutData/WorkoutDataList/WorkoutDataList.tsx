15ae70e55e4fd6040d8448510054e948190e6daa
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-21 01:12:56 -0700

    CWT-230 Update workoutDataList and workoutDataContext

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataList.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataList.tsx
@@ -1,34 +1,33 @@
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
-// import type { UseDisclosureHandlers } from '@mantine/hooks';
+import type { UseDisclosureHandlers } from '@mantine/hooks';
 
-// import { WorkoutDataContext } from '../../../../contexts/WorkoutDataContext';
+import { WorkoutDataContext } from '../../../../contexts/WorkoutDataContext';
 // import { WorkoutDataContainer as WorkoutData } from './WorkoutData';
 
-// interface WorkoutExerciseListProps {
-//   deleteExOverlayHandler: UseDisclosureHandlers;
-//   deleteSetOverlayHandler: UseDisclosureHandlers;
-// }
+interface WorkoutDataListProps {
+  deleteItemOverlayHandler: UseDisclosureHandlers;
+  deleteSetOverlayHandler: UseDisclosureHandlers;
+}
 
-export default function WorkoutDataList() {
+export default function WorkoutDataList({
+  deleteItemOverlayHandler,
+  deleteSetOverlayHandler,
+}: WorkoutDataListProps) {
   const workoutData = useWorkoutDraftStore((state) => state.workoutData);
-  const workoutExercises = workoutData.map((ex) => {
-    // console.log('exercise: ', JSON.stringify(ex));
-    if (ex.type !== 'exercise') return null;
-    // console.log('return exercises');
+  const workoutDataItems = workoutData.map((item) => {
     return (
-      // <WorkoutExerciseContext.Provider
-      //   key={ex.id}
-      //   value={{
-      //     workoutExercise: ex,
-      //     exerciseIndex: i,
-      //     deleteExOverlayHandler: deleteExOverlayHandler,
-      //     deleteSetOverlayHandler: deleteSetOverlayHandler,
-      //   }}
-      // >
-      <div>workout data placeholder</div>
-      // </WorkoutExerciseContext.Provider>
+      <WorkoutDataContext.Provider
+        key={item.id}
+        value={{
+          item: item,
+          deleteItemOverlayHandler: deleteItemOverlayHandler,
+          deleteSetOverlayHandler: deleteSetOverlayHandler,
+        }}
+      >
+        <div>workout data placeholder</div>
+      </WorkoutDataContext.Provider>
     );
   });
-  return <>{workoutExercises}</>;
+  return <>{workoutDataItems}</>;
 }
