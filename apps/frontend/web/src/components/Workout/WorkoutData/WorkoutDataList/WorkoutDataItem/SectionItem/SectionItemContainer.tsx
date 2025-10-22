daaff6c5482eba741cf3b9320cef79f314ca114a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-21 20:12:15 -0700

    CWT-230 Update section component

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemContainer.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemContainer.tsx
@@ -1,5 +1,22 @@
-import SectionItem from "./SectionItem";
+import * as React from 'react';
+
+import type { Section } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+
+import { WorkoutDataContext } from '../../../../../../contexts/WorkoutDataContext';
+import SectionItem from './SectionItem';
 
 export default function SectionItemContainer() {
-  return <SectionItem />
+  const section = React.useContext(WorkoutDataContext)!.item as Section;
+    const deleteRootItemOverlayHandler =
+      React.useContext(WorkoutDataContext)!.deleteRootItemOverlayHandler;
+
+  const mode = useWorkoutDraftStore((state) => state.mode);
+  const  setSectionIDToMod = useWorkoutDraftStore((state) => state.setSectionIDToMod)
+
+  const handleDeleteSectionClick = () => {
+    setSectionIDToMod(section.id);
+    deleteRootItemOverlayHandler.open();
+  };
+  return <SectionItem mode={mode!} handleDeleteSectionClick={handleDeleteSectionClick} />;
 }
