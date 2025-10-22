2fcefad18ca74d317aff33e12db4fae8deaeaf9c
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-21 20:12:15 -0700

    CWT-230 Update section component

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItem.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItem.tsx
@@ -1,3 +1,31 @@
-export default function SectionItem() {
-  return <div>section item</div>;
+import { Stack, Group, Text, Button } from '@mantine/core';
+
+import type { Mode } from '@cwt/schema/workouts';
+
+interface SectionItemProps {
+  mode: Mode;
+  handleDeleteSectionClick: () => void;
+}
+
+export default function SectionItem({
+  mode,
+  handleDeleteSectionClick,
+}: SectionItemProps) {
+  return (
+    <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
+      <Group>
+        <Text>Section</Text>
+        {mode !== 'log' && (
+          <Button
+            color="red"
+            variant="white"
+            onClick={() => handleDeleteSectionClick()}
+          >
+            Delete
+          </Button>
+        )}
+      </Group>
+      <Text>Exercises and Supersets placeholder</Text>
+    </Stack>
+  );
 }
