5286bd8cd5ab72efd0d4f2a489beb13d466ae6f2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-22 22:36:55 -0700

    CWT-230 Add reordering buttons to exercise item

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/ExerciseItem.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/ExerciseItem.tsx
@@ -1,4 +1,5 @@
 import { Stack, Text, Button, Group } from '@mantine/core';
+import { LuSquareArrowUp, LuSquareArrowDown } from 'react-icons/lu';
 
 import type { Mode } from '@cwt/schema/workouts';
 import SetList from './SetList';
@@ -17,29 +18,39 @@ export default function ExerciseItem({
   handleDeleteExerciseClick,
 }: ExerciseItemProps) {
   return (
-    <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
-      <Group>
-        <Text>{name}</Text>
+    <Group align="flex-start">
+      <Button.Group orientation="vertical">
+        <Button variant="transparent" p={1} my={2}>
+          <LuSquareArrowUp size={24} />
+        </Button>
+        <Button variant="transparent" p={1} my={2}>
+          <LuSquareArrowDown size={24} />
+        </Button>
+      </Button.Group>
+      <Stack bd="1px solid var(--mantine-color-default-border)" p="lg">
+        <Group>
+          <Text>{name}</Text>
+          {mode !== 'log' && (
+            <Button
+              color="red"
+              variant="white"
+              onClick={() => handleDeleteExerciseClick()}
+            >
+              Delete
+            </Button>
+          )}
+        </Group>
+        <SetList />
         {mode !== 'log' && (
           <Button
-            color="red"
-            variant="white"
-            onClick={() => handleDeleteExerciseClick()}
+            variant="outline"
+            color="dark"
+            onClick={() => handleAddSetClick()}
           >
-            Delete
+            Add Set
           </Button>
         )}
-      </Group>
-      <SetList />
-      {mode !== 'log' && (
-        <Button
-          variant="outline"
-          color="dark"
-          onClick={() => handleAddSetClick()}
-        >
-          Add Set
-        </Button>
-      )}
-    </Stack>
+      </Stack>
+    </Group>
   );
 }
