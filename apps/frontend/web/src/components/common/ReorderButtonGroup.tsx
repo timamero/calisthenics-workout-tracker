2aacd35f17aa35a08270cb6a888b8013fac1b0c4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-23 12:17:14 -0700

    CWT-230 Conditionally display up or down button based on order

--- apps/frontend/web/src/components/common/ReorderButtonGroup.tsx
+++ apps/frontend/web/src/components/common/ReorderButtonGroup.tsx
@@ -2,27 +2,35 @@ import { Button } from '@mantine/core';
 import { LuSquareArrowUp, LuSquareArrowDown } from 'react-icons/lu';
 
 interface ReorderButtonProps {
+  isFirst: boolean;
+  isLast: boolean;
   handleUpClick: () => void;
   handleDownClick: () => void;
 }
 
 export default function ReorderButtonGroup({
+  isFirst = false,
+  isLast = false,
   handleUpClick,
   handleDownClick,
 }: ReorderButtonProps) {
   return (
     <Button.Group orientation="vertical">
-      <Button
-        onClick={() => handleUpClick()}
-        variant="transparent"
-        p={1}
-        my={2}
-      >
-        <LuSquareArrowUp size={24} />
-      </Button>
-      <Button onClick={handleDownClick} variant="transparent" p={1} my={2}>
-        <LuSquareArrowDown size={24} />
-      </Button>
+      {!isFirst && (
+        <Button
+          onClick={() => handleUpClick()}
+          variant="transparent"
+          p={1}
+          my={2}
+        >
+          <LuSquareArrowUp size={24} />
+        </Button>
+      )}
+      {!isLast && (
+        <Button onClick={handleDownClick} variant="transparent" p={1} my={2}>
+          <LuSquareArrowDown size={24} />
+        </Button>
+      )}
     </Button.Group>
   );
 }
