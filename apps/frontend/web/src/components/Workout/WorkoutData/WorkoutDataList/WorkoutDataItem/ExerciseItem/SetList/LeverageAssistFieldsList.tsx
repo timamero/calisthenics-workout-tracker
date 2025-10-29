4d1077f301fd126a12c105f43216211d4383eaf6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-29 14:40:12 -0700

    CWT-230 Display int leverage field

---
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/LeverageAssistFieldsList.tsx
@@ -0,0 +1,53 @@
+import { useContext } from 'react';
+import { Group } from '@mantine/core';
+
+import type { Exercise } from '@cwt/schema/workouts';
+
+import { WorkoutDataItemContext } from '../../../../../../../contexts/WorkoutDataItemContext';
+import { SetContext } from '../../../../../../../contexts/SetContextUpdated';
+import NumeralInput from './NumeralInput';
+import { useLeveragesAssistsStore } from '@cwt/state/stores';
+
+export default function LeverageAssistFieldsList() {
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const tracked = exercise.tracked;
+  const set = useContext(SetContext)?.set;
+
+  const getLeverageOrAssistByID = useLeveragesAssistsStore(
+    (state) => state.getLeverageOrAssistByID,
+  );
+
+  let leverageFields;
+
+  if (tracked.includes('leverages')) {
+    leverageFields = set?.fields.leverages!.map((leverageField) => {
+      const leverage = getLeverageOrAssistByID(
+        leverageField.leverages_assists_id,
+      );
+
+      if (leverage.value_type === 'int') {
+        return (
+          <NumeralInput
+            key={leverageField.id}
+            label={`${leverage.name} (${leverage.value_int_unit})`}
+            fieldName="leverages"
+          />
+        );
+      } else {
+        return <div>options placeholder</div>;
+      }
+    });
+  }
+
+  // const fields = tracked.map((field, i) => {
+  //   switch (field) {
+  //     case 'reps':
+  //       return <NumeralInput key={`${field}-${i}`} label="reps" />;
+  //     case 'time':
+  //       return <DurationInput key={`${field}-${i}`} label="time" />;
+  //     default:
+  //       return <></>;
+  //   }
+  // });
+  return <Group align="center">{leverageFields}</Group>;
+}
