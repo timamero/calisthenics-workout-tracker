af39454a53a65bba6de0ee419e80dd31cf333632
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-29 23:03:54 -0700

    CWT-230 Fix handing updating field with leverage

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/NumeralInput.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/NumeralInput.tsx
@@ -4,18 +4,31 @@ import { TextInput } from '@mantine/core';
 import type { SetFields } from '@cwt/schema/workouts';
 
 import { SetContext } from '../../../../../../../contexts/SetContextUpdated';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
 
 interface NumeralInputProps {
   label: string;
-  fieldName: 'reps' | 'weight' | 'leverages' | 'assists';
+  fieldName: 'reps' | 'weight' | 'value';
+  fieldID?: string;
 }
 
-export default function NumeralInput({ label, fieldName }: NumeralInputProps) {
+export default function NumeralInput({
+  label,
+  fieldName,
+  fieldID,
+}: NumeralInputProps) {
   const set = React.useContext(SetContext)!.set;
   const handleSetFieldChange =
     React.useContext(SetContext)!.handleSetFieldChange;
 
+  const setLeverageIDToMod = useWorkoutDraftStore(
+    (state) => state.setLeverageIDToMod,
+  );
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (fieldID) {
+      setLeverageIDToMod(fieldID);
+    }
     if (event.currentTarget.value == '') {
       const updatedField: Partial<SetFields> = {
         [fieldName]: undefined,
@@ -29,25 +42,46 @@ export default function NumeralInput({ label, fieldName }: NumeralInputProps) {
     }
   };
 
-  const value =
-    fieldName === 'leverages'
-      ? set.fields.leverages![0].value === null
-        ? ''
-        : set.fields.leverages![0].value!.toString()
-      : set.fields[fieldName] === undefined
-        ? ''
-        : set.fields[fieldName]!.toString();
+  // const value =
+  //   fieldName === 'value'
+  //     ? set.fields.leverages![0].value === null
+  //       ? ''
+  //       : set.fields.leverages![0].value!.toString()
+  //     : set.fields[fieldName] === undefined
+  //       ? ''
+  //       : set.fields[fieldName]!.toString();
+  // console.log('NumeralInput - value: ', value);
+
+  if (fieldName === 'value') {
+    return (
+      <TextInput
+        w={68}
+        label={label}
+        placeholder={'0'}
+        value={
+          set.fields.leverages!.find((field) => field.id === fieldID)!.value ===
+            null ||
+          set.fields.leverages!.find((field) => field.id === fieldID)!.value ===
+            undefined
+            ? ''
+            : set.fields
+                .leverages!.find((field) => field.id === fieldID)!
+                .value!.toString()
+        }
+        onChange={handleChange}
+      />
+    );
+  }
   return (
     <TextInput
       w={68}
       label={label}
       placeholder={'0'}
-      value={value}
-      // value={
-      //   set.fields[fieldName] === undefined
-      //     ? ''
-      //     : set.fields[fieldName]!.toString()
-      // }
+      value={
+        set.fields[fieldName] === undefined
+          ? ''
+          : set.fields[fieldName]!.toString()
+      }
       onChange={handleChange}
     />
   );
