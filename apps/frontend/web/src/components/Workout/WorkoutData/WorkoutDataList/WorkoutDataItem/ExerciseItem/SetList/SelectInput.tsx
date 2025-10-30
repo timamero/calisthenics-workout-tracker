b2dd9750ffddd1ab8699b76ed9b7ebdf1e35634d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-30 01:08:54 -0700

    CWT-230 Fix type

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/SelectInput.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/SelectInput.tsx
@@ -1,57 +1,57 @@
-// import * as React from 'react';
-// import { TextInput } from '@mantine/core';
+import * as React from 'react';
+import { TextInput } from '@mantine/core';
 
-// import type { Leverage } from '@cwt/schema/workouts';
+import type { Leverage } from '@cwt/schema/workouts';
 
-// import { SetContext } from '../../../../../../../contexts/SetContextUpdated';
-// import { useWorkoutDraftStore } from '@cwt/state/stores';
+import { SetContext } from '../../../../../../../contexts/SetContextUpdated';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
 
-// interface NumeralInputProps {
-//   label: string;
-//   // fieldName: 'value';
-//   fieldID?: string;
-// }
+interface NumeralInputProps {
+  label: string;
+  // fieldName: 'value';
+  fieldID?: string;
+}
 
-// export default function SelectInput({ label, fieldID }: NumeralInputProps) {
-//   const set = React.useContext(SetContext)!.set;
-//   const handleSetFieldChange =
-//     React.useContext(SetContext)!.handleSetFieldChange;
+export default function SelectInput({ label, fieldID }: NumeralInputProps) {
+  const set = React.useContext(SetContext)!.set;
+  const handleSetFieldChange =
+    React.useContext(SetContext)!.handleSetFieldChange;
 
-//   const setLeverageIDToMod = useWorkoutDraftStore(
-//     (state) => state.setLeverageIDToMod,
-//   );
+  const setLeverageIDToMod = useWorkoutDraftStore(
+    (state) => state.setLeverageIDToMod,
+  );
 
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     if (fieldID) {
-//       setLeverageIDToMod(fieldID);
-//     }
-//     if (event.currentTarget.value == '') {
-//       const updatedField: Pick<Leverage, 'value'> = {
-//         value: null,
-//       };
-//       handleSetFieldChange(set.id, updatedField);
-//     } else {
-//       const updatedField: Pick<Leverage, 'value'> = {
-//         value: Number(event.currentTarget.value),
-//       };
-//       handleSetFieldChange(set.id, updatedField);
-//     }
-//   };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (fieldID) {
+      setLeverageIDToMod(fieldID);
+    }
+    if (event.currentTarget.value == '') {
+      const updatedField: Pick<Leverage, 'value'> = {
+        value: null,
+      };
+      handleSetFieldChange(set.id, updatedField);
+    } else {
+      const updatedField: Pick<Leverage, 'value'> = {
+        value: Number(event.currentTarget.value),
+      };
+      handleSetFieldChange(set.id, updatedField);
+    }
+  };
 
-//   return (
-//     <TextInput
-//       w={68}
-//       label={label}
-//       placeholder={'0'}
-//       value={
-//         set.fields.leverages!.find((field) => field.id === fieldID)!.value ===
-//         null
-//           ? ''
-//           : set.fields
-//               .leverages!.find((field) => field.id === fieldID)!
-//               .value!.toString()
-//       }
-//       onChange={handleChange}
-//     />
-//   );
-// }
+  return (
+    <TextInput
+      w={68}
+      label={label}
+      placeholder={'0'}
+      value={
+        set.fields.leverages!.find((field) => field.id === fieldID)!.value ===
+        null
+          ? ''
+          : set.fields
+              .leverages!.find((field) => field.id === fieldID)!
+              .value!.toString()
+      }
+      onChange={handleChange}
+    />
+  );
+}
