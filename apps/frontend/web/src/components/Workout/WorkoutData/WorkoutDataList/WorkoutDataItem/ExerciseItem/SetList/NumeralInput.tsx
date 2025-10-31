800dd65b835c14e4b4536a3ea004e04c821bb146
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-10-30 21:05:55 -0700

    CWT-230 Update numeral inputs for assist fields

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/NumeralInput.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/NumeralInput.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { TextInput } from '@mantine/core';
 
-import type { Leverage, SetFields } from '@cwt/schema/workouts';
+import type { Leverage, Assist, SetFields } from '@cwt/schema/workouts';
 
 import { SetContext } from '../../../../../../../contexts/SetContextUpdated';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
@@ -10,32 +10,40 @@ interface NumeralInputProps {
   label: string;
   fieldName: 'reps' | 'weight' | 'value';
   fieldID?: string;
+  trackingType?: string | null;
 }
 
 export default function NumeralInput({
   label,
   fieldName,
   fieldID,
+  trackingType = null,
 }: NumeralInputProps) {
   const set = React.useContext(SetContext)!.set;
   const handleSetFieldChange =
     React.useContext(SetContext)!.handleSetFieldChange;
 
-  const setLeverageIDToMod = useWorkoutDraftStore(
+  const setLeverageOrAssistIDToMod = useWorkoutDraftStore(
     (state) => state.setLeverageOrAssistIDToMod,
   );
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (fieldID) {
-      setLeverageIDToMod(fieldID);
+      setLeverageOrAssistIDToMod(fieldID);
     }
     if (event.currentTarget.value == '') {
-      const updatedField: Partial<SetFields> | Pick<Leverage, 'value'> = {
+      const updatedField:
+        | Partial<SetFields>
+        | Pick<Leverage, 'value'>
+        | Pick<Assist, 'value'> = {
         [fieldName]: undefined,
       };
       handleSetFieldChange(set.id, updatedField);
     } else {
-      const updatedField: Partial<SetFields> | Pick<Leverage, 'value'> = {
+      const updatedField:
+        | Partial<SetFields>
+        | Pick<Leverage, 'value'>
+        | Pick<Assist, 'value'> = {
         [fieldName]: Number(event.currentTarget.value),
       };
       handleSetFieldChange(set.id, updatedField);
@@ -52,7 +60,7 @@ export default function NumeralInput({
   //       : set.fields[fieldName]!.toString();
   // console.log('NumeralInput - value: ', value);
 
-  if (fieldName === 'value') {
+  if (fieldName === 'value' && trackingType === 'leverages') {
     return (
       <TextInput
         w={68}
@@ -72,17 +80,39 @@ export default function NumeralInput({
       />
     );
   }
-  return (
-    <TextInput
-      w={68}
-      label={label}
-      placeholder={'0'}
-      value={
-        set.fields[fieldName] === undefined
-          ? ''
-          : set.fields[fieldName]!.toString()
-      }
-      onChange={handleChange}
-    />
-  );
+  if (fieldName === 'value' && trackingType === 'assists') {
+    return (
+      <TextInput
+        w={68}
+        label={label}
+        placeholder={'0'}
+        value={
+          set.fields.assists!.find((field) => field.id === fieldID)!.value ===
+            null ||
+          set.fields.assists!.find((field) => field.id === fieldID)!.value ===
+            undefined
+            ? ''
+            : set.fields
+                .assists!.find((field) => field.id === fieldID)!
+                .value!.toString()
+        }
+        onChange={handleChange}
+      />
+    );
+  }
+  if (fieldName !== 'value') {
+    return (
+      <TextInput
+        w={68}
+        label={label}
+        placeholder={'0'}
+        value={
+          set.fields[fieldName] === undefined
+            ? ''
+            : set.fields[fieldName]!.toString()
+        }
+        onChange={handleChange}
+      />
+    );
+  }
 }
