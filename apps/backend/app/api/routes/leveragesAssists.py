2219c2138bff2cdcde1b79c59605d7ba455e4fcf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-03 11:48:21 -0800

    CWT-233 Fix routes and add new route to api router

--- apps/backend/app/api/routes/leveragesAssists.py
+++ apps/backend/app/api/routes/leveragesAssists.py
@@ -1,9 +1,12 @@
 from typing import List
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Request, HTTPException
 
-from ..utils.leveragesAssists import get_leverages_assists_list
-from ...core.dependencies import get_current_user
-from ...schemas.leveragesAssists import LeveragesAssistsResponseSchema
+from app.api.utils.leveragesAssists import get_leverages_assists_list
+from app.schemas.leveragesAssists import LeveragesAssistsResponseSchema
+
+from app.core.config import settings
+
+environment: str = settings.environment
 
 router = APIRouter(
     prefix="/leveragesAssists",
@@ -14,11 +17,23 @@ router = APIRouter(
 
 @router.get("", response_model=List[LeveragesAssistsResponseSchema])
 async def get_leverages_assists(
-    request: Request, current_user=Depends(get_current_user)
+    request: Request,
 ) -> List[LeveragesAssistsResponseSchema]:
     """
     Get list of all leverages and assists.
     Requires authentication.
     """
-    access_token = current_user.access_token if current_user else None
-    return get_leverages_assists_list(access_token)
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        if environment == "local":
+            leveragesAssists = get_leverages_assists_list()
+        else:
+            raise HTTPException(status_code=401, detail="Authentication required")
+
+    else:
+        access_token = auth_header.split(" ")[1]
+        leveragesAssists = get_leverages_assists_list(access_token)
+    if leveragesAssists is None:
+        raise HTTPException(status_code=400, detail="Invalid request")
+    return leveragesAssists
+    # return get_leverages_assists_list(access_token)
