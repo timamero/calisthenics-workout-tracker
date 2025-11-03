700b9bdec7e7b51206420e408c473b7984f13efc
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-03 11:48:21 -0800

    CWT-233 Fix routes and add new route to api router

--- apps/backend/app/api/main.py
+++ apps/backend/app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .routes import exercises, workout
+from .routes import exercises, workout, leveragesAssists
 
 
 api_router = APIRouter()
 api_router.include_router(exercises.router)
 api_router.include_router(workout.router)
+api_router.include_router(leveragesAssists.router)
