740feb30d4e77ecc16f72b684a3e5afe8f5c3908
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-05 22:31:22 -0800

    CWT-234 Update workout context and add to mobile

--- packages/context/src/lib/WorkoutContext.tsx
+++ packages/context/src/lib/WorkoutContext.tsx
@@ -1,14 +1,29 @@
-import * as React from 'react';
-import { type UseDisclosureHandlers } from '@mantine/hooks';
+import * as React from "react";
+import { type UseDisclosureHandlers } from "@mantine/hooks";
 
 interface WorkoutContextType {
-  addExerciseOverlayHandler: UseDisclosureHandlers;
-  deleteRootItemOverlayHandler: UseDisclosureHandlers;
-  deleteNestedItemOverlayHandler: UseDisclosureHandlers;
-  deleteSetOverlayHandler: UseDisclosureHandlers;
-  addSupersetOverlayHandler: UseDisclosureHandlers;
+  // Web
+  addExerciseOverlayHandler?: UseDisclosureHandlers;
+  deleteRootItemOverlayHandler?: UseDisclosureHandlers;
+  deleteNestedItemOverlayHandler?: UseDisclosureHandlers;
+  deleteSetOverlayHandler?: UseDisclosureHandlers;
+  addSupersetOverlayHandler?: UseDisclosureHandlers;
+
+  // Mobile
+  setIsAddExerciseDialogVisible?: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsDeleteRootItemDialogVisible?: React.Dispatch<
+    React.SetStateAction<boolean>
+  >;
+  setIsDeleteNestedItemDialogVisible?: React.Dispatch<
+    React.SetStateAction<boolean>
+  >;
+  setIsDeleteSetDialogVisible?: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsAddSupersetDialogVisible?: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsDeleteExerciseDialogVisible?: React.Dispatch<
+    React.SetStateAction<boolean>
+  >; // will be superseded
 }
 
 export const WorkoutContext = React.createContext<WorkoutContextType | null>(
-  null,
+  null
 );
