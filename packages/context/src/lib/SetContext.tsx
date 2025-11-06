60ec661565b92c72bc048ba94e70f3401721226f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-05 20:51:36 -0800

    CWT-234 Create context package

---
+++ packages/context/src/lib/SetContext.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import type { Set, SetFields } from '@cwt/schema/workouts';
+
+interface SetContextType {
+  set: Set;
+  setIndex: number;
+  handleSetFieldChange: (setIndex: number, updatedField: SetFields) => void;
+}
+
+export const SetContext = React.createContext<SetContextType | null>(null);
