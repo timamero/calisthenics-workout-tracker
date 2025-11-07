e4732545cf1852c86a9ad8178f8b1101d0f65688
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-06 21:12:19 -0800

    CWT-234 WorkoutDataList returns list of components

--- apps/frontend/mobile/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataList.tsx
+++ apps/frontend/mobile/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataList.tsx
@@ -1,35 +1,12 @@
-import { View } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
 
-import { CustomTheme } from '../../../../theme';
 import { Text } from '../../../../customText';
 
 export default function WorkoutDataList() {
-  const theme = useTheme() as CustomTheme;
+  const workoutData = useWorkoutDraftStore((state) => state.workoutData);
+  const workoutDataItems = workoutData.map((item, i) => {
+    return <Text key={i}>item</Text>;
+  });
 
-  return (
-    <View
-      style={{
-        borderColor: theme.colors.grey,
-        borderWidth: 2,
-        borderStyle: 'dashed',
-        display: 'flex',
-        alignItems: 'center',
-        marginInline: 20,
-        padding: 16,
-        gap: 16,
-      }}
-    >
-      <Text
-        variant="bodyLarge"
-        style={{
-          color: theme.colors.light,
-          fontWeight: 800,
-          textAlign: 'center',
-        }}
-      >
-        WorkoutDataList Placeholder
-      </Text>
-    </View>
-  );
+  return <>{workoutDataItems}</>;
 }
