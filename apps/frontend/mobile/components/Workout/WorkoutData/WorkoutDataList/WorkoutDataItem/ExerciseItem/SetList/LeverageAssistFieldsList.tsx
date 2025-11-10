94d690855ade28ef9229ba74ea9b9f7ce951a6bd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-09 21:17:38 -0800

    CWT-234 Add leverage assist int field tracking

---
+++ apps/frontend/mobile/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/LeverageAssistFieldsList.tsx
@@ -0,0 +1,95 @@
+import { useContext } from 'react';
+import { View } from 'react-native';
+
+import type { Exercise } from '@cwt/schema/workouts';
+import { WorkoutDataItemContext, SetContext } from '@cwt/context';
+
+import NumeralInput from './NumeralInput';
+import { useLeveragesAssistsStore } from '@cwt/state/stores';
+import { Text } from '../../../../../../../customText';
+
+export default function LeverageAssistFieldsList() {
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const tracked = exercise.tracked;
+  const set = useContext(SetContext)?.set;
+
+  const getLeverageOrAssistByID = useLeveragesAssistsStore(
+    (state) => state.getLeverageOrAssistByID,
+  );
+
+  let leverageFields;
+
+  if (tracked.includes('leverages')) {
+    leverageFields = set?.fields.leverages!.map((leverageField) => {
+      const leverage = getLeverageOrAssistByID(
+        leverageField.leverages_assists_id,
+      );
+      if (leverage.value_type === 'int') {
+        return (
+          <NumeralInput
+            key={leverageField.id}
+            label={`${leverage.name} (${leverage.value_int_unit})`}
+            fieldName="value"
+            fieldID={leverageField.id}
+            trackingType="leverages"
+          />
+        );
+      } else {
+        return <Text key={leverageField.id}>Select input placeholder</Text>;
+      }
+    });
+  }
+
+  let assistFields;
+
+  if (tracked.includes('assists')) {
+    assistFields = set?.fields.assists!.map((assistField) => {
+      const assist = getLeverageOrAssistByID(assistField.leverages_assists_id);
+      if (assist.value_type === 'int') {
+        return (
+          <NumeralInput
+            key={assistField.id}
+            label={`${assist.name} (${assist.value_int_unit})`}
+            fieldName="value"
+            fieldID={assistField.id}
+            trackingType="assists"
+          />
+        );
+      } else {
+        return <Text key={assistField.id}>Select input placeholder</Text>;
+      }
+    });
+  }
+
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 8,
+        justifyContent: 'center',
+      }}
+    >
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 24,
+          justifyContent: 'center',
+        }}
+      >
+        {leverageFields}
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 24,
+          justifyContent: 'center',
+        }}
+      >
+        {assistFields}
+      </View>
+    </View>
+  );
+}
