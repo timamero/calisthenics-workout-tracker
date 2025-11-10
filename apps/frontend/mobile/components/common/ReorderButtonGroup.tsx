ec24dde1f46e183253c9b6d8f562bf09bf275d3e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-10 12:01:17 -0800

    CWT-234 Create Reorder button compoennts and add to exercise item

---
+++ apps/frontend/mobile/components/common/ReorderButtonGroup.tsx
@@ -0,0 +1,53 @@
+import { View, StyleSheet } from 'react-native';
+import { IconButton, useTheme } from 'react-native-paper';
+
+import { CustomTheme } from '../../theme';
+
+interface ReorderButtonProps {
+  isFirst: boolean;
+  isLast: boolean;
+  handleUpPress: () => void;
+  handleDownPress: () => void;
+}
+
+export default function ReorderButtonGroup({
+  isFirst = false,
+  isLast = false,
+  handleUpPress,
+  handleDownPress,
+}: ReorderButtonProps) {
+  const theme = useTheme() as CustomTheme;
+  const styles = getStyles(theme);
+
+  return (
+    <View style={styles.buttonGroup}>
+      {!isFirst && (
+        <IconButton
+          onPress={() => handleUpPress()}
+          icon="arrow-up"
+          iconColor={theme.colors.light}
+          size={16}
+          style={{ margin: 0 }}
+        />
+      )}
+      {!isLast && (
+        <IconButton
+          onPress={handleDownPress}
+          icon="arrow-down"
+          iconColor={theme.colors.light}
+          size={16}
+          style={{ margin: 0 }}
+        />
+      )}
+    </View>
+  );
+}
+
+const getStyles = (theme: CustomTheme) =>
+  StyleSheet.create({
+    buttonGroup: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 4,
+    },
+  });
