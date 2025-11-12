82df18fdda7dc445411ceb589b661a654670fa7b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-12 02:23:45 -0800

    CWT-265 Fix common prop type

--- packages/schema/src/lib/ui/schemas/addExerciseOverlay.ts
+++ packages/schema/src/lib/ui/schemas/addExerciseOverlay.ts
@@ -1,37 +1,25 @@
 import * as React from 'react';
 import { ScrollView } from 'react-native';
 
-import { CombineExclusives } from '../../common';
+export type AddExerciseOverlayProps = {
+  opened?: boolean; // Web
+  isVisible?: boolean; // Mobile
 
-export type AddExerciseOverlayProps = CombineExclusives<
-  {
-    opened?: boolean;
-    isVisible?: boolean;
+  handler?: { close: () => void }; // Web
+  handleHideModal?: () => void; // Mobile
 
-    handler?: { close: () => void };
-    handleHideModal?: () => void;
+  workoutDataScrollViewRef?: React.RefObject<ScrollView | null>;
+};
 
-    workoutDataScrollViewRef?: React.RefObject<ScrollView | null>;
-  },
-  [['opened', 'isVisible'], ['handler', 'handleHideModal']]
->;
+export type AddExerciseOverlayUIProps = {
+  opened?: boolean; // Web
+  isVisible?: boolean; // Mobile
 
-export type AddExerciseOverlayUIProps = CombineExclusives<
-  {
-    opened?: boolean;
-    isVisible?: boolean;
+  handler?: { close: () => void }; // Web
+  handleHideModal?: () => void; // Mobile
 
-    handler?: { close: () => void };
-    handleHideModal?: () => void;
+  handleAddExerciseClick?: () => void; // Web
+  handleAddExercisePress?: () => void; // Mobile
 
-    handleAddExerciseClick?: () => void;
-    handleAddExercisePress?: () => void;
-
-    selectedExerciseIDToAdd: number | null;
-  },
-  [
-    ['opened', 'isVisible'],
-    ['handler', 'handleHideModal'],
-    ['handleAddExerciseClick', 'handleAddExercisePress'],
-  ]
->;
+  selectedExerciseIDToAdd: number | null;
+};
