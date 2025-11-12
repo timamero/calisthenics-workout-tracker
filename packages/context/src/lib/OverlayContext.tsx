7fb75d98651a9e00019efbe47b6fa74028fc91c6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-12 13:52:31 -0800

    CWT-265 Refactor AddExerciseOverlay for web

--- packages/context/src/lib/OverlayContext.tsx
+++ packages/context/src/lib/OverlayContext.tsx
@@ -1,10 +1,14 @@
 import { createContext } from "react";
+import { type UseDisclosureHandlers } from "@mantine/hooks";
 
 interface OverlayContextType {
-  isAddExerciseOverlayVisible?: boolean;
+  addExerciseOverlayOpened?: boolean; // Web only
+  isAddExerciseOverlayVisible?: boolean; // Mobile only
+
+  addExerciseOverlayHandler?: UseDisclosureHandlers; // Web only
   setIsAddExerciseOverlayVisible?: React.Dispatch<
     React.SetStateAction<boolean>
-  >;
+  >; // Mobile only
 }
 
 export const OverlayContext = createContext<OverlayContextType | null>(null);
