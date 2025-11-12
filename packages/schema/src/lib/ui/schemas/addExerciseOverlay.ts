25e4862e5c8f75d8fa569365aba1bacc1e493874
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-12 01:09:36 -0800

    CWT-265 Create common prop type

--- packages/schema/src/lib/ui/schemas/addExerciseOverlay.ts
+++ packages/schema/src/lib/ui/schemas/addExerciseOverlay.ts
@@ -1,4 +1,20 @@
+import * as React from 'react';
+import { ScrollView } from 'react-native';
+
 import { CombineExclusives } from '../../common';
+
+export type AddExerciseOverlayProps = CombineExclusives<
+  {
+    opened?: boolean;
+    isVisible?: boolean;
+
+    handler?: { close: () => void };
+    handleHideModal?: () => void;
+
+    workoutDataScrollViewRef?: React.RefObject<ScrollView | null>;
+  },
+  [['opened', 'isVisible'], ['handler', 'handleHideModal']]
+>;
 
 export type AddExerciseOverlayUIProps = CombineExclusives<
   {
