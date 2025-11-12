369399fdb873d77d63b8081a8363b7faeae56cbf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-11 23:38:46 -0800

    CWT-265 Split component by UI and logic

--- apps/frontend/mobile/components/AddExerciseOverlay/AddExerciseOverlay.tsx
+++ apps/frontend/mobile/components/AddExerciseOverlay/AddExerciseOverlay.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import { ScrollView, View } from 'react-native';
-import { useTheme, Button } from 'react-native-paper';
+import { ScrollView } from 'react-native';
 
 import {
   useWorkoutDraftStore,
   useExerciseLibraryStore,
 } from '@cwt/state/stores';
 
-import { CustomTheme } from '../../theme';
-
-import FullScreenModal from '../common/FullScreenModal';
-import ExerciseList from './ExerciseList';
+import AddExerciseOverlayUI from './AddExerciseOverlayUI';
 
 interface AddExerciseOverlayProps {
   isVisible: boolean;
@@ -23,8 +19,6 @@ export default function AddExerciseOverlay({
   handleHideModal,
   workoutDataScrollViewRef,
 }: AddExerciseOverlayProps) {
-  const theme = useTheme() as CustomTheme;
-
   const selectedExerciseIDToAdd = useWorkoutDraftStore(
     (state) => state.selectedExerciseIDToAdd,
   );
@@ -45,52 +39,11 @@ export default function AddExerciseOverlay({
     workoutDataScrollViewRef.current?.scrollToEnd({ animated: true });
   };
   return (
-    <FullScreenModal
-      title="Add Exercise"
-      visible={isVisible}
+    <AddExerciseOverlayUI
+      isVisible={isVisible}
+      selectedExerciseIDToAdd={selectedExerciseIDToAdd}
       handleHideModal={handleHideModal}
-    >
-      <ScrollView
-        style={{
-          backgroundColor: theme.colors.background,
-          paddingInline: 16,
-          paddingBlock: 16,
-        }}
-      >
-        <ExerciseList />
-      </ScrollView>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          paddingTop: 20,
-          borderTopWidth: 2,
-          borderTopColor: theme.colors.orange1,
-        }}
-      >
-        <Button
-          mode="outlined"
-          textColor={theme.colors.light}
-          onPress={() => handleHideModal()}
-          style={{
-            borderColor: 'rgb(134, 142, 150)',
-            borderRadius: 4,
-          }}
-        >
-          Cancel
-        </Button>
-        <Button
-          disabled={selectedExerciseIDToAdd === null}
-          mode="contained"
-          onPress={() => handleAddExercisePress()}
-          style={{
-            borderRadius: 4,
-          }}
-        >
-          Add Exercise
-        </Button>
-      </View>
-    </FullScreenModal>
+      handleAddExercisePress={handleAddExercisePress}
+    />
   );
 }
---
+++ apps/frontend/mobile/components/AddExerciseOverlay/AddExerciseOverlayUI.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { ScrollView, View } from 'react-native';
+import { useTheme, Button } from 'react-native-paper';
+
+import { CustomTheme } from '../../theme';
+
+import FullScreenModal from '../common/FullScreenModal';
+import ExerciseList from './ExerciseList';
+
+interface AddExerciseOverlayUIProps {
+  isVisible: boolean;
+  selectedExerciseIDToAdd: number | null;
+  handleHideModal: () => void;
+  handleAddExercisePress: () => void;
+}
+
+export default function AddExerciseOverlayUI({
+  isVisible,
+  selectedExerciseIDToAdd,
+  handleHideModal,
+  handleAddExercisePress,
+}: AddExerciseOverlayUIProps) {
+  const theme = useTheme() as CustomTheme;
+
+  return (
+    <FullScreenModal
+      title="Add Exercise"
+      visible={isVisible}
+      handleHideModal={handleHideModal}
+    >
+      <ScrollView
+        style={{
+          backgroundColor: theme.colors.background,
+          paddingInline: 16,
+          paddingBlock: 16,
+        }}
+      >
+        <ExerciseList />
+      </ScrollView>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          paddingTop: 20,
+          borderTopWidth: 2,
+          borderTopColor: theme.colors.orange1,
+        }}
+      >
+        <Button
+          mode="outlined"
+          textColor={theme.colors.light}
+          onPress={() => handleHideModal()}
+          style={{
+            borderColor: 'rgb(134, 142, 150)',
+            borderRadius: 4,
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          disabled={selectedExerciseIDToAdd === null}
+          mode="contained"
+          onPress={() => handleAddExercisePress()}
+          style={{
+            borderRadius: 4,
+          }}
+        >
+          Add Exercise
+        </Button>
+      </View>
+    </FullScreenModal>
+  );
+}
