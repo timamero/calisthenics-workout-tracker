dfe00ece09fbbc1b9e979e64700c8c024f013a37
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-13 12:29:30 -0800

    CWT-265 Update FAB style and position

--- apps/frontend/mobile/components/Workout/AddWorkoutItemButtons.tsx
+++ apps/frontend/mobile/components/Workout/AddWorkoutItemButtons.tsx
@@ -1,11 +1,13 @@
 import { useContext, useState } from 'react';
-import { useTheme, Portal, FAB } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { useTheme, FAB, Portal } from 'react-native-paper';
 
 import { CustomTheme } from '../../theme';
 import { WorkoutContext } from '@cwt/context';
 
 export default function AddWorkoutItemButtons() {
   const theme = useTheme() as CustomTheme;
+  const styles = getStyles(theme);
 
   const [state, setState] = useState({ open: false });
   const onStateChange = ({ open }: { open: boolean }) => setState({ open });
@@ -22,25 +24,35 @@ export default function AddWorkoutItemButtons() {
     <Portal>
       <FAB.Group
         open={open}
+        // variant="surface"
         visible
         icon={open ? 'close' : 'plus'}
+        style={styles.fabGroup}
+        fabStyle={styles.fab}
+        color={theme.colors.dark700}
         actions={[
           {
             icon: 'application',
             label: 'Add Section',
             labelTextColor: theme.colors.light,
+            style: styles.fab,
+            color: theme.colors.dark700,
             onPress: () => setIsAddSectionOverlayVisible?.(true),
           },
           {
             icon: 'alpha-s-circle',
             label: 'Add Superset',
             labelTextColor: theme.colors.light,
+            style: styles.fab,
+            color: theme.colors.dark700,
             onPress: () => setIsAddSupersetOverlayVisible?.(true),
           },
           {
             icon: 'arm-flex',
             label: 'Add Exercise',
             labelTextColor: theme.colors.light,
+            style: styles.fab,
+            color: theme.colors.dark700,
             onPress: () => setIsAddExerciseOverlayVisible?.(true),
           },
         ]}
@@ -54,3 +66,17 @@ export default function AddWorkoutItemButtons() {
     </Portal>
   );
 }
+
+const getStyles = (theme: CustomTheme) =>
+  StyleSheet.create({
+    fabGroup: {
+      position: 'absolute',
+      marginRight: 8,
+      paddingBottom: 48,
+      right: 0,
+      bottom: 0,
+    },
+    fab: {
+      backgroundColor: theme.colors.dark100,
+    },
+  });
