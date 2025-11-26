5412fbd7fde5805633a514514eae42d6e343d933
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-25 16:57:30 -0800

    CWT-236 Pass superset as prop

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
@@ -1,3 +1,10 @@
-export default function ExerciseSetGroup() {
+import type { Superset } from '@cwt/schema/workouts';
+
+interface ExerciseSetGroupProps {
+  superset: Superset;
+}
+
+export default function ExerciseSetGroup({ superset }: ExerciseSetGroupProps) {
+  console.log('ExerciseSetGroup - superset', superset);
   return <div>map over ExerciseSetGroupItemContainers</div>;
 }
