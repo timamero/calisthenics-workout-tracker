294a7818ed723dc9985b7b70f4d707b980cbef94
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-11-26 20:04:46 -0800

    CWT-236 Display exercise fields

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroupItem.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroupItem.tsx
@@ -1,4 +1,17 @@
+import { useContext } from 'react';
+import { Stack, Text } from '@mantine/core';
+
+import type {
+  Assist,
+  Exercise,
+  Leverage,
+  SetFields,
+} from '@cwt/schema/workouts';
 import { useExerciseLibraryStore } from '@cwt/state/stores';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+import { SetContext, WorkoutDataItemContext } from '@cwt/context';
+
+import SetContainer from '../ExerciseItem/SetList/SetContainer';
 
 import type { ExercisesGroupedBySetsType } from './ExerciseSetGroup';
 
@@ -14,22 +27,83 @@ export default function ExerciseSetGroupItem({
     exercisesGroupedBySets,
   );
 
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const parentSectionID = useContext(WorkoutDataItemContext)?.parentSectionID;
+  const parentSupersetID = useContext(WorkoutDataItemContext)?.parentSupersetID;
+
+  const setSetIDToMod = useWorkoutDraftStore((state) => state.setSetIDToMod);
+  const setExerciseIDToMod = useWorkoutDraftStore(
+    (state) => state.setExerciseIDToMod,
+  );
+  const setSupersetIDToMod = useWorkoutDraftStore(
+    (state) => state.setSupersetIDToMod,
+  );
+  const setSectionIDToMod = useWorkoutDraftStore(
+    (state) => state.setSectionIDToMod,
+  );
+  const updateField = useWorkoutDraftStore((state) => state.updateField);
+  const updateLeverageOrAssistField = useWorkoutDraftStore(
+    (state) => state.updateLeverageOrAssistField,
+  );
+
   const getExerciseNameById = useExerciseLibraryStore(
     (state) => state.getExerciseNameByID,
   );
 
+  const handleSetFieldChange = (
+    setID: string,
+    updatedField:
+      | Partial<SetFields>
+      | Pick<Leverage, 'value'>
+      | Pick<Assist, 'value'>,
+  ) => {
+    setSetIDToMod(setID);
+    setExerciseIDToMod(exercise.id);
+    if (parentSupersetID) {
+      setSupersetIDToMod(parentSupersetID);
+    }
+    if (parentSectionID) {
+      setSectionIDToMod(parentSectionID);
+    }
+    if (useWorkoutDraftStore.getState().leverageOrAssistIDToMod) {
+      updateLeverageOrAssistField(
+        updatedField as Pick<Leverage, 'value'> | Pick<Assist, 'value'>,
+      );
+    } else {
+      updateField(updatedField as Partial<SetFields>);
+    }
+  };
+
   const exercisesGroup = exercisesGroupedBySets.exercises.map((ex) => {
     return (
-      <div key={ex.id}>
-        <div>{getExerciseNameById(ex.exercise_id)}</div>
-        <div>set id: {ex.set.id}</div>
-      </div>
+      <Stack key={ex.id}>
+        <Text>{getExerciseNameById(ex.exercise_id)}</Text>
+        <WorkoutDataItemContext.Provider
+          value={{
+            item: ex,
+            parentType: 'superset',
+            // parentItemsLength: exercisesGroupedBySets.exercises.length,
+            parentSectionID: parentSectionID!,
+            parentSupersetID: parentSupersetID!,
+          }}
+        >
+          <SetContext.Provider
+            value={{
+              setIndex: exercisesGroupedBySets.setGroupNumber,
+              set: ex.set,
+              handleSetFieldChange: handleSetFieldChange,
+            }}
+          >
+            <SetContainer />
+          </SetContext.Provider>
+        </WorkoutDataItemContext.Provider>
+      </Stack>
     );
   });
   return (
-    <div>
-      <div>Set {exercisesGroupedBySets.setGroupNumber + 1}</div>
+    <Stack>
+      <Text>Set {exercisesGroupedBySets.setGroupNumber + 1}</Text>
       {exercisesGroup}
-    </div>
+    </Stack>
   );
 }
