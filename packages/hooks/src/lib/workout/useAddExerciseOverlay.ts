6767d159ca87beec141a444677ccbf9fea3bd0bb
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-01 22:20:30 -0800

    CWT-236 Create and implement hook for opening add exercise overlay

---
+++ packages/hooks/src/lib/workout/useAddExerciseOverlay.ts
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+
+import type { Section, Superset } from "@cwt/schema/workouts";
+import { useWorkoutDraftStore } from "@cwt/state/stores";
+import { WorkoutContext, WorkoutDataItemContext } from "@cwt/context";
+
+export default function useAddExerciseOverlay(
+  itemToAddExerciseToType?: "superset" | "section"
+) {
+  const item = useContext(WorkoutDataItemContext)?.item as Superset | Section;
+  const addExerciseOverlayHandler =
+    useContext(WorkoutContext)?.addExerciseOverlayHandler;
+  const supersetParentsSectionID = useContext(
+    WorkoutDataItemContext
+  )?.parentSectionID;
+
+  const setSupersetIDToMod = useWorkoutDraftStore(
+    (state) => state.setSupersetIDToMod
+  );
+  const setSectionIDToMod = useWorkoutDraftStore(
+    (state) => state.setSectionIDToMod
+  );
+
+  const handleOpenAddExerciseOverlay = () => {
+    if (itemToAddExerciseToType === "superset") {
+      setSupersetIDToMod(item.id);
+      if (supersetParentsSectionID) {
+        setSectionIDToMod(supersetParentsSectionID);
+      }
+    }
+
+    if (itemToAddExerciseToType === "section") {
+      setSectionIDToMod(item.id);
+    }
+
+    addExerciseOverlayHandler!.open();
+  };
+
+  return { handleOpenAddExerciseOverlay };
+}
