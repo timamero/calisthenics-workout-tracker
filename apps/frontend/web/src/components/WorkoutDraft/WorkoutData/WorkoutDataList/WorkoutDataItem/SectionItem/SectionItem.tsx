13f34d09babef4264ba13e909346d06a716f7ade
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-01 23:22:13 -0800

    CWT-236 Refactor and rename components

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItem.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItem.tsx
@@ -1,76 +1,44 @@
-import { Stack, Group, Text, Button } from '@mantine/core';
+import { useContext } from 'react';
 
-import type { Mode, Section } from '@cwt/schema/workouts';
+import type { Section } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+import { WorkoutDataItemContext } from '@cwt/context';
 
-import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
-import ItemsList from './ItemsList';
-import { useAddExerciseOverlay, useAddSuperset } from '@cwt/hooks';
+import SectionItemUI from './SectionItemUI';
+import {
+  useDeleteItem,
+  useReorderItem,
+  useAddExerciseOverlay,
+  useAddSuperset,
+} from '@cwt/hooks';
 
-interface SectionItemProps {
-  mode: Mode;
-  section: Section;
-  isFirst: boolean;
-  isLast: boolean;
-  handleUpClick: () => void;
-  handleDownClick: () => void;
-  handleDeleteSectionClick: () => void;
-}
+export default function SectionItem() {
+  const section = useContext(WorkoutDataItemContext)!.item as Section;
 
-export default function SectionItem({
-  mode,
-  section,
-  isFirst,
-  isLast,
-  handleUpClick,
-  handleDownClick,
-  handleDeleteSectionClick,
-}: SectionItemProps) {
+  const mode = useWorkoutDraftStore((state) => state.mode);
+  const rootWorkoutDataLength = useWorkoutDraftStore(
+    (state) => state.workoutData.length || 0,
+  );
+
+  const handleUpClick = useReorderItem(section).handleUpClick;
+  const handleDownClick = useReorderItem(section).handleDownClick;
+  const handleDeleteSectionClick = useDeleteItem(
+    'section',
+    section!.id,
+  ).handleDeleteItemClick;
   const handleOpenAddExerciseOverlay = useAddExerciseOverlay('section');
   const handleAddSupersetClick = useAddSuperset(section.id);
 
   return (
-    <Group align="flex-start">
-      {mode !== 'log' && (
-        <ReorderButtonGroup
-          handleUpClick={() => handleUpClick()}
-          handleDownClick={() => handleDownClick()}
-          isFirst={isFirst}
-          isLast={isLast}
-        />
-      )}
-      <Stack bg="gray.1" p="xl" bdrs="sm">
-        <Group>
-          <Text>Section</Text>
-          {mode !== 'log' && (
-            <Button
-              color="red"
-              variant="white"
-              onClick={() => handleDeleteSectionClick()}
-            >
-              Delete
-            </Button>
-          )}
-        </Group>
-        <ItemsList />
-        {mode !== 'log' && (
-          <Stack>
-            <Button
-              variant="filled"
-              color="orange.9"
-              onClick={() => handleOpenAddExerciseOverlay()}
-            >
-              Add Exercise
-            </Button>
-            <Button
-              variant="filled"
-              color="orange.9"
-              onClick={() => handleAddSupersetClick()}
-            >
-              Add Superset
-            </Button>
-          </Stack>
-        )}
-      </Stack>
-    </Group>
+    <SectionItemUI
+      mode={mode!}
+      isFirst={section!.order === 0}
+      isLast={section!.order === rootWorkoutDataLength - 1}
+      handleUpClick={handleUpClick}
+      handleDownClick={handleDownClick}
+      handleDeleteSectionClick={handleDeleteSectionClick}
+      handleOpenAddExerciseOverlay={handleOpenAddExerciseOverlay}
+      handleAddSupersetClick={handleAddSupersetClick}
+    />
   );
 }
---
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
@@ -0,0 +1,74 @@
+import { Stack, Group, Text, Button } from '@mantine/core';
+
+import type { Mode } from '@cwt/schema/workouts';
+
+import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
+import ItemsList from './ItemsList';
+
+interface SectionItemUIProps {
+  mode: Mode;
+  isFirst: boolean;
+  isLast: boolean;
+  handleUpClick: () => void;
+  handleDownClick: () => void;
+  handleDeleteSectionClick: () => void;
+  handleOpenAddExerciseOverlay: () => void;
+  handleAddSupersetClick: () => void;
+}
+
+export default function SectionItemUI({
+  mode,
+  isFirst,
+  isLast,
+  handleUpClick,
+  handleDownClick,
+  handleDeleteSectionClick,
+  handleOpenAddExerciseOverlay,
+  handleAddSupersetClick,
+}: SectionItemUIProps) {
+  return (
+    <Group align="flex-start">
+      {mode !== 'log' && (
+        <ReorderButtonGroup
+          handleUpClick={() => handleUpClick()}
+          handleDownClick={() => handleDownClick()}
+          isFirst={isFirst}
+          isLast={isLast}
+        />
+      )}
+      <Stack bg="gray.1" p="xl" bdrs="sm">
+        <Group>
+          <Text>Section</Text>
+          {mode !== 'log' && (
+            <Button
+              color="red"
+              variant="white"
+              onClick={() => handleDeleteSectionClick()}
+            >
+              Delete
+            </Button>
+          )}
+        </Group>
+        <ItemsList />
+        {mode !== 'log' && (
+          <Stack>
+            <Button
+              variant="filled"
+              color="orange.9"
+              onClick={() => handleOpenAddExerciseOverlay()}
+            >
+              Add Exercise
+            </Button>
+            <Button
+              variant="filled"
+              color="orange.9"
+              onClick={() => handleAddSupersetClick()}
+            >
+              Add Superset
+            </Button>
+          </Stack>
+        )}
+      </Stack>
+    </Group>
+  );
+}
