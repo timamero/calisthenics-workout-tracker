c41b8bd2256b00ad1c4c1bc086524d3f90a43684
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-02 23:15:23 -0800

    CWT-236 Rename components

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SetList/Set.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SetList/Set.tsx
@@ -1,65 +1,99 @@
-import { Stack, Text, Group, Button, Checkbox, Divider } from '@mantine/core';
+import { useContext } from 'react';
 
-import type { Mode } from '@cwt/schema/workouts';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+import type { Exercise } from '@cwt/schema/workouts';
 
-import FieldsList from './FieldsList';
-import LeverageAssistFieldsList from './LeverageAssistFieldsList';
+import { WorkoutContext } from '@cwt/context';
+import { WorkoutDataItemContext } from '@cwt/context';
+import { SetContext } from '@cwt/context';
+import SetUI from './SetUI';
 
-interface SetProps {
-  mode: Mode;
-  setsLength?: number;
-  setIndex: number;
-  isCompleted: boolean;
-  showDeleteButton?: boolean;
-  hasSupersetParentType?: boolean | null;
-  handleToggleCompleted: (value: boolean) => void;
-  handleDeleteSetClick: () => void;
-}
+export default function Set() {
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const parentType = useContext(WorkoutDataItemContext)?.parentType;
+  const parentSectionID = useContext(WorkoutDataItemContext)?.parentSectionID;
+  const parentSupersetID = useContext(WorkoutDataItemContext)?.parentSupersetID;
 
-export default function Set({
-  mode,
-  setsLength,
-  setIndex,
-  isCompleted,
-  showDeleteButton,
-  hasSupersetParentType,
-  handleToggleCompleted,
-  handleDeleteSetClick,
-}: SetProps) {
+  const set = useContext(SetContext)!.set;
+  const setIndex = useContext(SetContext)!.setIndex;
+  const deleteSetOverlayHandler =
+    useContext(WorkoutContext)!.deleteSetOverlayHandler;
+  const deleteSetInSupersetOverlayHandler =
+    useContext(WorkoutContext)!.deleteSetInSupersetOverlayHandler;
+  const sets = exercise.sets;
+
+  const mode = useWorkoutDraftStore((state) => state.mode);
+  const toggleCompleted = useWorkoutDraftStore(
+    (state) => state.toggleCompleted,
+  );
+  const setSetIDToMod = useWorkoutDraftStore((state) => state.setSetIDToMod);
+  const setSetIndexToMod = useWorkoutDraftStore(
+    (state) => state.setSetIndexToMod,
+  );
+  const setExerciseIDToMod = useWorkoutDraftStore(
+    (state) => state.setExerciseIDToMod,
+  );
+  const setSupersetIDToMod = useWorkoutDraftStore(
+    (state) => state.setSupersetIDToMod,
+  );
+  const setSectionIDToMod = useWorkoutDraftStore(
+    (state) => state.setSectionIDToMod,
+  );
+
+  const handleDeleteSetClick = () => {
+    if (parentSupersetID) {
+      setSupersetIDToMod(parentSupersetID);
+    }
+    if (parentSectionID) {
+      setSectionIDToMod(parentSectionID);
+    }
+
+    if (parentType !== 'superset') {
+      setSetIDToMod(set.id);
+      setExerciseIDToMod(exercise!.id);
+      if (deleteSetOverlayHandler) deleteSetOverlayHandler.open();
+    } else {
+      setSetIndexToMod(setIndex);
+      if (deleteSetInSupersetOverlayHandler)
+        deleteSetInSupersetOverlayHandler.open();
+    }
+  };
+
+  const handleToggleCompleted = (value: boolean) => {
+    setSetIDToMod(set.id);
+    setExerciseIDToMod(exercise.id);
+
+    if (parentSupersetID) {
+      setSupersetIDToMod(parentSupersetID);
+    }
+    if (parentSectionID) {
+      setSectionIDToMod(parentSectionID);
+    }
+    toggleCompleted(value);
+  };
+
+  if (sets) {
+    return (
+      <SetUI
+        mode={mode!}
+        setsLength={sets.length}
+        setIndex={setIndex}
+        isCompleted={set.completed}
+        handleToggleCompleted={handleToggleCompleted}
+        showDeleteButton={sets.length > 1}
+        handleDeleteSetClick={handleDeleteSetClick}
+      />
+    );
+  }
+  // In log mode, for single sets in exercise set group in superset
   return (
-    <Stack gap="xs" p="sm">
-      <Group>
-        {!hasSupersetParentType && (mode === 'build' || mode === 'edit') && (
-          <Text size="xs" fw={600}>{`Set ${setIndex + 1}`}</Text>
-        )}
-
-        {showDeleteButton && (mode === 'build' || mode === 'edit') && (
-          <Button
-            color="red"
-            variant="white"
-            onClick={() => handleDeleteSetClick()}
-          >
-            Delete
-          </Button>
-        )}
-      </Group>
-      <FieldsList />
-      <LeverageAssistFieldsList />
-      {mode === 'log' && (
-        <Group justify="flex-end">
-          <Checkbox
-            label="Completed"
-            labelPosition="left"
-            checked={isCompleted}
-            onChange={(event) =>
-              handleToggleCompleted(event.currentTarget.checked)
-            }
-          />
-        </Group>
-      )}
-      {setsLength && setsLength > 0 && setIndex !== setsLength - 1 && (
-        <Divider />
-      )}
-    </Stack>
+    <SetUI
+      mode={mode!}
+      setIndex={setIndex}
+      isCompleted={set.completed}
+      handleToggleCompleted={handleToggleCompleted}
+      hasSupersetParentType={true}
+      handleDeleteSetClick={handleDeleteSetClick}
+    />
   );
 }
---
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SetList/SetUI.tsx
@@ -0,0 +1,65 @@
+import { Stack, Text, Group, Button, Checkbox, Divider } from '@mantine/core';
+
+import type { Mode } from '@cwt/schema/workouts';
+
+import FieldsList from './FieldsList';
+import LeverageAssistFieldsList from './LeverageAssistFieldsList';
+
+interface SetUIProps {
+  mode: Mode;
+  setsLength?: number;
+  setIndex: number;
+  isCompleted: boolean;
+  showDeleteButton?: boolean;
+  hasSupersetParentType?: boolean | null;
+  handleToggleCompleted: (value: boolean) => void;
+  handleDeleteSetClick: () => void;
+}
+
+export default function SetUI({
+  mode,
+  setsLength,
+  setIndex,
+  isCompleted,
+  showDeleteButton,
+  hasSupersetParentType,
+  handleToggleCompleted,
+  handleDeleteSetClick,
+}: SetUIProps) {
+  return (
+    <Stack gap="xs" p="sm">
+      <Group>
+        {!hasSupersetParentType && (mode === 'build' || mode === 'edit') && (
+          <Text size="xs" fw={600}>{`Set ${setIndex + 1}`}</Text>
+        )}
+
+        {showDeleteButton && (mode === 'build' || mode === 'edit') && (
+          <Button
+            color="red"
+            variant="white"
+            onClick={() => handleDeleteSetClick()}
+          >
+            Delete
+          </Button>
+        )}
+      </Group>
+      <FieldsList />
+      <LeverageAssistFieldsList />
+      {mode === 'log' && (
+        <Group justify="flex-end">
+          <Checkbox
+            label="Completed"
+            labelPosition="left"
+            checked={isCompleted}
+            onChange={(event) =>
+              handleToggleCompleted(event.currentTarget.checked)
+            }
+          />
+        </Group>
+      )}
+      {setsLength && setsLength > 0 && setIndex !== setsLength - 1 && (
+        <Divider />
+      )}
+    </Stack>
+  );
+}
