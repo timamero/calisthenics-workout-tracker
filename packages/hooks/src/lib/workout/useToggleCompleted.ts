db75439a9148cf47f3033fe2dfc08112bf4df231
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-03 00:01:26 -0800

    CWT-236 Create hook to toggle set completed

---
+++ packages/hooks/src/lib/workout/useToggleCompleted.ts
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+
+import { WorkoutDataItemContext, SetContext } from "@cwt/context";
+import { useWorkoutDraftStore } from "@cwt/state/stores";
+import type { Exercise } from "@cwt/schema/workouts";
+
+/**
+ * Hook to handle toggling the completed status of a set within an exercise in a workout draft.
+ * It retrieves the necessary context and store functions to manage the toggle action,
+ * ensuring that the correct exercise, superset, and section IDs are set in the draft state.
+ *
+ * @returns A function that, when called with a boolean value, toggles the completed status of the set.
+ */
+export default function useToggleCompleted() {
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const set = useContext(SetContext)!.set;
+  const parentSectionID = useContext(WorkoutDataItemContext)?.parentSectionID;
+  const parentSupersetID = useContext(WorkoutDataItemContext)?.parentSupersetID;
+
+  const toggleCompleted = useWorkoutDraftStore(
+    (state) => state.toggleCompleted
+  );
+  const setSetIDToMod = useWorkoutDraftStore((state) => state.setSetIDToMod);
+  const setExerciseIDToMod = useWorkoutDraftStore(
+    (state) => state.setExerciseIDToMod
+  );
+  const setSupersetIDToMod = useWorkoutDraftStore(
+    (state) => state.setSupersetIDToMod
+  );
+  const setSectionIDToMod = useWorkoutDraftStore(
+    (state) => state.setSectionIDToMod
+  );
+
+  const handleToggleCompleted = (value: boolean) => {
+    setSetIDToMod(set.id);
+    setExerciseIDToMod(exercise.id);
+
+    if (parentSupersetID) {
+      setSupersetIDToMod(parentSupersetID);
+    }
+    if (parentSectionID) {
+      setSectionIDToMod(parentSectionID);
+    }
+    toggleCompleted(value);
+  };
+  return handleToggleCompleted;
+}
