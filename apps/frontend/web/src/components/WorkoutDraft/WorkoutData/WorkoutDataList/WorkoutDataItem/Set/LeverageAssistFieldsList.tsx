8073124f8daf30a17e2cf50bf925bdb5c1050b19
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-03 01:41:37 -0800

    CWT-236 Refactor and remove redundancy

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/LeverageAssistFieldsList.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/LeverageAssistFieldsList.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { Group, Stack } from '@mantine/core';
 
-import type { Exercise } from '@cwt/schema/workouts';
+import type { Exercise, Leverage, Assist } from '@cwt/schema/workouts';
 import { WorkoutDataItemContext } from '@cwt/context';
 import { SetContext } from '@cwt/context';
 
@@ -9,71 +9,68 @@ import NumeralInput from './NumeralInput';
 import { useLeveragesAssistsStore } from '@cwt/state/stores';
 import SelectInput from './SelectInput';
 
-export default function LeverageAssistFieldsList() {
-  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
-  const tracked = exercise.tracked;
-  const set = useContext(SetContext)?.set;
-
+function LeverageOrAssistField({
+  leverageOrAssistField,
+}: {
+  leverageOrAssistField: Leverage | Assist;
+}) {
   const getLeverageOrAssistByID = useLeveragesAssistsStore(
     (state) => state.getLeverageOrAssistByID,
   );
+  const leverageOrAssist = getLeverageOrAssistByID(
+    leverageOrAssistField.leverages_assists_id,
+  );
+
+  if (leverageOrAssist.value_type === 'int') {
+    return (
+      <NumeralInput
+        key={leverageOrAssistField.id}
+        label={`${leverageOrAssist.name} (${leverageOrAssist.value_int_unit})`}
+        fieldName="value"
+        fieldID={leverageOrAssistField.id}
+        trackingType="leverages"
+      />
+    );
+  } else {
+    return (
+      <SelectInput
+        key={leverageOrAssistField.id}
+        label={`${leverageOrAssist.name}`}
+        fieldID={leverageOrAssistField.id}
+        trackingType="leverages"
+      />
+    );
+  }
+}
+
+export default function LeverageAssistFieldsList() {
+  const exercise = useContext(WorkoutDataItemContext)?.item as Exercise;
+  const tracked = exercise.tracked;
+
+  const set = useContext(SetContext)?.set;
 
   let leverageFields;
 
   if (tracked.includes('leverages')) {
     leverageFields = set?.fields.leverages!.map((leverageField) => {
-      const leverage = getLeverageOrAssistByID(
-        leverageField.leverages_assists_id,
+      return (
+        <LeverageOrAssistField
+          key={leverageField.id}
+          leverageOrAssistField={leverageField}
+        />
       );
-
-      if (leverage.value_type === 'int') {
-        return (
-          <NumeralInput
-            key={leverageField.id}
-            label={`${leverage.name} (${leverage.value_int_unit})`}
-            fieldName="value"
-            fieldID={leverageField.id}
-            trackingType="leverages"
-          />
-        );
-      } else {
-        return (
-          <SelectInput
-            key={leverageField.id}
-            label={`${leverage.name}`}
-            fieldID={leverageField.id}
-            trackingType="leverages"
-          />
-        );
-      }
     });
   }
   let assistFields;
 
   if (tracked.includes('assists')) {
     assistFields = set?.fields.assists!.map((assistField) => {
-      const assist = getLeverageOrAssistByID(assistField.leverages_assists_id);
-
-      if (assist.value_type === 'int') {
-        return (
-          <NumeralInput
-            key={assistField.id}
-            label={`${assist.name} (${assist.value_int_unit})`}
-            fieldName="value"
-            fieldID={assistField.id}
-            trackingType="assists"
-          />
-        );
-      } else {
-        return (
-          <SelectInput
-            key={assistField.id}
-            label={`${assist.name}`}
-            fieldID={assistField.id}
-            trackingType="assists"
-          />
-        );
-      }
+      return (
+        <LeverageOrAssistField
+          key={assistField.id}
+          leverageOrAssistField={assistField}
+        />
+      );
     });
   }
 
