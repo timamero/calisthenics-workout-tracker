e7fcaf0a11f08454b760ecaf8698ada2a699e656
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-02 22:21:19 -0800

    CWT-236 Separate logic and ui

---
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroupItemUI.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from 'react';
+
+import { Stack, Text } from '@mantine/core';
+
+interface ExerciseSetGroupItemUIProps {
+  setNumber: number;
+  children: ReactNode;
+}
+
+export default function ExerciseSetGroupItemUI({
+  setNumber,
+  children,
+}: ExerciseSetGroupItemUIProps) {
+  return (
+    <Stack>
+      <Text size="xs" fw={600}>
+        Set {setNumber}
+      </Text>
+      <Stack
+        bd="1px solid var(--mantine-color-default-border)"
+        p="lg"
+        w={300}
+        bg="transparent"
+        bdrs="lg"
+      >
+        {children}
+      </Stack>
+    </Stack>
+  );
+}
