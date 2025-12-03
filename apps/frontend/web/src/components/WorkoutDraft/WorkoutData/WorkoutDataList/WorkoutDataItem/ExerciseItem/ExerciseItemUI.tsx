d402e46fdcc7db0904cfa32a53235f91811e67f7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-02 23:05:13 -0800

    CWT-236 Rename components

---
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/ExerciseItemUI.tsx
@@ -0,0 +1,72 @@
+import { Stack, Text, Button, Group } from '@mantine/core';
+
+import type { Mode } from '@cwt/schema/workouts';
+import SetList from './SetList';
+import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
+
+interface ExerciseItemUIProps {
+  mode: Mode;
+  name: string;
+  isFirst: boolean;
+  isLast: boolean;
+  handleUpClick: () => void;
+  handleDownClick: () => void;
+  handleAddSetClick: () => void;
+  handleDeleteExerciseClick: () => void;
+}
+
+export default function ExerciseItemUI({
+  mode,
+  name,
+  isFirst,
+  isLast,
+  handleUpClick,
+  handleDownClick,
+  handleAddSetClick,
+  handleDeleteExerciseClick,
+}: ExerciseItemUIProps) {
+  return (
+    <Group align="flex-start">
+      {mode !== 'log' && (
+        <ReorderButtonGroup
+          handleUpClick={() => handleUpClick()}
+          handleDownClick={() => handleDownClick()}
+          isFirst={isFirst}
+          isLast={isLast}
+        />
+      )}
+      <Stack
+        bd="1px solid var(--mantine-color-default-border)"
+        p="lg"
+        w={300}
+        bg="transparent"
+        bdrs="lg"
+      >
+        <Group>
+          <Text>{name}</Text>
+          {mode !== 'log' && (
+            <Button
+              color="red"
+              variant="white"
+              onClick={() => handleDeleteExerciseClick()}
+            >
+              Delete
+            </Button>
+          )}
+        </Group>
+        <Stack gap="xs">
+          <SetList />
+        </Stack>
+        {mode !== 'log' && (
+          <Button
+            variant="outline"
+            color="dark"
+            onClick={() => handleAddSetClick()}
+          >
+            Add Set
+          </Button>
+        )}
+      </Stack>
+    </Group>
+  );
+}
