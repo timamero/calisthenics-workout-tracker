4a80327e3940b68763c0627f420aedb86a3fa8e0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-11 22:23:12 -0800

    CWT-238 Update hook and create separate mobile hook

--- apps/frontend/web/src/components/WorkoutDraft/AddWorkoutItemButtons.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/AddWorkoutItemButtons.tsx
@@ -1,16 +1,11 @@
-// import { useContext } from 'react';
-
 import { Stack, Button } from '@mantine/core';
-// import { WorkoutContext } from '@cwt/context';
 import { useAddExerciseOverlay, useAddSuperset } from '@cwt/hooks';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 
 export default function AddWorkoutItemButtons() {
   const handleOpenAddExerciseOverlay = useAddExerciseOverlay();
   const addSection = useWorkoutDraftStore((state) => state.addSection);
-  // const addSectionOverlayHandler =
-  //   useContext(WorkoutContext)?.addSectionOverlayHandler;
-  const handleAddSupersetClick = useAddSuperset();
+  const { handleAddSupersetClick } = useAddSuperset();
   return (
     <Stack>
       <Button
