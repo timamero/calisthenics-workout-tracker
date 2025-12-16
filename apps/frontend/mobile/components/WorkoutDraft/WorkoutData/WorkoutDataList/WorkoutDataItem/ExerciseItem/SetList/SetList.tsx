859de64341cd0a0d12daec5ee4ce2c94ce3aa9fe
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-16 14:21:59 -0800

    CWT-238 Refactor handlers to use hooks

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/SetList.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseItem/SetList/SetList.tsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react';
 import { View } from 'react-native';
 
-import { Assist, Exercise, Leverage, SetFields } from '@cwt/schema/workouts';
-import { useWorkoutDraftStore } from '@cwt/state/stores';
-
+import { Exercise } from '@cwt/schema/workouts';
+import { useUpdateField } from '@cwt/hooks';
 import { SetContext, WorkoutDataItemContext } from '@cwt/context';
+
 import SetContainer from './SetContainer';
 
 export default function SetList() {
@@ -12,44 +12,10 @@ export default function SetList() {
   const parentSectionID = useContext(WorkoutDataItemContext)?.parentSectionID;
   const parentSupersetID = useContext(WorkoutDataItemContext)?.parentSupersetID;
 
-  const setSetIDToMod = useWorkoutDraftStore((state) => state.setSetIDToMod);
-  const setExerciseIDToMod = useWorkoutDraftStore(
-    (state) => state.setExerciseIDToMod,
+  const handleSetFieldChange = useUpdateField(
+    parentSectionID,
+    parentSupersetID,
   );
-  const setSupersetIDToMod = useWorkoutDraftStore(
-    (state) => state.setSupersetIDToMod,
-  );
-  const setSectionIDToMod = useWorkoutDraftStore(
-    (state) => state.setSectionIDToMod,
-  );
-  const updateField = useWorkoutDraftStore((state) => state.updateField);
-  const updateLeverageOrAssistField = useWorkoutDraftStore(
-    (state) => state.updateLeverageOrAssistField,
-  );
-
-  const handleSetFieldChange = (
-    setID: string,
-    updatedField:
-      | Partial<SetFields>
-      | Pick<Leverage, 'value'>
-      | Pick<Assist, 'value'>,
-  ) => {
-    setSetIDToMod(setID);
-    setExerciseIDToMod(exercise.id);
-    if (parentSupersetID) {
-      setSupersetIDToMod(parentSupersetID);
-    }
-    if (parentSectionID) {
-      setSectionIDToMod(parentSectionID);
-    }
-    if (useWorkoutDraftStore.getState().leverageOrAssistIDToMod) {
-      updateLeverageOrAssistField(
-        updatedField as Pick<Leverage, 'value'> | Pick<Assist, 'value'>,
-      );
-    } else {
-      updateField(updatedField as Partial<SetFields>);
-    }
-  };
 
   const setList = exercise.sets.map((set, i) => {
     return (
