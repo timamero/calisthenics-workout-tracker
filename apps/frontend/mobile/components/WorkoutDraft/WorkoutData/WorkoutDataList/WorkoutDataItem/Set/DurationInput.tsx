ab74d70b5490318ebb7a71bd75adbcbf1bc8c7c7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-16 23:29:29 -0800

    CWT-238 Update file architecture

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/DurationInput.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/DurationInput.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { TextInput } from 'react-native-paper';
 
-// import type { SetFields } from '@cwt/schema/workouts';
 import { getSecondsInDuration } from '@cwt/utils';
 import { SetContext } from '@cwt/context';
 import { useFieldInputChange } from '@cwt/hooks';
@@ -12,31 +11,8 @@ interface DurationInputProps {
 
 export default function DurationInput({ label }: DurationInputProps) {
   const set = React.useContext(SetContext)!.set;
-  // const handleSetFieldChange =
-  //   React.useContext(SetContext)!.handleSetFieldChange;
-
   const handleRestFieldChange = useFieldInputChange(label, 'duration');
-  // const handleRestFieldChange = (text: string) => {
-  //   // Allow empty string for controlled input
-  //   if (text === '') {
-  //     const updatedField: Partial<SetFields> = {
-  //       [label]: '',
-  //     };
-  //     handleSetFieldChange(set.id, updatedField);
-  //     return;
-  //   }
-  //   // Validate: only numbers, no leading zeros except for '0'
-  //   if (/^(0|[1-9][0-9]{0,2})$/.test(text)) {
-  //     const num = Number(text);
-  //     if (num >= 0 && num <= 300) {
-  //       const updatedField: Partial<SetFields> = {
-  //         [label]: 'PT' + text + 'S',
-  //       };
-  //       handleSetFieldChange(set.id, updatedField);
-  //     }
-  //   }
-  //   // Otherwise, do not update
-  // };
+
   return (
     <TextInput
       mode="outlined"
