5419ccc3d4e90f93293599e175cd292265b13c50
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-18 01:10:58 -0800

    CWT-238 Update styles

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetUI.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetUI.tsx
@@ -1,7 +1,9 @@
 import type { ReactNode } from 'react';
+import { useTheme } from 'react-native-paper';
 import { View } from 'react-native';
 
 import { Text } from '../../../../../../customText';
+import { CustomTheme } from '../../../../../../theme';
 
 interface ExerciseSetUIProps {
   exerciseName: string;
@@ -12,16 +14,23 @@ export default function ExerciseSetUI({
   exerciseName,
   children,
 }: ExerciseSetUIProps) {
+  const theme = useTheme() as CustomTheme;
   return (
     <View
       style={{
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
       }}
     >
-      <Text>{exerciseName}</Text>
+      <Text
+        style={{
+          color: theme.colors.light,
+          fontWeight: 800,
+          // textAlign: 'center',
+        }}
+      >
+        {exerciseName}
+      </Text>
       {children}
     </View>
   );
