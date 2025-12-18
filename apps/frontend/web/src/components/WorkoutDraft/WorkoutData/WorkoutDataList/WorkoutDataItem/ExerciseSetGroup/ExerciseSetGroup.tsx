ecacba6187fbeaf20861bba20c76d67162b9a825
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-17 22:50:52 -0800

    CWT-238 Use hook

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
@@ -1,24 +1,17 @@
-import { useContext } from 'react';
-import type { Superset } from '@cwt/schema/workouts';
-import { WorkoutDataItemContext } from '@cwt/context';
-import { groupExercisesBySet } from '@cwt/utils';
+import { useSupersetState } from '@cwt/hooks';
 
 import ExerciseSetGroupItem from './ExerciseSetGroupItem';
 
 export default function ExerciseSetGroup() {
-  const superset = useContext(WorkoutDataItemContext)!.item as Superset;
-  const exercisesGroupedBySetsList = groupExercisesBySet(superset);
-
-  const supersetParentsSectionID = useContext(
-    WorkoutDataItemContext,
-  )?.parentSectionID;
+  const { supersetID, exercisesGroupedBySetsList, supersetParentsSectionID } =
+    useSupersetState();
 
   const exercisesGroupedBySets = exercisesGroupedBySetsList.map((group) => {
     return (
       <ExerciseSetGroupItem
         key={group.setGroupNumber}
         exercisesGroupedBySets={group}
-        parentSupersetID={superset.id}
+        parentSupersetID={supersetID}
         parentSectionID={supersetParentsSectionID || null}
       />
     );
