82701d520e25ec914997336a2f37c73644fe8700
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-17 22:41:40 -0800

    CWT-238 Return component list

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
@@ -3,4 +3,10 @@ import { useSupersetState } from '@cwt/hooks';
 export default function ExerciseSetGroup() {
   const { supersetID, exercisesGroupedBySetsList, supersetParentsSectionID } =
     useSupersetState();
+
+  const exercisesGroupedBySets = exercisesGroupedBySetsList.map((group) => {
+    return <div key={group.setGroupNumber}></div>;
+  });
+
+  return <>{exercisesGroupedBySets}</>;
 }
