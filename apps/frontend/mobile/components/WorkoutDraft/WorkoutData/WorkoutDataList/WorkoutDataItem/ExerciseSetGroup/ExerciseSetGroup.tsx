c5095d8de4f3afe9ff98eb1ca99cadf59cd2c0bf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-12-17 23:08:23 -0800

    CWT-238 Replace placeholder with component

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/ExerciseSetGroup/ExerciseSetGroup.tsx
@@ -1,11 +1,20 @@
 import { useSupersetState } from '@cwt/hooks';
 
+import ExerciseSetGroupItem from './ExerciseSetGroupItem';
+
 export default function ExerciseSetGroup() {
   const { supersetID, exercisesGroupedBySetsList, supersetParentsSectionID } =
     useSupersetState();
 
   const exercisesGroupedBySets = exercisesGroupedBySetsList.map((group) => {
-    return <div key={group.setGroupNumber}></div>;
+    return (
+      <ExerciseSetGroupItem
+        key={group.setGroupNumber}
+        exercisesGroupedBySets={group}
+        parentSupersetID={supersetID}
+        parentSectionID={supersetParentsSectionID || null}
+      />
+    );
   });
 
   return <>{exercisesGroupedBySets}</>;
