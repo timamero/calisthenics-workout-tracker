1c8b8b5b218cca4d3f26e1e2248eccfe7954e514
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-01-05 14:42:33 -0800

    CWT-284 Move confirmation overlays and logic

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutOverlays/ConfirmationOverlays.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutOverlays/ConfirmationOverlays.tsx
@@ -1,6 +1,25 @@
-import { useWorkoutDraftStore } from '@cwt/state/stores';
-import { useWorkoutContextMobile } from '@cwt/hooks';
+import { useNavigation } from '@react-navigation/native';
+import type {
+  WorkoutBuildResponse,
+  WorkoutLogResponse,
+  Mode,
+} from '@cwt/schema/workouts';
+import {
+  useWorkoutDraftStore,
+  useWorkoutStopwatchStore,
+  useAuthStore,
+  useWorkoutLibraryStore,
+} from '@cwt/state/stores';
+import { useWorkoutContextMobile, useWorkoutSave } from '@cwt/hooks';
+import {
+  saveWorkoutConfirmationContent,
+  cancelWorkoutConfirmationContent,
+} from '@cwt/content';
 
+import {
+  postWorkoutBuild,
+  postWorkoutLog,
+} from '../../../services/workoutsService';
 import ConfirmationDialog from '../../common/ConfirmationDialog';
 
 export default function ConfirmationOverlays() {
@@ -45,6 +64,73 @@ export default function ConfirmationOverlays() {
     (state) => state.removeNestedItem,
   );
 
+  // Save and Cancel handlers
+  const mode = useWorkoutDraftStore((state) => state.mode) as Mode;
+  const resetWorkout = useWorkoutDraftStore((state) => state.resetWorkout);
+  const supabaseSession = useAuthStore((state) => state.session);
+  const completeWorkout = useWorkoutLibraryStore(
+    (state) => state.completeWorkout,
+  );
+  const resetTimer = useWorkoutStopwatchStore((state) => state.reset);
+
+  const isSaveWorkoutDialogVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers.isSaveWorkoutDialogVisible;
+  const setIsSaveWorkoutDialogVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers
+      .setIsSaveWorkoutDialogVisible;
+  const isCancelWorkoutDialogVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers
+      .isCancelWorkoutDialogVisible;
+  const setIsCancelWorkoutDialogVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers
+      .setIsCancelWorkoutDialogVisible;
+
+  const { setWorkoutToSaveWithUser, setWorkoutToSaveWithUserAndDuration } =
+    useWorkoutSave();
+
+  const navigation = useNavigation<any>();
+
+  const onCancelWorkoutPress = () => {
+    navigation.navigate('App', { screen: 'WorkoutDashboard' });
+    resetWorkout();
+    resetTimer();
+  };
+
+  const onSaveWorkoutPress = async () => {
+    if (mode === 'build') {
+      setWorkoutToSaveWithUser();
+    } else {
+      setWorkoutToSaveWithUserAndDuration();
+    }
+    const workoutToSave = useWorkoutDraftStore.getState().workoutToSave;
+    if (!supabaseSession || !workoutToSave) {
+      console.error('Session not found or workout data invalid');
+      return;
+    }
+
+    const body = JSON.stringify(workoutToSave);
+    let result: WorkoutBuildResponse | WorkoutLogResponse | null = null;
+
+    if (mode === 'build') {
+      result = await postWorkoutBuild(supabaseSession.access_token, body);
+    } else {
+      result = await postWorkoutLog(supabaseSession.access_token, body);
+    }
+    if (result) {
+      completeWorkout(workoutToSave, mode!);
+      resetWorkout();
+      resetTimer();
+    } else {
+      // TODO: Save to state called unsavedBuilds
+      resetWorkout();
+      resetTimer();
+      console.error('Workout post request failed');
+    }
+
+    setIsSaveWorkoutDialogVisible!(false);
+    navigation.navigate('App', { screen: 'WorkoutDashboard' });
+  };
+
   return (
     <>
       <ConfirmationDialog
@@ -87,22 +173,26 @@ export default function ConfirmationOverlays() {
         handleHideDialog={setIsDeleteSetInSupersetOverlayVisible!}
         onConfirmationPress={() => deleteSetInSuperset()}
       />
-      {/* <ConfirmationDialog
-        title={addSectionConfirmationContent().title}
-        message={addSectionConfirmationContent().message}
-        confirmButtonLabel={addSectionConfirmationContent().confirmButtonLabel}
-        isVisible={isAddSectionOverlayVisible!}
-        handleHideDialog={setIsAddSectionOverlayVisible!}
-        onConfirmationPress={() => addSection()}
+      <ConfirmationDialog
+        title={saveWorkoutConfirmationContent(mode).title}
+        message={saveWorkoutConfirmationContent(mode).message}
+        confirmButtonLabel={
+          saveWorkoutConfirmationContent(mode).confirmButtonLabel
+        }
+        isVisible={isSaveWorkoutDialogVisible!}
+        handleHideDialog={setIsSaveWorkoutDialogVisible!}
+        onConfirmationPress={onSaveWorkoutPress}
       />
       <ConfirmationDialog
-        title={addSupersetConfirmationContent().title}
-        message={addSupersetConfirmationContent().message}
-        confirmButtonLabel={addSupersetConfirmationContent().confirmButtonLabel}
-        isVisible={isAddSupersetOverlayVisible!}
-        handleHideDialog={setIsAddSupersetOverlayVisible!}
-        onConfirmationPress={() => addSuperset(sectionIDToMod)}
-      /> */}
+        title={cancelWorkoutConfirmationContent(mode).title}
+        message={cancelWorkoutConfirmationContent(mode).message}
+        confirmButtonLabel={
+          cancelWorkoutConfirmationContent(mode).confirmButtonLabel
+        }
+        isVisible={isCancelWorkoutDialogVisible!}
+        handleHideDialog={setIsCancelWorkoutDialogVisible!}
+        onConfirmationPress={onCancelWorkoutPress}
+      />
     </>
   );
 }
