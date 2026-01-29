bcb471b1de0f8e0f3419fdaad2b1d3c93ed0e6cf
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-01-28 19:45:27 -0800

    CWT-298 Load workout logs

--- apps/frontend/web/src/routes/history.tsx
+++ apps/frontend/web/src/routes/history.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title } from '@mantine/core';
 
+import { useWorkoutLibraryStore, useAuthStore } from '@cwt/state/stores';
+import type { WorkoutLogResponse } from '@cwt/schema/workouts';
+
+import { getWorkoutLogs } from '../services/workoutsService';
+
 export const Route = createFileRoute('/history')({
+  loader: async () => {
+    const displayedWorkoutLogs =
+      useWorkoutLibraryStore.getState().displayedWorkoutLogs;
+    if (displayedWorkoutLogs && displayedWorkoutLogs.length > 0) {
+      return { logs: displayedWorkoutLogs };
+    }
+    const supabaseSession = useAuthStore.getState().session;
+    if (supabaseSession?.access_token) {
+      const workoutLogs = await getWorkoutLogs(supabaseSession.access_token);
+      return { logs: workoutLogs };
+    }
+    return [];
+  },
   component: HistoryView,
 });
 
 function HistoryView() {
+  const workoutLogs: { logs: WorkoutLogResponse[] } = Route.useLoaderData();
+
+  const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
+
+  useEffect(() => {
+    setWorkouts(workoutLogs.logs, []);
+  }, [workoutLogs, setWorkouts]);
+
+  console.log('workoutLogs: ', workoutLogs.logs);
   return (
     <div>
       <Title>History Page</Title>
