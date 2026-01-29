62e333153c574bf565da302bfa1314ca6718aca2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-01-28 22:27:19 -0800

    CWT-298 Display limited workout log details

--- apps/frontend/web/src/routes/history.tsx
+++ apps/frontend/web/src/routes/history.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
-import { Title } from '@mantine/core';
+import { Title, Text, Stack } from '@mantine/core';
 
 import { useWorkoutLibraryStore, useAuthStore } from '@cwt/state/stores';
 import type { WorkoutLogResponse } from '@cwt/schema/workouts';
 
 import { getWorkoutLogs } from '../services/workoutsService';
+import CardButton from '../components/common/CardButton';
 
 export const Route = createFileRoute('/history')({
   loader: async () => {
@@ -33,11 +34,31 @@ function HistoryView() {
     setWorkouts(workoutLogs.logs, []);
   }, [workoutLogs, setWorkouts]);
 
+  const workoutLogCards = workoutLogs.logs.map((wo, i) => {
+    const workoutTitle = wo.title ? wo.title : `Workout Log ${i + 1}`;
+    const date = new Date(wo.date).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+    return (
+      <CardButton key={i}>
+        <Title order={3} size="h5">
+          {workoutTitle}
+        </Title>
+        <Text>{date}</Text>
+        <Text>{wo.goal}</Text>
+        <Text>{wo.description}</Text>
+        <Text>{wo.duration}</Text>
+      </CardButton>
+    );
+  });
+
   console.log('workoutLogs: ', workoutLogs.logs);
   return (
     <div>
-      <Title>History Page</Title>
-      <p>This page will display the user's past workout logs.</p>
+      <Title>Past Workouts</Title>
+      <Stack gap="lg">{workoutLogCards}</Stack>
     </div>
   );
 }
