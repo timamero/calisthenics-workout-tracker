2fae7ef1e4346389336be1a50cc56fb3401f53d3
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-02-03 20:12:42 -0800

    CWT-299 Use for loop instead of recursion to chunk

--- apps/frontend/web/src/routes/history.tsx
+++ apps/frontend/web/src/routes/history.tsx
@@ -75,11 +75,19 @@ function HistoryView() {
   );
 }
 
+// function chunk<T>(array: T[], size: number): T[][] {
+//   if (!array.length) {
+//     return [];
+//   }
+//   const head = array.slice(0, size);
+//   const tail = array.slice(size);
+//   return [head, ...chunk(tail, size)];
+// }
+
 function chunk<T>(array: T[], size: number): T[][] {
-  if (!array.length) {
-    return [];
+  const result: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
   }
-  const head = array.slice(0, size);
-  const tail = array.slice(size);
-  return [head, ...chunk(tail, size)];
+  return result;
 }
