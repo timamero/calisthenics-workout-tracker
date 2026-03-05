00138e9ebddeaa82d4c3b9ea3414b0e898f370e2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-04 22:44:40 -0800

    CWT-306 Create WorkoutLogPages component

---
+++ apps/frontend/mobile/components/WorkoutLogPages.tsx
@@ -0,0 +1,53 @@
+import { ScrollView } from 'react-native';
+import { useTheme } from 'react-native-paper';
+
+import { useWorkoutLibraryStore } from '@cwt/state/stores';
+import { formatDuration } from '@cwt/utils';
+
+import { CustomTheme } from '../theme';
+import { Text } from '../customText';
+import CardButton from '../components/common/CardButton';
+
+export default function WorkoutLogPages() {
+  const theme = useTheme() as CustomTheme;
+
+  const workoutLogs = useWorkoutLibraryStore(
+    (state) => state.displayedWorkoutLogs,
+  );
+  // console.log('workout logs', workoutLogs);
+
+  return (
+    <ScrollView>
+      {workoutLogs.map((wo) => {
+        const date = new Date(wo.date).toLocaleString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+        const duration = formatDuration(wo.duration!);
+        return (
+          <CardButton key={wo.workout_build_id}>
+            <Text
+              variant="headlineMedium"
+              style={{ color: theme.colors.light }}
+            >
+              {wo.title}
+            </Text>
+            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
+              {date}
+            </Text>
+            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
+              {wo.goal}
+            </Text>
+            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
+              {wo.description}
+            </Text>
+            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
+              {duration}
+            </Text>
+          </CardButton>
+        );
+      })}
+    </ScrollView>
+  );
+}
