3cb07f5c844560e895cae8f27d2de4f643d61135
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-04 22:44:40 -0800

    CWT-306 Create WorkoutLogPages component

--- apps/frontend/mobile/screens/HistoryScreen.tsx
+++ apps/frontend/mobile/screens/HistoryScreen.tsx
@@ -1,20 +1,11 @@
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
-import { useWorkoutLibraryStore } from '@cwt/state/stores';
-import { formatDuration } from '@cwt/utils';
-
 import { CustomTheme } from '../theme';
-import { Text } from '../customText';
-import CardButton from '../components/common/CardButton';
+import WorkoutLogPages from '../components/WorkoutLogPages';
 
 export default function HistoryScreen() {
   const theme = useTheme() as CustomTheme;
-
-  const workoutLogs = useWorkoutLibraryStore(
-    (state) => state.displayedWorkoutLogs,
-  );
-  // console.log('workout logs', workoutLogs);
 
   return (
     <View
@@ -25,38 +16,7 @@ export default function HistoryScreen() {
         backgroundColor: theme.colors.background,
       }}
     >
-      <ScrollView>
-        {workoutLogs.map((wo) => {
-          const date = new Date(wo.date).toLocaleString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          });
-          const duration = formatDuration(wo.duration!);
-          return (
-            <CardButton key={wo.workout_build_id}>
-              <Text
-                variant="headlineMedium"
-                style={{ color: theme.colors.light }}
-              >
-                {wo.title}
-              </Text>
-              <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-                {date}
-              </Text>
-              <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-                {wo.goal}
-              </Text>
-              <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-                {wo.description}
-              </Text>
-              <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-                {duration}
-              </Text>
-            </CardButton>
-          );
-        })}
-      </ScrollView>
+      <WorkoutLogPages />
     </View>
   );
 }
