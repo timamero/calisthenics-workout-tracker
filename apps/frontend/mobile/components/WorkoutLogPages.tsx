387cf9abb034a67945ec890d7431b52d952a74e1
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-04 22:53:16 -0800

    CWT-306 Add DataTable with placeholder content

--- apps/frontend/mobile/components/WorkoutLogPages.tsx
+++ apps/frontend/mobile/components/WorkoutLogPages.tsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useTheme, DataTable } from 'react-native-paper';
 
 import { useWorkoutLibraryStore } from '@cwt/state/stores';
 import { formatDuration } from '@cwt/utils';
@@ -15,9 +16,75 @@ export default function WorkoutLogPages() {
     (state) => state.displayedWorkoutLogs,
   );
   // console.log('workout logs', workoutLogs);
+  const [page, setPage] = useState<number>(0);
+  const [numberOfItemsPerPageList] = useState([2, 3, 4]);
+  const [itemsPerPage, onItemsPerPageChange] = useState(
+    numberOfItemsPerPageList[0],
+  );
+
+  const [items] = useState([
+    {
+      key: 1,
+      name: 'Cupcake',
+      calories: 356,
+      fat: 16,
+    },
+    {
+      key: 2,
+      name: 'Eclair',
+      calories: 262,
+      fat: 16,
+    },
+    {
+      key: 3,
+      name: 'Frozen yogurt',
+      calories: 159,
+      fat: 6,
+    },
+    {
+      key: 4,
+      name: 'Gingerbread',
+      calories: 305,
+      fat: 3.7,
+    },
+  ]);
+
+  const from = page * itemsPerPage;
+  const to = Math.min((page + 1) * itemsPerPage, items.length);
+
+  useEffect(() => {
+    setPage(0);
+  }, [itemsPerPage]);
 
   return (
     <ScrollView>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Dessert</DataTable.Title>
+          <DataTable.Title numeric>Calories</DataTable.Title>
+          <DataTable.Title numeric>Fat</DataTable.Title>
+        </DataTable.Header>
+
+        {items.slice(from, to).map((item) => (
+          <DataTable.Row key={item.key}>
+            <DataTable.Cell>{item.name}</DataTable.Cell>
+            <DataTable.Cell numeric>{item.calories}</DataTable.Cell>
+            <DataTable.Cell numeric>{item.fat}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+
+        <DataTable.Pagination
+          page={page}
+          numberOfPages={Math.ceil(items.length / itemsPerPage)}
+          onPageChange={(page) => setPage(page)}
+          label={`${from + 1}-${to} of ${items.length}`}
+          numberOfItemsPerPageList={numberOfItemsPerPageList}
+          numberOfItemsPerPage={itemsPerPage}
+          onItemsPerPageChange={onItemsPerPageChange}
+          showFastPaginationControls
+          selectPageDropdownLabel={'Rows per page'}
+        />
+      </DataTable>
       {workoutLogs.map((wo) => {
         const date = new Date(wo.date).toLocaleString('en-US', {
           year: 'numeric',
