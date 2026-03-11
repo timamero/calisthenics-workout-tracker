52a0f36d99e45cf947f05873d1cc840a21629736
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-10 22:16:02 -0700

    CWT-306 Update layout

--- apps/frontend/mobile/components/WorkoutLogPages.tsx
+++ apps/frontend/mobile/components/WorkoutLogPages.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { ScrollView } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView } from 'react-native';
 import { useTheme, DataTable } from 'react-native-paper';
 
 import { useWorkoutLibraryStore } from '@cwt/state/stores';
@@ -8,6 +8,7 @@ import { formatDuration, chunk } from '@cwt/utils';
 import { CustomTheme } from '../theme';
 import { Text } from '../customText';
 import CardButton from '../components/common/CardButton';
+import { defaultThemesByVersion } from 'react-native-paper/lib/typescript/core/theming';
 
 export default function WorkoutLogPages() {
   const theme = useTheme() as CustomTheme;
@@ -18,7 +19,7 @@ export default function WorkoutLogPages() {
   // console.log('workout logs', workoutLogs);
   const [activePage, setPage] = useState<number>(0);
   // const [examplePage, setExamplePage] = useState<number>(0);
-  const [numberOfItemsPerPageList] = useState([6, 4, 3]);
+  const [numberOfItemsPerPageList] = useState([6, 2, 3]);
   const [itemsPerPage, onItemsPerPageChange] = useState(
     numberOfItemsPerPageList[0],
   );
@@ -104,31 +105,39 @@ export default function WorkoutLogPages() {
   // }, [itemsPerPage]);
 
   return (
-    <ScrollView>
-      <DataTable>
-        {/* <DataTable.Header>
+    // <View style={{ flex: 1, backgroundColor: 'red' }}>
+    <>
+      <ScrollView
+        style={{
+          width: '100%',
+          flex: 1,
+          backgroundColor: theme.colors.dark700,
+        }}
+      >
+        <DataTable>
+          {/* <DataTable.Header>
           <DataTable.Title>Dessert</DataTable.Title>
           <DataTable.Title numeric>Calories</DataTable.Title>
           <DataTable.Title numeric>Fat</DataTable.Title>
         </DataTable.Header> */}
-        {items}
+          {items}
 
-        {/* {exampleItems.slice(from, to).map((item) => (
+          {/* {exampleItems.slice(from, to).map((item) => (
           <DataTable.Row key={item.key}>
-            <DataTable.Cell>
-              <CardButton>
-                <Text
-                  variant="headlineMedium"
-                  style={{ color: theme.colors.light }}
-                >
-                  {item.name}
-                </Text>
-              </CardButton>
-            </DataTable.Cell>
+          <DataTable.Cell>
+          <CardButton>
+          <Text
+          variant="headlineMedium"
+          style={{ color: theme.colors.light }}
+          >
+          {item.name}
+          </Text>
+          </CardButton>
+          </DataTable.Cell>
           </DataTable.Row>
         ))} */}
 
-        {/* <DataTable.Pagination
+          {/* <DataTable.Pagination
           page={examplePage}
           numberOfPages={Math.ceil(exampleItems.length / itemsPerPage)}
           onPageChange={(page) => setExamplePage(page)}
@@ -139,57 +148,29 @@ export default function WorkoutLogPages() {
           showFastPaginationControls
           selectPageDropdownLabel={'Rows per page'}
         /> */}
-        {/* Need to fix the pagination parameters */}
-        <DataTable.Pagination
-          page={activePage}
-          numberOfPages={Math.ceil(workoutLogs.length / itemsPerPage)}
-          onPageChange={(page) => setPage(page)}
-          label={`${from + 1}-${to} of ${workoutLogs.length}`}
-          numberOfItemsPerPageList={numberOfItemsPerPageList}
-          numberOfItemsPerPage={itemsPerPage}
-          onItemsPerPageChange={onItemsPerPageChange}
-          showFastPaginationControls
-          selectPageDropdownLabel={'Rows per page'}
-          // style={{
-          //   borderBottomWidth: 10,
-          // }}
-          theme={{
-            colors: {
-              onSurface: theme.colors.light,
-            },
-          }}
-        />
-      </DataTable>
-      {/* {workoutLogs.map((wo) => {
-        const date = new Date(wo.date).toLocaleString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
-        const duration = formatDuration(wo.duration!);
-        return (
-          <CardButton key={wo.workout_build_id}>
-            <Text
-              variant="headlineMedium"
-              style={{ color: theme.colors.light }}
-            >
-              {wo.title}
-            </Text>
-            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-              {date}
-            </Text>
-            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-              {wo.goal}
-            </Text>
-            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-              {wo.description}
-            </Text>
-            <Text variant="bodyMedium" style={{ color: theme.colors.light }}>
-              {duration}
-            </Text>
-          </CardButton>
-        );
-      })} */}
-    </ScrollView>
+          {/* Need to fix the pagination parameters */}
+        </DataTable>
+      </ScrollView>
+      <DataTable.Pagination
+        page={activePage}
+        numberOfPages={Math.ceil(workoutLogs.length / itemsPerPage)}
+        onPageChange={(page) => setPage(page)}
+        label={`${from + 1}-${to} of ${workoutLogs.length}`}
+        numberOfItemsPerPageList={numberOfItemsPerPageList}
+        numberOfItemsPerPage={itemsPerPage}
+        onItemsPerPageChange={onItemsPerPageChange}
+        showFastPaginationControls
+        selectPageDropdownLabel={'Rows per page'}
+        // style={{
+        //   borderBottomWidth: 10,
+        // }}
+        theme={{
+          colors: {
+            onSurface: theme.colors.light,
+          },
+        }}
+      />
+    </>
+    // </View>
   );
 }
