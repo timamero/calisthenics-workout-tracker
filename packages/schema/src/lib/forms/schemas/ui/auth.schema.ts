c768b335160ecc9e9ffc7f6249119bbb386fba54
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-16 19:42:27 -0700

    CWT-91 Add validation

--- packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
+++ packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
@@ -1,6 +1,9 @@
-import { z } from 'zod';
+import * as z from 'zod';
 
 export const AuthSchema = z.object({
-  email: z.email(),
-  password: z.string(),
+  email: z.email().min(1, { message: 'Email is required' }),
+  password: z
+    .string()
+    .min(4, { message: 'The username must be 4 characters or more' })
+    .max(16, { message: 'The password must be 16 characters or less' }),
 });
