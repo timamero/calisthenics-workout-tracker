94544a86873feb2dec7af5de89e82cc2f0e2dc80
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-17 01:33:39 -0700

    CWT-91 Create auth sign up schema

--- packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
+++ packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
@@ -4,3 +4,13 @@ export const AuthSchema = z.object({
   email: z.email(),
   password: z.string(),
 });
+
+export const AuthSignUpSchema = z
+  .object({
+    ...AuthSchema.shape,
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
