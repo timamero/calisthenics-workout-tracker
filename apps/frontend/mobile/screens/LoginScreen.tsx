db8c690fffa54fdb66aa7437beccd7a9579483a6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-17 13:24:54 -0700

    CWT-91 Add form validation to login

--- apps/frontend/mobile/screens/LoginScreen.tsx
+++ apps/frontend/mobile/screens/LoginScreen.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { useTheme, TextInput, Text, Button } from 'react-native-paper';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { signIn } from '@cwt/auth/signIn';
+import { AuthSchema, type Auth } from '@cwt/schema/forms';
+
 import { supabase } from '../services/supabaseClient';
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
   const theme = useTheme();
 
-  const handleLogin = async () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Auth>({
+    resolver: zodResolver(AuthSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleLogin = async ({ email, password }: Auth) => {
     const user = await signIn(supabase, email, password);
     console.log('User:', user);
   };
@@ -25,28 +41,65 @@ export default function LoginScreen() {
         gap: 16,
         paddingHorizontal: 16,
         alignItems: 'stretch',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         backgroundColor: theme.colors.background,
       }}
     >
       <Text variant="displaySmall">Log In</Text>
-      <TextInput
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Email"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            error={typeof errors?.email?.message === 'string'}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text>{errors?.email?.message}</Text>}
+      {/* <TextInput
         label="Email"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
         keyboardType="email-address"
+        error={typeof errors?.email?.message === 'string'}
+      /> */}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+            error={typeof errors?.password?.message === 'string'}
+          />
+        )}
+        name="password"
       />
-      <TextInput
+      {errors.email && <Text>{errors?.password?.message}</Text>}
+      {/* <TextInput
         label="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
-      />
+        error={typeof errors?.password?.message === 'string'}
+      /> */}
       <Button
         mode="contained"
         buttonColor={theme.colors.primary}
-        onPress={() => handleLogin()}
+        onPress={() => handleSubmit((d) => handleLogin(d))}
       >
         Log In
       </Button>
