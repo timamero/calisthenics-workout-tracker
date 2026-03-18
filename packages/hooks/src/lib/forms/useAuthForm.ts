3a0749bff5f6a8ec615b635b76a516561a0649e4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-18 16:47:21 -0700

    CWT-91 Create sign up auth hook

--- packages/hooks/src/lib/forms/useAuthForm.ts
+++ packages/hooks/src/lib/forms/useAuthForm.ts
@@ -3,21 +3,39 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { type SupabaseClient } from "@supabase/supabase-js";
 
 import { signIn } from "@cwt/auth/signIn";
-import { AuthSchema, type Auth } from "@cwt/schema/forms";
+import { createUser } from "@cwt/auth/createUser";
+import {
+  AuthSchema,
+  AuthSignUpSchema,
+  type Auth,
+  type AuthSignUp,
+} from "@cwt/schema/forms";
 import { AppTypeSchema } from "@cwt/schema/common";
 
-function useAuthFormLogic(supabase: SupabaseClient, appType: AppTypeSchema) {
+function useAuthFormLogic(
+  supabase: SupabaseClient,
+  appType: AppTypeSchema,
+  authType: "login" | "signup",
+) {
+  const defaultLoginValues = {
+    email: "",
+    password: "",
+  };
+
+  const defaultSignUpValues = {
+    ...defaultLoginValues,
+    confirmPassword: "",
+  };
+
   const {
     control,
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Auth>({
-    resolver: zodResolver(AuthSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+  } = useForm<Auth | AuthSignUp>({
+    resolver: zodResolver(authType === "login" ? AuthSchema : AuthSignUpSchema),
+    defaultValues:
+      authType === "login" ? defaultLoginValues : defaultSignUpValues,
   });
 
   const handleLogin = async ({ email, password }: Auth) => {
@@ -25,15 +43,36 @@ function useAuthFormLogic(supabase: SupabaseClient, appType: AppTypeSchema) {
     console.log("User:", user);
   };
 
+  const handleSignUp = async ({
+    email,
+    password,
+  }: Pick<AuthSignUp, "email" | "password">) => {
+    const user = createUser(supabase, email, password);
+    console.log("User:", user);
+  };
+
   if (appType === "web") {
-    return { handleLogin, handleSubmit, errors, register };
+    if (authType === "login") {
+      return { handleLogin, handleSubmit, errors, register };
+    }
+    return { handleSignUp, handleSubmit, errors, register };
   }
-  return { handleLogin, handleSubmit, errors, control };
+
+  if (authType === "login") {
+    return { handleLogin, handleSubmit, errors, control };
+  }
+  return { handleSignUp, handleSubmit, errors, control };
 }
 
 export function useAuthLogin(supabase: SupabaseClient) {
-  return useAuthFormLogic(supabase, "web");
+  return useAuthFormLogic(supabase, "web", "login");
 }
 export function useAuthLoginMobile(supabase: SupabaseClient) {
-  return useAuthFormLogic(supabase, "mobile");
+  return useAuthFormLogic(supabase, "mobile", "login");
+}
+export function useAuthSignUp(supabase: SupabaseClient) {
+  return useAuthFormLogic(supabase, "web", "signup");
+}
+export function useAuthSignUpMobile(supabase: SupabaseClient) {
+  return useAuthFormLogic(supabase, "mobile", "signup");
 }
