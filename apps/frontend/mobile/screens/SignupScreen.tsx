e0df8cedcc489ad954d4d7b379c21e67a71df5c4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-17 20:01:27 -0700

    CWT-91 Add form validation to sign up

--- apps/frontend/mobile/screens/SignupScreen.tsx
+++ apps/frontend/mobile/screens/SignupScreen.tsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { useTheme, TextInput, Text, Button } from 'react-native-paper';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-// eslint-disable-next-line import/no-unresolved
 import { createUser } from '@cwt/auth/createUser';
+import { AuthSignUpSchema, type AuthSignUp } from '@cwt/schema/forms';
+
 import { supabase } from '../services/supabaseClient';
 
 export default function SignupScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
 
   const theme = useTheme();
 
-  const handleSignUp = async () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthSignUp>({
+    resolver: zodResolver(AuthSignUpSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSignUp = async ({
+    email,
+    password,
+  }: Pick<AuthSignUp, 'email' | 'password'>) => {
     const user = await createUser(supabase, email, password);
     console.log('User:', user);
   };
@@ -27,34 +45,89 @@ export default function SignupScreen() {
         gap: 16,
         paddingHorizontal: 16,
         alignItems: 'stretch',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         backgroundColor: theme.colors.background,
       }}
     >
       <Text variant="displaySmall">Sign Up</Text>
-      <TextInput
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Email"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            value={value}
+            error={typeof errors?.email?.message === 'string'}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text>{errors?.email?.message}</Text>}
+      {/* <TextInput
         label="Email"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
         keyboardType="email-address"
+      /> */}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+            error={typeof errors?.password?.message === 'string'}
+          />
+        )}
+        name="password"
       />
-      <TextInput
+      {errors.email && <Text>{errors?.password?.message}</Text>}
+      {/* <TextInput
         label="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
+      /> */}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Confirm Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+            error={typeof errors?.confirmPassword?.message === 'string'}
+          />
+        )}
+        name="confirmPassword"
       />
-      <TextInput
+      {errors.email && <Text>{errors?.password?.message}</Text>}
+      {/* <TextInput
         label="Confirm Password"
         value={confirmPassword}
         onChangeText={setConfirmPassword}
         secureTextEntry
-      />
+      /> */}
       <Button
         mode="contained"
         buttonColor={theme.colors.primary}
-        onPress={() => handleSignUp()}
+        onPress={() => handleSubmit((d) => handleSignUp(d))}
       >
         Sign Up
       </Button>
