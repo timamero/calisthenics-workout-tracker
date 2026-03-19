54ff048d04d50ac979b35ff2c37d84512b1d6ac4
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-18 21:31:47 -0700

    CWT-91 Use auth form hooks

--- apps/frontend/mobile/screens/LoginScreen.tsx
+++ apps/frontend/mobile/screens/LoginScreen.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 import { useTheme, TextInput, Text, Button } from 'react-native-paper';
-import { useForm, Controller } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-import { signIn } from '@cwt/auth/signIn';
-import { AuthSchema, type Auth } from '@cwt/schema/forms';
+import { Controller } from 'react-hook-form';
+import { useAuthLoginMobile } from '@cwt/hooks';
 
 import { CustomTheme } from '../theme';
 import { supabase } from '../services/supabaseClient';
@@ -13,22 +10,7 @@ import { supabase } from '../services/supabaseClient';
 export default function LoginScreen() {
   const theme = useTheme() as CustomTheme;
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Auth>({
-    resolver: zodResolver(AuthSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  const handleLogin = async ({ email, password }: Auth) => {
-    const user = await signIn(supabase, email, password);
-    console.log('User:', user);
-  };
+  const auth = useAuthLoginMobile(supabase);
 
   return (
     <View
@@ -47,24 +29,25 @@ export default function LoginScreen() {
         Log In
       </Text>
       <Controller
+        control={auth.control}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             label="Email"
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
-            error={typeof errors?.email?.message === 'string'}
+            error={typeof auth.errors?.email?.message === 'string'}
           />
         )}
         name="email"
       />
-      {errors.email && (
+      {auth.errors.email && (
         <Text style={{ color: theme.colors.errorLight }}>
-          {errors?.email?.message}
+          {auth.errors?.email?.message}
         </Text>
       )}
       <Controller
-        control={control}
+        control={auth.control}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             label="Password"
@@ -72,20 +55,20 @@ export default function LoginScreen() {
             onChangeText={onChange}
             value={value}
             secureTextEntry
-            error={typeof errors?.password?.message === 'string'}
+            error={typeof auth.errors?.password?.message === 'string'}
           />
         )}
         name="password"
       />
-      {errors.password && (
+      {auth.errors.password && (
         <Text style={{ color: theme.colors.errorLight }}>
-          {errors?.password?.message}
+          {auth.errors?.password?.message}
         </Text>
       )}
       <Button
         mode="contained"
         buttonColor={theme.colors.primary}
-        onPress={handleSubmit((d) => handleLogin(d))}
+        onPress={auth.handleSubmit}
       >
         Log In
       </Button>
