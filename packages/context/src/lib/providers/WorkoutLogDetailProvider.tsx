6f8ae1fdc7a86ab7d8ae15319f20fcb0f27ac382
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-24 18:50:20 -0700

    CWT-307 Update context and provider for workout log detail overlay

--- packages/context/src/lib/providers/WorkoutLogDetailProvider.tsx
+++ packages/context/src/lib/providers/WorkoutLogDetailProvider.tsx
@@ -2,26 +2,45 @@ import { useState, type ReactNode } from "react";
 import { useDisclosure } from "@mantine/hooks";
 
 import { WorkoutLogResponse } from "@cwt/schema/workouts";
+import { AppTypeSchema } from "@cwt/schema/common";
 
 import { WorkoutLogDetailContext } from "../contexts/WorkoutLogDetailContext";
 
 export default function WorkoutLogDetailContextProvider({
+  appType,
   children,
 }: {
+  appType: AppTypeSchema;
   children: ReactNode;
 }) {
   const [workout, setWorkout] = useState<WorkoutLogResponse | null>(null);
 
+  // Web
   const [opened, handlers] = useDisclosure(false);
+  const webOverlayHandlers = {
+    opened,
+    handlers,
+  };
+
+  // Mobile
+  const [isOverlayVisible, setIsOverlayVisible] = useState<boolean>(false);
+  const mobileOverlayHandlers = {
+    isOverlayVisible,
+    setIsOverlayVisible,
+  };
 
   return (
     <WorkoutLogDetailContext.Provider
-      value={{
-        workout,
-        setWorkout,
-        opened,
-        handlers,
-      }}
+      value={
+        appType === "web"
+          ? {
+              appType,
+              workout,
+              setWorkout,
+              ...webOverlayHandlers,
+            }
+          : { appType, workout, setWorkout, ...mobileOverlayHandlers }
+      }
     >
       {children}
     </WorkoutLogDetailContext.Provider>
