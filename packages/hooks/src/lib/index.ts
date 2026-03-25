c4a0bbfd1104b840e45e04b4cb23bc4f8e44310e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-24 19:54:57 -0700

    CWT-307 Create hooks to use workout log details context

--- packages/hooks/src/lib/index.ts
+++ packages/hooks/src/lib/index.ts
@@ -1,3 +1,4 @@
-export * from './auth';
-export * from './exercises';
-export * from './workout';
+export * from "./auth";
+export * from "./exercises";
+export * from "./workout";
+export * from "./history";
