2b0cdfc5c1a55ac957668763be822f53fd2de825
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-29 23:38:49 -0700

    CWT-127 Move fetching of exercises to library screen

--- apps/frontend/mobile/screens/LibraryScreen.tsx
+++ apps/frontend/mobile/screens/LibraryScreen.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { ScrollView, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
 import { ExerciseResponse } from '@cwt/schema/exercises';
+import {
+  useExerciseLibraryStore,
+  useAuthStore,
+  useLeveragesAssistsStore,
+} from '@cwt/state/stores';
 
 import { ExerciseDetailContext } from '../contexts/ExerciseDetailContext';
+
+import { getExercises } from '../services/exercisesService';
+import { getLeveragesAssists } from '../services/leveragesAssistsService';
 
 import ExerciseList from '../components/ExerciseList';
 import SearchBar from '../components/SearchBar';
@@ -15,16 +23,54 @@ import ExerciseDetailOverlay from '../components/ExerciseDetailOverlay';
 export default function LibraryScreen() {
   const theme = useTheme();
 
-  const [visible, setVisible] = React.useState(false);
+  const supabaseSession = useAuthStore((state) => state.session);
+  const isExercisesFetched = useExerciseLibraryStore(
+    (state) => state.isExercisesFetched,
+  );
+  const setIsExercisesFetched = useExerciseLibraryStore(
+    (state) => state.setIsExercisesFetched,
+  );
+  const setExercises = useExerciseLibraryStore((state) => state.setExercises);
+
+  const setLeveragesAssists = useLeveragesAssistsStore(
+    (state) => state.setLeveragesAssists,
+  );
+
+  const [visible, setVisible] = useState(false);
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
 
-  const [exerciseDetail, setExerciseDetail] =
-    React.useState<ExerciseResponse | null>(null);
-  const [visibleExerciseDetail, setVisibleExerciseDetail] =
-    React.useState(false);
+  const [exerciseDetail, setExerciseDetail] = useState<ExerciseResponse | null>(
+    null,
+  );
+  const [visibleExerciseDetail, setVisibleExerciseDetail] = useState(false);
   const showExerciseDetailModal = () => setVisibleExerciseDetail(true);
   const hideExerciseDetailModal = () => setVisibleExerciseDetail(false);
+
+  useEffect(() => {
+    const asyncFetchData = async () => {
+      if (supabaseSession?.access_token && !isExercisesFetched) {
+        const leveragesAssists = await getLeveragesAssists(
+          supabaseSession.access_token,
+        );
+        if (leveragesAssists) {
+          setLeveragesAssists(leveragesAssists);
+        }
+        const exercises = await getExercises(supabaseSession.access_token);
+        if (exercises) {
+          setExercises(exercises);
+          setIsExercisesFetched(true);
+        }
+      }
+    };
+    asyncFetchData();
+  }, [
+    setExercises,
+    supabaseSession,
+    setLeveragesAssists,
+    isExercisesFetched,
+    setIsExercisesFetched,
+  ]);
 
   return (
     <ExerciseDetailContext.Provider
