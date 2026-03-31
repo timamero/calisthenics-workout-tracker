3fb96b822448789f3aab996bd307824da1c97371
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-03-31 00:38:14 -0700

    CWT-127 Fix handling auth change events

--- packages/hooks/src/lib/auth/useSupabaseAuth.ts
+++ packages/hooks/src/lib/auth/useSupabaseAuth.ts
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { SupabaseClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { SupabaseClient, type JwtPayload } from "@supabase/supabase-js";
 import { useAuthStore } from "@cwt/state/stores";
 
 /**
@@ -13,72 +13,120 @@ export default function useSupabaseAuth(client: SupabaseClient) {
   const setLoading = useAuthStore((state) => state.setLoading);
   const setSession = useAuthStore((state) => state.setSession);
   const setUser = useAuthStore((state) => state.setUser);
-  React.useEffect(() => {
+
+  useEffect(() => {
     // Use mounted flag to prevent state updates after unmount
     let mounted = true;
 
-    const getSession = async () => {
-      console.log("getSession func: set loading to true");
-      setLoading(true);
-
-      const {
-        data: { session },
-      } = await client.auth.getSession();
+    const {
+      data: { subscription },
+    } = client.auth.onAuthStateChange((_event, session) => {
       if (mounted) {
-        console.log("getSession func: set session and user");
-        setSession(session);
-        setUser(session!.user);
-      }
-    };
-
-    getSession();
-
-    const { data: listener } = client.auth.onAuthStateChange(
-      (_event, session) => {
-        if (mounted) {
-          console.log("onAuthStateChange: _event = ", _event);
-          console.log("onAuthStateChange: set loading to true");
-          setLoading(true);
-
-          switch (_event) {
-            case "SIGNED_IN":
-              console.log("User signed in");
-            case "USER_UPDATED":
-              console.log(
-                "onAuthStateChange USER_UPDATED: user = ",
-                session!.user,
-              );
-              setSession(session);
-              setUser(session!.user);
-              console.log(
-                "onAuthStateChange: User signed in or updated, session set.",
-              );
-              break;
-            case "SIGNED_OUT":
-              setSession(null);
-              console.log(
-                "onAuthStateChange: User signed out, session cleared.",
-              );
-              break;
-            case "PASSWORD_RECOVERY":
-              console.log("onAuthStateChange: Password recovery initiated.");
-              // TODO: Initiate password recovery here
-              break;
-            default:
-              // TODO: Handle other events
-              console.log(`onAuthStateChange: Unhandled auth event: ${_event}`);
-              break;
-          }
-
-          setLoading(false);
+        setLoading(true);
+        switch (_event) {
+          case "SIGNED_IN":
+            console.log("User signed in");
+            setSession(session);
+            setUser(session!.user);
+            break;
+          case "INITIAL_SESSION":
+          case "TOKEN_REFRESHED":
+          case "MFA_CHALLENGE_VERIFIED":
+            console.log(
+              "onAuthStateChange INITIAL_SESSION, TOKEN_REFRESHED, MFA_CHALLENGE_VERIFIED: session = ",
+              session,
+            );
+            break;
+          case "USER_UPDATED":
+            // TODO: Handle user update
+            break;
+          case "SIGNED_OUT":
+            setSession(null);
+            setUser(null);
+            console.log("onAuthStateChange: User signed out, session cleared.");
+            break;
+          case "PASSWORD_RECOVERY":
+            console.log("onAuthStateChange: Password recovery initiated.");
+            // TODO: Initiate password recovery here
+            break;
+          default:
+            // TODO: Handle other events
+            console.log(`onAuthStateChange: Unhandled auth event: ${_event}`);
+            break;
         }
-      },
-    );
+        setLoading(false);
+      }
+    });
 
-    setLoading(false);
     return () => {
       mounted = false;
-      listener.subscription.unsubscribe();
+      subscription.unsubscribe();
     };
-  }, [setSession, client]);
+  }, []);
+
+  // original
+  // const setLoading = useAuthStore((state) => state.setLoading);
+  // const setSession = useAuthStore((state) => state.setSession);
+  // const setUser = useAuthStore((state) => state.setUser);
+  // useEffect(() => {
+  //   // Use mounted flag to prevent state updates after unmount
+  //   let mounted = true;
+  //   const getSession = async () => {
+  //     console.log("getSession func: set loading to true");
+  //     setLoading(true);
+  //     const {
+  //       data: { session },
+  //     } = await client.auth.getSession();
+  //     if (mounted) {
+  //       console.log("getSession func: set session and user");
+  //       setSession(session);
+  //       setUser(session!.user);
+  //     }
+  //   };
+  //   getSession();
+  //   const { data: listener } = client.auth.onAuthStateChange(
+  //     (_event, session) => {
+  //       if (mounted) {
+  //         console.log("onAuthStateChange: _event = ", _event);
+  //         console.log("onAuthStateChange: set loading to true");
+  //         setLoading(true);
+  //         switch (_event) {
+  //           case "SIGNED_IN":
+  //             console.log("User signed in");
+  //           case "USER_UPDATED":
+  //             console.log(
+  //               "onAuthStateChange USER_UPDATED: user = ",
+  //               session!.user,
+  //             );
+  //             setSession(session);
+  //             setUser(session!.user);
+  //             console.log(
+  //               "onAuthStateChange: User signed in or updated, session set.",
+  //             );
+  //             break;
+  //           case "SIGNED_OUT":
+  //             setSession(null);
+  //             console.log(
+  //               "onAuthStateChange: User signed out, session cleared.",
+  //             );
+  //             break;
+  //           case "PASSWORD_RECOVERY":
+  //             console.log("onAuthStateChange: Password recovery initiated.");
+  //             // TODO: Initiate password recovery here
+  //             break;
+  //           default:
+  //             // TODO: Handle other events
+  //             console.log(`onAuthStateChange: Unhandled auth event: ${_event}`);
+  //             break;
+  //         }
+  //         setLoading(false);
+  //       }
+  //     },
+  //   );
+  //   setLoading(false);
+  //   return () => {
+  //     mounted = false;
+  //     listener.subscription.unsubscribe();
+  //   };
+  // }, [setSession, client]);
 }
