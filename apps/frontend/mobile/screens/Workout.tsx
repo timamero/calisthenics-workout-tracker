b70bf6cf06a8ade05bcc153908a42015e574553b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-01 00:01:36 -0700

    CWT-127 Use hooks to fetch data

--- apps/frontend/mobile/screens/Workout.tsx
+++ apps/frontend/mobile/screens/Workout.tsx
@@ -1,17 +1,10 @@
-import { useEffect } from 'react';
 import { View } from 'react-native';
 
 import { useTheme } from 'react-native-paper';
 
-import {
-  useExerciseLibraryStore,
-  useAuthStore,
-  useLeveragesAssistsStore,
-} from '@cwt/state/stores';
+import { useExerciseLibraryStore } from '@cwt/state/stores';
 
-import { getExercises } from '../services/exercisesService';
-import { getLeveragesAssists } from '../services/leveragesAssistsService';
-
+import { useFetchExercises } from '../hooks/useFetchExercises';
 import { Text } from '../customText';
 import { CustomTheme } from '../theme';
 
@@ -20,44 +13,12 @@ import WorkoutDraft from '../components/WorkoutDraft';
 export default function WorkoutScreen() {
   const theme = useTheme() as CustomTheme;
 
-  const supabaseSession = useAuthStore((state) => state.session);
   const loading = useExerciseLibraryStore((state) => state.loading);
-  const setLoading = useExerciseLibraryStore((state) => state.setLoading);
-  const setExercises = useExerciseLibraryStore((state) => state.setExercises);
   const isExercisesSet = useExerciseLibraryStore((state) =>
     state.displayedExercises === null ? false : true,
   );
-  const setLeveragesAssists = useLeveragesAssistsStore(
-    (state) => state.setLeveragesAssists,
-  );
-  useEffect(() => {
-    const asyncFetchData = async () => {
-      if (supabaseSession?.access_token && !isExercisesSet) {
-        const leveragesAssists = await getLeveragesAssists(
-          supabaseSession.access_token,
-        );
-        if (leveragesAssists) {
-          setLeveragesAssists(leveragesAssists);
-        }
-        const exercises = await getExercises(supabaseSession.access_token);
-        if (exercises) {
-          setExercises(exercises);
-          setLoading(false);
-        }
-      }
-    };
 
-    if (!isExercisesSet) {
-      console.log('fetching and setting the data');
-      asyncFetchData();
-    }
-  }, [
-    setExercises,
-    supabaseSession,
-    setLeveragesAssists,
-    isExercisesSet,
-    setLoading,
-  ]);
+  useFetchExercises();
 
   if (!isExercisesSet || loading) {
     return (
