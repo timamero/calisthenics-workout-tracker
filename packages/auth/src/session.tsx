b3c7c31864236cfe41bd0e098302f6324ee4ee93
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-01 21:59:02 -0700

    CWT-322 Update dev logs

--- packages/auth/src/session.tsx
+++ packages/auth/src/session.tsx
@@ -4,16 +4,14 @@
  * @returns {Promise<Object|null>} The current session object or null if an error occurred.
  * @throws Will log an error if retrieving the session fails.
  */
-import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from "@supabase/supabase-js";
 
-export const session = async (supabase:SupabaseClient) => {
-  console.log('Retrieving session from Supabase...');
-  const { data, error } = await supabase.auth.getSession()
+export const session = async (supabase: SupabaseClient) => {
+  const { data, error } = await supabase.auth.getSession();
 
   if (error) {
-    console.error('Error retrieving session', error)
-    return null
+    console.error("Error retrieving session", error);
+    return null;
   }
-  console.log('Session retrieved successfully')
-  return data.session
-}
+  return data.session;
+};
