9f257faa499e104c4846ae881684d061d4a8e122
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-06 14:29:02 -0700

    Add dev logs

--- packages/auth/src/signOut.tsx
+++ packages/auth/src/signOut.tsx
@@ -3,13 +3,14 @@
  * @param {Object} supabase - The Supabase client instance.
  * @throws Will log an error if signing out fails.
  */
-import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from "@supabase/supabase-js";
 
-export const signOut = async (supabase:SupabaseClient) => {
-  const { error } = await supabase.auth.signOut()
+export const signOut = async (supabase: SupabaseClient) => {
+  console.log("signOut || calling supabase.auth.signOut");
+  const { error } = await supabase.auth.signOut();
 
   if (error) {
-    console.error('Error signing out user:', error)
-    return null
+    console.error("Error signing out user:", error);
+    return null;
   }
-}
+};
