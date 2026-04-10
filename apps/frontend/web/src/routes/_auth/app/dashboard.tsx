684595927e573927fd1390acf2b66cd215e30c05
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-10 12:10:07 -0700

    Update routes to fix active nav link behavior
    The home link stayed active no matter what link was pressed. It seems
    like the value of active state cannot be changed with code because it
    depends on the value of aria-current, which is managed by TanStack
    Router. The probelm was caused by the home page set to the index page,
    giving it the route of the parent to all the other routes. To fix this
    problem I had to create the dashboard route, move the content of index
    into dashboard. Then in index, set a redirect to dashboard.

---
+++ apps/frontend/web/src/routes/_auth/app/dashboard.tsx
@@ -0,0 +1,165 @@
+import { useEffect } from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+import { Title, Stack, Group, ScrollArea, Text } from '@mantine/core';
+
+import {
+  useWorkoutDraftStore,
+  useWorkoutLibraryStore,
+  useAuthStore,
+} from '@cwt/state/stores';
+import type {
+  WorkoutBuildResponse,
+  WorkoutLogResponse,
+} from '@cwt/schema/workouts';
+
+import {
+  getWorkoutBuilds,
+  getWorkoutLogs,
+} from '../../../services/workoutsService';
+
+import CardButton from '../../../components/common/CardButton';
+import LargeButton from '../../../components/common/LargeButton';
+
+export const Route = createFileRoute('/_auth/app/dashboard')({
+  loader: async () => {
+    const displayedWorkoutBuilds =
+      useWorkoutLibraryStore.getState().displayedWorkoutBuilds;
+    const displayedWorkoutLogs =
+      useWorkoutLibraryStore.getState().displayedWorkoutLogs;
+    if (
+      displayedWorkoutBuilds &&
+      displayedWorkoutBuilds.length > 0 &&
+      displayedWorkoutLogs &&
+      displayedWorkoutLogs.length > 0
+    ) {
+      return { logs: displayedWorkoutLogs, builds: displayedWorkoutBuilds };
+    }
+    const supabaseSession = useAuthStore.getState().session;
+    if (supabaseSession?.access_token) {
+      const workoutBuilds = await getWorkoutBuilds(
+        supabaseSession.access_token,
+      );
+      const workoutLogs = await getWorkoutLogs(supabaseSession.access_token);
+      return { logs: workoutLogs, builds: workoutBuilds };
+    }
+    return [];
+  },
+  pendingComponent: () => (
+    <div>
+      <p>Loading</p>
+    </div>
+  ),
+  component: AppView,
+});
+
+function AppView() {
+  const workouts: {
+    logs: WorkoutLogResponse[];
+    builds: WorkoutBuildResponse[];
+  } = Route.useLoaderData();
+
+  const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
+
+  // On initial load, set the workout builds in the store
+  useEffect(() => {
+    setWorkouts(workouts.logs, workouts.builds);
+  }, [workouts, setWorkouts]);
+
+  const initializeWorkout = useWorkoutDraftStore(
+    (state) => state.initializeWorkout,
+  );
+
+  const handleCreateWorkoutBuildClick = () => {
+    initializeWorkout('build');
+  };
+  const handleCreateWorkoutLogClick = () => {
+    initializeWorkout('edit');
+  };
+
+  let workoutBuildCards = null;
+  let workoutLogCards = null;
+
+  if (workouts.builds) {
+    workoutBuildCards = workouts.builds.map((wo, i) => {
+      const workoutTitle = wo.title ? wo.title : `Workout Template ${i + 1}`;
+      const date = wo.created_at ? wo.created_at : new Date();
+      const dateString =
+        typeof date === 'string' ? date : date.toLocaleDateString();
+      return (
+        <CardButton key={i}>
+          <Title order={3} size="h5">
+            {workoutTitle}
+          </Title>
+          <Text>{dateString}</Text>
+        </CardButton>
+      );
+    });
+  }
+
+  if (workouts.logs) {
+    workoutLogCards = workouts.logs.map((wo, i) => {
+      const workoutTitle = wo.title ? wo.title : `Workout Log ${i + 1}`;
+      const date = wo.date;
+      return (
+        <CardButton key={i}>
+          <Title order={3} size="h5">
+            {workoutTitle}
+          </Title>
+          <Text>{date}</Text>
+        </CardButton>
+      );
+    });
+  }
+  return (
+    <Stack align="center" justify="flex-start">
+      <Title order={1}>Welcome Back</Title>
+      <Text size="xl">
+        Progress isn't always a straight line. Just keep moving.
+      </Text>
+      <Stack gap="xl" mt={80}>
+        <Title order={2} size="h2">
+          Start Workout
+        </Title>
+        <Group w="100%">
+          <LargeButton
+            to="/workout"
+            onButtonClick={handleCreateWorkoutLogClick}
+          >
+            <Text fw={700} ta="center">
+              Start Blank Workout
+            </Text>
+          </LargeButton>
+          <LargeButton
+            to="/workout"
+            onButtonClick={handleCreateWorkoutBuildClick}
+          >
+            <Text fw={700} ta="center">
+              Create a Workout Template
+            </Text>
+          </LargeButton>
+        </Group>
+        {workoutBuildCards && workoutBuildCards.length > 0 && (
+          <Stack>
+            <Text fw={700} size="xl">
+              Start Workout From Template
+            </Text>
+            <ScrollArea w={400}>
+              <Group wrap="nowrap">{workoutBuildCards}</Group>
+            </ScrollArea>
+          </Stack>
+        )}
+
+        {workoutLogCards && workoutLogCards.length > 0 && (
+          <Stack>
+            <Text fw={700} size="xl">
+              Start Workout From Recent Workout
+            </Text>
+            <ScrollArea w={400}>
+              <Group wrap="nowrap">{workoutLogCards}</Group>
+            </ScrollArea>
+          </Stack>
+        )}
+      </Stack>
+    </Stack>
+  );
+}
