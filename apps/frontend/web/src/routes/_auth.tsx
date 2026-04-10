236a73038093ab43618621aac837157502062d3a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-10 15:57:01 -0700

    CWT-159 Move WorkoutProvider

--- apps/frontend/web/src/routes/_auth.tsx
+++ apps/frontend/web/src/routes/_auth.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-// import { Title, Stack, Text } from '@mantine/core';
+import { createFileRoute, redirect, Outlet } from '@tanstack/react-router';
+
 import { useAuthStore } from '@cwt/state/stores';
+import { WorkoutContextProvider } from '@cwt/context';
 
 export const Route = createFileRoute('/_auth')({
   beforeLoad: () => {
@@ -11,4 +12,13 @@ export const Route = createFileRoute('/_auth')({
       });
     }
   },
+  component: AuthLayout,
 });
+
+function AuthLayout() {
+  return (
+    <WorkoutContextProvider appType="web">
+      <Outlet />
+    </WorkoutContextProvider>
+  );
+}
