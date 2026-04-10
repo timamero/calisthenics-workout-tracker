8fbfac41645c68a35ee1027e5a04dc409534738e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-10 12:08:37 -0700

    CWT-159 Clean up code

--- apps/frontend/web/src/routes/_auth.tsx
+++ apps/frontend/web/src/routes/_auth.tsx
@@ -4,18 +4,11 @@ import { useAuthStore } from '@cwt/state/stores';
 
 export const Route = createFileRoute('/_auth')({
   beforeLoad: () => {
-    console.log('_auth || before load called');
     const user = useAuthStore.getState().user;
-    console.log('_auth || user: ', user);
     if (!user) {
       throw redirect({
         to: '/login',
       });
     }
   },
-  // component: AuthLayout,
 });
-
-// export function AuthLayout() {s
-//   return <Outlet />;
-// }
