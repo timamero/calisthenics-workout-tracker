f1c286e9af9f05096f7047cfc7556066c00ec63f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-10 15:57:01 -0700

    CWT-159 Move WorkoutProvider

--- apps/frontend/web/src/routes/__root.tsx
+++ apps/frontend/web/src/routes/__root.tsx
@@ -1,17 +1,15 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import { WorkoutContextProvider } from '@cwt/context';
-
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
   return (
-    <WorkoutContextProvider appType="web">
+    <>
       <Outlet />
       <TanStackRouterDevtools />
-    </WorkoutContextProvider>
+    </>
   );
 }
