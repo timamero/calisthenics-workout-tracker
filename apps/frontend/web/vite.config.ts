5fad83affe34978fc20ee298fe4456168d9dcb6e
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-15 22:24:08 -0700

    Use updated tanstackRouter object and fix import order

--- apps/frontend/web/vite.config.ts
+++ apps/frontend/web/vite.config.ts
@@ -1,13 +1,15 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
   ],
   test: {
