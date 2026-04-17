e0020d10d3d9c9d86ecac92a2d129d53b114c3a2
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-16 22:25:11 -0700

    CWT-326 Use explicit imports instead of wildcard

--- packages/hooks/src/lib/index.ts
+++ packages/hooks/src/lib/index.ts
@@ -1,5 +1,38 @@
-export * from "./auth";
-export * from "./exercises";
-export * from "./forms";
-export * from "./workout";
-export * from "./history";
+export { useSupabaseAuth } from './auth';
+export { useFilterSelectors, useSearchSelectors } from './exercises';
+export {
+  useAuthLogin,
+  useAuthLoginMobile,
+  useAuthSignUp,
+  useAuthSignUpMobile,
+} from './forms';
+export {
+  useAddExercise,
+  useAddExerciseMobile,
+  useParentItemsLength,
+  useReorderItem,
+  useReorderItemMobile,
+  useDeleteItem,
+  useDeleteItemMobile,
+  useUpdateWorkoutTitle,
+  useAddExerciseOverlay,
+  useAddExerciseOverlayMobile,
+  useAddSuperset,
+  useAddSupersetMobile,
+  useAddSet,
+  useAddSetMobile,
+  useDeleteSet,
+  useDeleteSetMobile,
+  useToggleCompleted,
+  useUpdateField,
+  useFieldInputChange,
+  useSupersetState,
+  useWorkoutContext,
+  useWorkoutContextWeb,
+  useWorkoutContextMobile,
+  useWorkoutSave,
+} from './workout';
+export {
+  useWorkoutLogDetailContextWeb,
+  useWorkoutLogDetailContextMobile,
+} from './history';
