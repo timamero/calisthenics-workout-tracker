b7b7225cdd98f854a8e548bad68af6eefc3f491a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-18 20:45:42 -0700

    CWT-382 Clean up code and remove superseded files

--- apps/frontend/web/src/routes/_auth/dashboard/library.tsx
+++ apps/frontend/web/src/routes/_auth/dashboard/library.tsx
@@ -6,10 +6,6 @@ import { IoFilterOutline } from 'react-icons/io5';
 
 import type { ExerciseResponse } from '@cwt/schema/exercises';
 import { ExerciseDetailContext } from '@cwt/context';
-// import { useAuthStore, useExerciseLibraryStore } from '@cwt/state/stores';
-
-// import { useSetExercisesData } from '../../../hooks/useSetExercisesData';
-// import { getExercises } from '../../../services/exercisesService';
 
 import ExercisesList from '../../../components/ExercisesList';
 import ExercisesFilterOverlay from '../../../components/ExercisesFilterOverlay';
@@ -17,30 +13,10 @@ import ExerciseDetailOverlay from '../../../components/ExerciseDetailOverlay';
 import ExerciseSearchBar from '../../../components/ExerciseSearchBar';
 
 export const Route = createFileRoute('/_auth/dashboard/library')({
-  // loader: async () => {
-  //   const displayedExercises =
-  //     useExerciseLibraryStore.getState().displayedExercises;
-
-  //   if (displayedExercises) {
-  //     console.log('library :: returning displayedExercises');
-  //     return displayedExercises;
-  //   }
-
-  //   console.log('library :: fetching exercises from database');
-  //   const supabaseSession = useAuthStore.getState().session;
-  //   if (supabaseSession?.access_token) {
-  //     console.time('fetch exercises in library');
-  //     const exercises = await getExercises(supabaseSession.access_token);
-  //     console.timeEnd('fetch exercises in library');
-  //     return exercises;
-  //   }
-  // },
   component: LibraryView,
 });
 
 function LibraryView() {
-  // const exercises: ExerciseResponse[] = Route.useLoaderData();
-
   const [exerciseDetail, setExerciseDetail] = useState<ExerciseResponse | null>(
     null,
   );
@@ -48,24 +24,10 @@ function LibraryView() {
   const [filterOpened, filterHandler] = useDisclosure(false);
   const [detailOpened, detailHandlers] = useDisclosure(false);
 
-  // const loading = useExerciseLibraryStore((state) => state.loading);
-  // const isExercisesSet = useExerciseLibraryStore((state) =>
-  //   state.displayedExercises === null ? false : true,
-  // );
-
   const handleClickFilter = () => {
     filterHandler.open();
   };
 
-  // useSetExercisesData(exercises);
-
-  // if (!isExercisesSet || loading) {
-  //   return (
-  //     <Stack>
-  //       <Title size="h6">Loading</Title>
-  //     </Stack>
-  //   );
-  // }
   return (
     <ExerciseDetailContext.Provider
       value={{
