036d291709f2b79f054425eed7ac8cca32131c1a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-18 23:14:10 -0700

    CWT-382 Move fetching of logs and templates

--- apps/frontend/web/src/routes/_auth/dashboard/history.tsx
+++ apps/frontend/web/src/routes/_auth/dashboard/history.tsx
@@ -1,51 +1,51 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { Title, Stack } from '@mantine/core';
 
-import { useWorkoutLibraryStore, useAuthStore } from '@cwt/state/stores';
+// import { useWorkoutLibraryStore } from '@cwt/state/stores';
 import { WorkoutLogDetailContextProvider } from '@cwt/context';
-import type { WorkoutLogResponse } from '@cwt/schema/workouts';
+// import type { WorkoutLogResponse } from '@cwt/schema/workouts';
 
-import { getWorkoutLogs } from '../../../services/workoutsService';
+// import { getWorkoutLogs } from '../../../services/workoutsService';
 
 import WorkoutLogPages from '../../../components/WorkoutLogPages';
 
 export const Route = createFileRoute('/_auth/dashboard/history')({
-  loader: async () => {
-    let logs: WorkoutLogResponse[] | null = null;
+  // loader: async () => {
+  //   let logs: WorkoutLogResponse[] | null = null;
 
-    const displayedWorkoutLogs =
-      useWorkoutLibraryStore.getState().displayedWorkoutLogs;
+  //   const displayedWorkoutLogs =
+  //     useWorkoutLibraryStore.getState().displayedWorkoutLogs;
 
-    if (displayedWorkoutLogs && displayedWorkoutLogs.length > 0) {
-      logs = displayedWorkoutLogs as WorkoutLogResponse[];
-    }
-    const supabaseSession = useAuthStore.getState().session;
-    if (supabaseSession?.access_token) {
-      if (!logs) {
-        console.time('fetch workout logs in history');
-        const fetchedWorkoutLogs = await getWorkoutLogs(
-          supabaseSession.access_token,
-        );
-        console.timeEnd('fetch workout logs in history');
-        logs = fetchedWorkoutLogs;
-      }
-    }
-    return { logs };
-  },
+  //   if (displayedWorkoutLogs && displayedWorkoutLogs.length > 0) {
+  //     logs = displayedWorkoutLogs as WorkoutLogResponse[];
+  //   }
+  //   const supabaseSession = useAuthStore.getState().session;
+  //   if (supabaseSession?.access_token) {
+  //     if (!logs) {
+  //       console.time('fetch workout logs in history');
+  //       const fetchedWorkoutLogs = await getWorkoutLogs(
+  //         supabaseSession.access_token,
+  //       );
+  //       console.timeEnd('fetch workout logs in history');
+  //       logs = fetchedWorkoutLogs;
+  //     }
+  //   }
+  //   return { logs };
+  // },
   component: HistoryView,
 });
 
 function HistoryView() {
-  const data: {
-    logs: WorkoutLogResponse[];
-  } = Route.useLoaderData();
+  // const data: {
+  //   logs: WorkoutLogResponse[];
+  // } = Route.useLoaderData();
 
-  const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
+  // const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
 
-  useEffect(() => {
-    setWorkouts(data.logs, []);
-  }, [data, setWorkouts]);
+  // useEffect(() => {
+  //   setWorkouts(data.logs, []);
+  // }, [data, setWorkouts]);
 
   return (
     <WorkoutLogDetailContextProvider>
