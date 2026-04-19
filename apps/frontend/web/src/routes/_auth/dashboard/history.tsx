f42ebcc2802b3308556560d8d671b3db070e922b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-18 20:41:51 -0700

    CWT-382 Move exercise fetching

--- apps/frontend/web/src/routes/_auth/dashboard/history.tsx
+++ apps/frontend/web/src/routes/_auth/dashboard/history.tsx
@@ -5,29 +5,29 @@ import { Title, Stack } from '@mantine/core';
 import {
   useWorkoutLibraryStore,
   useAuthStore,
-  useExerciseLibraryStore,
+  // useExerciseLibraryStore,
 } from '@cwt/state/stores';
 import { WorkoutLogDetailContextProvider } from '@cwt/context';
 import type { WorkoutLogResponse } from '@cwt/schema/workouts';
-import type { ExerciseResponse } from '@cwt/schema/exercises';
+// import type { ExerciseResponse } from '@cwt/schema/exercises';
 
 import { getWorkoutLogs } from '../../../services/workoutsService';
-import { getExercises } from '../../../services/exercisesService';
+// import { getExercises } from '../../../services/exercisesService';
 
 import WorkoutLogPages from '../../../components/WorkoutLogPages';
-import { useSetExercisesData } from '../../../hooks/useSetExercisesData';
+// import { useSetExercisesData } from '../../../hooks/useSetExercisesData';
 
 export const Route = createFileRoute('/_auth/dashboard/history')({
   loader: async () => {
     let logs: WorkoutLogResponse[] | null = null;
-    let exercises: ExerciseResponse[] | null = null;
+    // let exercises: ExerciseResponse[] | null = null;
 
-    const displayedExercises =
-      useExerciseLibraryStore.getState().displayedExercises;
-    if (displayedExercises) {
-      console.log('history :: setting exercises to displayedExercises');
-      exercises = displayedExercises;
-    }
+    // const displayedExercises =
+    //   useExerciseLibraryStore.getState().displayedExercises;
+    // if (displayedExercises) {
+    //   console.log('history :: setting exercises to displayedExercises');
+    //   exercises = displayedExercises;
+    // }
 
     const displayedWorkoutLogs =
       useWorkoutLibraryStore.getState().displayedWorkoutLogs;
@@ -45,16 +45,17 @@ export const Route = createFileRoute('/_auth/dashboard/history')({
         console.timeEnd('fetch workout logs in history');
         logs = fetchedWorkoutLogs;
       }
-      if (!exercises) {
-        console.time('fetch exercises in history');
-        const fetchedExercises = await getExercises(
-          supabaseSession.access_token,
-        );
-        console.timeEnd('fetch exercises in history');
-        exercises = fetchedExercises;
-      }
+      // if (!exercises) {
+      //   console.time('fetch exercises in history');
+      //   const fetchedExercises = await getExercises(
+      //     supabaseSession.access_token,
+      //   );
+      //   console.timeEnd('fetch exercises in history');
+      //   exercises = fetchedExercises;
+      // }
     }
-    return { logs, exercises };
+    return { logs };
+    // return { logs, exercises };
   },
   component: HistoryView,
 });
@@ -62,12 +63,12 @@ export const Route = createFileRoute('/_auth/dashboard/history')({
 function HistoryView() {
   const data: {
     logs: WorkoutLogResponse[];
-    exercises: ExerciseResponse[];
+    // exercises: ExerciseResponse[];
   } = Route.useLoaderData();
 
   const setWorkouts = useWorkoutLibraryStore((state) => state.setWorkouts);
 
-  useSetExercisesData(data.exercises);
+  // useSetExercisesData(data.exercises);
 
   useEffect(() => {
     setWorkouts(data.logs, []);
