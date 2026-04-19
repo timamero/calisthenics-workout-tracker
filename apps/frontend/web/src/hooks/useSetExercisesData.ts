73b50a28e373b74bddf151e1d493a81ec1862f0a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-18 20:20:33 -0700

    CWT-382 Add dev logs

--- apps/frontend/web/src/hooks/useSetExercisesData.ts
+++ apps/frontend/web/src/hooks/useSetExercisesData.ts
@@ -9,8 +9,17 @@ export function useSetExercisesData(exercises: ExerciseResponse[]) {
   const isExercisesSet = useExerciseLibraryStore((state) =>
     state.displayedExercises === null ? false : true,
   );
+
+  // Exercises are already set
+  // if (!exercises) {
+  //   console.log('useSetExerciseData :: exiting hook, exercises already set');
+  //   return null
+  // }
+  console.log('useSetExerciseData :: isExercisesSet', isExercisesSet);
   useEffect(() => {
+    console.log('useSetExerciseData :: useEffect called');
     if (!isExercisesSet) {
+      console.log('useSetExerciseData :: setting exercises and loading');
       setExercises(exercises);
       setLoading(false);
     }
