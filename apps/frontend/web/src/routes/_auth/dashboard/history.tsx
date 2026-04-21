53759911e1ce228167578aa40238e9bc1edb6b1d
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-21 16:10:57 -0700

    CWT-383 Remove comments and debug styles

--- apps/frontend/web/src/routes/_auth/dashboard/history.tsx
+++ apps/frontend/web/src/routes/_auth/dashboard/history.tsx
@@ -13,13 +13,10 @@ function HistoryView() {
   return (
     <WorkoutLogDetailContextProvider>
       <Stack
-        // h="100%"
-        // mah={1200}
         mih="100%"
         pb="lg"
         flex={1}
         display="flex"
-        bg="grape.6"
         style={{ flexDirection: 'column' }}
       >
         <Title>Past Workouts</Title>
