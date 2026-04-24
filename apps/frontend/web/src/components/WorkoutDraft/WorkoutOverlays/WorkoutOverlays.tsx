b3769ab7bf7a4e4dd06733216d2fe7e90d35eea7
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-23 23:14:08 -0700

    CWT-329 Remove AddExerciseOverlay

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutOverlays/WorkoutOverlays.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutOverlays/WorkoutOverlays.tsx
@@ -1,11 +1,5 @@
-import AddExerciseOverlay from './AddExerciseOverlay';
 import ConfirmationOverlays from './ConfirmationOverlays';
 
 export default function WorkoutOverlays() {
-  return (
-    <>
-      <AddExerciseOverlay />
-      <ConfirmationOverlays />
-    </>
-  );
+  return <ConfirmationOverlays />;
 }
