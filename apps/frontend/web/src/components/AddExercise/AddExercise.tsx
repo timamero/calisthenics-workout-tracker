1b9b3202ecd965befb590c782825cf022de41509
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-23 19:33:40 -0700

    CWT-329 Create new handleAddExerciseClick function

--- apps/frontend/web/src/components/AddExercise/AddExercise.tsx
+++ apps/frontend/web/src/components/AddExercise/AddExercise.tsx
@@ -1,15 +1,25 @@
 import { useAddExercise } from '@cwt/hooks';
+import { useNavigate } from '@tanstack/react-router';
 // import { useWorkoutContextWeb } from '@cwt/hooks';
 
 import AddExerciseUI from './AddExerciseUI';
 
 export default function AddExercise() {
-  const { selectedExerciseIDToAdd, handleAddExerciseClick } = useAddExercise();
+  const navigate = useNavigate();
+
+  const { selectedExerciseIDToAdd, handleAddExercise } = useAddExercise();
 
   // const opened =
   //   useWorkoutContextWeb().webOverlayHandlers?.addExerciseOverlayOpened;
   // const handler =
   //   useWorkoutContextWeb().webOverlayHandlers?.addExerciseOverlayHandler;
+
+  const handleAddExerciseClick = () => {
+    handleAddExercise();
+    navigate({
+      to: '/workout',
+    });
+  };
 
   return (
     <AddExerciseUI
