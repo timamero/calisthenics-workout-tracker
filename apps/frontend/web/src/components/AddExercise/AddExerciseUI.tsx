ed704993740f34c43d828210adebc0dbf08e0916
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-23 20:23:02 -0700

    CWT-329 Update layout

--- apps/frontend/web/src/components/AddExercise/AddExerciseUI.tsx
+++ apps/frontend/web/src/components/AddExercise/AddExerciseUI.tsx
@@ -1,4 +1,4 @@
-import { Group, Button, Stack, ScrollArea } from '@mantine/core';
+import { Group, Button, Stack } from '@mantine/core';
 
 // import type { AddExerciseOverlayUIProps } from '@cwt/schema/workouts';
 
@@ -16,55 +16,40 @@ export default function AddExerciseOverlayUI({
   handleAddExerciseClick,
 }: AddExerciseUIProps) {
   return (
-    // <Modal
-    //   opened={opened!}
-    //   onClose={() => handler!.close()}
-    //   title="Add Exercise"
-    //   fullScreen
-    //   styles={{
-    //     title: {
-    //       fontFamily: 'var(--mantine-font-family-headings)',
-    //       fontWeight: 700,
-    //     },
-    //   }}
-    // >
-    <div>
-      <div style={{ position: 'relative', minHeight: '90vh' }}>
-        <ScrollArea h="90vh" style={{ paddingBottom: 40 }}>
-          <Stack gap="lg" align="center">
-            <ExercisesList />
-          </Stack>
-        </ScrollArea>
-        <Group
-          mt="lg"
-          justify="center"
-          bg="gray.0"
-          style={{
-            position: 'sticky',
-            bottom: 0,
-            background: 'var(--mantine-color-body)',
-            zIndex: 2,
-            padding: '16px 0',
-            boxShadow: '0 -2px 8px rgba(0,0,0,0.04)',
-          }}
+    <Stack gap="xl">
+      {/* <ScrollArea h="90vh" style={{ paddingBottom: 40 }}> */}
+      <Stack gap="lg" align="center">
+        <ExercisesList />
+      </Stack>
+      {/* </ScrollArea> */}
+      <Group
+        mt="lg"
+        justify="center"
+        bg="gray.0"
+        style={{
+          position: 'sticky',
+          bottom: 0,
+          background: 'var(--mantine-color-body)',
+          zIndex: 2,
+          padding: '16px 0',
+          boxShadow: '0 -2px 8px rgba(0,0,0,0.04)',
+        }}
+      >
+        <Button
+          color="gray"
+          variant="outline"
+          onClick={() => console.log('clicked close')}
         >
-          <Button
-            color="gray"
-            variant="outline"
-            onClick={() => console.log('clicked close')}
-          >
-            Cancel
-          </Button>
-          <Button
-            color="orange"
-            onClick={() => handleAddExerciseClick?.()}
-            data-disabled={selectedExerciseIDToAdd === null}
-          >
-            Add Exercise
-          </Button>
-        </Group>
-      </div>
-    </div>
-    // </Modal>
+          Cancel
+        </Button>
+        <Button
+          color="orange"
+          onClick={() => handleAddExerciseClick?.()}
+          data-disabled={selectedExerciseIDToAdd === null}
+        >
+          Add Exercise
+        </Button>
+      </Group>
+    </Stack>
   );
 }
