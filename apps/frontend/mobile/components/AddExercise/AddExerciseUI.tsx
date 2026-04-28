13463acb1073bdc6b6da4287436fbddb146505db
Fatima Camero <54559570+timamero@users.noreply.github.com>
2026-04-28 00:06:14 -0700

    CWT-329 Show add exercises content

---
+++ apps/frontend/mobile/components/AddExercise/AddExerciseUI.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { ScrollView, View } from 'react-native';
+import { useTheme, Button } from 'react-native-paper';
+
+// import type { AddExerciseOverlayUIProps } from '@cwt/schema/workouts';
+
+import { CustomTheme } from '../../theme';
+
+// import FullScreenModal from '../common/FullScreenModal';
+import ExerciseList from './ExerciseList';
+
+interface AddExerciseUIProps {
+  selectedExerciseIDToAdd: number | null;
+  handleAddExercisePress: () => void;
+}
+
+export default function AddExerciseUI({
+  // isVisible,
+  selectedExerciseIDToAdd,
+  // setIsVisible,
+  handleAddExercisePress,
+}: AddExerciseUIProps) {
+  const theme = useTheme() as CustomTheme;
+
+  return (
+    <>
+      <ScrollView
+        style={{
+          backgroundColor: theme.colors.background,
+          paddingInline: 16,
+          paddingBlock: 16,
+        }}
+      >
+        <ExerciseList />
+      </ScrollView>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          paddingTop: 20,
+          borderTopWidth: 2,
+          borderTopColor: theme.colors.orange1,
+        }}
+      >
+        <Button
+          mode="outlined"
+          textColor={theme.colors.light}
+          onPress={() => console.log('pressed cancel')}
+          style={{
+            borderColor: 'rgb(134, 142, 150)',
+            borderRadius: 4,
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          disabled={selectedExerciseIDToAdd === null}
+          mode="contained"
+          onPress={() => handleAddExercisePress?.()}
+          style={{
+            borderRadius: 4,
+          }}
+          theme={{
+            colors: {
+              surfaceDisabled: theme.colors.dark400,
+              onSurfaceDisabled: theme.colors.dark700,
+            },
+          }}
+        >
+          Add Exercise
+        </Button>
+      </View>
+    </>
+  );
+}
---
+++ apps/frontend/mobile/components/AddExercise/ExerciseList.tsx
@@ -0,0 +1,32 @@
+import {
+  useExerciseLibraryStore,
+  useWorkoutDraftStore,
+} from '@cwt/state/stores';
+
+import ExerciseCard from './ExerciseCard';
+
+export default function ExerciseList() {
+  const exercises = useExerciseLibraryStore(
+    (state) => state.displayedExercises,
+  );
+
+  const selectedExerciseIDToAdd = useWorkoutDraftStore(
+    (state) => state.selectedExerciseIDToAdd,
+  );
+  const setSelectedExerciseIDToAdd = useWorkoutDraftStore(
+    (state) => state.setSelectedExerciseIDToAdd,
+  );
+
+  return (
+    <>
+      {exercises?.map((exercise) => (
+        <ExerciseCard
+          exercise={exercise}
+          key={exercise.id}
+          onExercisePress={() => setSelectedExerciseIDToAdd(exercise.id)}
+          isSelected={exercise.id === selectedExerciseIDToAdd ? true : false}
+        />
+      ))}
+    </>
+  );
+}
