c7d05c1d7519614cef5eccc90771af2441588713
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-04-30 21:47:58 -0700

    CWT-329 Disable profile page

--- apps/frontend/mobile/screens/Home.tsx
+++ apps/frontend/mobile/screens/Home.tsx
@@ -17,23 +17,24 @@ export default function HomeScreen() {
 
   // useFetchLeveragesAssists();
 
-  React.useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button
-          mode="text"
-          icon="account"
-          onPress={() => navigation.navigate('Profile')}
-          style={{
-            marginRight: 24,
-          }}
-          textColor={theme.colors.grey}
-        >
-          Profile
-        </Button>
-      ),
-    });
-  }, [navigation, theme.colors.grey]);
+  // temporarily disabled until v0.1.0-alpha.2
+  // React.useEffect(() => {
+  //   navigation.setOptions({
+  //     headerRight: () => (
+  //       <Button
+  //         mode="text"
+  //         icon="account"
+  //         onPress={() => navigation.navigate('Profile')}
+  //         style={{
+  //           marginRight: 24,
+  //         }}
+  //         textColor={theme.colors.grey}
+  //       >
+  //         Profile
+  //       </Button>
+  //     ),
+  //   });
+  // }, [navigation, theme.colors.grey]);
 
   return (
     <View
