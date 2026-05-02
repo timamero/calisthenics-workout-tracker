3b0b81f68e05873379e0cbc34aba1cf81e12beab
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-02 13:39:38 -0700

    CWT-386 Create and render edit name input

--- apps/frontend/mobile/screens/SettingsScreen.tsx
+++ apps/frontend/mobile/screens/SettingsScreen.tsx
@@ -1,15 +1,23 @@
 import { View, Text } from 'react-native';
 import { useTheme, Button } from 'react-native-paper';
 
-import { signOut } from '@cwt/auth';
+import { signOut, updateUserName } from '@cwt/auth';
+import { useUser } from '@cwt/hooks';
 
 import { supabase } from '../services/supabaseClient';
+import TextInputWithEdit from '../components/common/TextInputWithEdit';
 
 export default function SettingsScreen() {
   const theme = useTheme();
 
+  const name = useUser().name;
+
   const handleLogOut = async () => {
     await signOut(supabase);
+  };
+
+  const handleOnSave = (text: string) => {
+    updateUserName(supabase, text);
   };
 
   return (
@@ -17,12 +25,16 @@ export default function SettingsScreen() {
       style={{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.colors.primary,
+        justifyContent: 'flex-start',
+        backgroundColor: theme.colors.background,
       }}
     >
-      <Text>Settings Screen</Text>
-      <Text>This page will contain the app settings.</Text>
+      <Text>Settings</Text>
+      <Text>Name</Text>
+      <TextInputWithEdit
+        initialValue={name ? name : ''}
+        onSave={handleOnSave}
+      />
       <Button
         mode="outlined"
         textColor={theme.colors.outline}
