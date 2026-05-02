7ff127702f7032eb921d7436fd014d7e0f147d9d
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-01 20:03:37 -0700

    CWT-386 Create updateUserName func and update architecture

---
+++ packages/auth/src/lib/index.ts
@@ -0,0 +1,8 @@
+export { createUser } from "./createUser";
+export { session } from "./session";
+export { signIn } from "./signIn";
+export { signOut } from "./signOut";
+export { supabaseClient } from "./supabase";
+export type { SupabaseClient, Session, User } from "./supabaseTypes";
+export { updateUserName } from "./updateUserName";
+export { user } from "./user";
