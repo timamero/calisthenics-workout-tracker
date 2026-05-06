5ac0cba33490199be34f7cc71e6fda6e28425f05
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-06 12:33:55 -0700

    CWT-384 Create layout component and conditionally display header

--- apps/frontend/web/src/routes/_auth/workout/route.tsx
+++ apps/frontend/web/src/routes/_auth/workout/route.tsx
@@ -1,39 +1,11 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
-import { AppShell, Button } from '@mantine/core';
+import { createFileRoute } from '@tanstack/react-router';
 
-import { useWorkoutContextWeb } from '@cwt/hooks';
+import WorkoutLayout from '../../../components/layouts/WorkoutLayout';
 
 export const Route = createFileRoute('/_auth/workout')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const cancelOverlayHandler =
-    useWorkoutContextWeb().webOverlayHandlers?.cancelOverlayHandler;
-
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: true, desktop: true },
-      }}
-      padding="md"
-    >
-      <AppShell.Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Button
-          variant="outline"
-          color="gray"
-          ml={16}
-          onClick={() => cancelOverlayHandler!.open()}
-        >
-          Cancel
-        </Button>
-      </AppShell.Header>
-      <AppShell.Main>
-        <Outlet />
-      </AppShell.Main>
-    </AppShell>
-  );
+  return <WorkoutLayout />;
 }
