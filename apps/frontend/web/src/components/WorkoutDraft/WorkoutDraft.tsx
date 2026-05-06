3a8b2d387e96b94c69483a131d7efe32a9e9b814
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-06 15:30:08 -0700

    CWT-384 Refactor title to use TextInputWithEdit component

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutDraft.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutDraft.tsx
@@ -1,13 +1,22 @@
 import { Stack } from '@mantine/core';
 
+import { useWorkoutDraftStore } from '@cwt/state/stores';
+
 import WorkoutData from './WorkoutData';
-import { WorkoutTitleContainer as WorkoutTitle } from './WorkoutTitle';
 import WorkoutOverlays from './WorkoutOverlays';
+import { TextInputWithEdit } from '../common/TextInputWithEdit';
 
 export default function WorkoutDraft() {
+  const workoutTitle = useWorkoutDraftStore((state) => state.workoutTitle);
+  const setWorkoutTitle = useWorkoutDraftStore(
+    (state) => state.setWorkoutTitle,
+  );
   return (
     <Stack gap="xl" align="center">
-      <WorkoutTitle />
+      <TextInputWithEdit
+        initialValue={workoutTitle!}
+        onSave={setWorkoutTitle}
+      />
       <Stack gap="xl" align="center">
         <WorkoutData />
       </Stack>
