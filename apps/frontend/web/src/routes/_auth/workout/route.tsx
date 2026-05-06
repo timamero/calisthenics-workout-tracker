639f9577c24c0c64799e3994a2ea06dc477e38c1
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-06 00:47:09 -0700

    CWT-384 Move cancel button to header

--- apps/frontend/web/src/routes/_auth/workout/route.tsx
+++ apps/frontend/web/src/routes/_auth/workout/route.tsx
@@ -1,11 +1,16 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
-import { AppShell } from '@mantine/core';
+import { AppShell, Button } from '@mantine/core';
+
+import { useWorkoutContextWeb } from '@cwt/hooks';
 
 export const Route = createFileRoute('/_auth/workout')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const cancelOverlayHandler =
+    useWorkoutContextWeb().webOverlayHandlers?.cancelOverlayHandler;
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -16,8 +21,15 @@ function RouteComponent() {
       }}
       padding="md"
     >
-      <AppShell.Header>
-        <div>Logo</div>
+      <AppShell.Header style={{ display: 'flex', alignItems: 'center' }}>
+        <Button
+          variant="outline"
+          color="gray"
+          ml={16}
+          onClick={() => cancelOverlayHandler!.open()}
+        >
+          Cancel
+        </Button>
       </AppShell.Header>
       <AppShell.Main>
         <Outlet />
