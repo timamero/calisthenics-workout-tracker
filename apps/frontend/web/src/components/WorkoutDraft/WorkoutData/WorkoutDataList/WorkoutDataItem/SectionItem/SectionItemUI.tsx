10494a4e2e4fc080c2dc4fabb0bed6ba2a65fb06
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-07 00:52:28 -0700

    CWT-384 Create WorkoutItemMenu component

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
@@ -1,11 +1,4 @@
-import { useState } from 'react';
-import {
-  IoEllipsisVertical,
-  IoTrashBin,
-  IoChevronUpCircle,
-  IoChevronDownCircle,
-} from 'react-icons/io5';
-import { Stack, Group, Button, Menu, ActionIcon, Box } from '@mantine/core';
+import { Stack, Group, Button, Box } from '@mantine/core';
 import { Link } from '@tanstack/react-router';
 
 import type { Mode } from '@cwt/schema/workouts';
@@ -13,6 +6,7 @@ import type { Mode } from '@cwt/schema/workouts';
 import ItemsList from './ItemsList';
 import { TextInputWithEdit } from '../../../../../common/TextInputWithEdit';
 import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
+import WorkoutItemMenu from '../WorkoutItemMenu';
 
 interface SectionItemUIProps {
   mode: Mode;
@@ -27,14 +21,6 @@ interface SectionItemUIProps {
   handleSetSectionTitle: (title: string) => void;
 }
 
-// function MenuLabel({ children }: { children: ReactNode }) {
-//   return (
-//     <Menu.Label style={{ display: 'flex', justifyContent: 'center' }}>
-//       {children}
-//     </Menu.Label>
-//   );
-// }
-
 export default function SectionItemUI({
   mode,
   isFirst,
@@ -47,24 +33,6 @@ export default function SectionItemUI({
   handleAddSupersetClick,
   handleSetSectionTitle,
 }: SectionItemUIProps) {
-  const [menuOpened, setMenuOpened] = useState(false);
-
-  const onUpClick = () => {
-    // setMenuOpened(() => false);
-    // handleUpClick();
-    setMenuOpened(false);
-    setTimeout(() => {
-      handleUpClick();
-    }, 100);
-  };
-  const onDownClick = () => {
-    // setMenuOpened(() => false);
-    // handleDownClick();
-    setMenuOpened(false);
-    setTimeout(() => {
-      handleDownClick();
-    }, 100);
-  };
   return (
     <Stack
       bg="lime.0"
@@ -103,49 +71,13 @@ export default function SectionItemUI({
         </Group>
         {(mode === 'edit' || mode === 'build') && (
           <Box p="md">
-            <Menu opened={menuOpened} shadow="md" width={200}>
-              <Menu.Target>
-                <ActionIcon
-                  variant="transparent"
-                  size="md"
-                  p="xs"
-                  w="min-content"
-                  h="min-content"
-                  onClick={() => setMenuOpened(!menuOpened)}
-                >
-                  <IoEllipsisVertical size={24} />
-                </ActionIcon>
-              </Menu.Target>
-
-              <Menu.Dropdown bg="gray.2">
-                <Menu.Item
-                  c="red"
-                  onClick={() => {
-                    setMenuOpened(false);
-                    handleDeleteSectionClick();
-                  }}
-                  leftSection={<IoTrashBin size={18} />}
-                >
-                  Delete Section
-                </Menu.Item>
-                {!isFirst && (
-                  <Menu.Item
-                    onClick={() => onUpClick()}
-                    leftSection={<IoChevronUpCircle size={18} />}
-                  >
-                    Move Up
-                  </Menu.Item>
-                )}
-                {!isLast && (
-                  <Menu.Item
-                    onClick={() => onDownClick()}
-                    leftSection={<IoChevronDownCircle size={18} />}
-                  >
-                    Move Down
-                  </Menu.Item>
-                )}
-              </Menu.Dropdown>
-            </Menu>
+            <WorkoutItemMenu
+              handleUpClick={handleUpClick}
+              handleDownClick={handleDownClick}
+              handleDeleteClick={handleDeleteSectionClick}
+              isFirst={isFirst}
+              isLast={isLast}
+            />
           </Box>
         )}
       </Group>
