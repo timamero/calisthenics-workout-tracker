b51d3f020d751a12cac8f77f2de2c2a09a317504
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-07 01:19:43 -0700

    CWT-384 Add menu to each workout item and update style

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SupersetItem/SupersetItemUI.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SupersetItem/SupersetItemUI.tsx
@@ -1,4 +1,4 @@
-import { Stack, Group, Text, Button } from '@mantine/core';
+import { Stack, Group, Text, Button, Box } from '@mantine/core';
 import { Link } from '@tanstack/react-router';
 
 import type { Mode, Superset } from '@cwt/schema/workouts';
@@ -6,6 +6,7 @@ import type { Mode, Superset } from '@cwt/schema/workouts';
 import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
 import ExerciseSetGroup from '../ExerciseSetGroup';
 import ExercisesList from './ExercisesList';
+import WorkoutItemMenu from '../WorkoutItemMenu';
 
 interface SupersetItemUIProps {
   mode: Mode;
@@ -28,32 +29,37 @@ export default function SupersetItemUI({
   handleOpenAddExerciseClick,
 }: SupersetItemUIProps) {
   return (
-    <Stack
-      bg="transparent"
-      p="xs"
-      bd="4px solid var(--mantine-color-default-border)"
-      bdrs="md"
-      w="100%"
-      maw={600}
-    >
-      <Group>
+    <Stack bg="transparent" w="100%" maw={600} align="center" gap={0}>
+      <Group
+        bd="2px solid var(--mantine-color-default-border)"
+        bdrs="lg"
+        wrap="nowrap"
+        w="100%"
+      >
+        <Group justify="space-between" wrap="nowrap" w="100%" p="xs">
+          {(mode === 'edit' || mode === 'build') && (
+            <ReorderButtonGroup
+              handleUpClick={() => handleUpClick()}
+              handleDownClick={() => handleDownClick()}
+              isFirst={isFirst}
+              isLast={isLast}
+            />
+          )}
+          <Group flex={1} h={104} align="center" justify="center">
+            <Text>Superset</Text>
+          </Group>
+        </Group>
         {(mode === 'edit' || mode === 'build') && (
-          <ReorderButtonGroup
-            handleUpClick={() => handleUpClick()}
-            handleDownClick={() => handleDownClick()}
-            isFirst={isFirst}
-            isLast={isLast}
-          />
-        )}
-        <Text>Superset</Text>
-        {(mode === 'edit' || mode === 'build') && (
-          <Button
-            color="red"
-            variant="white"
-            onClick={() => handleDeleteSupersetClick()}
-          >
-            Delete
-          </Button>
+          <Box p="md">
+            <WorkoutItemMenu
+              itemType="superset"
+              handleUpClick={handleUpClick}
+              handleDownClick={handleDownClick}
+              handleDeleteClick={handleDeleteSupersetClick}
+              isFirst={isFirst}
+              isLast={isLast}
+            />
+          </Box>
         )}
       </Group>
       {mode === 'edit' || mode === 'build' ? (
