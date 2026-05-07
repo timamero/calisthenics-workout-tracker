069792f409d3bd490b49bd15eab4a7eeae2477dc
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-07 00:08:08 -0700

    CWT-384 Create dropdown menu

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
@@ -1,9 +1,15 @@
-import { Stack, Group, Button } from '@mantine/core';
+import { useState } from 'react';
+import {
+  IoEllipsisVertical,
+  IoTrashBin,
+  IoChevronUpCircle,
+  IoChevronDownCircle,
+} from 'react-icons/io5';
+import { Stack, Group, Button, Menu, ActionIcon } from '@mantine/core';
 import { Link } from '@tanstack/react-router';
 
 import type { Mode } from '@cwt/schema/workouts';
 
-import ReorderButtonGroup from '../../../../../common/ReorderButtonGroup';
 import ItemsList from './ItemsList';
 import { TextInputWithEdit } from '../../../../../common/TextInputWithEdit';
 
@@ -20,6 +26,14 @@ interface SectionItemUIProps {
   handleSetSectionTitle: (title: string) => void;
 }
 
+// function MenuLabel({ children }: { children: ReactNode }) {
+//   return (
+//     <Menu.Label style={{ display: 'flex', justifyContent: 'center' }}>
+//       {children}
+//     </Menu.Label>
+//   );
+// }
+
 export default function SectionItemUI({
   mode,
   isFirst,
@@ -32,6 +46,24 @@ export default function SectionItemUI({
   handleAddSupersetClick,
   handleSetSectionTitle,
 }: SectionItemUIProps) {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const onUpClick = () => {
+    // setMenuOpened(() => false);
+    // handleUpClick();
+    setMenuOpened(false);
+    setTimeout(() => {
+      handleUpClick();
+    }, 100);
+  };
+  const onDownClick = () => {
+    // setMenuOpened(() => false);
+    // handleDownClick();
+    setMenuOpened(false);
+    setTimeout(() => {
+      handleDownClick();
+    }, 100);
+  };
   return (
     <Stack
       bg="lime.0"
@@ -43,14 +75,6 @@ export default function SectionItemUI({
       align="center"
     >
       <Group>
-        {(mode === 'edit' || mode === 'build') && (
-          <ReorderButtonGroup
-            handleUpClick={() => handleUpClick()}
-            handleDownClick={() => handleDownClick()}
-            isFirst={isFirst}
-            isLast={isLast}
-          />
-        )}
         <TextInputWithEdit
           initialValue={title}
           onSave={handleSetSectionTitle}
@@ -59,13 +83,47 @@ export default function SectionItemUI({
           maxLength={70}
         />
         {(mode === 'edit' || mode === 'build') && (
-          <Button
-            color="red"
-            variant="white"
-            onClick={() => handleDeleteSectionClick()}
-          >
-            Delete
-          </Button>
+          <Menu opened={menuOpened} shadow="md" width={200}>
+            <Menu.Target>
+              <ActionIcon
+                variant="transparent"
+                size="md"
+                w="min-content"
+                onClick={() => setMenuOpened(!menuOpened)}
+              >
+                <IoEllipsisVertical size={30} />
+              </ActionIcon>
+            </Menu.Target>
+
+            <Menu.Dropdown bg="gray.2">
+              <Menu.Item
+                c="red"
+                onClick={() => {
+                  setMenuOpened(false);
+                  handleDeleteSectionClick();
+                }}
+                leftSection={<IoTrashBin size={18} />}
+              >
+                Delete Section
+              </Menu.Item>
+              {!isFirst && (
+                <Menu.Item
+                  onClick={() => onUpClick()}
+                  leftSection={<IoChevronUpCircle size={18} />}
+                >
+                  Move Up
+                </Menu.Item>
+              )}
+              {!isLast && (
+                <Menu.Item
+                  onClick={() => onDownClick()}
+                  leftSection={<IoChevronDownCircle size={18} />}
+                >
+                  Move Down
+                </Menu.Item>
+              )}
+            </Menu.Dropdown>
+          </Menu>
         )}
       </Group>
       <ItemsList />
