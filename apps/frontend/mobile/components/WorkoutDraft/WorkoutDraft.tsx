f0966a73a13a04960f613f0703ab10699b351f7a
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-07 23:18:19 -0700

    CWT-384 Create BottomAppBar component

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutDraft.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutDraft.tsx
@@ -5,31 +5,23 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
-  StyleSheet,
 } from 'react-native';
-import { useTheme, Button, SegmentedButtons, Appbar } from 'react-native-paper';
+import { useTheme, Button } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import type { Mode } from '@cwt/schema/workouts';
-import {
-  useWorkoutDraftStore,
-  useWorkoutStopwatchStore,
-} from '@cwt/state/stores';
+import { useWorkoutDraftStore } from '@cwt/state/stores';
 import { useWorkoutContextMobile } from '@cwt/hooks';
 
 import WorkoutDraftContext from '../../contexts/WorkoutDraftContext';
 import WorkoutData from './WorkoutData';
 import WorkoutOverlays from './WorkoutOverlays';
-import AddWorkoutItemButtons from './AddWorkoutItemButtons';
 import type { CustomTheme } from '../../theme';
 import TextInputWithEdit from '../common/TextInputWithEdit';
 import CustomButton from '../common/CustomButton';
-
-const BOTTOM_APPBAR_HEIGHT = 80;
+import BottomAppBar from './BottomAppBar';
 
 export default function WorkoutDraft() {
-  const { bottom } = useSafeAreaInsets();
   const theme = useTheme() as CustomTheme;
   const navigation = useNavigation<any>();
 
@@ -40,8 +32,6 @@ export default function WorkoutDraft() {
     useWorkoutContextMobile().mobileOverlayHandlers
       .setIsSaveWorkoutDialogVisible!;
 
-  const setIsAddWorkoutItemButtonsVisible =
-    useContext(WorkoutDraftContext)?.setIsAddWorkoutItemButtonsVisible!;
   const workoutDataScrollViewRef =
     useContext(WorkoutDraftContext)?.workoutDataScrollViewRef!;
 
@@ -50,22 +40,7 @@ export default function WorkoutDraft() {
     (state) => state.setWorkoutTitle,
   );
 
-  const startTimer = useWorkoutStopwatchStore((state) => state.start);
-  const stopTimer = useWorkoutStopwatchStore((state) => state.stop);
   const mode = useWorkoutDraftStore((state) => state.mode) as Mode;
-  const setMode = useWorkoutDraftStore((state) => state.setMode);
-
-  const handleSetMode = (modeValue: Mode) => {
-    if (modeValue === 'log') {
-      setMode('log');
-      setIsAddWorkoutItemButtonsVisible(false);
-      startTimer();
-    } else if (modeValue === 'edit') {
-      setMode('edit');
-      setIsAddWorkoutItemButtonsVisible(true);
-      stopTimer();
-    }
-  };
 
   useEffect(() => {
     navigation.setOptions({
@@ -141,59 +116,9 @@ export default function WorkoutDraft() {
         </ScrollView>
       </KeyboardAvoidingView>
 
-      <Appbar
-        elevated
-        style={[
-          styles.bottom,
-          {
-            height: BOTTOM_APPBAR_HEIGHT + bottom,
-            backgroundColor: theme.colors.elevation.level1,
-          },
-        ]}
-        safeAreaInsets={{ bottom }}
-      >
-        <SegmentedButtons
-          density="small"
-          value={mode}
-          onValueChange={(value: Mode) => handleSetMode(value)}
-          style={{
-            position: 'absolute',
-            left: 32,
-            bottom: (BOTTOM_APPBAR_HEIGHT + bottom) / 2,
-            transform: 'translateY(8px)',
-          }}
-          buttons={[
-            {
-              value: 'edit',
-              label: 'Edit',
-              style: { width: 50 },
-              labelStyle: { fontFamily: 'Manrope-SemiBold' },
-            },
-            {
-              value: 'log',
-              label: 'Log',
-              style: { width: 40 },
-              labelStyle: { fontFamily: 'Manrope-SemiBold' },
-            },
-          ]}
-        />
-        <AddWorkoutItemButtons />
-      </Appbar>
+      <BottomAppBar />
+
       <WorkoutOverlays workoutDataScrollViewRef={workoutDataScrollViewRef} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  bottom: {
-    backgroundColor: 'aquamarine',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  fab: {
-    position: 'absolute',
-    right: 16,
-  },
-});
