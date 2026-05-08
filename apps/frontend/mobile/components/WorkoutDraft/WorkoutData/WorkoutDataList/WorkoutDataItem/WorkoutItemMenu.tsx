a1dea2185ccecd817176f1c74e2a59c4c225b6cf
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-08 00:07:57 -0700

    CWT-384 Create WorkoutItemMenu component

---
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/WorkoutItemMenu.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { Menu, IconButton } from 'react-native-paper';
+
+interface WorkoutItemMenuProps {
+  itemType: 'superset' | 'section' | 'exercise';
+  isFirst: boolean;
+  isLast: boolean;
+  handleUpPress: () => void;
+  handleDownPress: () => void;
+  handleDeletePress: () => void;
+}
+
+export default function WorkoutItemMenu({
+  itemType,
+  isFirst = false,
+  isLast = false,
+  handleUpPress,
+  handleDownPress,
+  handleDeletePress,
+}: WorkoutItemMenuProps) {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+  return (
+    <Menu
+      visible={visible}
+      onDismiss={closeMenu}
+      anchor={<IconButton icon="dots-vertical" size={24} onPress={openMenu} />}
+    >
+      <Menu.Item
+        leadingIcon="trash-can"
+        onPress={() => {
+          closeMenu();
+          handleDeletePress();
+        }}
+        title="Delete"
+      />
+      {!isFirst && (
+        <Menu.Item
+          leadingIcon="chevron-up-circle"
+          onPress={() => {
+            closeMenu();
+            handleUpPress();
+          }}
+          title="Move Up"
+        />
+      )}
+      {!isLast && (
+        <Menu.Item
+          leadingIcon="chevron-down-circle"
+          onPress={() => {
+            closeMenu();
+            handleDownPress();
+          }}
+          title="Move Down"
+        />
+      )}
+    </Menu>
+  );
+}
