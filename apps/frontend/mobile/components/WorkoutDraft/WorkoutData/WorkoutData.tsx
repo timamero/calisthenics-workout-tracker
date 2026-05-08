281af6ab17f1cac0e467ffd8a3a396694c61e324
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-08 02:13:25 -0700

    CWT-384 Clean up code

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutData.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutData.tsx
@@ -1,17 +1,9 @@
-// import { RefObject } from 'react';
-// import { ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
-
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 import { Mode } from '@cwt/schema/workouts';
 
 import EmptyWorkoutPlaceholder from './EmptyWorkoutPlaceholder';
 import WorkoutDataList from './WorkoutDataList';
 
-// interface WorkoutDataProps {
-//   scrollViewRef: RefObject<ScrollView | null>;
-// }
-
-// export default function WorkoutData({ scrollViewRef }: WorkoutDataProps) {
 export default function WorkoutData() {
   const mode = useWorkoutDraftStore((state) => state.mode) as Mode;
   const workoutExercisesLength = useWorkoutDraftStore(
@@ -19,19 +11,9 @@ export default function WorkoutData() {
   ).length;
 
   return (
-    // <KeyboardAvoidingView
-    //   style={{ flexGrow: 1 }}
-    //   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    // >
-    //   <ScrollView
-    //     style={{ marginBottom: 104, flexGrow: 1 }}
-    //     ref={scrollViewRef}
-    //   >
     <>
       {workoutExercisesLength === 0 && <EmptyWorkoutPlaceholder mode={mode} />}
       <WorkoutDataList />
     </>
-    //   </ScrollView>
-    // </KeyboardAvoidingView>
   );
 }
