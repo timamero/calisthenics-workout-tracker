540ab046eaf287d2d490257d951524e33a6d12bf
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-10 00:46:53 -0700

    CWT-384 Fix hook

--- packages/hooks/src/lib/workout/useParentItemsLength.ts
+++ packages/hooks/src/lib/workout/useParentItemsLength.ts
@@ -1,16 +1,17 @@
-import { useContext } from 'react';
-import { useWorkoutDraftStore } from '@cwt/state/stores';
-import { WorkoutDataItemContext } from '@cwt/context';
+import { useContext } from "react";
+import { useWorkoutDraftStore } from "@cwt/state/stores";
+import { WorkoutDataItemContext } from "@cwt/context";
 
 export default function useParentItemsLength() {
   const parentSectionID = useContext(WorkoutDataItemContext)?.parentSectionID;
+  const parentSupersetID = useContext(WorkoutDataItemContext)?.parentSupersetID;
   const parentLength = useContext(WorkoutDataItemContext)?.parentItemsLength;
 
   const rootWorkoutDataLength = useWorkoutDraftStore(
-    (state) => state.workoutData.length || 0
+    (state) => state.workoutData.length || 0,
   );
 
-  if (!parentSectionID) {
+  if (!parentSectionID && !parentSupersetID) {
     return rootWorkoutDataLength;
   }
   return parentLength ? parentLength : 0;
