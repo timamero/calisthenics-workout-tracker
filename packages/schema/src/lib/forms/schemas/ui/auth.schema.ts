a7c9810efd173b5ad4e3b40ebec43d390afb821d
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-11 11:54:46 -0700

    CWT-384 Update auth sign up schema

--- packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
+++ packages/schema/src/lib/forms/schemas/ui/auth.schema.ts
@@ -4,13 +4,19 @@ export const AuthSchema = z.object({
   email: z.email(),
   password: z
     .string()
-    .min(4, { message: 'The username must be 4 characters or more' })
-    .max(16, { message: 'The password must be 16 characters or less' }),
+    .min(4, { message: 'The password must be 4 characters or more' })
+    .max(48, { message: 'The password must be 48 characters or less' }),
 });
 
 export const AuthSignUpSchema = z
   .object({
     ...AuthSchema.shape,
+    username: z
+      .string()
+      .min(4, { message: 'The username must be 4 characters or more' })
+      .max(24, {
+        message: 'The username must be 24 characters or less',
+      }),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
