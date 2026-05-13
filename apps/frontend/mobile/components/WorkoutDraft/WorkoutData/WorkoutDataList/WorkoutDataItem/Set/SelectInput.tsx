137754001a3f7a61d3ef17262685fe2d14412ee6
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-13 02:13:55 -0700

    CWT-382 Update label style

--- apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/SelectInput.tsx
+++ apps/frontend/mobile/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/SelectInput.tsx
@@ -1,26 +1,34 @@
 import { useContext, useState } from 'react';
 import {
   View,
-  Modal,
-  TouchableOpacity,
-  Text,
-  FlatList,
-  StyleSheet,
+  // Modal,
+  // TouchableOpacity,
+  // Text,
+  // FlatList,
+  // StyleSheet,
 } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useTheme, Menu } from 'react-native-paper';
 import { SetContext } from '@cwt/context';
-import type { CustomTheme } from '../../../../../../theme';
 import {
   useLeveragesAssistsStore,
   useWorkoutDraftStore,
 } from '@cwt/state/stores';
 import { useFieldInputChange } from '@cwt/hooks';
 
+import { Text } from '../../../../../../customText';
+import CustomButton from '../../../../../common/CustomButton';
+import type { CustomTheme } from '../../../../../../theme';
+
 interface SelectInputProps {
   label: string;
   fieldID: string;
   trackingType?: string | null;
 }
+
+type OptionType = {
+  key: string;
+  value: string;
+};
 
 export default function SelectInput({
   label,
@@ -45,59 +53,81 @@ export default function SelectInput({
 
   const handleChange = useFieldInputChange('value', 'select', fieldID);
 
-  const options = leverageOrAssist.value_options.map((option: string) => ({
-    key: option,
-    value: option,
-  }));
+  const options: OptionType[] = leverageOrAssist.value_options.map(
+    (option: string) => ({
+      key: option,
+      value: option,
+    }),
+  );
 
   const selectedValue =
     trackingType === 'leverages'
       ? set.fields.leverages?.find((field) => field.id === fieldID)?.value
       : set.fields.assists?.find((field) => field.id === fieldID)?.value;
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-  const styles = StyleSheet.create({
-    container: {
-      minWidth: 150,
-      position: 'relative',
-    },
-    button: {
-      borderWidth: 1,
-      borderColor: theme.colors.outline,
-      backgroundColor: 'transparent',
-      padding: 12,
-      borderRadius: 4,
-    },
-    buttonText: {
-      color: theme.colors.onBackground,
-    },
-    modal: {
-      margin: 0,
-      justifyContent: 'flex-end',
-    },
-    dropdown: {
-      maxHeight: 200,
-      backgroundColor: theme.colors.background,
-      borderColor: theme.colors.outline,
-      borderWidth: 1,
-      borderRadius: 4,
-    },
-    option: {
-      padding: 12,
-      borderBottomWidth: 1,
-      borderBottomColor: theme.colors.outline,
-    },
-    optionText: {
-      color: theme.colors.onBackground,
-    },
-  });
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const styles = StyleSheet.create({
+  //   container: {
+  //     minWidth: 150,
+  //     position: 'relative',
+  //   },
+  //   button: {
+  //     borderWidth: 1,
+  //     borderColor: theme.colors.outline,
+  //     backgroundColor: 'transparent',
+  //     padding: 12,
+  //     borderRadius: 4,
+  //   },
+  //   buttonText: {
+  //     color: theme.colors.onBackground,
+  //   },
+  //   modal: {
+  //     margin: 0,
+  //     justifyContent: 'flex-end',
+  //   },
+  //   dropdown: {
+  //     position: 'absolute',
+  //     top: 0,
+  //     maxHeight: 200,
+  //     backgroundColor: theme.colors.background,
+  //     borderColor: theme.colors.outline,
+  //     borderWidth: 1,
+  //     borderRadius: 4,
+  //   },
+  //   option: {
+  //     padding: 12,
+  //     borderBottomWidth: 1,
+  //     borderBottomColor: theme.colors.outline,
+  //   },
+  //   optionText: {
+  //     color: theme.colors.onBackground,
+  //   },
+  // });
 
   if (mode === 'read') {
     return (
       <View>
-        <Text style={{ color: theme.colors.onBackground }}>{label}</Text>
-        <Text style={{ color: theme.colors.onBackground }}>
+        <Text
+          style={{
+            color: theme.colors.onBackground,
+            fontFamily: 'SourceCodePro-Regular',
+          }}
+        >
+          {label}
+        </Text>
+        <Text
+          style={{
+            color: theme.colors.onBackground,
+            fontFamily: 'SourceCodePro-Regular',
+          }}
+        >
           {selectedValue}
         </Text>
       </View>
@@ -105,37 +135,47 @@ export default function SelectInput({
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => setIsOpen(true)}>
-        <Text style={styles.buttonText}>{selectedValue || label}</Text>
-      </TouchableOpacity>
-
-      <Modal
-        visible={isOpen}
-        transparent
-        animationType="slide"
-        onRequestClose={() => setIsOpen(false)}
+    <View style={{ display: 'flex', gap: 4 }}>
+      <Text variant="labelMedium">{leverageOrAssist.name}</Text>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <CustomButton
+            mode="contained"
+            style={{
+              borderRadius: 4,
+              borderWidth: 1,
+              borderColor: theme.colors.outline,
+            }}
+            labelStyle={{ fontFamily: 'SourceCodePro-Regular' }}
+            onPress={openMenu}
+            theme={{
+              colors: {
+                primary: theme.colors.background,
+                onPrimary: theme.colors.onBackground,
+              },
+            }}
+          >
+            {selectedValue || label}
+          </CustomButton>
+        }
+        anchorPosition="top"
       >
-        <TouchableOpacity style={{ flex: 1 }} onPress={() => setIsOpen(false)}>
-          <View style={styles.dropdown}>
-            <FlatList
-              data={options}
-              keyExtractor={(item) => item.key}
-              renderItem={({ item }) => (
-                <TouchableOpacity
-                  style={styles.option}
-                  onPress={() => {
-                    handleChange(item.value);
-                    setIsOpen(false);
-                  }}
-                >
-                  <Text style={styles.optionText}>{item.value}</Text>
-                </TouchableOpacity>
-              )}
+        {options.map((option) => {
+          return (
+            <Menu.Item
+              key={option.key}
+              titleStyle={{ fontFamily: 'SourceCodePro-Regular', fontSize: 14 }}
+              onPress={() => {
+                closeMenu();
+                handleChange(option.value);
+              }}
+              title={option.value}
             />
-          </View>
-        </TouchableOpacity>
-      </Modal>
+          );
+        })}
+      </Menu>
     </View>
   );
 }
