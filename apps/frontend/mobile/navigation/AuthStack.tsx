957ccd74d0ec756b12209f72c504541dc841bc32
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-14 00:20:15 -0700

    CWT-379 Add header with logo

--- apps/frontend/mobile/navigation/AuthStack.tsx
+++ apps/frontend/mobile/navigation/AuthStack.tsx
@@ -1,10 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { IconButton, Button } from 'react-native-paper';
 
 import LandingScreen from '../screens/LandingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import OnboardingComplete from '../screens/OnboardingComplete';
+import { Image, View } from 'react-native';
+import { Text } from '../customText';
 
 const authScreenOptions = {
   title: '',
@@ -23,7 +26,24 @@ export const AuthStack = createNativeStackNavigator({
   screens: {
     Landing: {
       screen: LandingScreen,
-      options: { headerShown: false },
+      options: {
+        headerTitle: () => (
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 8,
+            }}
+          >
+            <Image
+              style={{ width: 32, height: 32 }}
+              source={require('../assets/logo-240x240.png')}
+            />
+            <Text variant="headlineSmall">Torque</Text>
+          </View>
+        ),
+      },
     },
     Login: {
       screen: LoginScreen,
