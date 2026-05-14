f0893661b3a59145108aee463b20188572e70e06
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-13 22:49:57 -0700

    CWT-379 Create DashboardLayout and update LogoButton style

--- apps/frontend/web/src/components/layouts/SiteLayout.tsx
+++ apps/frontend/web/src/components/layouts/SiteLayout.tsx
@@ -1,15 +1,9 @@
-import { Link, Outlet } from '@tanstack/react-router';
-import {
-  AppShell,
-  Image,
-  Text,
-  UnstyledButton,
-  Group,
-  Box,
-} from '@mantine/core';
+import { Outlet } from '@tanstack/react-router';
+import { AppShell, Group } from '@mantine/core';
 
-import Logo from '../../assets/logo-min-opt-160x160.svg';
-import classes from './SiteLayout.module.css';
+// import Logo from '../../assets/logo-min-opt-160x160.svg';
+import LogoButton from './LogoButton';
+// import classes from './SiteLayout.module.css';
 
 export default function SiteLayout() {
   // const [opened, { toggle }] = useDisclosure();
@@ -27,31 +21,8 @@ export default function SiteLayout() {
         {/* <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" /> */}
 
         {/* <div>Logo</div> */}
-        <Group h="100%" px="xl">
-          <UnstyledButton
-            component={Link}
-            to="/"
-            className={classes.logoButton}
-          >
-            <Group gap={8}>
-              <Box className={classes.logoButtonIconWrapper} p="xs" bdrs={32}>
-                <Image
-                  // radius="md"
-                  w={16}
-                  style={{ aspectRatio: 0.92 }}
-                  src={Logo}
-                />
-              </Box>
-              <Text
-                className={classes.logoButtonText}
-                ff="heading"
-                fw="bolder"
-                size="xl"
-              >
-                Torque
-              </Text>
-            </Group>
-          </UnstyledButton>
+        <Group h="100%" px="md">
+          <LogoButton linkTo="/" />
         </Group>
       </AppShell.Header>
 
--- apps/frontend/web/src/routes/_auth/dashboard/route.tsx
+++ apps/frontend/web/src/routes/_auth/dashboard/route.tsx
@@ -1,59 +1,61 @@
 import {
   createFileRoute,
-  Outlet,
-  Link,
-  linkOptions,
+  // Outlet,
+  // Link,
+  // linkOptions,
 } from '@tanstack/react-router';
 
-import { AppShell, Burger, NavLink } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+// import { AppShell, Burger, NavLink } from '@mantine/core';
+// import { useDisclosure } from '@mantine/hooks';
+import DashBoardLayout from '../../../components/layouts/DashboardLayout';
 
 export const Route = createFileRoute('/_auth/dashboard')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [opened, { toggle }] = useDisclosure();
+  // const [opened, { toggle }] = useDisclosure();
 
-  const navLinks = [
-    linkOptions({ label: 'Home', to: '/dashboard/home' }),
-    linkOptions({ label: 'Library', to: '/dashboard/library' }),
-    linkOptions({ label: 'Past Workouts', to: '/dashboard/history' }),
-    // linkOptions({ label: 'Profile', to: '/dashboard/user' }), // temporarily disabled until v0.1.0-alpha.2
-    linkOptions({ label: 'Settings', to: '/dashboard/settings' }),
-  ];
+  // const navLinks = [
+  //   linkOptions({ label: 'Home', to: '/dashboard/home' }),
+  //   linkOptions({ label: 'Library', to: '/dashboard/library' }),
+  //   linkOptions({ label: 'Past Workouts', to: '/dashboard/history' }),
+  //   // linkOptions({ label: 'Profile', to: '/dashboard/user' }), // temporarily disabled until v0.1.0-alpha.2
+  //   linkOptions({ label: 'Settings', to: '/dashboard/settings' }),
+  // ];
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-      style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
-    >
-      <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        <div>Logo</div>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        {navLinks.map((link) => {
-          return (
-            <NavLink
-              key={link.to}
-              label={link.label}
-              component={Link}
-              to={link.to}
-              onClick={() => opened && toggle()}
-            />
-          );
-        })}
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <Outlet />
-      </AppShell.Main>
-    </AppShell>
+    <DashBoardLayout />
+    // <AppShell
+    //   header={{ height: 60 }}
+    //   navbar={{
+    //     width: 300,
+    //     breakpoint: 'sm',
+    //     collapsed: { mobile: !opened },
+    //   }}
+    //   padding="md"
+    //   style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+    // >
+    //   <AppShell.Header>
+    //     <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+    //     <div>Logo</div>
+    //   </AppShell.Header>
+    //   <AppShell.Navbar p="md">
+    //     {navLinks.map((link) => {
+    //       return (
+    //         <NavLink
+    //           key={link.to}
+    //           label={link.label}
+    //           component={Link}
+    //           to={link.to}
+    //           onClick={() => opened && toggle()}
+    //         />
+    //       );
+    //     })}
+    //   </AppShell.Navbar>
+    //   <AppShell.Main>
+    //     <Outlet />
+    //   </AppShell.Main>
+    // </AppShell>
   );
 }
