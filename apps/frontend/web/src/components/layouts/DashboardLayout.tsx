d0e7b1b80657305cc06fc634f2903f2dc6a181ca
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-13 22:49:57 -0700

    CWT-379 Create DashboardLayout and update LogoButton style

---
+++ apps/frontend/web/src/components/layouts/DashboardLayout.tsx
@@ -0,0 +1,53 @@
+import { Outlet, Link, linkOptions } from '@tanstack/react-router';
+
+import { AppShell, Burger, NavLink, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import LogoButton from './LogoButton';
+
+export default function DashBoardLayout() {
+  const [opened, { toggle }] = useDisclosure();
+
+  const navLinks = [
+    linkOptions({ label: 'Home', to: '/dashboard/home' }),
+    linkOptions({ label: 'Library', to: '/dashboard/library' }),
+    linkOptions({ label: 'Past Workouts', to: '/dashboard/history' }),
+    // linkOptions({ label: 'Profile', to: '/dashboard/user' }), // temporarily disabled until v0.1.0-alpha.2
+    linkOptions({ label: 'Settings', to: '/dashboard/settings' }),
+  ];
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+      style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+    >
+      <AppShell.Header>
+        <Group justify="space-between" h="100%" px="md">
+          <LogoButton linkTo="/dashboard/home" />
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        {navLinks.map((link) => {
+          return (
+            <NavLink
+              key={link.to}
+              label={link.label}
+              component={Link}
+              to={link.to}
+              onClick={() => opened && toggle()}
+            />
+          );
+        })}
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  );
+}
