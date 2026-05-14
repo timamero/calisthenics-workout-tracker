a59f114c057b939004aecc105e54317f6c1d43b6
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-13 22:22:26 -0700

    CWT-397 Create SiteLayout component and update styles

--- apps/frontend/web/src/routes/_site/route.tsx
+++ apps/frontend/web/src/routes/_site/route.tsx
@@ -1,6 +1,14 @@
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
-import { AppShell, Image, Text, UnstyledButton, Group } from '@mantine/core';
-import Logo from '../../assets/logo-min-opt-160x160.svg';
+import { createFileRoute } from '@tanstack/react-router';
+// import {
+//   AppShell,
+//   Image,
+//   Text,
+//   UnstyledButton,
+//   Group,
+//   Box,
+// } from '@mantine/core';
+// import Logo from '../../assets/logo-min-opt-160x160.svg';
+import SiteLayout from '../../components/layouts/SiteLayout';
 // import { useDisclosure } from '@mantine/hooks';
 
 export const Route = createFileRoute('/_site')({
@@ -10,41 +18,44 @@ export const Route = createFileRoute('/_site')({
 function RouteComponent() {
   // const [opened, { toggle }] = useDisclosure();
   return (
-    <AppShell
-      padding="md"
-      header={{ height: 60 }}
-      // navbar={{
-      //   width: 300,
-      //   breakpoint: 'sm',
-      //   collapsed: { mobile: !opened },
-      // }}
-    >
-      <AppShell.Header>
-        {/* <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" /> */}
+    <SiteLayout />
+    // <AppShell
+    //   padding="md"
+    //   header={{ height: 60 }}
+    //   // navbar={{
+    //   //   width: 300,
+    //   //   breakpoint: 'sm',
+    //   //   collapsed: { mobile: !opened },
+    //   // }}
+    // >
+    //   <AppShell.Header>
+    //     {/* <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" /> */}
 
-        {/* <div>Logo</div> */}
-        <Group bg="lime.2" h="100%" px="xl">
-          <UnstyledButton component={Link} to="/">
-            <Group>
-              <Image
-                radius="md"
-                w={24}
-                style={{ aspectRatio: 0.92 }}
-                src={Logo}
-              />
-              <Text ff="heading" fw="bolder" size="xl">
-                Torque
-              </Text>
-            </Group>
-          </UnstyledButton>
-        </Group>
-      </AppShell.Header>
+    //     {/* <div>Logo</div> */}
+    //     <Group h="100%" px="xl">
+    //       <UnstyledButton component={Link} to="/">
+    //         <Group gap={8}>
+    //           <Box bg="white" p="xs" bdrs={32}>
+    //             <Image
+    //               radius="md"
+    //               w={24}
+    //               style={{ aspectRatio: 0.92 }}
+    //               src={Logo}
+    //             />
+    //           </Box>
+    //           <Text ff="heading" fw="bolder" size="xl">
+    //             Torque
+    //           </Text>
+    //         </Group>
+    //       </UnstyledButton>
+    //     </Group>
+    //   </AppShell.Header>
 
-      {/* <AppShell.Navbar>Navbar</AppShell.Navbar> */}
+    //   {/* <AppShell.Navbar>Navbar</AppShell.Navbar> */}
 
-      <AppShell.Main>
-        <Outlet />
-      </AppShell.Main>
-    </AppShell>
+    //   <AppShell.Main>
+    //     <Outlet />
+    //   </AppShell.Main>
+    // </AppShell>
   );
 }
