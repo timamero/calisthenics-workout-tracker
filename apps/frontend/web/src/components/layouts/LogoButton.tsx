500743086f68fee79d3f166868d27372a5aabe99
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-13 22:49:57 -0700

    CWT-379 Create DashboardLayout and update LogoButton style

---
+++ apps/frontend/web/src/components/layouts/LogoButton.tsx
@@ -0,0 +1,30 @@
+import { Link } from '@tanstack/react-router';
+import { Image, Text, UnstyledButton, Group, Box } from '@mantine/core';
+
+import Logo from '../../assets/logo-min-opt-160x160.svg';
+import classes from './LogoButton.module.css';
+
+export default function LogoButton({ linkTo }: { linkTo: string }) {
+  return (
+    <UnstyledButton component={Link} to={linkTo} className={classes.logoButton}>
+      <Group gap={8}>
+        <Box className={classes.logoButtonIconWrapper} p="xs" bdrs={32}>
+          <Image
+            // radius="md"
+            w={16}
+            style={{ aspectRatio: 0.92 }}
+            src={Logo}
+          />
+        </Box>
+        <Text
+          className={classes.logoButtonText}
+          ff="heading"
+          fw="bolder"
+          size="xl"
+        >
+          Torque
+        </Text>
+      </Group>
+    </UnstyledButton>
+  );
+}
