2e90290adcb45c224c32f19321551c2de74c6fc6
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-14 21:17:26 -0700

    CWT-388 Generate landing page and back up old landing page

---
+++ apps/frontend/web/src/routes/_site/indexOld.tsx
@@ -0,0 +1,46 @@
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { Title, Stack, Button, Text, Group } from '@mantine/core';
+
+export const Route = createFileRoute('/_site/indexOld')({
+  component: HomeView,
+});
+
+function HomeView() {
+  return (
+    <Stack align="center" justify="center" h="100vh">
+      <Stack align="center">
+        <Title order={1} size={64}>
+          Torque
+        </Title>
+        <Text size="xl">
+          Your Quick & Easy Path to Calisthenics Fitness. Track Your Progress
+          Effortlessly.
+        </Text>
+      </Stack>
+
+      <Stack mt={80}>
+        <Title order={2}>Sign Up or Log In</Title>
+        <Group align="flex-start" justify="center" mt={20}>
+          <Button
+            component={Link}
+            to="/signup"
+            variant="filled"
+            size="md"
+            radius="md"
+          >
+            Sign Up
+          </Button>
+          <Button
+            component={Link}
+            to="/login"
+            variant="outline"
+            size="md"
+            radius="md"
+          >
+            Log In
+          </Button>
+        </Group>
+      </Stack>
+    </Stack>
+  );
+}
