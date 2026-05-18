95ab0dd12a5e66250f9a53e1610cbba07ee67946
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-18 00:33:45 -0700

    CWT-384 Update workout item styles

--- apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
+++ apps/frontend/web/src/components/WorkoutDraft/WorkoutData/WorkoutDataList/WorkoutDataItem/SectionItem/SectionItemUI.tsx
@@ -1,4 +1,4 @@
-import { Stack, Group, Box } from '@mantine/core';
+import { Stack, Group, Box, Paper } from '@mantine/core';
 // import { Link } from '@tanstack/react-router';
 
 import type { Mode } from '@cwt/schema/workouts';
@@ -35,70 +35,79 @@ export default function SectionItemUI({
   handleSetSectionTitle,
 }: SectionItemUIProps) {
   return (
-    <Stack
-      bg="violet.0"
-      bdrs="sm"
-      bd="2px solid violet.9"
-      w="100%"
-      // maw={840}
-      align="center"
-      pb={mode === 'edit' || mode === 'build' ? 0 : 'lg'}
+    <Paper
       my="xl"
+      radius="md"
+      w="100%"
+      shadow="sm"
+      bg="elevation.5"
+      bd="1px solid var(--mantine-color-lime-2)"
     >
-      <Group
-        align="flex-start"
-        justify="space-between"
+      <Stack
+        // bg="gray.0"
+        // bdrs="sm"
+        // bd="2px solid violet.9"
         w="100%"
-        wrap="nowrap"
-        bg="violet.1"
-        style={{ borderBottom: '1px solid var(--mantine-color-violet-9)' }}
+        // maw={840}
+        align="center"
+        pb={mode === 'edit' || mode === 'build' ? 0 : 'lg'}
+        // my="xl"
       >
-        <Group justify="space-between" wrap="nowrap" w="100%" p="xs">
-          {(mode === 'edit' || mode === 'build') && (
-            <ReorderButtonGroup
-              handleUpClick={() => handleUpClick()}
-              handleDownClick={() => handleDownClick()}
-              isFirst={isFirst}
-              isLast={isLast}
-            />
-          )}
-          <Group flex={1} h="max-content" align="center" justify="center">
-            <TextInputWithEdit
-              initialValue={title}
-              onSave={handleSetSectionTitle}
-              hideEdit={mode === 'log' ? true : false}
-              variant={'title'}
-              maxLength={70}
-              titleOrder={2}
-              titleSize="h3"
-              hideEditLabel={true}
-            />
+        <Group
+          align="flex-start"
+          justify="space-between"
+          w="100%"
+          wrap="nowrap"
+          // bg="elevation.4"
+          style={{ borderBottom: '1px solid var(--mantine-color-lime-2)' }}
+        >
+          <Group justify="space-between" wrap="nowrap" w="100%" p="xs">
+            {(mode === 'edit' || mode === 'build') && (
+              <ReorderButtonGroup
+                handleUpClick={() => handleUpClick()}
+                handleDownClick={() => handleDownClick()}
+                isFirst={isFirst}
+                isLast={isLast}
+              />
+            )}
+            <Group flex={1} h="max-content" align="center" justify="center">
+              <TextInputWithEdit
+                initialValue={title}
+                onSave={handleSetSectionTitle}
+                hideEdit={mode === 'log' ? true : false}
+                variant={'title'}
+                maxLength={70}
+                titleOrder={2}
+                titleSize="h3"
+                hideEditLabel={true}
+              />
+            </Group>
           </Group>
+          {(mode === 'edit' || mode === 'build') && (
+            <Box p="xs">
+              <WorkoutItemMenu
+                itemType="section"
+                handleUpClick={handleUpClick}
+                handleDownClick={handleDownClick}
+                handleDeleteClick={handleDeleteSectionClick}
+                isFirst={isFirst}
+                isLast={isLast}
+              />
+            </Box>
+          )}
         </Group>
-        {(mode === 'edit' || mode === 'build') && (
-          <Box p="xs">
-            <WorkoutItemMenu
-              itemType="section"
-              handleUpClick={handleUpClick}
-              handleDownClick={handleDownClick}
-              handleDeleteClick={handleDeleteSectionClick}
-              isFirst={isFirst}
-              isLast={isLast}
-            />
-          </Box>
-        )}
-      </Group>
-      <Stack w="100%" px={{ base: 'xxs', md: 'xl' }} gap="lg">
-        <ItemsList />
-      </Stack>
-      {(mode === 'edit' || mode === 'build') && (
-        <Stack align="flex-end" w="100%" p="sm">
-          <AddItemMenu
-            handleAddExerciseClick={handleAddExerciseClick}
-            handleAddSupersetClick={handleAddSupersetClick}
-          />
+        <Stack w="100%" px={{ base: 'xxs', md: 'xl' }} gap="lg">
+          <ItemsList />
         </Stack>
-      )}
-    </Stack>
+        {(mode === 'edit' || mode === 'build') && (
+          <Stack align="flex-end" w="100%" p="sm">
+            <AddItemMenu
+              handleAddExerciseClick={handleAddExerciseClick}
+              handleAddSupersetClick={handleAddSupersetClick}
+            />
+          </Stack>
+        )}
+      </Stack>
+    </Paper>
   );
 }
