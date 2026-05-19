38bdd5779cc853811d80573d460a590353ae8eae
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-18 18:54:24 -0700

    CWT-429 Refine landing page design

--- apps/frontend/mobile/navigation/AuthStack.tsx
+++ apps/frontend/mobile/navigation/AuthStack.tsx
@@ -1,13 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { IconButton, Button } from 'react-native-paper';
 
 import LandingScreen from '../screens/LandingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import OnboardingComplete from '../screens/OnboardingComplete';
-import { Image, View } from 'react-native';
-import { Text } from '../customText';
 
 const authScreenOptions = {
   title: '',
@@ -21,29 +18,11 @@ const authScreenOptions = {
   headerTintColor: '#242424',
 };
 
-// Auth Stack Navigator
 export const AuthStack = createNativeStackNavigator({
   screens: {
     Landing: {
       screen: LandingScreen,
-      options: {
-        headerTitle: () => (
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 8,
-            }}
-          >
-            <Image
-              style={{ width: 36, height: 36 }}
-              source={require('../assets/logo-240x240.png')}
-            />
-            <Text variant="headlineSmall">Torque</Text>
-          </View>
-        ),
-      },
+      options: { headerShown: false },
     },
     Login: {
       screen: LoginScreen,
