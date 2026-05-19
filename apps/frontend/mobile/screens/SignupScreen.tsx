466ce2ec18d105ed78212f83df9b9b20a17254a7
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-19 16:21:02 -0700

    CWT-429 Fix view so keyboard doesn't block input or button

--- apps/frontend/mobile/screens/SignupScreen.tsx
+++ apps/frontend/mobile/screens/SignupScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { useTheme, TextInput, Text } from 'react-native-paper';
 import { Controller } from 'react-hook-form';
 
@@ -37,159 +37,169 @@ export default function SignupScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{ paddingBottom: 16 }}>
-        <Text variant="headlineMedium">{siteContent().signupHeading}</Text>
-        <Text variant="bodyLarge">{siteContent().signupSubtext}</Text>
-      </View>
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="Username"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            autoCapitalize="none"
-            value={value}
-            error={typeof auth.errors.username?.message === 'string'}
-          />
-        )}
-        name="username"
-      />
-      {auth.errors.username && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.username?.message}
-        </Text>
-      )}
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="First Name"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            autoCapitalize="words"
-            value={value}
-            error={typeof auth.errors.firstName?.message === 'string'}
-          />
-        )}
-        name="firstName"
-      />
-      {auth.errors.firstName && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.firstName?.message}
-        </Text>
-      )}
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="Last Name"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            autoCapitalize="words"
-            value={value}
-            error={typeof auth.errors.lastName?.message === 'string'}
-          />
-        )}
-        name="lastName"
-      />
-      {auth.errors.lastName && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.lastName?.message}
-        </Text>
-      )}
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="Email"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            autoCapitalize="none"
-            keyboardType="email-address"
-            value={value}
-            error={typeof auth.errors.email?.message === 'string'}
-          />
-        )}
-        name="email"
-      />
-      {auth.errors.email && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.email?.message}
-        </Text>
-      )}
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="Password"
-            right={
-              <TextInput.Icon
-                icon={isPasswordVisible ? 'eye' : 'eye-off'}
-                onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-              />
-            }
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            secureTextEntry={!isPasswordVisible}
-            error={typeof auth.errors.password?.message === 'string'}
-          />
-        )}
-        name="password"
-      />
-      {auth.errors.password && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.password?.message}
-        </Text>
-      )}
-      <Controller
-        control={auth.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            label="Confirm Password"
-            right={
-              <TextInput.Icon
-                icon={isConfirmPasswordVisible ? 'eye' : 'eye-off'}
-                onPress={() =>
-                  setIsConfirmPasswordVisible(!isConfirmPasswordVisible)
-                }
-              />
-            }
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            secureTextEntry={!isConfirmPasswordVisible}
-            error={typeof auth.errors.confirmPassword?.message === 'string'}
-          />
-        )}
-        name="confirmPassword"
-      />
-      {auth.errors.confirmPassword && (
-        <Text style={{ color: theme.colors.error }}>
-          {auth.errors.confirmPassword?.message}
-        </Text>
-      )}
-      {auth.authError && (
-        <Text style={{ color: theme.colors.error }}>{auth.authError}</Text>
-      )}
-      <CustomButton
-        mode="contained"
-        onPress={handleSubmitPress}
-        disabled={
-          auth.isLoading ||
-          auth.errors.email ||
-          auth.errors.password ||
-          auth.errors.confirmPassword
-            ? true
-            : false
-        }
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={styles.container}
       >
-        Sign Up
-      </CustomButton>
-    </View>
+        <View style={{ paddingBottom: 16 }}>
+          <Text variant="headlineMedium">{siteContent().signupHeading}</Text>
+          <Text variant="bodyLarge">{siteContent().signupSubtext}</Text>
+        </View>
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="Username"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              autoCapitalize="none"
+              value={value}
+              error={typeof auth.errors.username?.message === 'string'}
+            />
+          )}
+          name="username"
+        />
+        {auth.errors.username && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.username?.message}
+          </Text>
+        )}
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="First Name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              autoCapitalize="words"
+              value={value}
+              error={typeof auth.errors.firstName?.message === 'string'}
+            />
+          )}
+          name="firstName"
+        />
+        {auth.errors.firstName && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.firstName?.message}
+          </Text>
+        )}
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="Last Name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              autoCapitalize="words"
+              value={value}
+              error={typeof auth.errors.lastName?.message === 'string'}
+            />
+          )}
+          name="lastName"
+        />
+        {auth.errors.lastName && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.lastName?.message}
+          </Text>
+        )}
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="Email"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              value={value}
+              error={typeof auth.errors.email?.message === 'string'}
+            />
+          )}
+          name="email"
+        />
+        {auth.errors.email && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.email?.message}
+          </Text>
+        )}
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="Password"
+              right={
+                <TextInput.Icon
+                  icon={isPasswordVisible ? 'eye' : 'eye-off'}
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                />
+              }
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              secureTextEntry={!isPasswordVisible}
+              error={typeof auth.errors.password?.message === 'string'}
+            />
+          )}
+          name="password"
+        />
+        {auth.errors.password && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.password?.message}
+          </Text>
+        )}
+        <Controller
+          control={auth.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              label="Confirm Password"
+              right={
+                <TextInput.Icon
+                  icon={isConfirmPasswordVisible ? 'eye' : 'eye-off'}
+                  onPress={() =>
+                    setIsConfirmPasswordVisible(!isConfirmPasswordVisible)
+                  }
+                />
+              }
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              secureTextEntry={!isConfirmPasswordVisible}
+              error={typeof auth.errors.confirmPassword?.message === 'string'}
+            />
+          )}
+          name="confirmPassword"
+        />
+        {auth.errors.confirmPassword && (
+          <Text style={{ color: theme.colors.error }}>
+            {auth.errors.confirmPassword?.message}
+          </Text>
+        )}
+        {auth.authError && (
+          <Text style={{ color: theme.colors.error }}>{auth.authError}</Text>
+        )}
+        <View style={{ paddingBottom: 120 }}>
+          <CustomButton
+            mode="contained"
+            onPress={handleSubmitPress}
+            disabled={
+              auth.isLoading ||
+              auth.errors.email ||
+              auth.errors.password ||
+              auth.errors.confirmPassword
+                ? true
+                : false
+            }
+          >
+            Sign Up
+          </CustomButton>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
