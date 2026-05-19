addd6b8d9803d91bda232d82eabd6cb6492d1d3f
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-18 19:06:46 -0700

    CWT-429 Create siteContent

--- packages/content/src/lib/index.ts
+++ packages/content/src/lib/index.ts
@@ -4,10 +4,12 @@ import {
   emptyWorkoutPlaceholderContent,
 } from "./workoutContent";
 import { startWorkoutContent } from "./startWorkoutContent";
+import { siteContent } from "./siteContent";
 
 export {
   saveWorkoutConfirmationContent,
   cancelWorkoutConfirmationContent,
   emptyWorkoutPlaceholderContent,
   startWorkoutContent,
+  siteContent,
 };
