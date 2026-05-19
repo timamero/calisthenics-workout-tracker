a9e4f13e0ace84816d41a4ff9d305c8aba470a65
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-18 17:29:04 -0700

    CWT-429 Update landing view design

--- apps/frontend/mobile/screens/LandingScreen.tsx
+++ apps/frontend/mobile/screens/LandingScreen.tsx
@@ -16,28 +16,94 @@ export default function LandingScreen() {
         flex: 1,
         gap: 16,
         paddingHorizontal: 16,
+        paddingBlock: 16,
         alignItems: 'stretch',
         justifyContent: 'center',
         backgroundColor: theme.colors.background,
       }}
     >
-      <Text variant="displaySmall" style={{ color: theme.colors.onBackground }}>
-        Sign Up or Log In
-      </Text>
-      <Button
-        mode="contained"
-        buttonColor={theme.colors.primary}
-        onPress={() => navigation.navigate('Signup')}
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingHorizontal: 16,
+          paddingBlock: 16,
+        }}
       >
-        Sign Up
-      </Button>
-      <Button
-        mode="outlined"
-        textColor={theme.colors.outline}
-        onPress={() => navigation.navigate('Login')}
+        <Text
+          variant="headlineLarge"
+          style={{ color: theme.colors.onBackground }}
+        >
+          Torque
+        </Text>
+        <View style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
+          <Text
+            style={{
+              color: theme.colors.dark2,
+              fontFamily: 'Manrope-Bold',
+              textTransform: 'uppercase',
+              fontSize: 20,
+              lineHeight: 22,
+              letterSpacing: 0.64,
+            }}
+          >
+            Generate force.
+          </Text>
+          <Text
+            style={{
+              color: theme.colors.onBackground,
+              fontFamily: 'Manrope-Bold',
+              textTransform: 'uppercase',
+              fontSize: 20,
+              lineHeight: 22,
+              letterSpacing: 0.64,
+            }}
+          >
+            Anywhere.
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          justifyContent: 'center',
+        }}
       >
-        Log In
-      </Button>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 16,
+            paddingHorizontal: 16,
+            paddingBlock: 16,
+            justifyContent: 'center',
+            transform: 'translateY(-40px)',
+          }}
+        >
+          <Text
+            variant="headlineMedium"
+            style={{ color: theme.colors.onBackground }}
+          >
+            Sign Up or Log In
+          </Text>
+          <Button
+            mode="contained"
+            buttonColor={theme.colors.primary}
+            onPress={() => navigation.navigate('Signup')}
+          >
+            Sign Up
+          </Button>
+          <Button
+            mode="outlined"
+            textColor={theme.colors.outline}
+            onPress={() => navigation.navigate('Login')}
+          >
+            Log In
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }
