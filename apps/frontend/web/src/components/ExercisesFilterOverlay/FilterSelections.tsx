86f4ff4b028e5320104826ce8641472614a32986
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-20 23:43:09 -0700

    CWT-429 Update Filter overlay design

--- apps/frontend/web/src/components/ExercisesFilterOverlay/FilterSelections.tsx
+++ apps/frontend/web/src/components/ExercisesFilterOverlay/FilterSelections.tsx
@@ -2,9 +2,7 @@ import { memo } from 'react';
 import { Stack, Text, Group } from '@mantine/core';
 import { useShallow } from 'zustand/shallow';
 
-// import { useStore } from '@cwt/state/store';
 import { useExercisesFilterStore } from '@cwt/state/stores';
-// import { type FilterGroup } from '@cwt/state/types';
 import type { FilterCheckboxKey } from '@cwt/schema/exercises';
 import type { Attributes } from '@cwt/schema/exercises';
 
@@ -39,17 +37,21 @@ export default function FilterSelections() {
 
   const filterGroupNames = filterCheckboxSelections.map((s) => s.keyName);
   const uniqueFilterGroupNames = [...new Set(filterGroupNames)];
-
+  console.log('unique: ', uniqueFilterGroupNames);
   return (
     <>
       {uniqueFilterGroupNames.map((keyName) => {
         return (
-          <Stack gap="sm" key={keyName}>
+          <Stack gap="sm" key={keyName} w="100%">
             <Text
+              ff="heading"
               tt="uppercase"
-              style={{ fontFamily: 'var(--mantine-font-family-headings)' }}
+              size="md"
+              fw={700}
+              c="gray.7"
+              lts="var(--mantine-letter-spacing-wider)"
             >
-              {keyName}
+              {keyName.split('_').join(' ')}
             </Text>
             <Group gap={4}>
               <Selections
