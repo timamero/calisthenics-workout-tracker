dc4217bf0830e9d9e7ce862cc463e35d746e6cda
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-21 16:21:55 -0700

    CWT-429 Add pending component

--- apps/frontend/web/src/routes/_auth/workout/add-exercise.tsx
+++ apps/frontend/web/src/routes/_auth/workout/add-exercise.tsx
@@ -1,10 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router';
 import AddExercise from '../../../components/Workout/AddExercise';
+import DefaultLoader from '../../../components/common/DefaultLoader';
+import { Stack } from '@mantine/core';
 
 export const Route = createFileRoute('/_auth/workout/add-exercise')({
   component: RouteComponent,
+  pendingComponent: Loader,
 });
 
+function Loader() {
+  return (
+    <Stack w="100%" h="100vh" justify="center">
+      <DefaultLoader />
+    </Stack>
+  );
+}
+
 function RouteComponent() {
+  console.log('add-exercise || add-exercise page mounted');
   return <AddExercise />;
 }
