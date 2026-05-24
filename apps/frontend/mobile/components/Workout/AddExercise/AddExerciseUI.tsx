c78de5dcc1ed15b698f47ff187244e40393f7a65
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-23 20:09:32 -0700

    CWT-429 Update layout and elevations

--- apps/frontend/mobile/components/Workout/AddExercise/AddExerciseUI.tsx
+++ apps/frontend/mobile/components/Workout/AddExercise/AddExerciseUI.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, View } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useTheme, Surface } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { CustomTheme } from '../../../theme';
@@ -26,59 +26,73 @@ export default function AddExerciseUI({
 
   return (
     <>
+      <Surface elevation={1} style={{ zIndex: 2 }}>
+        <View
+          style={{
+            paddingBlock: 16,
+            paddingInline: 8,
+            paddingTop: 16 + top,
+            backgroundColor: theme.colors.elevation.level2,
+            borderBottomWidth: 1,
+            borderColor: theme.colors.gray3,
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <SearchBar />
+          <Filter />
+        </View>
+      </Surface>
       <ScrollView
         style={{
           backgroundColor: theme.colors.background,
           paddingInline: 16,
-          paddingBottom: 16,
-          paddingTop: 16 + top,
+          paddingBlock: 16,
           width: '100%',
+          zIndex: 0,
         }}
       >
         <View style={{ paddingBottom: 40 }}>
-          <SearchBar />
-          <Filter />
           <ExerciseList />
         </View>
       </ScrollView>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          width: '100%',
-          paddingBlock: 20,
-          borderTopWidth: 2,
-          // borderTopColor: theme.colors.orange1,
-        }}
-      >
-        <CustomButton
-          mode="outlined"
-          // textColor={theme.colors.light}
-          onPress={() => handleCancelPress()}
-          style={{
-            borderColor: 'rgb(134, 142, 150)',
-            borderRadius: 4,
-          }}
-        >
-          Cancel
-        </CustomButton>
-        <CustomButton
-          disabled={selectedExerciseIDToAdd === null}
-          mode="contained"
-          onPress={() => handleAddExercisePress?.()}
-          style={{
-            borderRadius: 4,
-          }}
-          // theme={{
-          //   colors: {
-          //     surfaceDisabled: theme.colors.dark400,
-          //     onSurfaceDisabled: theme.colors.dark700,
-          //   },
-          // }}
-        >
-          Add Exercise
-        </CustomButton>
+      <View style={{ width: '100%' }}>
+        <Surface elevation={3}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              width: '100%',
+              paddingBlock: 20,
+              borderTopWidth: 1,
+              borderColor: theme.colors.gray3,
+            }}
+          >
+            <CustomButton
+              mode="outlined"
+              onPress={() => handleCancelPress()}
+              style={{
+                borderColor: 'rgb(134, 142, 150)',
+                borderRadius: 4,
+              }}
+            >
+              Cancel
+            </CustomButton>
+            <CustomButton
+              disabled={selectedExerciseIDToAdd === null}
+              mode="contained"
+              onPress={() => handleAddExercisePress?.()}
+              style={{
+                borderRadius: 4,
+              }}
+            >
+              Add Exercise
+            </CustomButton>
+          </View>
+        </Surface>
       </View>
       <FilterOverlay />
     </>
