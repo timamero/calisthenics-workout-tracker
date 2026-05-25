b1b77447ecef50b22bea4fadd9fbcc11784658fa
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-25 02:48:30 -0700

    CWT-373 Fix imports and exports

--- packages/mocks/src/lib/index.ts
+++ packages/mocks/src/lib/index.ts
@@ -1,11 +1,11 @@
-import { sampleExercises } from "./sampleExercises";
-import { sampleLeveragesAssists } from "./sampleLeveragesAssistst";
-import { sampleWorkoutBuilds } from "./sampleWorkoutBuilds";
-import { sampleWorkoutLogs } from "./sampleWorkoutLogs";
+import { sampleExercises } from './sampleExercises';
+import { sampleSetProgressions } from './sampleSetProgressions';
+import { sampleWorkoutBuilds } from './sampleWorkoutBuilds';
+import { sampleWorkoutLogs } from './sampleWorkoutLogs';
 
 export {
   sampleExercises,
-  sampleLeveragesAssists,
+  sampleSetProgressions,
   sampleWorkoutBuilds,
   sampleWorkoutLogs,
 };
