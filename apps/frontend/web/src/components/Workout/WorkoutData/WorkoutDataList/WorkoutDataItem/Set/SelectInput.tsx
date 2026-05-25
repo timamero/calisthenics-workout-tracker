ed6d1677a83578275611e03867c7b473795da6d3
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-25 03:54:57 -0700

    CWT-373 Fix conditionals to replace leverage and assist with set progression

--- apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/SelectInput.tsx
+++ apps/frontend/web/src/components/Workout/WorkoutData/WorkoutDataList/WorkoutDataItem/Set/SelectInput.tsx
@@ -2,94 +2,57 @@ import { useContext } from 'react';
 import { Select } from '@mantine/core';
 
 import { SetContext } from '@cwt/context';
-import { useLeveragesAssistsStore } from '@cwt/state/stores';
+import { useSetProgressionsStore } from '@cwt/state/stores';
 import { useFieldInputChange } from '@cwt/hooks';
 import { useWorkoutDraftStore } from '@cwt/state/stores';
 import TextInputReading from './TextInputReading';
 
-interface NumeralInputProps {
+interface SelectInputProps {
   label: string;
   fieldID: string;
-  trackingType?: string | null;
 }
 
-export default function SelectInput({
-  label,
-  fieldID,
-  trackingType = null,
-}: NumeralInputProps) {
+export default function SelectInput({ label, fieldID }: SelectInputProps) {
   const set = useContext(SetContext)!.set;
 
-  const leverageOrAssistID =
-    trackingType === 'leverages'
-      ? (set.fields.leverages?.find((field) => field.id === fieldID)!
-          .leverages_assists_id as number)
-      : (set.fields.assists?.find((field) => field.id === fieldID)!
-          .leverages_assists_id as number);
+  const setProgressionID = set.fields.setProgressions?.find(
+    (field) => field.id === fieldID,
+  )!.set_progression_id as number;
 
-  const leverageOrAssist = useLeveragesAssistsStore((state) =>
-    state.getLeverageOrAssistByID(leverageOrAssistID),
+  const setProgression = useSetProgressionsStore((state) =>
+    state.getSetProgressionByID(setProgressionID),
   );
   const mode = useWorkoutDraftStore((state) => state.mode);
 
   const handleChange = useFieldInputChange('value', 'select', fieldID);
 
-  const options = leverageOrAssist.value_options;
-  if (trackingType === 'leverages') {
-    if (mode === 'read') {
-      return (
-        <TextInputReading
-          label={label}
-          value={
-            set.fields.leverages?.find((field) => field.id === fieldID)?.value
-              ? (set.fields.leverages.find((field) => field.id === fieldID)!
-                  .value as string)
-              : ''
-          }
-        />
-      );
-    }
+  const options = setProgression.value_options;
+  if (mode === 'read') {
     return (
-      <Select
-        w={192}
+      <TextInputReading
         label={label}
-        data={options}
         value={
-          set.fields.leverages?.find((field) => field.id === fieldID)?.value
-            ? (set.fields.leverages.find((field) => field.id === fieldID)!
+          set.fields.setProgressions?.find((field) => field.id === fieldID)
+            ?.value
+            ? (set.fields.setProgressions.find((field) => field.id === fieldID)!
                 .value as string)
-            : null
+            : ''
         }
-        onChange={handleChange}
-      />
-    );
-  } else if (trackingType === 'assists') {
-    if (mode === 'read') {
-      return (
-        <TextInputReading
-          label={label}
-          value={
-            set.fields.assists?.find((field) => field.id === fieldID)?.value
-              ? (set.fields.assists.find((field) => field.id === fieldID)!
-                  .value as string)
-              : ''
-          }
-        />
-      );
-    }
-    return (
-      <Select
-        w={192}
-        label={label}
-        data={options}
-        value={
-          set.fields.assists?.find((field) => field.id === fieldID)?.value
-            ? (set.fields.assists.find((field) => field.id === fieldID)!
-                .value as string)
-            : null
-        }
-        onChange={handleChange}
       />
     );
   }
+  return (
+    <Select
+      w={192}
+      label={label}
+      data={options}
+      value={
+        set.fields.setProgressions?.find((field) => field.id === fieldID)?.value
+          ? (set.fields.setProgressions.find((field) => field.id === fieldID)!
+              .value as string)
+          : null
+      }
+      onChange={handleChange}
+    />
+  );
 }
