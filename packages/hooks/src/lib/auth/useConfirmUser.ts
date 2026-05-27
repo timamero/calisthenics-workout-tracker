1dd4ff8aaf042df0f625d924f18b6988b13227a7
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-27 01:27:23 -0700

    CWT-441 Fix successful confirmation flow

--- packages/hooks/src/lib/auth/useConfirmUser.ts
+++ packages/hooks/src/lib/auth/useConfirmUser.ts
@@ -5,21 +5,28 @@ import { confirmUser } from '@cwt/auth';
 export default function useConfirmUser(supabase: SupabaseClient) {
   const [status, setStatus] = useState<
     'idle' | 'pending' | 'confirmed' | 'error'
-  >('pending');
+  >('idle');
   const handleConfirmUser = async (tokenHash: string) => {
+    // if (status === 'idle') {
+    setStatus('pending');
     try {
+      console.log('useConfirmUser || calling handleConfirmUser');
       const user = await confirmUser(supabase, tokenHash);
       if (!user) {
-        setStatus('error');
+        // setStatus('error');
         console.error('Auth error at confirmation');
         return null;
+      } else {
+        console.log('useConfirmUser || setting status to confirmed');
+        setStatus('confirmed');
+        return user;
       }
-      return user;
     } catch (error) {
       setStatus('error');
       console.error(error);
       return null;
     }
+    // }
   };
 
   return {
