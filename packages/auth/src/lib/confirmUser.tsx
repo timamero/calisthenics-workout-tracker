c6dd40292d2db777832dc000dfa0f84eb4f23709
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-27 12:40:13 -0700

    CWT-441 Implement resend confirmation link

--- packages/auth/src/lib/confirmUser.tsx
+++ packages/auth/src/lib/confirmUser.tsx
@@ -3,20 +3,25 @@
  * @param {Object} supabase - The Supabase client instance.
  * @throws Will log an error confirmation fails.
  */
-import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from "@supabase/supabase-js";
 
 export const confirmUser = async (
   supabase: SupabaseClient,
   tokenHash: string,
 ) => {
-  const { data, error } = await supabase.auth.verifyOtp({
-    token_hash: tokenHash,
-    type: 'email',
-  });
+  try {
+    const { data, error } = await supabase.auth.verifyOtp({
+      token_hash: tokenHash,
+      type: "email",
+    });
 
-  if (error) {
-    console.error('Error confirming user:', error);
-    return null;
+    if (error) {
+      console.error(error);
+    } else {
+      return data.user;
+    }
+  } catch (error) {
+    console.error("Error confirming user:", error);
   }
-  return data.user;
+  return null;
 };
