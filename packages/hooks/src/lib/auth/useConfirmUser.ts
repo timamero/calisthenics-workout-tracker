ba675d77145adeebd924aa5428b315cfc8e4c34a
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-27 14:01:03 -0700

    CWT-441 Clean code

--- packages/hooks/src/lib/auth/useConfirmUser.ts
+++ packages/hooks/src/lib/auth/useConfirmUser.ts
@@ -11,12 +11,9 @@ export default function useConfirmUser(supabase: SupabaseClient) {
     try {
       const user = await confirmUser(supabase, tokenHash);
       if (!user) {
-        // console.error('Auth error at confirmation');
-        console.log("useConfirmUser || no user returned, returning null");
         setStatus("error");
         return null;
       } else {
-        console.log("useConfirmUser || user confirmed, setting status");
         setStatus("confirmed");
         return user;
       }
