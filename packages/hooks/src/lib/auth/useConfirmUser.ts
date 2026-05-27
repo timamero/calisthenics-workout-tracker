14d791498f1171868d5c385307a0b8cf690e00af
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-26 20:38:52 -0700

    CWT-441 Show user success message on email confirmation

--- packages/hooks/src/lib/auth/useConfirmUser.ts
+++ packages/hooks/src/lib/auth/useConfirmUser.ts
@@ -1,40 +1,31 @@
 import { useState } from "react";
 import { type SupabaseClient } from "@supabase/supabase-js";
-import { useAuthStore } from "@cwt/state/stores";
 import { confirmUser } from "@cwt/auth";
 
 export default function useConfirmUser(supabase: SupabaseClient) {
-  // const [isLoading, setIsLoading] = useState(false);
-  const [status, setStatus] = useState<
-    "idle" | "pending" | "confirmed" | "failed"
-  >("idle");
-  // const user = useAuthStore((state) => state.user);
-  const setUser = useAuthStore((state) => state.setUser);
-
-  const handleConfirmUser = async (email: string, token: string) => {
+  const [status, setStatus] = useState<"pending" | "confirmed" | "error">(
+    "pending",
+  );
+  const handleConfirmUser = async (tokenHash: string) => {
     try {
-      setStatus("pending");
-      const user = await confirmUser(supabase, email, token);
+      const user = await confirmUser(supabase, tokenHash);
       if (!user) {
+        setStatus("error");
         console.error("Auth error at confirmation");
-        setStatus("failed");
         return null;
       }
-      console.log("useConfirmUser || confirmation successful");
-      setStatus("confirmed");
-      // setUser(user);
+      console.log("useConfirmUser || confirmed user");
       return user;
     } catch (error) {
-      setStatus("failed");
+      setStatus("error");
       console.error(error);
       return null;
-    } finally {
-      setStatus("idle");
     }
   };
 
   return {
     status,
+    setStatus,
     handleConfirmUser,
   };
 }
