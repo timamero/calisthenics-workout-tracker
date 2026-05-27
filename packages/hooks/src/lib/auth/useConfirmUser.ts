8c9d502b195afef2ae1a654cb2aef3640035062b
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-27 12:40:13 -0700

    CWT-441 Implement resend confirmation link

--- packages/hooks/src/lib/auth/useConfirmUser.ts
+++ packages/hooks/src/lib/auth/useConfirmUser.ts
@@ -1,24 +1,27 @@
-import { useState } from 'react';
-import { type SupabaseClient } from '@supabase/supabase-js';
-import { confirmUser } from '@cwt/auth';
+import { useState } from "react";
+import { type SupabaseClient } from "@supabase/supabase-js";
+import { confirmUser } from "@cwt/auth";
 
 export default function useConfirmUser(supabase: SupabaseClient) {
   const [status, setStatus] = useState<
-    'idle' | 'pending' | 'confirmed' | 'error'
-  >('idle');
+    "idle" | "pending" | "confirmed" | "error"
+  >("idle");
   const handleConfirmUser = async (tokenHash: string) => {
-    setStatus('pending');
+    setStatus("pending");
     try {
       const user = await confirmUser(supabase, tokenHash);
       if (!user) {
-        console.error('Auth error at confirmation');
+        // console.error('Auth error at confirmation');
+        console.log("useConfirmUser || no user returned, returning null");
+        setStatus("error");
         return null;
       } else {
-        setStatus('confirmed');
+        console.log("useConfirmUser || user confirmed, setting status");
+        setStatus("confirmed");
         return user;
       }
     } catch (error) {
-      setStatus('error');
+      setStatus("error");
       console.error(error);
       return null;
     }
