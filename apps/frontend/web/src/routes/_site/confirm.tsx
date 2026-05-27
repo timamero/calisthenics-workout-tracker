10039a1a1e9f13dd76c2d3aa9e81a8883be462b3
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-26 20:38:52 -0700

    CWT-441 Show user success message on email confirmation

--- apps/frontend/web/src/routes/_site/confirm.tsx
+++ apps/frontend/web/src/routes/_site/confirm.tsx
@@ -3,12 +3,12 @@ import { createFileRoute } from '@tanstack/react-router';
 import { Title, Text, Container, Stack } from '@mantine/core';
 
 import { useAuthStore } from '@cwt/state/stores';
-// import { useConfirmUser } from '@cwt/hooks';
+import { useConfirmUser } from '@cwt/hooks';
 
-// import { supabase } from '../../services/supabaseClient';
+import { supabase } from '../../services/supabaseClient';
 import { useDefaultSize } from '../../hooks';
 
-// import DefaultLoader from '../../components/common/DefaultLoader';
+import DefaultLoader from '../../components/common/DefaultLoader';
 
 export const Route = createFileRoute('/_site/confirm')({
   // beforeLoad: () => {
@@ -25,14 +25,12 @@ export const Route = createFileRoute('/_site/confirm')({
 function ConfirmationView() {
   // const navigate = useNavigate();
 
-  // const [token, setToken] = useState(null);
-  // const [email, setEmail] = useState(null);
+  // const [status, setStatus] = useState<'pending' | 'confirmed' | 'expired'>(
+  //   'pending',
+  // );
   const user = useAuthStore((state) => state.user);
   const defaultSize = useDefaultSize();
-  // const { status, handleConfirmUser } = useConfirmUser(supabase);
-
-  // console.log('confirm || user', user);
-  // console.log('confirm || status', status);
+  const { status, setStatus, handleConfirmUser } = useConfirmUser(supabase);
 
   useEffect(() => {
     // Get the query string from the current URL
@@ -42,21 +40,28 @@ function ConfirmationView() {
     const urlParams = new URLSearchParams(queryString);
 
     // Extract the token
-    const token = urlParams.get('token_hash');
+    const tokenHash = urlParams.get('token_hash');
     const email = urlParams.get('email');
 
-    if (token && email) {
-      console.log('confirm || Token found:', token);
+    if (tokenHash && email && !user) {
+      console.log('confirm || Token found:', tokenHash);
       console.log('confirm || Email found:', email);
       console.log('confirming user');
       // setToken(tokenParam)
       // setEmail()
-      // handleConfirmUser(email, token);
+      handleConfirmUser(tokenHash);
       // Store it in localStorage, sessionStorage, or state
     } else {
       console.log('confirmation || No token in URL');
+      setStatus('error');
     }
-  }, []);
+  }, [handleConfirmUser, user, setStatus]);
+
+  useEffect(() => {
+    if (user) {
+      setStatus('confirmed');
+    }
+  }, [user, setStatus]);
 
   // useEffect(() => {
   //   if (user) {
@@ -66,9 +71,10 @@ function ConfirmationView() {
   //   }
   // }, [user, navigate]);
 
-  // if (status === 'pending') {
+  // if (isConfirmed) {
   //   return <DefaultLoader />;
   // }
+  console.log('confirm || status', status);
 
   if (user && !user.user_metadata.email_verified) {
     return (
@@ -95,29 +101,33 @@ function ConfirmationView() {
     );
   }
 
-  // if (status === 'confirmed') {
-  //   return (
-  //     <Container py="xl">
-  //       <Stack align="center" w="100%" gap={0}>
-  //         <Title
-  //           order={1}
-  //           mb="md"
-  //           fz={{ base: 'h3', md: 'h2' }}
-  //           lh="xxs"
-  //           ta="center"
-  //           style={(theme) => ({
-  //             letterSpacing: theme.other.letterSpacing.tight,
-  //           })}
-  //         >
-  //           Email confirmed
-  //         </Title>
-  //         <Text fw={500} fz={defaultSize} c="dark.3" mt={-12} ta="center">
-  //           Your account is now active. Log in below to access your account.
-  //         </Text>
-  //       </Stack>
-  //     </Container>
-  //   );
-  // }
+  if (status === 'pending') {
+    return <DefaultLoader />;
+  }
+
+  if (status === 'confirmed') {
+    return (
+      <Container py="xl">
+        <Stack align="center" w="100%" gap={0}>
+          <Title
+            order={1}
+            mb="md"
+            fz={{ base: 'h3', md: 'h2' }}
+            lh="xxs"
+            ta="center"
+            style={(theme) => ({
+              letterSpacing: theme.other.letterSpacing.tight,
+            })}
+          >
+            Email confirmed
+          </Title>
+          <Text fw={500} fz={defaultSize} c="dark.3" mt={-12} ta="center">
+            Your account is now active. Log in below to access your account.
+          </Text>
+        </Stack>
+      </Container>
+    );
+  }
 
   return (
     <Container py="xl">
