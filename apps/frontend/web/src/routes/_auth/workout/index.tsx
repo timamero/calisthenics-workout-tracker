61d45671cadff7429ed7c12e751be3efd22db828
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-01 02:14:17 -0700

    Add pending component

--- apps/frontend/web/src/routes/_auth/workout/index.tsx
+++ apps/frontend/web/src/routes/_auth/workout/index.tsx
@@ -1,10 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Stack } from '@mantine/core';
 
 import WorkoutDraft from '../../../components/Workout';
+import DefaultLoader from '../../../components/common/DefaultLoader';
 
 export const Route = createFileRoute('/_auth/workout/')({
   component: WorkoutView,
+  pendingComponent: Loader,
 });
+
+function Loader() {
+  return (
+    <Stack w="100%" h="100vh" justify="center">
+      <DefaultLoader />
+    </Stack>
+  );
+}
 
 function WorkoutView() {
   return <WorkoutDraft />;
