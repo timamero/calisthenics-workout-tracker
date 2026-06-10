7da1abc2fa0affc2dd9853a21b20bd870fda2f8d
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-10 14:23:06 -0700

    CWT-459 Format file

--- apps/backend/app/api/utils/workout.py
+++ apps/backend/app/api/utils/workout.py
@@ -60,16 +60,11 @@ def update_workout_log(
         print(f"Error updating workout: {e}")
 
 
-def delete_workout_log(
-    workout_log_id: int, access_token: str | None = None
-):
+def delete_workout_log(workout_log_id: int, access_token: str | None = None):
     supabase = get_supabase_client(access_token)
     try:
         response = (
-            supabase.table("workout_logs")
-            .delete()
-            .eq("id", workout_log_id)
-            .execute()
+            supabase.table("workout_logs").delete().eq("id", workout_log_id).execute()
         )
         return response.data[0]
     except Exception as e:
