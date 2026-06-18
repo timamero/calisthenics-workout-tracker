5f518e477ca3c032f23ecc65ff742adb36031fe0
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-17 22:28:00 -0700

    CWT-459 Create delete log confirmation overlay

---
+++ apps/frontend/mobile/components/WorkoutLogDetail/DeleteLogConfirmationOverlay.tsx
@@ -0,0 +1,65 @@
+import {
+  useWorkoutContextMobile,
+  useWorkoutLogDetailContextMobile,
+} from '@cwt/hooks';
+import { useAuthStore, useWorkoutLibraryStore } from '@cwt/state/stores';
+import { useNavigation } from '@react-navigation/native';
+
+import ConfirmationDialog from '../common/ConfirmationDialog';
+import { deleteWorkoutLog } from '../../services/workoutsService';
+
+export default function DeleteLogConfirmationOverlay() {
+  const navigation = useNavigation<any>();
+  const supabaseSession = useAuthStore((state) => state.session);
+  const deleteWorkout = useWorkoutLibraryStore((state) => state.deleteWorkout);
+
+  const workoutLogDetail = useWorkoutLogDetailContextMobile().workout;
+  const isDeleteLogOverlayVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers?.isDeleteLogOverlayVisible;
+  const setIsDeleteLogOverlayVisible =
+    useWorkoutContextMobile().mobileOverlayHandlers
+      ?.setIsDeleteLogOverlayVisible;
+
+  const setDetailWorkout = useWorkoutLogDetailContextMobile().setWorkout;
+
+  if (!workoutLogDetail?.id) {
+    console.error('Error: WorkoutID not found');
+    return null;
+  }
+
+  const handleDeleteLogPress = async () => {
+    if (!supabaseSession) {
+      console.error('Session not found');
+      return;
+    }
+
+    const body = JSON.stringify({ id: workoutLogDetail.id });
+
+    try {
+      const result = await deleteWorkoutLog(supabaseSession.access_token, body);
+      if (result) {
+        deleteWorkout(workoutLogDetail.id);
+        setDetailWorkout(null);
+        navigation.navigate('App', { screen: 'History' });
+      } else {
+        console.error(
+          'Workout delete request failed to delete log with ID: ',
+          workoutLogDetail.id,
+        );
+      }
+    } catch (e) {
+      console.error('Error deleting log:', e);
+    }
+  };
+
+  return (
+    <ConfirmationDialog
+      title="Delete Log"
+      message="Are you sure you want to delete this log?"
+      confirmButtonLabel="Delete"
+      isVisible={isDeleteLogOverlayVisible || false}
+      handleHideDialog={setIsDeleteLogOverlayVisible!}
+      onConfirmationPress={handleDeleteLogPress}
+    />
+  );
+}
