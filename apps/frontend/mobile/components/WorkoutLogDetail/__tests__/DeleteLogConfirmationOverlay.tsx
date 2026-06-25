7001360430a2813f94547c7fab0237663a498823
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-24 22:27:20 -0700

    CWT-459 Create test to confirm overlay is visible

--- apps/frontend/mobile/components/WorkoutLogDetail/__tests__/DeleteLogConfirmationOverlay.tsx
+++ apps/frontend/mobile/components/WorkoutLogDetail/__tests__/DeleteLogConfirmationOverlay.tsx
@@ -1,5 +1,45 @@
+import { render, screen } from '../../../utils/testUtils';
+
+import DeleteLogConfirmationOverlay from '../DeleteLogConfirmationOverlay';
+
+// --- Mocks ---
+const mockNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: mockNavigate,
+  }),
+}));
+
+jest.mock('@cwt/hooks', () => ({
+  useWorkoutContextMobile: () => ({
+    mobileOverlayHandlers: {
+      isDeleteLogOverlayVisible: true,
+    },
+  }),
+  useWorkoutLogDetailContextMobile: () => ({
+    workout: { id: '1' },
+  }),
+}));
+
+jest.mock('@cwt/state/stores', () => ({
+  useAuthStore: jest.fn(),
+  useWorkoutLibraryStore: jest.fn(),
+  useWorkoutDraftStore: jest.fn(),
+}));
+
 describe('DeleteConfirmationOverlay', () => {
-  it('test that this runs', () => {
-    expect(true).toBe(true);
+  it('renders when isDeleteLogOverlayVisible is true', () => {
+    render(<DeleteLogConfirmationOverlay />);
+
+    const title = screen.getByText(/Delete Log/i);
+    const message = screen.getByText(
+      /Are you sure you want to delete this log?/i,
+    );
+
+    expect(title).toBeOnTheScreen();
+    expect(message).toBeOnTheScreen();
   });
+
+  // TODO: Complete creating tests for this component
 });
