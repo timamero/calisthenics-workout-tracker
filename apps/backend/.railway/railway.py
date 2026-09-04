f4724414a76b1c30a7a5df7ec5c07999363254e3
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-09-04 14:38:44 -0700

    CWT-489 Update build command

--- apps/backend/.railway/railway.py
+++ apps/backend/.railway/railway.py
@@ -15,7 +15,8 @@ def main(ctx=None):
             rootDirectory="/apps/backend/",
         ),
         build={
-            "buildCommand": "poetry install --no-root --only main",
+            "buildCommand": """poetry config virtualenvs.create false && poetry
+                install --no-root --only main""",
             "buildEnvironment": "V3",
             "builder": "RAILPACK",
             "watchPatterns": ["/apps/backend/**"],
