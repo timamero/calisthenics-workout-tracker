cb674bc34ec4cfeb4dd8272b91ebe8a5889efe77
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-09-09 21:03:20 -0700

    CWT-421 Refactor read_filtered_exercises to use dependency injection for token

--- apps/backend/app/api/routes/exercises.py
+++ apps/backend/app/api/routes/exercises.py
@@ -2,6 +2,7 @@ from typing import List, Annotated
 import time
 
 from fastapi import APIRouter, Request, HTTPException, Query, Depends
+from fastapi.security import OAuth2PasswordBearer
 from pyrate_limiter import Duration, Limiter, Rate
 from fastapi_limiter.depends import RateLimiter
 
@@ -14,6 +15,15 @@ router = APIRouter(prefix="/exercises")
 
 standard_api_limit = Limiter(Rate(60, Duration.MINUTE))
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def header_token():
+    if settings.environment == "local-isolated":
+        return None
+    else:
+        return oauth2_scheme
+
 
 @router.get(
     "",
@@ -21,22 +31,26 @@ standard_api_limit = Limiter(Rate(60, Duration.MINUTE))
     dependencies=[Depends(RateLimiter(limiter=standard_api_limit))],
 )
 def read_filtered_exercises(
-    filter_query: Annotated[ExerciseFilterParams, Query()], request: Request
+    filter_query: Annotated[ExerciseFilterParams, Query()],
+    token: Annotated[str, Depends(header_token())],
 ):
     """
     Retrieve a list of exercises.
     """
+
     startTime = time.perf_counter()
-    auth_header = request.headers.get("Authorization")
-    if not auth_header or not auth_header.startswith("Bearer "):
-        if settings.environment == "local-isolated":
-            exercises = get_exercises(filter_query)
+    print("DEBUG1: token at start: ", token)
+    if settings.environment == "local-isolated":
+        print("DEBUG1.1: getting exercises")
+        exercises = get_exercises(filter_query)
+        print("DEBUG1.2: retrieved exercises")
+    else:
+        if token:
+            print("DEBUG3: token retriedved: ", token)
+            exercises = get_exercises(filter_query, token)
+            print("DEBUG3.1: retrieved exercises")
         else:
             raise HTTPException(status_code=401, detail="Authentication required")
-
-    else:
-        access_token = auth_header.split(" ")[1]
-        exercises = get_exercises(filter_query, access_token)
 
     if not exercises:
         raise HTTPException(status_code=400, detail="Invalid request")
